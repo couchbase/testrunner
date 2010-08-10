e23be1e20da65775b70aae63c09437e46f72d503
Randi Harper <sektie@gmail.com>
2010-08-10 14:11:46 -0700

    Improved logging.

--- tests/ascii-through-moxi-set/run.sh
+++ tests/ascii-through-moxi-set/run.sh
@@ -11,12 +11,14 @@ RETVAL=0
 
 if [ -z "$SERVER" ]; then
 	for entry in `cat $SERVERFILE`; do
+		echo "[$TESTNAME] Running ascii-through-moxi-set.pl -s $entry"
 		tests/$TESTNAME/ascii-through-moxi-set.pl -s $entry
 		if [ $? -eq 1 ]; then
 			RETVAL=1
 		fi	
 	done
 else
+	echo "[$TESTNAME] Running ascii-through-moxi-set.pl -s $SERVER"
 	tests/$TESTNAME/ascii-through-moxi-set.pl -s $SERVER
 	if [ $? -eq 1 ]; then
 		RETVAL=1
