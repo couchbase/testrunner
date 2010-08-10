3742e2d4ade12ad6d18bdd55b4b37378f5b60486
Randi Harper <sektie@gmail.com>
2010-08-10 15:29:50 -0700

    Add test for prepend.

---
+++ tests/ascii-through-moxi-prepend/ascii-through-moxi-prepend.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Getopt::Std;
+use IO::Socket::INET;
+
+
+my %opts;
+
+getopts('s:', \%opts);
+my $server = $opts{'s'};
+
+my $sock = new IO::Socket::INET (
+	PeerAddr => $server,
+	PeerPort => '11211',
+	Proto    => 'tcp'
+);
+
+unless ($sock) {
+	print "Couldn't connect to $server.\n";
+	exit 1;
+}
+
+# delete the key just in case.
+print $sock "delete a\r\n";
+my $input = <$sock>;
+
+# do the set - don't bother checking the output
+print $sock "set a 0 0 1\r\nb\r\n";
+$input = <$sock>;
+
+# now the append
+print $sock "set a 0 0 1\r\nc\r\n";
+$input = <$sock>;
+if ($input !~ /^STORED/) {
+	print "Unexpected data from set: $input (expected: STORED)\n";
+	exit 1;
+}
+
+$ now check the output
+print $sock "get a\r\n";
+$input = <$sock>;
+if ($input !~ /^VALUE a 0 2/) {
+	print "Unexpected data from get: $input (expected: VALUE a 0 2)\n";
+	exit 1;
+} 
+
+$input = <$sock>;
+if ($input !~ /^bc/) {
+	print "Unexpected data from get: $input (expected: bc)\n";
+	exit 1;
+} 
+
+$input = <$sock>;
+if ($input !~ /^END/) {
+	print "Unexpected data from get: $input (expected: END)\n";
+	exit 1;
+} 
+	
+
+exit 0;
---
+++ tests/ascii-through-moxi-prepend/run.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# Variables passed:
+#    SERVER
+#    SERVERFILE
+#    CONFIGFILE
+#    TESTNAME
+#    KEYFILE
+
+RETVAL=0
+
+if [ -z "$SERVER" ]; then
+	for entry in `cat $SERVERFILE`; do
+		echo "[$TESTNAME] Running ascii-through-moxi-prepend.pl -s $entry"
+		tests/$TESTNAME/ascii-through-moxi-prepend.pl -s $entry
+		if [ $? -eq 1 ]; then
+			RETVAL=1
+		fi	
+	done
+else
+	echo "[$TESTNAME] Running ascii-through-moxi-prepend.pl -s $SERVER"
+	tests/$TESTNAME/ascii-through-moxi-prepend.pl -s $SERVER
+	if [ $? -eq 1 ]; then
+		RETVAL=1
+	fi	
+fi
+
+exit $RETVAL
