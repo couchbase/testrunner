cb879a52c786aa2c504c2bfcec25294ec090b7a7
Randi Harper <sektie@gmail.com>
2010-08-10 12:51:28 -0700

    Get ssh key info from environment variable set in testrunner.sh

--- tests/uninstall/run.sh
+++ tests/uninstall/run.sh
@@ -6,17 +6,16 @@
 #    CONFIGFILE
 #    TESTNAME
 #    VERSION
+#    KEYFILE
 
-export SSHKEY="/Users/randi/.ssh/ustest20090719.pem"
-# export SSHKEY="~/.ssh/ustest20090719.pem"
 
 RETVAL=0
 
 if [ -z "$SERVER" ]; then
 	for entry in `cat $SERVERFILE`; do
 		echo "[$TESTNAME] Uninstalling $entry"
-		ssh -i $SSHKEY root@$entry "rpm -e northscale-server" 2>/dev/null >/dev/null
-		OUTPUT=`ssh -i ${SSHKEY} root@$entry "find /etc/opt/NorthScale /opt/NorthScale /var/opt/NorthScale -type f 2>/dev/null" | grep -v default | grep -v rpmsave | grep -v ns_1` 
+		ssh -i $KEYFILE root@$entry "rpm -e northscale-server" 2>/dev/null >/dev/null
+		OUTPUT=`ssh -i $KEYFILE root@$entry "find /etc/opt/NorthScale /opt/NorthScale /var/opt/NorthScale -type f 2>/dev/null" | grep -v default | grep -v rpmsave | grep -v ns_1` 
 
 		if [ -n "$OUTPUT" ]; then
 			echo "[$TESTNAME] $OUTPUT"
@@ -26,8 +25,8 @@ if [ -z "$SERVER" ]; then
 	SERVER=""
 else
 	echo "[$TESTNAME] Uninstalling $entry"
-	ssh -i $SSHKEY root@$SERVER "rpm -e northscale-server" 2>/dev/null >/dev/null
-	OUTPUT=`ssh -i $SSHKEY root@$SERVER "find /etc/opt/NorthScale /opt/NorthScale /var/opt/NorthScale -type f 2>/dev/null| grep -v default | grep -v rpmsave| grep -v ns_1 "`
+	ssh -i $KEYFILE root@$SERVER "rpm -e northscale-server" 2>/dev/null >/dev/null
+	OUTPUT=`ssh -i $KEYFILE root@$SERVER "find /etc/opt/NorthScale /opt/NorthScale /var/opt/NorthScale -type f 2>/dev/null| grep -v default | grep -v rpmsave| grep -v ns_1 "`
 
 	if [ -n "$OUTPUT" ]; then
 		echo "[$TESTNAME] $OUTPUT"
