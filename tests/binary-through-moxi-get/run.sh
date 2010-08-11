1e36ae0cc25fbd27d32e88665be52332d9734577
Randi Harper <sektie@gmail.com>
2010-08-11 10:58:48 -0700

    Add binary through moxi get test.

---
+++ tests/binary-through-moxi-get/run.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#
+# Variables passed:
+#    SERVER
+#    SERVERFILE
+#    CONFIGFILE
+#    TESTNAME
+#    KEYFILE
+
+RETVAL=0
+
+if [ -z "$SERVER" ]; then
+	for entry in `cat $SERVERFILE`; do
+		echo "[$TESTNAME] Running get against $entry"
+		# do the set first so we know we have valid data to get.
+		lib/binclient.py $entry set a 1
+		OUTPUT=`lib/binclient.py $entry get a`
+		if [ $? -eq 1 ] || [ "$OUTPUT" -ne "1" ]; then
+			echo "[$TESTNAME] got unexpected output: $OUTPUT (expected: 1)"
+			RETVAL=1
+		fi	
+	done
+else
+	echo "[$TESTNAME] Running get against $SERVER"
+	# do the set first so we know we have valid data to get.
+	lib/binclient.py $entry set a 1
+	OUTPUT=`lib/binclient.py $entry get a`
+	if [ $? -eq 1 ] || [ "$OUTPUT" -ne "1" ]; then
+		echo "[$TESTNAME] got unexpected output: $OUTPUT (expected: 1)"
+		RETVAL=1
+	fi	
+fi
+
+exit $RETVAL
