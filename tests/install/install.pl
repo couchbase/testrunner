930ca2d658445f3491968f2dd22f8b1ae21b1f6a
Randi Harper <sektie@gmail.com>
2010-08-12 15:48:46 -0700

    Added support for ubuntu.

--- tests/install/install.pl
+++ tests/install/install.pl
@@ -11,27 +11,41 @@ my $sshkey = $ENV{'KEYFILE'};
 my $version = $ENV{'VERSION'};
 
 # figure out what OS we're using
-my @output = `ssh -i $sshkey root\@$opts{'s'} "cat /etc/redhat-release ; uname -m"`;
+my $output = `ssh -i $sshkey root\@$opts{'s'} "cat /etc/redhat-release 2>/dev/null"`;
+chomp($output);
 
-foreach my $line (@output) {
-	chomp($line);
+# It'd be prettier if we just ||'ed this, but we might someday have
+# different packages for 5.2 and 5.4 so I'm keeping them separate
+# for now.
+if ($output =~ /^CentOS release 5\.[24] \(Final\)/) {
+	$os = "rhel_5.4";
+} 
 
-	# It'd be prettier if we just ||'ed this, but we might someday have
-	# different packages for 5.2 and 5.4 so I'm keeping them separate
-	# for now.
-	if ($line =~ /^CentOS release 5.2 \(Final\)$/) {
-		$os = "rhel_5.4";
-	} elsif ($line =~ /^CentOS release 5.4 \(Final\)$/) {
-		$os = "rhel_5.4";
-	} 
+unless ($os) {
+	$output = `ssh -i $sshkey root\@$opts{'s'} "cat /etc/lsb-release 2>/dev/null| grep DISTRIB_DESCRIPTION"`;
+	chomp $output;
+	
+	if ($output =~ /Ubuntu 10.04 LTS/) {
+		$os = "ubuntu_10.04";
+	}
+}
 
-	if ($line =~ /^x86_64$/) {
-		$arch = "x86_64";
-	} elsif ($line =~ /^x86$/) {
+
+
+$output = `ssh -i $sshkey root\@$opts{'s'} "uname -m"`;
+chomp $output;
+
+# 64 bit is always specified as x86_64, but 32 bit can be specified as x86 or i686.
+if ($output =~ /^x86_64$/) {
+	$arch = "x86_64";
+} elsif ($output =~ /^x86$/ || $output =~ /^i686$/) {
+	if ($os =~ /rhel_5.4/) {
 		$arch = "x86";
-	} elsif ($line =~ /^i686$/) {
-		$arch = "x86";
-	} 
+	} elsif ($os =~ /ubuntu_10.04/ || $os =~ /ubuntu_9.04/) {
+		$arch = "i686";
+	}
+} else {
+	print "Unknown OS/arch combo.\n";
 }
 
 my $file = "northscale-server_".$os."_".$arch."_".$version.".tar.gz";
@@ -40,7 +54,11 @@ chomp $md5sum;
 $md5sum =~ s/ .*$//;
 
 # first, remove any old installs and misc directories
-`ssh -i $sshkey root\@$opts{'s'} "rpm -e northscale-server; rm -rf /var/opt /opt /etc/opt; cd /tmp;" 2>&1 >/dev/null`;
+if ($os =~ /rhel_5.4/) {
+	`ssh -i $sshkey root\@$opts{'s'} "rpm -e northscale-server; rm -rf /var/opt /opt /etc/opt; cd /tmp;" 2>&1 >/dev/null`;
+} elsif ($os =~ /ubuntu_10.04/ || $os =~ /ubuntu_9.04/) {
+	`ssh -i $sshkey root\@$opts{'s'} "dpkg -r northscale-server; rm -rf /var/opt /opt /etc/opt; cd /tmp;" 2>&1 >/dev/null`;
+}
 
 # now, get the md5sum of a file if it exists
 my $r_md5sum = `ssh -i $sshkey root\@$opts{'s'} "md5sum /tmp/$file 2>/dev/null"`;
@@ -53,6 +71,11 @@ if ($md5sum ne $r_md5sum) {
 	$command .= " wget -q http://builds.hq.northscale.net/latestbuilds/$file &&";
 }
 
-$command .= " tar -zxf $file; rpm -i northscale-server*.rpm";
+$command .= " tar -zxf $file;";
 
+if ($os =~ /rhel_5.4/) {
+	$command .= " rpm -i northscale-server*.rpm";
+} elsif ($os =~ /ubuntu_10.04/ || $os =~ /ubuntu_9.04/) {
+	$command .= " dpkg -i northscale-server*.deb";
+}
 `ssh -i $sshkey root\@$opts{'s'} "$command" 2>&1 >/dev/null`;
