a0ce3d0a7659f56b4fe372cfff66e4ee586a6f0f
Randi Harper <sektie@gmail.com>
2010-08-12 14:31:13 -0700

    Adding server-failover test. This test does do a rebalance after removing a node. If you pass it a list of servers, it will remove the second node from the cluster on the first node.

---
+++ tests/server-failover/server-failover.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+my $sshkey = $ENV{'KEYFILE'};
+
+my $node = shift;
+my $newnode = shift;
+
+# if we're behind some kind of NAT or whatever, the ip address may be different from what we think it is. sigh.
+# grab it from the server-info
+
+my $output = `ssh -i $sshkey root\@$node "/opt/NorthScale/bin/cli/membase server-info -c $newnode:8080"`;
+chomp $output;
+
+if ($output =~ /\"hostname\": \"([\d\.]+):/) {
+	$newnode = $1;
+} else {
+	print "Error: Couldn't get hostname from server-info.\n";
+	print "$output\n";
+	exit 1;
+}
+
+$output = `ssh -i $sshkey root\@$node "/opt/NorthScale/bin/cli/membase failover -c $node:8080 --server-failover=$newnode"`;
+
+if ($output !~ /^SUCCESS: failover ns_1\@$newnode/) {
+	print "Error: $output\n";
+	exit 1;
+}
+
+print "Rebalancing.\n";
+$output = `ssh -i $sshkey root\@$node "/opt/NorthScale/bin/cli/membase rebalance -c $node:8080"`;
+chomp $output;
+
+if ($output !~ /SUCCESS: rebalanced cluster/) {
+	print "Error: Couldn't rebalance.\n";
+	print "$output\n";
+	exit 1;
+}
+
+exit 0;
