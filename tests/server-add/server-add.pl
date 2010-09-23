b408dd878272acab9bdff0e29c4285c501e15850
Keith Batten <kbatten@gmail.com>
2010-09-23 16:49:39 -0700

    supply username and password when adding a server

--- tests/server-add/server-add.pl
+++ tests/server-add/server-add.pl
@@ -7,7 +7,7 @@ my $sshkey = $ENV{'KEYFILE'};
 my $node = shift;
 my $newnode = shift;
 
-my $output = `ssh -i $sshkey root\@$node "/opt/NorthScale/bin/cli/membase server-add -c 127.0.0.1:8080 --server-add=$newnode"`;
+my $output = `ssh -i $sshkey root\@$node "/opt/NorthScale/bin/cli/membase server-add -c 127.0.0.1:8080 -u Administrator -p password --server-add=$newnode --server-add-username=Administrator --server-add-password=password"`;
 chomp $output;
 
 if ($output !~ /^SUCCESS: server-add $newnode:8080/) {
