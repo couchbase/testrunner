cdb8043bb7e19cf41ba85ff542653473b82c4f18
Keith Batten <kbatten@gmail.com>
2010-09-24 14:20:31 -0700

    no longer fail if /var/opt/membase still exists after uninstall

--- tests/uninstall/run.sh
+++ tests/uninstall/run.sh
@@ -15,7 +15,9 @@ if [ -z "$SERVER" ]; then
 	for entry in `cat $SERVERFILE`; do
 		echo "[$TESTNAME] Uninstalling $entry"
 		ssh -i $KEYFILE root@$entry "rpm -e membase-server || dpkg -r membase-server" 2>/dev/null >/dev/null
-		OUTPUT=`ssh -i $KEYFILE root@$entry "find /etc/opt/membase /opt/membase /var/opt/membase -type f 2>/dev/null" | grep -v default | grep -v rpmsave | grep -v ns_1` 
+                # right now we don't remove /var/opt diretories
+#		OUTPUT=`ssh -i $KEYFILE root@$entry "find /etc/opt/membase /opt/membase /var/opt/membase -type f 2>/dev/null" | grep -v default | grep -v rpmsave | grep -v ns_1` 
+		OUTPUT=`ssh -i $KEYFILE root@$entry "find /etc/opt/membase /opt/membase -type f 2>/dev/null" | grep -v default | grep -v rpmsave | grep -v ns_1` 
 
 		if [ -n "$OUTPUT" ]; then
 			echo "[$TESTNAME] $OUTPUT"
@@ -26,12 +28,16 @@ if [ -z "$SERVER" ]; then
 else
 	echo "[$TESTNAME] Uninstalling $entry"
 	ssh -i $KEYFILE root@$SERVER "rpm -e membase-server || dpkg -r membase-server" 2>/dev/null >/dev/null
-	OUTPUT=`ssh -i $KEYFILE root@$SERVER "find /etc/opt/membase /opt/membase /var/opt/membase -type f 2>/dev/null| grep -v default | grep -v rpmsave| grep -v ns_1 "`
+        # right now we don't remove /var/opt diretories
+#	OUTPUT=`ssh -i $KEYFILE root@$SERVER "find /etc/opt/membase /opt/membase /var/opt/membase -type f 2>/dev/null| grep -v default | grep -v rpmsave| grep -v ns_1 "`
+	OUTPUT=`ssh -i $KEYFILE root@$SERVER "find /etc/opt/membase /opt/membase -type f 2>/dev/null| grep -v default | grep -v rpmsave| grep -v ns_1 "`
 
 	if [ -n "$OUTPUT" ]; then
 		echo "[$TESTNAME] $OUTPUT"
 		RETVAL=1
 	fi
 fi
+
+sleep 5
 
 exit $RETVAL
