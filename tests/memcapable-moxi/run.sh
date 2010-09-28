04e208ad764fe4b2a1cb25fc34d3a45996940b23
Keith Batten <kbatten@gmail.com>
2010-09-28 13:13:16 -0700

    fail test if memcached isn't running

--- tests/memcapable-moxi/run.sh
+++ tests/memcapable-moxi/run.sh
@@ -32,8 +32,14 @@ ret=0
 
 for S in $(echo $SERVER) ; do
     echo "[$TESTNAME] $S"
-    memcapable -h $S -p 11211 2>&1 | grep -v -e setq -e flushq -e addq -e replaceq -e deleteq | grep FAIL
+    res=$(memcapable -h $S -p 11211 2>&1 | grep -v -e setq -e flushq -e addq -e replaceq -e deleteq)
+    echo "$res" | grep FAIL
     if [[ $? -eq 0 ]] ; then
+        ret=1
+    fi
+    echo "$res" | grep pass &> /dev/null
+    if [[ $? -eq 1 ]] ; then
+        echo "[$TESTNAME] Unable to connect to server"
         ret=1
     fi
 done
