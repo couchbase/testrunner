aade260fd2061de069e83aa99e1c7e8cc8a6d0b7
Keith Batten <kbatten@gmail.com>
2010-09-28 13:12:34 -0700

    update default rest port

--- tests/server-add/server-add.pl
+++ tests/server-add/server-add.pl
@@ -7,10 +7,10 @@ my $sshkey = $ENV{'KEYFILE'};
 my $node = shift;
 my $newnode = shift;
 
-my $output = `ssh -i $sshkey root\@$node "/opt/NorthScale/bin/cli/membase server-add -c 127.0.0.1:8080 -u Administrator -p password --server-add=$newnode --server-add-username=Administrator --server-add-password=password"`;
+my $output = `ssh -i $sshkey root\@$node "/opt/NorthScale/bin/cli/membase server-add -c 127.0.0.1:8091 -u Administrator -p password --server-add=$newnode --server-add-username=Administrator --server-add-password=password"`;
 chomp $output;
 
-if ($output !~ /^SUCCESS: server-add $newnode:8080/) {
+if ($output !~ /^SUCCESS: server-add $newnode:8091/) {
 	print "Error: $output\n";
 	exit 1;
 }
