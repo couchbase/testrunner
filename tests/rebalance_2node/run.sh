2a9e4a900d7052efef09222b8f37ca6e15e41362
Keith Batten <kbatten@gmail.com>
2010-10-06 14:52:23 -0700

    hide ssh stderr messages

--- tests/rebalance_2node/run.sh
+++ tests/rebalance_2node/run.sh
@@ -40,7 +40,7 @@ SLAVE=$(echo $SERVER | cut -d "," -f 2)
 echo "[$TESTNAME] Running Add/remove rebalance on \"$SERVER\""
 
 echo "[$TESTNAME] Setting 100 keys"
-ssh $MASTER "for i in {0..0} ; do x=\"\" ; for j in {0..99} ; do x=\"\${x}set key_\${i}\${j} 0 0 1\n1\r\n\" ; done ; echo -e \"\$x\" | nc localhost 11211 > /dev/null; done"
+ssh $MASTER "for i in {0..0} ; do x=\"\" ; for j in {0..99} ; do x=\"\${x}set key_\${i}\${j} 0 0 1\n1\r\n\" ; done ; echo -e \"\$x\" | nc localhost 11211 > /dev/null; done" 2>/dev/null
 
 ret=0
 echo "[$TESTNAME] Adding server $SLAVE to $MASTER"
@@ -50,7 +50,7 @@ if [[ $count -ne 2 ]] ; then
     echo "[$TESTNAME] Add server failed"
     ret=1
 fi 
-status=$(ssh $MASTER /opt/membase/bin/cli/membase server-list -c $MASTER -u Administrator -p password -o json | sed -e 's/^.*"balanced":\(.*\),"rebalanceStatus.*$/\1/')
+status=$(ssh $MASTER /opt/membase/bin/cli/membase server-list -c $MASTER -u Administrator -p password -o json 2> /dev/null| sed -e 's/^.*"balanced":\(.*\),"rebalanceStatus.*$/\1/')
 if [[ $status == "false" ]] ; then
     echo "[$TESTNAME] Cluster is not balanced after add"
     ret=1
@@ -63,7 +63,7 @@ if [[ $count -ne 1 ]] ; then
     echo "[$TESTNAME] Remove server failed"
     ret=1
 fi
-status=$(ssh $MASTER /opt/membase/bin/cli/membase server-list -c $MASTER -u Administrator -p password -o json | sed -e 's/^.*"balanced":\(.*\),"rebalanceStatus.*$/\1/')
+status=$(ssh $MASTER /opt/membase/bin/cli/membase server-list -c $MASTER -u Administrator -p password -o json 2> /dev/null | sed -e 's/^.*"balanced":\(.*\),"rebalanceStatus.*$/\1/')
 if [[ $status == "false" ]] ; then
     echo "[$TESTNAME] Cluster is not balanced after add"
     ret=1
--- tests/uninstall/run.sh
+++ tests/uninstall/run.sh
@@ -14,9 +14,9 @@ RETVAL=0
 if [ -z "$SERVER" ]; then
 	for entry in `cat $SERVERFILE`; do
 		echo "[$TESTNAME] Uninstalling $entry"
-		ssh -i $KEYFILE root@$entry "rpm -e membase-server || dpkg -r membase-server" 2>/dev/null >/dev/null
+		ssh -i $KEYFILE root@$entry "rpm -e membase-server || dpkg -r membase-server" &>/dev/null
                 # right now we don't remove /var/opt diretories
-		OUTPUT=`ssh -i $KEYFILE root@$entry "find /etc/opt/membase /opt/membase -type f 2>/dev/null" | grep -v -e default -e rpmsave -e ns_1 -e pyc -e erl_crash.dump` 
+		OUTPUT=`ssh -i $KEYFILE root@$entry "find /etc/opt/membase /opt/membase -type f 2>/dev/null" 2> /dev/null | grep -v -e default -e rpmsave -e ns_1 -e pyc -e erl_crash.dump -e ip`
 
 		if [ -n "$OUTPUT" ]; then
 			echo "[$TESTNAME] $OUTPUT"
@@ -26,9 +26,9 @@ if [ -z "$SERVER" ]; then
 	SERVER=""
 else
 	echo "[$TESTNAME] Uninstalling $entry"
-	ssh -i $KEYFILE root@$SERVER "rpm -e membase-server || dpkg -r membase-server" 2>/dev/null >/dev/null
+	ssh -i $KEYFILE root@$SERVER "rpm -e membase-server || dpkg -r membase-server" &>/dev/null
         # right now we don't remove /var/opt diretories
-	OUTPUT=`ssh -i $KEYFILE root@$SERVER "find /etc/opt/membase /opt/membase -type f 2>/dev/null" | grep -v -e default -e rpmsave -e ns_1 -e pyc -e erl_crash.dump` 
+	OUTPUT=`ssh -i $KEYFILE root@$SERVER "find /etc/opt/membase /opt/membase -type f 2>/dev/null" 2> /dev/null | grep -v -e default -e rpmsave -e ns_1 -e pyc -e erl_crash.dump -e ip`
 
 	if [ -n "$OUTPUT" ]; then
 		echo "[$TESTNAME] $OUTPUT"
