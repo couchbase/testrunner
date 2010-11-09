aa2943cb6d55c084a8ab800302e8950b7af42344
Keith Batten <kbatten@gmail.com>
2010-11-08 17:30:50 -0800

    add support for both local and remote operation

--- tests/rebalance-2node/run.sh
+++ tests/rebalance-2node/run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Variables passed:
 #    SERVERS
@@ -10,35 +10,47 @@ TIMEOUT=120
 MASTER=$(echo $SERVERS | cut -d " " -f 1)
 SLAVE=$(echo $SERVERS | cut -d " " -f 2)
 
+MASTER_IP=$(echo $MASTER | cut -d ":" -f 1)
+MASTER_REST=$(echo $MASTER | cut -d ":" -f 2)
+MASTER_MOXI=$(echo $MASTER | cut -d ":" -f 3)
+MASTER_MEMCACHED=$(echo $MASTER | cut -d ":" -f 4)
+
+SLAVE_IP=$(echo $SLAVE | cut -d ":" -f 1)
+SLAVE_REST=$(echo $SLAVE | cut -d ":" -f 2)
+SLAVE_MOXI=$(echo $SLAVE | cut -d ":" -f 3)
+SLAVE_MEMCACHED=$(echo $SLAVE | cut -d ":" -f 4)
+
 echo "[$TESTNAME] Running Add/remove rebalance on \"$MASTER $SLAVE\""
 
 echo "[$TESTNAME] Setting 100 keys"
-ssh $MASTER "for i in {0..0} ; do x=\"\" ; for j in {0..99} ; do x=\"\${x}set key_\${i}\${j} 0 0 1\n1\r\n\" ; done ; echo -e \"\$x\" | nc localhost 11211 > /dev/null; done" 2>/dev/null
+for i in {0..99} ; do
+    echo -e "set $i 0 0 1\n1\r" | nc $MASTER_IP $MASTER_MOXI &> /dev/null
+done
 
 ret=0
 echo "[$TESTNAME] Adding server $SLAVE to $MASTER"
-ssh $MASTER /opt/membase/bin/cli/membase rebalance -c $MASTER:8091 --server-add=$SLAVE:8091 -u Administrator -p password --server-add-username=Administrator --server-add-password=password &> /dev/null
-count=$(ssh $MASTER /opt/membase/bin/cli/membase server-list -c $MASTER -u Administrator -p password 2> /dev/null | grep "healthy active" | wc -l)
+$MEMBASE_CLI rebalance -c $MASTER_IP:$MASTER_REST --server-add=$SLAVE_IP:$SLAVE_REST -u $REST_USER -p $REST_PASSWORD --server-add-username=$REST_USER --server-add-password=$REST_PASSWORD &> /dev/null
+count=$($MEMBASE_CLI server-list -c $MASTER_IP:$MASTER_REST -u $REST_USER -p $REST_PASSWORD 2> /dev/null | grep "healthy active" | wc -l)
 if [[ $count -ne 2 ]] ; then
     echo "[$TESTNAME] Add server failed"
     ret=1
 fi 
-status=$(ssh $MASTER /opt/membase/bin/cli/membase server-list -c $MASTER -u Administrator -p password -o json 2> /dev/null| sed -e 's/^.*"balanced":\(.*\),"rebalanceStatus.*$/\1/')
+status=$($MEMBASE_CLI server-list -c $MASTER_IP:$MASTER_REST -u $REST_USER -p $REST_PASSWORD -o json 2> /dev/null| sed -e 's/^.*"balanced":\(.*\),"rebalanceStatus.*$/\1/')
 if [[ $status == "false" ]] ; then
     echo "[$TESTNAME] Cluster is not balanced after add"
     ret=1
 fi
 
 echo "[$TESTNAME] Removing server $SLAVE from $MASTER"
-ssh $MASTER /opt/membase/bin/cli/membase rebalance -c $MASTER:8091 --server-remove=$SLAVE:8091 -u Administrator -p password &> /dev/null
-count=$(ssh $MASTER /opt/membase/bin/cli/membase server-list -c $MASTER -u Administrator -p password 2> /dev/null | grep "healthy active" | wc -l)
+$MEMBASE_CLI rebalance -c $MASTER_IP:$MASTER_REST --server-remove=$SLAVE_IP:$SLAVE_REST -u $REST_USER -p $REST_PASSWORD &> /dev/null
+count=$($MEMBASE_CLI server-list -c $MASTER_IP:$MASTER_REST -u $REST_USER -p $REST_PASSWORD 2> /dev/null | grep "healthy active" | wc -l)
 if [[ $count -ne 1 ]] ; then
     echo "[$TESTNAME] Remove server failed"
     ret=1
 fi
-status=$(ssh $MASTER /opt/membase/bin/cli/membase server-list -c $MASTER -u Administrator -p password -o json 2> /dev/null | sed -e 's/^.*"balanced":\(.*\),"rebalanceStatus.*$/\1/')
+status=$($MEMBASE_CLI server-list -c $MASTER_IP:$MASTER_REST -u $REST_USER -p $REST_PASSWORD -o json 2> /dev/null| sed -e 's/^.*"balanced":\(.*\),"rebalanceStatus.*$/\1/')
 if [[ $status == "false" ]] ; then
-    echo "[$TESTNAME] Cluster is not balanced after add"
+    echo "[$TESTNAME] Cluster is not balanced after remove"
     ret=1
 fi
 
