29f6629d982690f20cf1b49106c24936ce38ccb1
Steve Yen <steve.yen@gmail.com>
2010-11-17 12:53:14 -0800

    added flags check to simple-set-get test
    Change-Id: I64ce5f4da5fb85d017d8f413de35775209ece2ca
    Reviewed-on: http://review.membase.org/3711
    Tested-by: Keith Batten <kbatten@gmail.com>
    Reviewed-by: Keith Batten <kbatten@gmail.com>

--- tests/simple-set-get/run.sh
+++ tests/simple-set-get/run.sh
@@ -12,9 +12,9 @@ ret=0
 for SERVER in $SERVERS ; do
     SERVER_IP=$(echo $SERVER | cut -f 1 -d ":")
     SERVER_MOXI=$(echo $SERVER | cut -f 3 -d ":")
-    echo "[$TESTNAME] Checking set/get $S"
+    echo "[$TESTNAME] Checking set/get key_$$"
 
-    echo -e "set key_$$ 0 0 1\na\r" | nc $SERVER_IP $SERVER_MOXI | tr -d '\015' | grep STORED &> /dev/null
+    echo -e "set key_$$ 123 0 1\na\r" | nc $SERVER_IP $SERVER_MOXI | tr -d '\015' | grep STORED &> /dev/null
     if [[ $? -ne 0 ]] ; then
         echo "[$TESTNAME] failed to set key key_$$"
         ret=1
@@ -22,6 +22,11 @@ for SERVER in $SERVERS ; do
     echo -e "get key_$$\r" | nc $SERVER_IP $SERVER_MOXI | grep VALUE &> /dev/null
     if [[ $? -ne 0 ]] ; then
         echo "[$TESTNAME] failed to get key key_$$"
+        ret=1
+    fi
+    echo -e "get key_$$\r" | nc $SERVER_IP $SERVER_MOXI | grep 123 &> /dev/null
+    if [[ $? -ne 0 ]] ; then
+        echo "[$TESTNAME] failed to get key key_$$ flags"
         ret=1
     fi
 done
