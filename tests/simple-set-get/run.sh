53e6ed376524835170295c969262a2fa6ab5e730
Steve Yen <steve.yen@gmail.com>
2010-11-17 12:58:57 -0800

    simple-get-set test needs \r\n
    Change-Id: I63b35bf30a5e7f64cd2b9e58db40e78fd81a320a
    Reviewed-on: http://review.membase.org/3712
    Tested-by: Keith Batten <kbatten@gmail.com>
    Reviewed-by: Keith Batten <kbatten@gmail.com>

--- tests/simple-set-get/run.sh
+++ tests/simple-set-get/run.sh
@@ -14,7 +14,7 @@ for SERVER in $SERVERS ; do
     SERVER_MOXI=$(echo $SERVER | cut -f 3 -d ":")
     echo "[$TESTNAME] Checking set/get key_$$"
 
-    echo -e "set key_$$ 123 0 1\na\r" | nc $SERVER_IP $SERVER_MOXI | tr -d '\015' | grep STORED &> /dev/null
+    echo -e "set key_$$ 123 0 1\r\na\r" | nc $SERVER_IP $SERVER_MOXI | tr -d '\015' | grep STORED &> /dev/null
     if [[ $? -ne 0 ]] ; then
         echo "[$TESTNAME] failed to set key key_$$"
         ret=1
