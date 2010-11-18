65fac88d041ebc3be328a3dd303887810b560e6e
Keith Batten <kbatten@gmail.com>
2010-11-17 16:15:01 -0800

    point to the correct server ip when creating the default bucket
    Change-Id: I8b9deb8b7ed7f558b5ad0bd7d932af14981a4fdb
    Reviewed-on: http://review.membase.org/3718
    Reviewed-by: Steve Yen <steve.yen@gmail.com>
    Tested-by: Steve Yen <steve.yen@gmail.com>

--- tests/reset/run.sh
+++ tests/reset/run.sh
@@ -27,7 +27,7 @@ sleep 2
 for SERVER in $SERVERS ; do
     SERVER_IP=$(echo $SERVER | cut -f 1 -d ":")
     echo "[$TESTNAME] creating default bucket on $SERVER"
-    ssh -i $KEYFILE root@$SERVER /opt/membase/bin/cli/membase bucket-create -c localhost -u Administrator -p password --bucket=default --bucket-type=membase --bucket-password="" --bucket-ramsize=300 --bucket-replica=1 &> /dev/null &
+    ssh -i $KEYFILE root@$SERVER_IP /opt/membase/bin/cli/membase bucket-create -c localhost -u Administrator -p password --bucket=default --bucket-type=membase --bucket-password="" --bucket-ramsize=300 --bucket-replica=1 &> /dev/null &
 done
 wait
 sleep 10
