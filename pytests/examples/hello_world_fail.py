5a785a55f6f417fbb7350a45a32b77cf302d01ad
farshid <farshid.ghods@gmail.com>
2011-04-25 10:44:44 -0700

    hello-world examples using pyunit and logger
    hello-world examples using pyunit and logger
    
    Change-Id: If6474665a982def7045fc48bc1a91255d6242cb0
    Reviewed-on: http://review.membase.org/5516
    Reviewed-by: Farshid Ghods <farshid.ghods@gmail.com>
    Tested-by: Farshid Ghods <farshid.ghods@gmail.com>

---
+++ pytests/examples/hello_world_fail.py
@@ -0,0 +1,29 @@
+import unittest
+import logger
+
+class HelloWorldFailTest(unittest.TestCase):
+
+    input = None
+    log = None
+    
+    def setUp(self):
+        self.log = logger.Logger.get_logger()
+        self.input = TestInputSingleton.input
+        self.log.info('test setUp invoked')
+        self.log.info('getting server ips')
+        servers = self.input.servers
+        for server in servers:
+            self.log.info("server ip : {0}".format(server.ip))
+
+    def test_hello_fail_1(self):
+        self.log.info('running test_hello_fail_1')
+        i = 1
+        self.assertEquals(i,2)
+
+    def test_hello_fail_2(self):
+        self.log.info('running test_hello_fail_2')
+        i = 2
+        self.assertEquals(i,3)
+
+    def tearDown(self):
+        self.log.info('tearDown invoked')
