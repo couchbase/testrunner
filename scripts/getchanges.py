f5fbb26cfa9dadce0a7ed40cd24bfd66c7afcec6
farshid <farshid.ghods@gmail.com>
2011-04-27 09:25:27 -0700

    added getchanges and grabdiags scripts
    added getchanges and grabdiags scripts
    
    getchanges script will find the related changes.txt file
    from builds.hq :
    usage  :
    python scripts/getchanges.py basestar-69 or
    python scripts/getchanges.py basestar-69-abgkh
    grabdiags script will go over all the servers in the
    input file and grabs the diagnostics from each one
    it also zips the file using python ZipFile library
    usage :
    python scripts/grabdiags abc.ini
    
    Change-Id: Ie779b61cb4b9a2b382e0fac1f2578e6bb2acdca8
    Reviewed-on: http://review.membase.org/5739
    Reviewed-by: Farshid Ghods <farshid.ghods@gmail.com>
    Tested-by: Farshid Ghods <farshid.ghods@gmail.com>

---
+++ scripts/getchanges.py
@@ -0,0 +1,23 @@
+import sys
+import urllib2
+
+sys.path.append('lib')
+
+from builds.build_query import BuildQuery
+
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) == 2:
+        version = args[1]
+        builds, changes = BuildQuery().get_all_builds()
+        build = BuildQuery().find_membase_build_with_version(builds, version)
+        if build.change and build.change.url:
+            try:
+                print "downloading {0}".format(build.change.url)
+                page = urllib2.urlopen(build.change.url)
+                changes = open('changes.txt', 'w')
+                changes.write('{0}'.format(page.read()))
+                changes.close()
+            except urllib2.HTTPError as error:
+                print 'unable to download {0}'.format(build.change.url)
+  
---
+++ scripts/grabdiags.py
@@ -0,0 +1,42 @@
+import sys
+import os
+import urllib2
+import zipfile
+import base64
+
+sys.path.append('.')
+sys.path.append('lib')
+
+from TestInput import TestInputParser
+
+def create_headers(username, password):
+    authorization = base64.encodestring('%s:%s' % (username, password))
+    return {'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic %s' % authorization,
+            'Accept': '*/*'}
+
+
+if __name__ == "__main__":
+    input = TestInputParser.get_test_input(sys.argv)
+    for serverInfo in input.servers:
+        print "grabbing diags from ".format(serverInfo.ip)
+        diag_url = "http://{0}:8091/diag".format(serverInfo.ip)
+        print diag_url
+        req = urllib2.Request(diag_url)
+        req.headers = create_headers(input.membase_settings.rest_username,
+                                     input.membase_settings.rest_password)
+        filename = "{0}-diag.txt".format(serverInfo.ip)
+        page = urllib2.urlopen(req)
+        with open(filename,'wb') as output:
+            os.write(1,"downloading {0} ...".format(serverInfo.ip))
+            while True:
+                buffer = page.read(65536)
+                if not buffer:
+                    break
+                output.write(buffer)
+                os.write(1,".")
+        zipped = zipfile.ZipFile("{0}.zip".format(filename), "w",compression=zipfile.ZIP_DEFLATED)
+        zipped.write(filename)
+        zipped.close()
+        os.remove(filename)
+        print "downloaded and zipped diags @ : {0}".format("{0}.zip".format(filename))
