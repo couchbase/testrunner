488043a7506466cdbc17b765f7c8ca154f7aa740
farshid <farshid.ghods@gmail.com>
2011-04-29 10:52:38 -0700

    refactored replicationtests to use data_helper
    refactored replicationtests to use data_helper
    
    instead of using the classic one thread key insert we can
    use data_helper to inject keys in parallel threads and etc
    
    Change-Id: Idfbc20f559d637946bc77d493fd6fef1cf0b6b85

--- pytests/management/replication/replicationtests.py
+++ pytests/management/replication/replicationtests.py
@@ -7,12 +7,11 @@ from mc_bin_client import MemcachedClient, MemcachedError
 from membase.api.rest_client import RestConnection, RestHelper
 from membase.helper.bucket_helper import BucketOperationHelper
 from membase.helper.cluster_helper import ClusterOperationHelper
-from remote.remote_util import RemoteMachineShellConnection
+from memcached.helper.data_helper import MemcachedClientHelper
 
 log = logger.Logger.get_logger()
 
 class ReplicationTests(unittest.TestCase):
-
     servers = None
     keys = None
     clients = None
@@ -31,104 +30,70 @@ class ReplicationTests(unittest.TestCase):
         #from previous test runs
 
 
-    def _load_data(self,fill_ram_percentage = 1.0):
-        if fill_ram_percentage <= 0.0:
-            fill_ram_percentage = 5.0
-        master = self.servers[0]
-        #populate key
-        rest = RestConnection(master)
-        testuuid = uuid.uuid4()
-        info = rest.get_bucket(self.bucket_name)
-        emptySpace = info.stats.ram - info.stats.memUsed
-        self.log.info('emptySpace : {0} fill_ram_percentage : {1}'.format(emptySpace, fill_ram_percentage))
-        fill_space = (emptySpace * fill_ram_percentage)/100.0
-        self.log.info("fill_space {0}".format(fill_space))
-        # each packet can be 10 KB
-        packetSize = int(10 *1024)
-        number_of_buckets = int(fill_space) / packetSize
-        self.log.info('packetSize: {0}'.format(packetSize))
-        self.log.info('memory usage before key insertion : {0}'.format(info.stats.memUsed))
-        self.log.info('inserting {0} new keys to memcached @ {0}'.format(number_of_buckets,master))
-        self.keys = ["key_%s_%d" % (testuuid, i) for i in range(number_of_buckets)]
-        client = MemcachedClient(master.ip, 11220)
-        self.keys_not_pushed = []
-        for key in self.keys:
-            vbucketId = crc32.crc32_hash(key) & 1023 # or & 0x3FF
-            client.vbucketId = vbucketId
-            try:
-                payload = self.generate_payload(key + '\0\r\n\0\0\n\r\0',10 * 1024)
-                client.set(key, 0, 0, payload)
-            except MemcachedError as error:
-                self.log.error(error)
-                self.log.error("unable to push key : {0} to bucket : {1}".format(key, client.vbucketId))
-                self.keys_not_pushed.append(key)
-        client.close()
-
     def test_failover_1_replica_0_1_percent(self):
-        self._test_failover_body(fill_ram_percentage=0.1,number_of_replicas=1)
+        self._test_failover_body(fill_ram_percentage=0.1, number_of_replicas=1)
 
     def test_failover_1_replica_1_percent(self):
-        self._test_failover_body(fill_ram_percentage=1,number_of_replicas=1)
+        self._test_failover_body(fill_ram_percentage=1, number_of_replicas=1)
 
     def test_failover_1_replica_10_percent(self):
-        self._test_failover_body(fill_ram_percentage=10,number_of_replicas=1)
+        self._test_failover_body(fill_ram_percentage=10, number_of_replicas=1)
 
     def test_failover_1_replica_50_percent(self):
-        self._test_failover_body(fill_ram_percentage=50,number_of_replicas=1)
+        self._test_failover_body(fill_ram_percentage=50, number_of_replicas=1)
 
     def test_failover_1_replica_99_percent(self):
-        self._test_failover_body(fill_ram_percentage=99,number_of_replicas=1)
-
+        self._test_failover_body(fill_ram_percentage=99, number_of_replicas=1)
 
 
     def test_replication_1_replica_0_1_percent(self):
-        self._test_body(fill_ram_percentage=0.1,number_of_replicas=1)
+        self._test_body(fill_ram_percentage=0.1, number_of_replicas=1)
 
     def test_replication_1_replica_1_percent(self):
-        self._test_body(fill_ram_percentage=1,number_of_replicas=1)
+        self._test_body(fill_ram_percentage=1, number_of_replicas=1)
 
     def test_replication_1_replica_10_percent(self):
-        self._test_body(fill_ram_percentage=10,number_of_replicas=1)
+        self._test_body(fill_ram_percentage=10, number_of_replicas=1)
 
     def test_replication_1_replica_50_percent(self):
-        self._test_body(fill_ram_percentage=50,number_of_replicas=1)
+        self._test_body(fill_ram_percentage=50, number_of_replicas=1)
 
     def test_replication_1_replica_99_percent(self):
-        self._test_body(fill_ram_percentage=99,number_of_replicas=1)
+        self._test_body(fill_ram_percentage=99, number_of_replicas=1)
 
 
     def test_replication_2_replica_0_1_percent(self):
-        self._test_body(fill_ram_percentage=0.1,number_of_replicas=2)
+        self._test_body(fill_ram_percentage=0.1, number_of_replicas=2)
 
     def test_replication_2_replica_1_percent(self):
-        self._test_body(fill_ram_percentage=1,number_of_replicas=2)
+        self._test_body(fill_ram_percentage=1, number_of_replicas=2)
 
     def test_replication_2_replica_10_percent(self):
-        self._test_body(fill_ram_percentage=10,number_of_replicas=2)
+        self._test_body(fill_ram_percentage=10, number_of_replicas=2)
 
     def test_replication_2_replica_50_percent(self):
-        self._test_body(fill_ram_percentage=50,number_of_replicas=2)
+        self._test_body(fill_ram_percentage=50, number_of_replicas=2)
 
     def test_replication_2_replica_99_percent(self):
-        self._test_body(fill_ram_percentage=99,number_of_replicas=2)
+        self._test_body(fill_ram_percentage=99, number_of_replicas=2)
 
 
     def test_replication_3_replica_0_1_percent(self):
-        self._test_body(fill_ram_percentage=0.1,number_of_replicas=3)
+        self._test_body(fill_ram_percentage=0.1, number_of_replicas=3)
 
     def test_replication_3_replica_1_percent(self):
-        self._test_body(fill_ram_percentage=1,number_of_replicas=3)
+        self._test_body(fill_ram_percentage=1, number_of_replicas=3)
 
     def test_replication_3_replica_10_percent(self):
-        self._test_body(fill_ram_percentage=10,number_of_replicas=3)
+        self._test_body(fill_ram_percentage=10, number_of_replicas=3)
 
     def test_replication_3_replica_50_percent(self):
-        self._test_body(fill_ram_percentage=50,number_of_replicas=3)
+        self._test_body(fill_ram_percentage=50, number_of_replicas=3)
 
     def test_replication_3_replica_99_percent(self):
-        self._test_body(fill_ram_percentage=99,number_of_replicas=3)
+        self._test_body(fill_ram_percentage=99, number_of_replicas=3)
 
-    def _check_vbuckets(self,number_of_replicas):
+    def _check_vbuckets(self, number_of_replicas):
         #this method makes sure for each vbucket there is x number
         #of replicas
         #each vbucket should have x replicas
@@ -142,7 +107,10 @@ class ReplicationTests(unittest.TestCase):
             for vbucket in vbuckets:
                 if len(vbucket.replica) != number_of_replicas:
                     self.log.error("vbucket # {0} number of replicas : {1} vs expected : {2}".format(index,
-                    len(vbucket.replica),number_of_replicas))
+                                                                                                     len(
+                                                                                                         vbucket.replica)
+                                                                                                     ,
+                                                                                                     number_of_replicas))
                     failed_verification.append(index)
                 index += 1
         if not failed_verification:
@@ -151,15 +119,15 @@ class ReplicationTests(unittest.TestCase):
     #visit each node and get the data to verify the replication
 
     #update keys
-    def _update_keys(self,version):
+    def _update_keys(self, version):
         client = MemcachedClient(self.servers[0].ip, 11220)
         self.updated_keys = []
         for key in self.keys:
             vbucketId = crc32.crc32_hash(key) & 1023 # or & 0x3FF
             client.vbucketId = vbucketId
-            value = '{0}{1}'.format(key,version)
+            value = '{0}'.format(version)
             try:
-                client.set(key, 0, 0, value)
+                client.append(key, value)
                 self.updated_keys.append(key)
             except MemcachedError as error:
                 self.log.error(error)
@@ -169,29 +137,24 @@ class ReplicationTests(unittest.TestCase):
 
     #verify
 
-    def _verify_minimum_requirement(self,number_of_replicas):
+    def _verify_minimum_requirement(self, number_of_replicas):
         # we should at least have
         # x = ips.length
         #-
-        self.assertTrue(len(self.servers)/(1+number_of_replicas) >= 1,"there are not enough number of nodes available")
+        self.assertTrue(len(self.servers) / (1 + number_of_replicas) >= 1,
+                        "there are not enough number of nodes available")
 
-    def _create_bucket(self,number_of_replicas=1):
-
+    def _create_bucket(self, number_of_replicas=1):
         self.bucket_name = 'ReplicationTest-{0}'.format(uuid.uuid4())
         ip_rest = RestConnection(self.servers[0])
         self.log.info('creating bucket : {0}'.format(self.bucket_name))
         ip_rest.create_bucket(bucket=self.bucket_name,
-                           ramQuotaMB=256,
-                           replicaNumber=number_of_replicas,
-                           proxyPort=11220)
+                              ramQuotaMB=256,
+                              replicaNumber=number_of_replicas,
+                              proxyPort=11220)
         msg = 'create_bucket succeeded but bucket {0} does not exist'.format(self.bucket_name)
         self.assertTrue(BucketOperationHelper.wait_for_bucket_creation(self.bucket_name,
                                                                        ip_rest), msg=msg)
-        remote = RemoteMachineShellConnection(self.servers[0])
-        info = remote.extract_remote_info()
-        remote.terminate_process(info, 'memcached')
-        remote.terminate_process(info, 'moxi')
-
         BucketOperationHelper.wait_till_memcached_is_ready_or_assert([self.servers[0]],
                                                                      bucket_port=11220,
                                                                      test=self)
@@ -200,7 +163,7 @@ class ReplicationTests(unittest.TestCase):
         ClusterOperationHelper.cleanup_cluster(self.servers)
         BucketOperationHelper.delete_all_buckets_or_assert(self.servers, self)
 
-    def _verify_data(self,version):
+    def _verify_data(self, version):
         #verify all the keys
         master = self.servers[0]
         client = MemcachedClient(master.ip, 11220)
@@ -214,51 +177,37 @@ class ReplicationTests(unittest.TestCase):
                     vbucketId = crc32.crc32_hash(key) & 1023 # or & 0x3FF
                     client.vbucketId = vbucketId
                     flag, keyx, value = client.get(key=key)
-                    expected = '{0}{1}'.format(key,version)
-                    self.assertEquals(value, expected,
-                                      msg='values do not match . expected : {0} actual : {1}'.format(value,expected))
-    #                self.log.info("verified key #{0} : {1} value : {2}".format(index,key,value))
+                    self.assertTrue(value.endswith(version),
+                                    msg='values do not match . key value should endwith {0}'.format(version))
+                    #                self.log.info("verified key #{0} : {1} value : {2}".format(index,key,value))
                 except MemcachedError as error:
                     self.log.error(error)
-                    self.log.error("memcachedError : {0} - unable to get a pre-inserted key : {0}".format(error.status,key))
+                    self.log.error(
+                        "memcachedError : {0} - unable to get a pre-inserted key : {0}".format(error.status, key))
                     keys_failed.append(key)
                     all_verified = False
-#            except :
-#                self.log.error("unknown errors unable to get a pre-inserted key : {0}".format(key))
-#                keys_failed.append(key)
-#                all_verified = False
+                    #            except :
+                    #                self.log.error("unknown errors unable to get a pre-inserted key : {0}".format(key))
+                    #                keys_failed.append(key)
+                    #                all_verified = False
 
         client.close()
         self.assertTrue(all_verified,
                         'unable to verify #{0} keys'.format(len(keys_failed)))
-    
+
 
     def add_nodes_and_rebalance(self):
-        index = 0
-        otpNodes = []
         master = self.servers[0]
+        ClusterOperationHelper.add_all_nodes_or_assert(master,
+                                                       self.servers,
+                                                       self.input.membase_settings,
+                                                       self)
         rest = RestConnection(master)
-        for serverInfo in self.servers:
-            if index > 0:
-                self.log.info('adding node : {0} to the cluster'.format(serverInfo.ip))
-                otpNode = rest.add_node(user=self.input.membase_settings.rest_username,
-                                        password=self.input.membase_settings.rest_password,
-                                        remoteIp=serverInfo.ip)
-                if otpNode:
-                    self.log.info('added node : {0} to the cluster'.format(otpNode.id))
-                    otpNodes.append(otpNode)
-#            remote = RemoteMachineShellConnection(ip=ip,
-#                                                  username='root',
-#                                                  pkey_location=os.getenv("KEYFILE"))
-#            remote.execute_command('killall -9 memcached')
-            index += 1
-            #rebalance
-        #let's kill all memcached
         nodes = rest.node_statuses()
         otpNodeIds = []
         for node in nodes:
             otpNodeIds.append(node.id)
-        rebalanceStarted = rest.rebalance(otpNodeIds,[])
+        rebalanceStarted = rest.rebalance(otpNodeIds, [])
         self.assertTrue(rebalanceStarted,
                         "unable to start rebalance on master node {0}".format(master.ip))
         self.log.info('started rebalance operation on master node {0}'.format(master.ip))
@@ -278,16 +227,31 @@ class ReplicationTests(unittest.TestCase):
     #part 3 : add nodes and rebalance
     #part 4 : update keys
 
-    def _test_body(self,fill_ram_percentage = 1,number_of_replicas=1):
+    def _test_body(self, fill_ram_percentage=1, number_of_replicas=1):
+        master = self.servers[0]
         self._verify_minimum_requirement(number_of_replicas)
         self._cleanup_cluster()
         self.log.info('cluster is setup')
         self._create_bucket(number_of_replicas)
         self.log.info('created the bucket')
-        self._load_data(fill_ram_percentage=0.1)
+        #let's use data_helper
+        distribution = {10: 0.2, 20: 0.5, 10 * 1024: 0.25, 30: 0.05}
+        MemcachedClientHelper.load_bucket(master,
+                                          self.bucket_name,
+                                          11220,
+                                          0.1,
+                                          distribution,
+                                          40)
         self.add_nodes_and_rebalance()
         self.log.info('loading more data into the bucket')
-        self._load_data(fill_ram_percentage=fill_ram_percentage)
+        inserted_keys, rejected_keys =\
+        MemcachedClientHelper.load_bucket(master,
+                                          self.bucket_name,
+                                          11220,
+                                          fill_ram_percentage,
+                                          distribution,
+                                          40)
+        self.keys = inserted_keys
         self.log.info('updating all keys by appending _20 to each value')
         self._update_keys('20')
         self.log.info('verifying keys now...._20')
@@ -300,17 +264,32 @@ class ReplicationTests(unittest.TestCase):
         self.log.info('verifying keys now...._20')
         self._verify_data('30')
 
-    def _test_failover_body(self,fill_ram_percentage = 1,number_of_replicas=1):
+    def _test_failover_body(self, fill_ram_percentage=1, number_of_replicas=1):
         self._verify_minimum_requirement(number_of_replicas)
         self._cleanup_cluster()
         self.log.info('cluster is setup')
         self._create_bucket(number_of_replicas)
         self.log.info('created the bucket')
         # tiny amount of data in the bucket
-        self._load_data(fill_ram_percentage=0.1)
+        distribution = {10: 0.2, 20: 0.5, 10 * 1024: 0.25, 30: 0.05}
+        MemcachedClientHelper.load_bucket(self.servers[0],
+                                          self.bucket_name,
+                                          11220,
+                                          0.1,
+                                          distribution,
+                                          40)
         self.add_nodes_and_rebalance()
         self.log.info('loading more data into the bucket')
-        self._load_data(fill_ram_percentage=fill_ram_percentage)
+        distribution = {100: 0.2, 1024: 0.5, 10 * 1024: 0.25, 500 * 1024: 0.05}
+        inserted_keys, rejected_keys =\
+        MemcachedClientHelper.load_bucket(self.servers[0],
+                                          self.bucket_name,
+                                          11220,
+                                          fill_ram_percentage,
+                                          distribution,
+                                          40)
+
+        self.keys = inserted_keys
         self.log.info('updating all keys by appending _20 to each value')
         self._update_keys('20')
         self.log.info('verifying keys now...._20')
@@ -336,6 +315,3 @@ class ReplicationTests(unittest.TestCase):
 
     def tearDown(self):
         self._cleanup_cluster()
-
-    def generate_payload(self, pattern, size):
-        return (pattern * (size / len(pattern))) + pattern[0:(size % len(pattern))]
