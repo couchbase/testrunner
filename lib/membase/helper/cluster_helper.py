9f0a1a39ec7607398acfc96233c3a39676a0c23f
farshid <farshid.ghods@gmail.com>
2011-04-28 23:54:17 -0700

    refactored add_rebalance test
    refactored add_rebalance test
    
    -promoted bunch of reusable methods into cluster_helper
    and bucket_helper
    -use data_helper for memcached load injection
    
    Change-Id: I7d395d9d7289b40f1265ff84dd2233dfe8f0c960
    Reviewed-on: http://review.membase.org/5810
    Reviewed-by: Farshid Ghods <farshid.ghods@gmail.com>
    Tested-by: Farshid Ghods <farshid.ghods@gmail.com>

--- lib/membase/helper/cluster_helper.py
+++ lib/membase/helper/cluster_helper.py
@@ -5,6 +5,32 @@ import os
 
 class ClusterOperationHelper(object):
     #the first ip is taken as the master ip
+
+    @staticmethod
+    def add_all_nodes_or_assert(master,all_servers,rest_settings,test_case):
+        log = logger.Logger.get_logger()
+        otpNodes = []
+        all_nodes_added = True
+        rest = RestConnection(master)
+        for serverInfo in all_servers:
+            if serverInfo.ip != master.ip:
+                log.info('adding node : {0} to the cluster'.format(serverInfo.ip))
+                otpNode = rest.add_node(rest_settings.rest_username,
+                                        rest_settings.rest_password,
+                                        serverInfo.ip)
+                if otpNode:
+                    log.info('added node : {0} to the cluster'.format(otpNode.id))
+                    otpNodes.append(otpNode)
+                else:
+                    all_nodes_added = False
+        if not all_nodes_added:
+            if test_case:
+                test_case.assertTrue(all_nodes_added,
+                                     msg="unable to add all the nodes to the cluster")
+            else:
+                log.error("unable to add all the nodes to the cluster")
+        return otpNodes
+
     @staticmethod
     def cleanup_cluster(servers):
         log = logger.Logger.get_logger()
@@ -17,13 +43,6 @@ class ClusterOperationHelper(object):
             allNodes.append(node.id)
             if node.id.find(master.ip) < 0 and node.id.find('127.0.0.1') < 0:
                 toBeEjectedNodes.append(node.id)
-                for server in servers:
-                    if server.ip == node.ip:
-                        remote = RemoteMachineShellConnection(server)
-                        info = remote.extract_remote_info()
-                        remote.terminate_process(info, 'memcached')
-                        remote.terminate_process(info, 'moxi')
-                        break
             #let's rebalance to remove all the nodes from the master
                 #this is not the master , let's remove it
                 #now load data into the main bucket
@@ -32,3 +51,17 @@ class ClusterOperationHelper(object):
             helper = RestHelper(rest)
             removed = helper.remove_nodes(knownNodes=allNodes,ejectedNodes=toBeEjectedNodes)
             log.info("removed all the nodes from this cluster ? ".format(removed))
+
+    @staticmethod
+    def rebalance_params_for_declustering(master,all_nodes):
+        log = logger.Logger.get_logger()
+        otpNodeIds = [node.id for node in all_nodes]
+        knownNodes = []
+        knownNodes.extend(otpNodeIds)
+        if 'ns_1@' + master.ip in otpNodeIds:
+            otpNodeIds.remove('ns_1@' + master.ip)
+        if 'ns_1@127.0.0.1' in otpNodeIds:
+            otpNodeIds.remove('ns_1@127.0.0.1')
+        ejectedNodes = otpNodeIds
+        log.info('ejectedNodes : {0} , knownNodes : {1}'.format(ejectedNodes,knownNodes))
+        return knownNodes,ejectedNodes
--- pytests/management/add_rebalance.py
+++ pytests/management/add_rebalance.py
@@ -1,27 +1,16 @@
-import os
 import unittest
-import socket
-import zlib
-import ctypes
-import uuid
 from TestInput import TestInputSingleton
 import logger
-import crc32
-from mc_bin_client import MemcachedClient, MemcachedError
 from membase.api.rest_client import RestConnection, RestHelper
 from membase.helper.bucket_helper import BucketOperationHelper
-from membase.helper.cluster_helper import ClusterOperationHelper
+from membase.helper.cluster_helper import ClusterOperationHelper as ClusterHelper
 from membase.helper.rebalance_helper import RebalanceHelper
-from remote.remote_util import RemoteMachineShellConnection
+from memcached.helper.data_helper import MemcachedClientHelper as MemcachedHelper
 
 class AddRebalanceNodesTest(unittest.TestCase):
-    version = None
     input = None
     servers = None
-    keys = None
-    clients = None
     bucket_name = None
-    keys_not_pushed = None
     log = None
 
     def setUp(self):
@@ -29,7 +18,7 @@ class AddRebalanceNodesTest(unittest.TestCase):
         self.input = TestInputSingleton.input
         self.assertTrue(self.input, msg="input parameters missing...")
         self.servers = self.input.servers
-        ClusterOperationHelper.cleanup_cluster(self.servers)
+        ClusterHelper.cleanup_cluster(self.servers)
         BucketOperationHelper.delete_all_buckets_or_assert(self.servers, self)
         self.bucket_name = 'default'
         rest = RestConnection(self.servers[0])
@@ -42,51 +31,47 @@ class AddRebalanceNodesTest(unittest.TestCase):
                            ramQuotaMB=256,
                            replicaNumber=1,
                            proxyPort=11211)
-        remote = RemoteMachineShellConnection(self.servers[0])
-        info = remote.extract_remote_info()
-        remote.terminate_process(info, 'memcached')
-        remote.terminate_process(info, 'moxi')
         BucketOperationHelper.wait_till_memcached_is_ready_or_assert(servers=[self.servers[0]],
                                                                      bucket_port=11211,
                                                                      test=self)
 
-    #let's try to fill up ram up to y percentage
-    def _load_data(self, fill_ram_percentage=10.0):
-        if fill_ram_percentage <= 0.0:
-            fill_ram_percentage = 5.0
+    #add them all rebalance
+    def add_rebalance_remove(self, ram_load_ratio):
+        #add nodes
         master = self.servers[0]
-        client = MemcachedClient(master.ip, 11211)
-        #populate key
+        #size distribution ?
+        distribution = {1024: 0.4, 10 * 1024: 0.4, 40 * 1024: 0.2}
+        MemcachedHelper.load_bucket(master, self.bucket_name, 11211, ram_load_ratio, distribution)
         rest = RestConnection(master)
-        testuuid = uuid.uuid4()
-        info = rest.get_bucket(self.bucket_name)
-        emptySpace = info.stats.ram - info.stats.memUsed
-        self.log.info('emptySpace : {0} fill_ram_percentage : {1}'.format(emptySpace, fill_ram_percentage))
-        fill_space = (emptySpace * fill_ram_percentage) / 100.0
-        self.log.info("fill_space {0}".format(fill_space))
-        # each packet can be 10 KB
-        packetSize = int(10 * 1024)
-        number_of_buckets = int(fill_space) / packetSize
-        self.log.info('packetSize: {0}'.format(packetSize))
-        self.log.info('memory usage before key insertion : {0}'.format(info.stats.memUsed))
-        self.log.info('inserting {0} new keys to memcached @ {0}'.format(number_of_buckets, master.ip))
-        self.keys = ["key_%s_%d" % (testuuid, i) for i in range(number_of_buckets)]
-        self.keys_not_pushed = []
-        for key in self.keys:
-            vbucketId = crc32.crc32_hash(key) & 1023 # or & 0x3FF
-            client.vbucketId = vbucketId
-            payload = self.generate_payload(key + '\0\r\n\0\0\n\r\0', 10 * 1024)
-            flag = socket.htonl(ctypes.c_uint32(zlib.adler32(payload)).value)
-            try:
-                client.set(key, 0, flag, payload)
-            except MemcachedError as error:
-                self.log.error(error)
-                self.log.error("unable to push key : {0} to bucket : {1}".format(key, client.vbucketId))
-                self.keys_not_pushed.append(key)
-        client.close()
+        ClusterHelper.add_all_nodes_or_assert(master, self.servers,
+                                              self.input.membase_settings, self)
+        nodes = rest.node_statuses()
+        otpNodeIds = [node.id for node in nodes]
+        before_info = rest.get_bucket(self.bucket_name)
+        rebalanceStarted = rest.rebalance(otpNodeIds, [])
+        self.assertTrue(rebalanceStarted,
+                        "unable to start rebalance on master node {0}".format(master.ip))
+        self.log.info('started rebalance operation on master node {0}'.format(master.ip))
+        rebalance_succeeded = rest.monitorRebalance()
+        self.assertTrue(rebalance_succeeded,
+                        "rebalance operation for nodes: {0} was not successful".format(otpNodeIds))
+        self.log.info('rebalance operation succeeded for nodes: {0}'.format(otpNodeIds))
+        after_info = rest.get_bucket(self.bucket_name)
+        RebalanceHelper.verify_maps(before_info.forward_map, after_info.vbuckets)
+        #now remove the nodes
+        #make sure its rebalanced and node statuses are healthy
+        helper = RestHelper(rest)
+        self.assertTrue(helper.is_cluster_healthy(), "cluster status is not healthy")
+        self.assertTrue(helper.is_cluster_rebalanced(), "cluster is not balanced")
+        self.assertTrue(helper.wait_for_replication(180),
+                        msg="replication did not complete")
+        knownNodes, ejectedNodes =\
+        ClusterHelper.rebalance_params_for_declustering(master=master,all_nodes=nodes)
+        helper.remove_nodes(knownNodes, ejectedNodes)
 
-
-    #let's parametrize number of nodes!!
+    def tearDown(self):
+        #let's just clean up the buckets in the master node
+        BucketOperationHelper.delete_all_buckets_or_assert([self.servers[0]], self)
 
 
     def test_rebalance_0_1_percent_ram(self):
@@ -106,150 +91,3 @@ class AddRebalanceNodesTest(unittest.TestCase):
 
     def test_rebalance_99_percent_ram(self):
         self.add_rebalance_remove(99.0)
-
-    #add them all rebalance
-    def add_rebalance_remove(self, load_percentage):
-        self._load_data(load_percentage)
-        #add nodes
-        index = 0
-        otpNodes = []
-        master = self.servers[0]
-        rest = RestConnection(master)
-        all_nodes_added = True
-        for serverInfo in self.servers:
-            if index > 0:
-                self.log.info('adding node : {0} to the cluster'.format(serverInfo.ip))
-                otpNode = rest.add_node(user=self.input.membase_settings.rest_username,
-                                        password=self.input.membase_settings.rest_password,
-                                        remoteIp=serverInfo.ip)
-                if otpNode:
-                    self.log.info('added node : {0} to the cluster'.format(otpNode.id))
-                    otpNodes.append(otpNode)
-                else:
-                    all_nodes_added = False
-            index += 1
-            #rebalance
-        #create knownNodes
-        #let's kill all memcached
-        self.assertTrue(all_nodes_added,
-                        msg="unable tgit logo add the nodes to the cluster")
-        nodes = rest.node_statuses()
-        otpNodeIds = []
-        for node in nodes:
-            otpNodeIds.append(node.id)
-            #vbucketmapbefore
-        #how about the main node ?
-        before_info = rest.get_bucket(self.bucket_name)
-
-        rebalanceStarted = rest.rebalance(otpNodeIds,[])
-        self.assertTrue(rebalanceStarted,
-                        "unable to start rebalance on master node {0}".format(master.ip))
-        self.log.info('started rebalance operation on master node {0}'.format(master.ip))
-        rebalanceSucceeded = rest.monitorRebalance()
-        self.assertTrue(rebalanceSucceeded,
-                        "rebalance operation for nodes: {0} was not successful".format(otpNodeIds))
-        self.log.info('rebalance operaton succeeded for nodes: {0}'.format(otpNodeIds))
-        after_info = rest.get_bucket(self.bucket_name)
-        RebalanceHelper.verify_maps(before_info.forward_map, after_info.vbuckets)
-        #now remove the nodes
-        #make sure its rebalanced and node statuses are healthy
-        helper = RestHelper(rest)
-        self.assertTrue(helper.is_cluster_healthy, "cluster status is not healthy")
-        self.assertTrue(helper.is_cluster_rebalanced, "cluster is not balanced")
-        self.assertTrue(helper.wait_for_replication(180),
-                        msg="replication did not complete")
-
-        allnodes = []
-        allnodes.extend(otpNodeIds)
-        if 'ns_1@' + master.ip in otpNodeIds:
-            otpNodeIds.remove('ns_1@' + master.ip)
-        if 'ns_1@127.0.0.1' + master.ip in otpNodeIds:
-            otpNodeIds.remove('ns_1@127.0.0.1')
-        helper.remove_nodes(knownNodes=allnodes,
-                            ejectedNodes=otpNodeIds)
-
-        #add them all rebalance
-
-    #    def add_rebalance_remove_one_by_one(self, load_percentage):
-    #        self._load_data(load_percentage)
-    #        #add nodes
-    #        index = 0
-    #        otpNodes = []
-    #        master = self.ips[0]
-    #        rest = RestConnection(ip=master,
-    #                              username='Administrator',
-    #                              password='password')
-    #        all_nodes_added = True
-    #        for ip in self.ips:
-    #            if index > 0:
-    #                self.log.info('adding node : {0} to the cluster'.format(ip))
-    #                otpNode = rest.add_node(user='Administrator',
-    #                                        password='password',
-    #                                        remoteIp=ip)
-    #                if otpNode:
-    #                    self.log.info('added node : {0} to the cluster'.format(otpNode.id))
-    #                    otpNodes.append(otpNode)
-    ##                    remote = RemoteMachineShellConnection(ip=ip,
-    ##                                                          username='root',
-    ##                                                          pkey_location=os.getenv("KEYFILE"))
-    ##                    remote.execute_command('killall -9 memcached;killall -9 moxi;')
-    ##                    self.log.info('killing memcached and moxi on {0} after adding the node.'.format(ip))
-    ##                    time.sleep(5)
-    #                else:
-    #                    all_nodes_added = False
-    #            index += 1
-    #            #rebalance
-    #        #create knownNodes
-    #        #let's kill all memcached
-    #        self.assertTrue(all_nodes_added,
-    #                        msg="unable to add the nodes to the cluster")
-    #        otpNodeIds = ['ns_1@' + master]
-    #        for otpNode in otpNodes:
-    #            otpNodeIds.append(otpNode.id)
-    #            #vbucketmapbefore
-    #        before_info = rest.get_bucket(self.bucket_name)
-    #
-    #        rebalanceStarted = rest.rebalance(otpNodeIds)
-    #        self.assertTrue(rebalanceStarted,
-    #                        "unable to start rebalance on master node {0}".format(master))
-    #        self.log.info('started rebalance operation on master node {0}'.format(master))
-    #        rebalanceSucceeded = rest.monitorRebalance()
-    #        self.assertTrue(rebalanceSucceeded,
-    #                        "rebalance operation for nodes: {0} was not successful".format(otpNodeIds))
-    #        self.log.info('rebalance operaton succeeded for nodes: {0}'.format(otpNodeIds))
-    #        after_info = rest.get_bucket(self.bucket_name)
-    #        RebalanceHelper.verify_maps(before_info.forward_map, after_info.vbuckets)
-    #        #now remove the nodes
-    #        #make sure its rebalanced and node statuses are healthy
-    #        helper = RestHelper(rest)
-    #        self.assertTrue(helper.is_cluster_healthy, "cluster status is not healthy")
-    #        self.assertTrue(helper.is_cluster_rebalanced, "cluster is not balanced")
-    #        self.assertTrue(helper.wait_for_replication(180),
-    #                        msg="replication did not complete")
-    #
-    #        allnodes = []
-    #        allnodes.extend(otpNodeIds)
-    #        otpNodeIds.remove('ns_1@' + master)
-    #        helper.remove_nodes(knownNodes=allnodes,
-    #                            ejectedNodes=otpNodeIds)
-
-
-    #        self.log.info('ejected node {0} from master {1}'.format(otpNode.id, master))
-
-    def tearDown(self):
-        #let's just clean up the buckets in the master node
-        BucketOperationHelper.delete_all_buckets_or_assert([self.servers[0]], self)
-
-
-    def extract_server_ips(self):
-        servers_string = os.getenv("SERVERS")
-        servers = servers_string.split(" ")
-        return [server[:server.index(':')] for server in servers]
-
-    def generate_payload(self, pattern, size):
-        return (pattern * (size / len(pattern))) + pattern[0:(size % len(pattern))]
-
-
-        #test cases for adding the node and rebalancing one by one
-
-#test case : keep injecting data while test is running
