ca212ac9e87f774644f27fcbf5d8a09cb3a41dc2
farshid <farshid.ghods@gmail.com>
2011-04-29 00:00:02 -0700

    handle urlopen error in grabdiags
    handle urlopen error in grabdiags
    
    membase-server:8091 terminated the connection while
    urllib2 was downloading the diags.
    we need to handle this error and ignore it
    
    Change-Id: I2a31b15a1f3b4612135f4537fa07d16ba3356567

--- scripts/grabdiags.py
+++ scripts/grabdiags.py
@@ -22,21 +22,25 @@ if __name__ == "__main__":
         print "grabbing diags from ".format(serverInfo.ip)
         diag_url = "http://{0}:8091/diag".format(serverInfo.ip)
         print diag_url
-        req = urllib2.Request(diag_url)
-        req.headers = create_headers(input.membase_settings.rest_username,
-                                     input.membase_settings.rest_password)
-        filename = "{0}-diag.txt".format(serverInfo.ip)
-        page = urllib2.urlopen(req)
-        with open(filename,'wb') as output:
-            os.write(1,"downloading {0} ...".format(serverInfo.ip))
-            while True:
-                buffer = page.read(65536)
-                if not buffer:
-                    break
-                output.write(buffer)
-                os.write(1,".")
-        zipped = zipfile.ZipFile("{0}.zip".format(filename), "w",compression=zipfile.ZIP_DEFLATED)
-        zipped.write(filename)
-        zipped.close()
-        os.remove(filename)
-        print "downloaded and zipped diags @ : {0}".format("{0}.zip".format(filename))
+        try:
+            req = urllib2.Request(diag_url)
+            req.headers = create_headers(input.membase_settings.rest_username,
+                                         input.membase_settings.rest_password)
+            filename = "{0}-diag.txt".format(serverInfo.ip)
+            page = urllib2.urlopen(req)
+            with open(filename, 'wb') as output:
+                os.write(1, "downloading {0} ...".format(serverInfo.ip))
+                while True:
+                    buffer = page.read(65536)
+                    if not buffer:
+                        break
+                    output.write(buffer)
+                    os.write(1, ".")
+            zipped = zipfile.ZipFile("{0}.zip".format(filename), "w", compression=zipfile.ZIP_DEFLATED)
+            zipped.write(filename)
+            zipped.close()
+            os.remove(filename)
+            print "downloaded and zipped diags @ : {0}".format("{0}.zip".format(filename))
+        except urllib2.URLError as error:
+            print "unable to obtain diags from {0}".format(diag_url)
+
