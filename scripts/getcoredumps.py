45d0e5a173b77068dabe209a7da9b4e80400f90c
farshid <farshid.ghods@gmail.com>
2011-05-14 16:55:15 -0700

    download and renamed erlang core files
    download and renamed erlang core files
    
    Change-Id: I7b64a8e6d798a373b41b01c69b6b775bf5754825

--- scripts/getcoredumps.py
+++ scripts/getcoredumps.py
@@ -22,15 +22,17 @@ if __name__ == "__main__":
                 core_files.extend(remote.file_starts_with("/tmp/", "core"))
                 i = 0
                 for core_file in core_files:
-                    if core_file.startswith('erl'):
+                    if core_file.find('erl_crash.dump') != -1:
                         #let's just copy that file back
                         erl_crash_file_name = "erlang-{0}-{1}.log".format(serverInfo.ip, i)
+                        erl_crash_path = "/opt/membase/var/lib/membase/{0}".format(erl_crash_file_name)
+                        remote.execute_command('cp {0} {1}'.format(core_file, erl_crash_path))
                         destination = "{0}/{1}".format(os.getcwd(), erl_crash_file_name)
                         if remote.get_file(remotepath="/opt/membase/var/lib/membase/",
                                            filename=erl_crash_file_name,
                                            todir=destination):
                             print 'downloaded core file : {0}'.format(destination)
-                        i += 1
+                            i += 1
                     else:
                         command = "/opt/membase/bin/analyze_core"
                         core_file_name = "core-{0}-{1}.log".format(serverInfo.ip, i)
@@ -40,8 +42,10 @@ if __name__ == "__main__":
                         destination = "{0}/{1}".format(os.getcwd(), core_file_name)
                         if remote.get_file(remotepath="/tmp", filename=core_file_name, todir=destination):
                             print 'downloaded core file : {0}'.format(destination)
-                        i += 1
-                remote.execute_command('mkdir -p /tmp/backup;mv -f /tmp/core* /tmp/backup/')
+                            i += 1
+                if i > 0:
+                    remote.execute_command('mkdir -p /tmp/backup;mv -f /tmp/core* /tmp/backup/;')
+                    remote.execute_command('mv -f /opt/membase/var/lib/membase/erl_crash.dump /tmp/backup')
         except Exception as ex:
             print ex
             #ignoring all exceptions
