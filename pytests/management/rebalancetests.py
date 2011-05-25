a6d376ffe40e0b82fd2e53dcd056e5dfd25bf01d
farshid <farshid.ghods@gmail.com>
2011-05-24 19:33:45 -0700

    added dgm tests to rebalance
    Change-Id: I97581f51b1ce154f8d5642b396b9e2a72baea686

--- pytests/management/rebalancetests.py
+++ pytests/management/rebalancetests.py
@@ -1,3 +1,4 @@
+from random import shuffle
 import time
 import unittest
 import uuid
@@ -36,6 +37,39 @@ class RebalanceBaseTest(unittest.TestCase):
         ClusterOperationHelper.cleanup_cluster(servers)
         ClusterHelper.wait_for_ns_servers_or_assert(servers, testcase)
         BucketOperationHelper.delete_all_buckets_or_assert(servers, testcase)
+
+
+    @staticmethod
+    def rebalance_in(servers,how_many):
+        log = logger.Logger.get_logger()
+        rest = RestConnection(servers[0])
+        nodes = rest.node_statuses()
+        #choose how_many nodes from self._servers which are not part of
+        # nodes
+        nodeIps = [node.ip for node in nodes]
+        log.info("current nodes : {0}".format(nodeIps))
+        toBeAdded = []
+        selection = servers[1:]
+        shuffle(selection)
+        for server in selection:
+            if not server.ip in nodeIps:
+                toBeAdded.append(server)
+            if len(toBeAdded) == how_many:
+                break
+
+        for server in toBeAdded:
+            rest.add_node('Administrator','password',server.ip)
+            #check if its added ?
+        otpNodes = [node.id for node in rest.node_statuses()]
+        started = rest.rebalance(otpNodes,[])
+        msg = "rebalance operation started ? {0}"
+        log.info(msg.format(started))
+        if started:
+            result = rest.monitorRebalance()
+            msg = "successfully rebalanced out selected nodes from the cluster ? {0}"
+            log.info(msg.format(result))
+            return result
+        return False
 
 #load data. add one node rebalance , rebalance out
 class IncrementalRebalanceInTests(unittest.TestCase):
@@ -90,7 +124,7 @@ class IncrementalRebalanceInTests(unittest.TestCase):
             MemcachedClientHelper.load_bucket(serverInfo=master,
                                               ram_load_ratio=load_ratio,
                                               value_size_distribution=distribution,
-                                              number_of_threads=20)
+                                              number_of_threads=5)
             self.log.info('inserted {0} keys'.format(inserted_count))
             rest.rebalance(otpNodes=otpNodeIds, ejectedNodes=[])
             self.assertTrue(rest.monitorRebalance(),
@@ -430,3 +464,189 @@ class RebalanceTestsWithMutationLoadTests(unittest.TestCase):
 
     def test_heavy_load(self):
         self._common_test_body(40.0)
+
+# fill bucket len(nodes)X and then rebalance nodes one by one
+class IncrementalRebalanceInDgmTests(unittest.TestCase):
+    pass
+
+    def setUp(self):
+        self._input = TestInputSingleton.input
+        self._servers = self._input.servers
+        self.log = logger.Logger().get_logger()
+        RebalanceBaseTest.common_setup(self._input, 'default', self)
+
+    def tearDown(self):
+        RebalanceBaseTest.common_tearDown(self._servers, self)
+
+    #load data add one node , rebalance add another node rebalance
+    def _common_test_body(self, dgm_ratio):
+        master = self._servers[0]
+        rest = RestConnection(master)
+        items_inserted_count = 0
+        self.log.info("inserting some items in the master before adding any nodes")
+        distribution = {10: 0.5, 20: 0.5}
+        inserted_count, rejected_count =\
+        MemcachedClientHelper.load_bucket(serverInfo=master,
+                                          ram_load_ratio=0.1,
+                                          value_size_distribution=distribution,
+                                          number_of_threads=20)
+        items_inserted_count += inserted_count
+
+        nodes = rest.node_statuses()
+        while len(nodes) < len(self._servers):
+            self.assertTrue(RebalanceBaseTest.rebalance_in(self._servers, 1),
+                            msg="unable to add and rebalance another node")
+            distribution = {1 * 512: 0.4, 1 * 1024: 0.5, 2 * 1024: 0.1}
+            inserted_count, rejected_count =\
+            MemcachedClientHelper.load_bucket(serverInfo=master,
+                                              ram_load_ratio=dgm_ratio * 100,
+                                              value_size_distribution=distribution,
+                                              number_of_threads=5)
+            self.log.info('inserted {0} keys'.format(inserted_count))
+            items_inserted_count += inserted_count
+            final_replication_state = RestHelper(rest).wait_for_replication(120)
+            msg = "replication state after waiting for up to 2 minutes : {0}"
+            self.log.info(msg.format(final_replication_state))
+            start_time = time.time()
+            stats = rest.get_bucket_stats()
+            while time.time() < (start_time + 120) and stats["curr_items"] != items_inserted_count:
+                self.log.info("curr_items : {0} versus {1}".format(stats["curr_items"], items_inserted_count))
+                time.sleep(5)
+                stats = rest.get_bucket_stats()
+            RebalanceHelper.print_taps_from_all_nodes(rest, 'default')
+            self.log.info("curr_items : {0} versus {1}".format(stats["curr_items"], items_inserted_count))
+            stats = rest.get_bucket_stats()
+            msg = "curr_items : {0} is not equal to actual # of keys inserted : {1}"
+            self.assertEquals(stats["curr_items"], items_inserted_count,
+                              msg=msg.format(stats["curr_items"], items_inserted_count))
+        BucketOperationHelper.delete_all_buckets_or_assert(self._servers, self)
+
+    def test_1_5x(self):
+        self._common_test_body(1.5)
+
+    def test_2_x(self):
+        self._common_test_body(2)
+
+    def test_3_x(self):
+        self._common_test_body(3)
+
+    def test_5_x(self):
+        self._common_test_body(5)
+
+
+class RebalanceSwapTests(unittest.TestCase):
+    pass
+
+    def setUp(self):
+        self._input = TestInputSingleton.input
+        self._servers = self._input.servers
+        self.log = logger.Logger().get_logger()
+        RebalanceBaseTest.common_setup(self._input, 'default', self)
+
+    def tearDown(self):
+        RebalanceBaseTest.common_tearDown(self._servers, self)
+
+    def rebalance_in(self,how_many):
+        rest = RestConnection(self._servers[0])
+        nodes = rest.node_statuses()
+        #choose how_many nodes from self._servers which are not part of
+        # nodes
+        nodeIps = [node.ip for node in nodes]
+        self.log.info("current nodes : {0}".format(nodeIps))
+        toBeAdded = []
+        selection = self._servers[1:]
+        shuffle(selection)
+        for server in selection:
+            if server.ip in nodeIps:
+                toBeAdded.append(server)
+            if len(toBeAdded) == how_many:
+                break
+
+        for server in toBeAdded:
+            rest.add_node('Administrator','password',server.ip)
+            #check if its added ?
+        otpNodes = [node.id for node in nodes]
+        started = rest.rebalance(otpNodes,[])
+        msg = "rebalance operation started ? {0}"
+        self.log.info(msg.format(started))
+        if started:
+            result = rest.monitorRebalance()
+            msg = "successfully rebalanced out selected nodes from the cluster ? {0}"
+            self.log.info(msg.format(result))
+            return result
+        return False
+
+    #load data add one node , rebalance add another node rebalance
+    #swap one node with another node
+    #swap two nodes with two another nodes
+    def _common_test_body(self, swap_count):
+        msg = "minimum {0} nodes required for running rebalance swap of {1} nodes"
+        self.assertTrue(len(self._servers) >= (2 * swap_count),
+                        msg=msg.format((2 * swap_count), swap_count))
+        master = self._servers[0]
+        rest = RestConnection(master)
+        creds = self._input.membase_settings
+        items_inserted_count = 0
+
+        self.log.info("inserting some items in the master before adding any nodes")
+        distribution = {10: 0.5, 20: 0.5}
+        inserted_count, rejected_count =\
+        MemcachedClientHelper.load_bucket(serverInfo=master,
+                                          ram_load_ratio=0.1,
+                                          value_size_distribution=distribution,
+                                          number_of_threads=20)
+        items_inserted_count += inserted_count
+
+        self.assertTrue(self.rebalance_in(swap_count - 1))
+        #now add nodes for being swapped
+        #eject the current nodes and add new ones
+        # add 2 * swap -1 nodes
+        nodeIps = [node.ip for node in rest.node_statuses()]
+        self.log.info("current nodes : {0}".format(nodeIps))
+        toBeAdded = []
+        selection = self._servers[1:]
+        shuffle(selection)
+        for server in selection:
+            if not server.ip in nodeIps:
+                toBeAdded.append(server)
+            if len(toBeAdded) == swap_count:
+                break
+        toBeEjected = [node.id for node in rest.node_statuses()]
+        for server in toBeAdded:
+            rest.add_node(creds.rest_username, creds.rest_password, server.ip)
+
+        currentNodes = [node.id for node in rest.node_statuses()]
+        started = rest.rebalance(currentNodes,toBeEjected)
+        msg = "rebalance operation started ? {0}"
+        self.log.info(msg.format(started))
+        self.assertTrue(started, "rebalance operation did not start for swapping out the nodes")
+        result = rest.monitorRebalance()
+        msg = "successfully swapped out {0} nodes from the cluster ? {0}"
+        self.assertTrue(result,msg.format(swap_count))
+        #rest will have to change now?
+        rest = RestConnection(toBeAdded[0])
+        final_replication_state = RestHelper(rest).wait_for_replication(120)
+        msg = "replication state after waiting for up to 2 minutes : {0}"
+        self.log.info(msg.format(final_replication_state))
+        start_time = time.time()
+        stats = rest.get_bucket_stats()
+        while time.time() < (start_time + 120) and stats["curr_items"] != items_inserted_count:
+            self.log.info("curr_items : {0} versus {1}".format(stats["curr_items"], items_inserted_count))
+            time.sleep(5)
+            stats = rest.get_bucket_stats()
+        RebalanceHelper.print_taps_from_all_nodes(rest,'default')
+        self.log.info("curr_items : {0} versus {1}".format(stats["curr_items"], items_inserted_count))
+        stats = rest.get_bucket_stats()
+        msg = "curr_items : {0} is not equal to actual # of keys inserted : {1}"
+        self.assertEquals(stats["curr_items"], items_inserted_count,
+                          msg=msg.format(stats["curr_items"], items_inserted_count))
+
+
+    def test_swap_1(self):
+        self._common_test_body(1)
+
+    def test_swap_2(self):
+        self._common_test_body(2)
+
+    def test_swap_4(self):
+        self._common_test_body(4)
