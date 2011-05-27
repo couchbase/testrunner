5cd7ac01f74a81c242be6a467865fe42fe101bc2
farshid <farshid.ghods@gmail.com>
2011-05-26 20:33:06 -0700

    updating amazon inis
    Change-Id: I12435c827e97270e3229c594bb8755d6b8e5846f

--- pytests/management/install.py
+++ pytests/management/install.py
@@ -8,6 +8,7 @@ import logger
 from membase.api.exception import ServerUnavailableException
 from membase.api.rest_client import RestConnection
 from remote.remote_util import RemoteMachineShellConnection
+from threading import Thread
 
 
 # we will have InstallUninstall as one test case
@@ -42,59 +43,81 @@ class InstallTest(unittest.TestCase):
             self.machine_infos[serverInfo.ip] = info
             remote_client.disconnect()
 
+    def _test_install(self,serverInfo,version,builds):
+        query = BuildQuery()
+        info = self.machine_infos[serverInfo.ip]
+        build = query.find_membase_build(builds,
+                                         'membase-server-enterprise',
+                                         info.deliverable_type,
+                                         info.architecture_type,
+                                         version)
+        if not build:
+            self.log.info('find community edition build')
+            build = query.find_membase_build(builds,
+                                             'membase-server-community',
+                                             info.deliverable_type,
+                                             info.architecture_type,
+                                             version)
+            #try community ?
+        if not build:
+            self.fail('unable to find any {0} build for {1} for arch : {2} '.format(info.distribution_type,
+                                                                                    info.architecture_type,
+                                                                                    version))
+        print 'for machine : ', info.architecture_type, info.distribution_type, 'relevant build : ', build
+        remote_client = RemoteMachineShellConnection(serverInfo)
+        remote_client.membase_uninstall()
+        if 'amazon' in self.input.test_params:
+            build.url = build.url.replace("http://builds.hq.northscale.net/latestbuilds/",
+                                          "http://packages.northscale.com/latestbuilds/")
+            build.url = build.url.replace("enterprise", "community")
+            build.name = build.name.replace("enterprise", "community")
+        downloaded = remote_client.download_build(build)
+        self.assertTrue(downloaded, 'unable to download binaries :'.format(build.url))
+        remote_client.membase_install(build)
+        #TODO: we should poll the 8091 port until it is up and running
+        self.log.info('wait 5 seconds for membase server to start')
+        time.sleep(5)
+        rest = RestConnection(serverInfo)
+        start_time = time.time()
+        cluster_initialized = False
+        while time.time() < (start_time + (10 * 60)):
+            try:
+                rest.init_cluster(username=serverInfo.rest_username, password=serverInfo.rest_password)
+                cluster_initialized = True
+                break
+            except ServerUnavailableException:
+                self.log.error("error happened while initializing the cluster @ {0}".format(serverInfo.ip))
+            self.log.info('sleep for 5 seconds before trying again ...')
+            time.sleep(5)
+        self.assertTrue(cluster_initialized,
+                        "error happened while initializing the cluster @ {0}".format(serverInfo.ip))
+        if not cluster_initialized:
+            self.log.error("error happened while initializing the cluster @ {0}".format(serverInfo.ip))
+            raise Exception("error happened while initializing the cluster @ {0}".format(serverInfo.ip))
+        rest.init_cluster_memoryQuota(200)
+
+    #this method should only be used for amazon tests
+    def test_install_parallel(self):
+        # find the right deliverable for this os?
+        query = BuildQuery()
+        version = self.input.test_params['version']
+        builds, changes = query.get_all_builds()
+
+        threads = []
+        for serverInfo in self.servers:
+            new_thread = Thread(None, self._test_install, None, (serverInfo, version, builds))
+            new_thread.start()
+            threads.append(new_thread)
+
+        self.log.info("waiting for all installer threads to complete...")
+        for t in threads:
+            self.log.info("thread {0} finished".format(t))
+
+
     def test_install(self):
         # find the right deliverable for this os?
         query = BuildQuery()
         version = self.input.test_params['version']
         builds, changes = query.get_all_builds()
         for serverInfo in self.servers:
-            info = self.machine_infos[serverInfo.ip]
-            build = query.find_membase_build(builds,
-                                             'membase-server-enterprise',
-                                             info.deliverable_type,
-                                             info.architecture_type,
-                                             version)
-            if not build:
-                self.log.info('find community edition build')
-                build = query.find_membase_build(builds,
-                                                 'membase-server-community',
-                                                 info.deliverable_type,
-                                                 info.architecture_type,
-                                                 version)
-                #try community ?
-            if not build:
-                self.fail('unable to find any {0} build for {1} for arch : {2} '.format(info.distribution_type,
-                                                                                        info.architecture_type,
-                                                                                        version))
-            print 'for machine : ', info.architecture_type, info.distribution_type, 'relevant build : ', build
-            remote_client = RemoteMachineShellConnection(serverInfo)
-            remote_client.membase_uninstall()
-            if 'amazon' in self.input.test_params:
-                build.url = build.url.replace("http://builds.hq.northscale.net/latestbuilds/",
-                                              "http://packages.northscale.com/latestbuilds/")
-                build.url = build.url.replace("enterprise", "community")
-                build.name = build.name.replace("enterprise", "community")
-            downloaded = remote_client.download_build(build)
-            self.assertTrue(downloaded, 'unable to download binaries :'.format(build.url))
-            remote_client.membase_install(build)
-            #TODO: we should poll the 8091 port until it is up and running
-            self.log.info('wait 5 seconds for membase server to start')
-            time.sleep(5)
-            rest = RestConnection(serverInfo)
-            start_time = time.time()
-            cluster_initialized = False
-            while time.time() < (start_time + (10 * 60)):
-                try:
-                    rest.init_cluster(username=serverInfo.rest_username, password=serverInfo.rest_password)
-                    cluster_initialized = True
-                    break
-                except ServerUnavailableException:
-                    self.log.error("error happened while initializing the cluster @ {0}".format(serverInfo.ip))
-                self.log.info('sleep for 5 seconds before trying again ...')
-                time.sleep(5)
-            self.assertTrue(cluster_initialized,
-                            "error happened while initializing the cluster @ {0}".format(serverInfo.ip))
-            if not cluster_initialized:
-                self.log.error("error happened while initializing the cluster @ {0}".format(serverInfo.ip))
-                raise Exception("error happened while initializing the cluster @ {0}".format(serverInfo.ip))
-            rest.init_cluster_memoryQuota(200)
+            self._test_install(serverInfo,version,builds)
