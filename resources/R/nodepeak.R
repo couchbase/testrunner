d46c7d69974ae3584493601d8f966f53cd1433d8
farshid <farshid.ghods@gmail.com>
2011-10-21 08:41:07 -0700

    generate pdf file for all test cases
    Change-Id: I9468adb59db1e49275f500646e89180adb5828e7
    Reviewed-on: http://review.couchbase.org/10267
    Reviewed-by: Steve Yen <steve.yen@gmail.com>
    Reviewed-by: Farshid Ghods <farshid.ghods@gmail.com>
    Tested-by: Farshid Ghods <farshid.ghods@gmail.com>

--- resources/R/nodepeak.R
+++ resources/R/nodepeak.R
@@ -12,47 +12,48 @@ require(ggplot2, quietly=TRUE)
 args <- commandArgs(TRUE)
 cat(paste("args : ",args,""),sep="\n")
 args <- unlist(strsplit(args," "))
-tname <- args[1]
-#tname = "NPP-01-1k"
-cat(paste("Test Case : ", tname),sep="\n")
-pdf(paste(tname,sep="",".pdf"))
-
-
-xx <- fromJSON(file=paste("http://ec2-50-16-117-7.compute-1.amazonaws.com:5984/pythonperformance","/_design/rviews/_view/mbstats", sep=''))$rows
-bb <- plyr::ldply(xx, unlist)
-names(bb) <- c('id','build','ram','os','doc_id','test','rowid','mem_used','flusher_todo')
-bb <- transform(bb,build=paste(gsub("couchbase-","",build)))
-bb <- transform(bb,build=paste(gsub("membase-","",build)))
-b <- transform(bb,rowid=as.numeric(rowid), mem_used=as.numeric(mem_used)/1000000,ram=as.numeric(ram), flusher_todo=as.numeric(flusher_todo),unique_col=paste(substring(doc_id,28,32),'-',build))	
-b <- b[b$test==tname,]
-p <- ggplot(b,aes(rowid,mem_used,fill=unique_col)) + labs(x="",y="")
-p <- p + geom_line(aes(rowid, mem_used,color=unique_col))
-p <- p + labs(y='Memory Usage in MB', x="Build Number")
-p <- p + opts(title=paste("memcached mem_used"))
-print(p)
-p <- ggplot(b,aes(rowid,flusher_todo,fill=unique_col)) + labs(x="",y="")
-p <- p + geom_line(aes(rowid, flusher_todo,color=unique_col))
-p <- p + opts(title=paste("memcached flusher_todo"))
-p <- p + labs(y='Number of Items In the Outgoing Queue', x="Build Number")
-print(p)
-
-
-xx <- fromJSON(file=paste("http://ec2-50-16-117-7.compute-1.amazonaws.com:5984/pythonperformance","/_design/rviews/_view/procstats", sep=''))$rows
-bb <- plyr::ldply(xx, unlist)
-names(bb) <- c('id','build','ram','os','doc_id','test','process','rowid','time','resident')
-bb <- transform(bb,build=paste(gsub("couchbase-","",build)))
-bb <- transform(bb,build=paste(gsub("membase-","",build)))
-b <- transform(bb,rowid=as.numeric(rowid), resident=as.numeric(resident)/1000000,ram=as.numeric(ram),unique_col=paste(substring(doc_id,28,32),'-',build))	
-b <- b[b$test==tname,]
-processes = factor(bb$process)
-for(pname in levels(processes)) {
-	cat(paste(pname,"\n"))
-	df <- b[b$process==pname,]
-	p <- ggplot(df,aes(rowid,resident,fill=unique_col)) + labs(x="",y="")
-	p <- p + geom_line(aes(rowid, resident,color=unique_col))
-	p <- p + labs(y='Resident Memory Size', x="Build Number")
-	p <- p + opts(title=paste("process: ",pname," resident memory size ","test :",tname))
+for(arg in args) {
+	tname <- arg
+	#tname = "NPP-01-1k"
+	cat(paste("Test Case : ", tname),sep="\n")
+	pdf(paste(tname,sep="",".pdf"))
+	xx <- fromJSON(file=paste("http://ec2-50-16-117-7.compute-1.amazonaws.com:5984/pythonperformance","/_design/rviews/_view/mbstats", sep=''))$rows
+	bb <- plyr::ldply(xx, unlist)
+	names(bb) <- c('id','build','ram','os','doc_id','test','rowid','mem_used','flusher_todo')
+	bb <- transform(bb,build=paste(gsub("couchbase-","",build)))
+	bb <- transform(bb,build=paste(gsub("membase-","",build)))
+	b <- transform(bb,rowid=as.numeric(rowid), mem_used=as.numeric(mem_used)/1000000,ram=as.numeric(ram), flusher_todo=as.numeric(flusher_todo),unique_col=paste(substring(doc_id,28,32),'-',build))	
+	b <- b[b$test==tname,]
+	p <- ggplot(b,aes(rowid,mem_used,fill=unique_col)) + labs(x="",y="")
+	p <- p + geom_line(aes(rowid, mem_used,color=unique_col))
+	p <- p + labs(y='Memory Usage in MB', x="Build Number")
+	p <- p + opts(title=paste("memcached mem_used"))
 	print(p)
+	p <- ggplot(b,aes(rowid,flusher_todo,fill=unique_col)) + labs(x="",y="")
+	p <- p + geom_line(aes(rowid, flusher_todo,color=unique_col))
+	p <- p + opts(title=paste("memcached flusher_todo"))
+	p <- p + labs(y='Number of Items In the Outgoing Queue', x="Build Number")
+	print(p)
+
+
+	xx <- fromJSON(file=paste("http://ec2-50-16-117-7.compute-1.amazonaws.com:5984/pythonperformance","/_design/rviews/_view/procstats", sep=''))$rows
+	bb <- plyr::ldply(xx, unlist)
+	names(bb) <- c('id','build','ram','os','doc_id','test','process','rowid','time','resident')
+	bb <- transform(bb,build=paste(gsub("couchbase-","",build)))
+	bb <- transform(bb,build=paste(gsub("membase-","",build)))
+	b <- transform(bb,rowid=as.numeric(rowid), resident=as.numeric(resident)/1000000,ram=as.numeric(ram),unique_col=paste(substring(doc_id,28,32),'-',build))	
+	b <- b[b$test==tname,]
+	processes = factor(bb$process)
+	for(pname in levels(processes)) {
+		cat(paste(pname,"\n"))
+		df <- b[b$process==pname,]
+		p <- ggplot(df,aes(rowid,resident,fill=unique_col)) + labs(x="",y="")
+		p <- p + geom_line(aes(rowid, resident,color=unique_col))
+		p <- p + labs(y='Resident Memory Size', x="Build Number")
+		p <- p + opts(title=paste("process: ",pname," resident memory size ","test :",tname))
+		print(p)
+	}
+	dev.off()
+
 }
 
-dev.off()
