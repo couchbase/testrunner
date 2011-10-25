de1002a9f66347e6de7cd225a9f46ee5cda9706b
Karan Kumar <kkumar86@gmail.com>
2011-10-25 11:49:34 -0700

    Perf: Plot builds for each metrics
    Metrics: OPS, latency and Drain rate
    
    Change-Id: I59cbbe01775b196ea68521e90b6a7adf7ea0e6aa
    Reviewed-on: http://review.couchbase.org/10360
    Tested-by: Karan Kumar <karan@couchbase.com>
    Reviewed-by: Karan Kumar <karan@couchbase.com>

--- resources/R/generateGraph.R
+++ resources/R/generateGraph.R
@@ -24,17 +24,11 @@ for(arg in args) {
 	bb <- transform(bb, build=paste(gsub("membase-", "", build)))
 	bb <- transform(bb, rowid=as.numeric(rowid), ram=as.numeric(ram), test_time=as.numeric(test_time), drainRate=as.numeric(drainRate), unique_col=paste(substring(doc_id, 28, 32), '-', build))
  	bb <- bb[bb$test==tname,]
-	builds = factor(bb$build)
-	for(a_build in levels(builds)) {
-		filtered <- bb[bb$build == a_build,]
-		max_time <- max(filtered$test_time)
-		graphed <- bb[bb$build == a_build & bb$test_time == max_time,]
-		p <- ggplot(graphed,aes(rowid, drainRate, fill=build)) + labs(x="samples", y="ms")
-		p <- p + geom_line(aes(rowid, drainRate, color=build))
-		p <- p + labs(y='ms', x="samples")
-		p <- p + opts(title=paste("Drain Rate"))
-		print(p)
-	}
+	p <- ggplot(bb,aes(rowid, drainRate, fill=build)) + labs(x="samples", y="ms")
+	p <- p + geom_line(aes(rowid, drainRate, color=build))
+	p <- p + labs(y='ms', x="samples")
+	p <- p + opts(title=paste("Drain Rate"))
+	print(p)
 	
 	# Latency
 	opsstats <- fromJSON(file=paste("http://ec2-50-16-117-7.compute-1.amazonaws.com:5984/pythonperformance","/_design/rviews/_view/opsstats", sep=''))$rows
@@ -45,39 +39,26 @@ for(arg in args) {
 	bb <- transform(bb, rowid=as.numeric(rowid),ram=as.numeric(ram), test_time=as.numeric(test_time), startTime=as.numeric(startTime), endTime=as.numeric(endTime), unique_col=paste(substring(doc_id,	28,32),'-',build))
 	bb <- transform(bb, avgLatency = (endTime-startTime))
 	bb <- bb[bb$test == tname,]
-	builds = factor(bb$build)
-	for(a_build in levels(builds)) {
-		filtered <- bb[bb$build == a_build,]
-		max_time <- max(filtered$test_time)
-		graphed <- bb[bb$build == a_build & bb$test_time == max_time,]
-		p <- ggplot(graphed,aes(rowid, avgLatency, fill=build)) + labs(x="samples", y="ms")
-		p <- p + geom_line(aes(rowid, avgLatency, color=build))
-		p <- p + labs(y='ms', x="samples")
-		p <- p + opts(title=paste("Avg. Latency"))
-		print(p)
-	}
+	p <- ggplot(bb,aes(rowid, avgLatency, fill=build)) + labs(x="samples", y="ms")
+	p <- p + geom_line(aes(rowid, avgLatency, color=build))
+	p <- p + labs(y='ms', x="samples")
+	p <- p + opts(title=paste("Avg. Latency"))
+	print(p)
 	
 	# OPS
 	bb <- plyr::ldply(opsstats,unlist)
 	names(bb) <- c('id','build','ram','os','doc_id','test','test_time','test_name','rowid','startTime','endTime','totalGets','totalSets')	
 	bb <- transform(bb,build=paste(gsub("couchbase-","",build)))
 	bb <- transform(bb,build=paste(gsub("membase-","",build)))
-
 	bb <- transform(bb, rowid=as.numeric(rowid),ram=as.numeric(ram), test_time=as.numeric(test_time),opsPerSecond=(as.numeric(totalGets)+as.numeric(totalSets))/(as.numeric(endTime) - 		as.numeric(startTime)),unique_col=paste(substring(doc_id,28,32),'-',build))
 	bb <- bb[bb$test == tname,]
 	builds = factor(bb$build)
-	for(a_build in levels(builds)) {
-		filtered <- bb[bb$build == a_build,]
-		max_time <- max(filtered$test_time)
-		graphed <- bb[bb$build == a_build & bb$test_time == max_time,]
-		p <- ggplot(graphed,aes(rowid, opsPerSecond, fill=build)) + labs(x="samples", y="OPS")
-		p <- p + geom_line(aes(rowid, opsPerSecond, color=build))
-		p <- p + labs(y='OPS', x="samples")
-		p <- p + opts(title=paste("OPS"))
-		print(p)
-	}
+	p <- ggplot(bb, aes(rowid, opsPerSecond, fill=build)) + labs(x="samples", y="OPS")
+	p <- p + geom_line(aes(rowid, opsPerSecond, color=build))
+	p <- p + labs(y='OPS', x="samples")
+	p <- p + opts(title=paste("OPS"))
+	print(p)
 
-	
 dev.off()	
 	
 	
