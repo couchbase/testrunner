f2e12b26ef947b573c399552d840767cd4f03430
Karan Kumar <kkumar86@gmail.com>
2011-10-25 23:11:19 -0700

    Perf: Plot latency,ops,drain & memory
    Change-Id: Icc48a7fe1b88ec89bd049b6c687f1cb0b198da2a
    Reviewed-on: http://review.couchbase.org/10379
    Tested-by: Karan Kumar <karan@couchbase.com>
    Reviewed-by: Karan Kumar <karan@couchbase.com>

--- resources/R/generateGraph.R
+++ resources/R/generateGraph.R
@@ -6,6 +6,7 @@ require(reshape, quietly=TRUE)
 require(plyr, quietly=TRUE)
 require(rjson, quietly=TRUE)
 require(ggplot2, quietly=TRUE)
+require(gridExtra, quietly=TRUE)
 args <- commandArgs(TRUE)
 cat(paste("args : ",args,""),sep="\n")
 args <- unlist(strsplit(args," "))
@@ -15,50 +16,131 @@ for(arg in args) {
 	print(tname)
 	cat(paste("Test Case : ", tname),sep="\n")
 	pdf(paste(tname,sep="",".pdf"))
-	
+	data_frame <- data.frame()
+	data_frame <- data.frame(t(rep(NA, 13)))
+ 	names(data_frame)<- c('id', 'build', 'ram', 'os', 'doc_id', 'test', 'test_time', 'test_name', 'max_items', 'min_value_size', 'rowid', 'drainRate', 'unique_col')	
+
+
 	# Drain Rate
+	# Does not import the null values if present in json 
 	membasestats <- fromJSON(file=paste("http://ec2-50-16-117-7.compute-1.amazonaws.com:5984/pythonperformance","/_design/rviews/_view/membasestats", sep=''))$rows
 	bb <- plyr::ldply(membasestats, unlist)
-	names(bb) <- c('id', 'build', 'ram', 'os', 'doc_id', 'test', 'test_time', 'test_name', 'rowid', 'drainRate')	
+	names(bb) <- c('id', 'build', 'ram', 'os', 'doc_id', 'test', 'test_time', 'test_name', 'max_items', 'min_value_size', 'rowid', 'drainRate')	
 	bb <- transform(bb, build=paste(gsub("couchbase-", "", build)))
 	bb <- transform(bb, build=paste(gsub("membase-", "", build)))
 	bb <- transform(bb, rowid=as.numeric(rowid), ram=as.numeric(ram), test_time=as.numeric(test_time), drainRate=as.numeric(drainRate), unique_col=paste(substring(doc_id, 28, 32), '-', build))
  	bb <- bb[bb$test==tname,]
-	p <- ggplot(bb,aes(rowid, drainRate, fill=build)) + labs(x="samples", y="ms")
+ 	(temp_data_frame <- data_frame[FALSE, ])
+	print(temp_data_frame)
+	builds = factor(bb$build)
+	for(a_build in levels(builds)) {
+		filtered <- bb[bb$build == a_build,]
+		max_time <- max(filtered$test_time)
+		print(max_time)
+		graphed <- bb[bb$build == a_build & bb$test_time == max_time,]
+		temp_data_frame <- rbind(temp_data_frame,  graphed)
+	}
+	p <- ggplot(temp_data_frame, aes(rowid, drainRate, fill=build)) + labs(x="samples", y="ms")
+	#p  <-   p + stat_smooth(se = FALSE)
 	p <- p + geom_line(aes(rowid, drainRate, color=build))
 	p <- p + labs(y='ms', x="samples")
-	p <- p + opts(title=paste("Drain Rate"))
+	#labs <- cbind('test_name',temp_data_frame$test_name[0])
+	#labs <- cbind('test_time',temp_data_frame$test_time[0])
+	#labs <- cbind('items',temp_data_frame$max_items[0])
+	#labs <- cbind('value-size',temp_data_frame$min_value_size[0])
+	#g <- tableGrob(labs)
+	#grid.arrange(p, g, nrow=4, widths=c(5,5))
+	p <- p + opts(title=paste("Disk Write Queue"))
 	print(p)
 	
 	# Latency
+	data_frame <- data.frame()
+	data_frame <- data.frame(t(rep(NA, 16)))
+ 	names(data_frame)<- c('id', 'build', 'ram', 'os', 'doc_id', 'test', 'test_time', 'test_name', 'max_items', 'min_value_size', 'rowid', 'startTime','endTime','totalGets','totalSets',	'unique_col')	
+
 	opsstats <- fromJSON(file=paste("http://ec2-50-16-117-7.compute-1.amazonaws.com:5984/pythonperformance","/_design/rviews/_view/opsstats", sep=''))$rows
 	bb <- plyr::ldply(opsstats, unlist)
-	names(bb) <- c('id', 'build', 'ram', 'os', 'doc_id', 'test', 'test_time', 'test_name', 'rowid','startTime','endTime','totalGets','totalSets')	
+	names(bb) <- c('id', 'build', 'ram', 'os', 'doc_id', 'test', 'test_time', 'test_name', 'max_items', 'min_value_size', 'rowid', 'startTime', 'endTime', 'totalGets', 'totalSets')	
 	bb <- transform(bb, build=paste(gsub("couchbase-","",build)))
 	bb <- transform(bb, build=paste(gsub("membase-","",build)))
 	bb <- transform(bb, rowid=as.numeric(rowid),ram=as.numeric(ram), test_time=as.numeric(test_time), startTime=as.numeric(startTime), endTime=as.numeric(endTime), unique_col=paste(substring(doc_id,	28,32),'-',build))
 	bb <- transform(bb, avgLatency = (endTime-startTime))
 	bb <- bb[bb$test == tname,]
-	p <- ggplot(bb,aes(rowid, avgLatency, fill=build)) + labs(x="samples", y="ms")
-	p <- p + geom_line(aes(rowid, avgLatency, color=build))
+	(temp_data_frame <- data_frame[FALSE, ])
+
+	builds = factor(bb$build)
+	for(a_build in levels(builds)) {
+		filtered <- bb[bb$build == a_build,]
+		max_time <- max(filtered$test_time)
+		print(max_time)
+		graphed <- bb[bb$build == a_build & bb$test_time == max_time,]
+		temp_data_frame <- rbind(temp_data_frame,  graphed)
+	}
+	p <- ggplot(temp_data_frame, aes(rowid, avgLatency, fill=build)) + labs(x="samples", y="ms")
+	p  <-   p + stat_smooth(se = FALSE)
+	#p <- p + geom_line(aes(rowid, avgLatency, color=build))
 	p <- p + labs(y='ms', x="samples")
 	p <- p + opts(title=paste("Avg. Latency"))
 	print(p)
 	
 	# OPS
 	bb <- plyr::ldply(opsstats,unlist)
-	names(bb) <- c('id','build','ram','os','doc_id','test','test_time','test_name','rowid','startTime','endTime','totalGets','totalSets')	
+	names(bb) <- c('id', 'build', 'ram', 'os', 'doc_id', 'test', 'test_time', 'test_name', 'max_items', 'min_value_size', 'rowid', 'startTime', 'endTime', 'totalGets', 'totalSets')	
 	bb <- transform(bb,build=paste(gsub("couchbase-","",build)))
 	bb <- transform(bb,build=paste(gsub("membase-","",build)))
+
 	bb <- transform(bb, rowid=as.numeric(rowid),ram=as.numeric(ram), test_time=as.numeric(test_time),opsPerSecond=(as.numeric(totalGets)+as.numeric(totalSets))/(as.numeric(endTime) - 		as.numeric(startTime)),unique_col=paste(substring(doc_id,28,32),'-',build))
 	bb <- bb[bb$test == tname,]
+	(temp_data_frame <- data_frame[FALSE, ])
 	builds = factor(bb$build)
-	p <- ggplot(bb, aes(rowid, opsPerSecond, fill=build)) + labs(x="samples", y="OPS")
-	p <- p + geom_line(aes(rowid, opsPerSecond, color=build))
+	for(a_build in levels(builds)) {
+		filtered <- bb[bb$build == a_build,]
+		max_time <- max(filtered$test_time)
+		print(max_time)
+		graphed <- bb[bb$build == a_build & bb$test_time == max_time,]
+		temp_data_frame <- rbind(temp_data_frame,  graphed)
+	}
+	p <- ggplot(temp_data_frame,aes(rowid, opsPerSecond, fill=build)) + labs(x="samples", y="OPS")
+	p  <-   p + stat_smooth(se = FALSE)
+	#p <- p + geom_line(aes(rowid, opsPerSecond, color=build))
 	p <- p + labs(y='OPS', x="samples")
 	p <- p + opts(title=paste("OPS"))
 	print(p)
 
+	# System Stats (Memory)
+	data_frame <- data.frame()
+	data_frame <- data.frame(t(rep(NA, 15)))
+ 	names(data_frame)<- c('id', 'build', 'ram', 'os', 'doc_id', 'test', 'test_time', 'test_name', 'max_items', 'min_value_size', 'process', 'rowid', 'time_sample', 'rss', 'unique_col')	
+
+	systemstats <- fromJSON(file=paste("http://ec2-50-16-117-7.compute-1.amazonaws.com:5984/pythonperformance","/_design/rviews/_view/systemstats", sep=''))$rows
+	bb <- plyr::ldply(systemstats, unlist)
+	names(bb) <- c('id', 'build', 'ram', 'os', 'doc_id', 'test', 'test_time', 'test_name', 'max_items', 'min_value_size', 'process', 'rowid', 'time_sample', 'rss')		
+	bb <- transform(bb, build=paste(gsub("couchbase-", "", build)))
+	bb <- transform(bb, build=paste(gsub("membase-", "", build)))
+	bb <- transform(bb, rowid=as.numeric(rowid), ram=as.numeric(ram), test_time=as.numeric(test_time), time_sample=as.numeric(time_sample), rss=as.numeric(rss), unique_col=paste(substring(doc_id, 28, 32), '-', build))
+	bb <- bb[bb$test==tname,]
+	(temp_data_frame <- data_frame[FALSE, ])
+	builds = factor(bb$build)
+	for(a_build in levels(builds)) {
+		filtered <- bb[bb$build == a_build,]
+		max_time <- max(filtered$test_time)
+		print(max_time)
+		graphed <- bb[bb$build == a_build & bb$test_time == max_time,]
+		temp_data_frame <- rbind(temp_data_frame,  graphed)
+	}
+	p <- ggplot(temp_data_frame, aes(rowid, rss, fill=build)) + labs(x="samples", y="Memory")
+	#p  <-   p + stat_smooth(se = FALSE)
+	p <- p + geom_line(aes(rowid, rss, color=build))
+	p <- p + labs(y='Memory (in MB)', x="samples")
+	#labs <- cbind('test_name',temp_data_frame$test_name[0])
+	#labs <- cbind('test_time',temp_data_frame$test_time[0])
+	#labs <- cbind('items',temp_data_frame$max_items[0])
+	#labs <- cbind('value-size',temp_data_frame$min_value_size[0])
+	#g <- tableGrob(labs)
+	#grid.arrange(p, g, nrow=4, widths=c(5,5))
+	p <- p + opts(title=paste("beam.smp"))
+	print(p)
+	
 dev.off()	
 	
 	
