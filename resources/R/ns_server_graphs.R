e87200c0ab43b1bcc8c4188855be915f5e4f291a
Karan Kumar <kkumar86@gmail.com>
2011-11-01 21:15:47 -0700

    Perf: Fix Graphs
    This patch has following fixes:-
    1) No smoothing for drain rate
    2) Adding test info for CPU
    3) Adding plots for Memory Usage (beam.smp and memcached)
    
    Change-Id: I3b955fc8785519913c27096e31804b45d923f58d
    Reviewed-on: http://review.couchbase.org/10543
    Tested-by: Karan Kumar <karan@couchbase.com>
    Reviewed-by: Karan Kumar <karan@couchbase.com>

--- resources/R/ns_server_graphs.R
+++ resources/R/ns_server_graphs.R
@@ -51,7 +51,7 @@ for(arg in args) {
 #	p <- p + geom_line(aes(timestamp, ops, color=build))
 	testName <- unlist(strsplit(temp_data_frame$test_name[1], "\\."))[4]
 	print(testName)
-	values <- c(paste('test:', testName), paste('items:', temp_data_frame$total_items[1]), paste('value_size:',comma(temp_data_frame$min_value_size[1])), paste('total_gets:', comma(temp_data_frame$total_gets[1])), paste('total_sets:', comma(temp_data_frame$total_sets[1])), paste('total_creates:', comma(temp_data_frame$total_creates[1])), paste('run_time:', temp_data_frame$run_time[1]) )
+	values <- c(paste('test:', testName), paste('items:', temp_data_frame$total_items[1]), paste('value_size:',comma(temp_data_frame$min_value_size[1])), paste('total_gets:', comma(temp_data_frame$total_gets[1])), paste('total_sets:', comma(temp_data_frame$total_sets[1])), paste('total_creates:', comma(temp_data_frame$total_creates[1])))
 	print(values)
 	print(p)
 	pushViewport(viewport())
@@ -82,8 +82,8 @@ for(arg in args) {
 
 	cat("generating disk drain rate graph")
 	p <- ggplot(temp_data_frame, aes(timestamp, drain_rate, fill=build, label=ops)) + labs(x="----time (sec)--->", y="drain_rate")
-	#p <- p + geom_line(aes(timestamp, drain_rate, color=build))
-	p  <-  p + stat_smooth(se = TRUE)
+	p <- p + geom_line(aes(timestamp, drain_rate, color=build))
+	#p  <-  p + stat_smooth(se = TRUE)
 	p <- p + opts(title=paste("Drain Rate"))
     p <- p + opts(panel.background = theme_rect(colour = 'black', fill = 'white', size = 1, linetype='solid'))
     p <- p + opts(axis.ticks = theme_segment(colour = 'red', size = 1, linetype = 'solid'))
@@ -99,6 +99,81 @@ for(arg in args) {
     
     
     #system stats
+    # System Stats (Memory for Beam.smp)
+    cat("Generating Memory usage for beam.smp and memcached")
+	data_frame <- data.frame()
+	data_frame <- data.frame(t(rep(NA, 21)))
+ 	names(data_frame)<- c('id', 'build', 'ram', 'os', 'doc_id', 'test', 'test_time', 'test_name', 'max_items', 'min_value_size', 'total_gets', 'total_creates', 'total_sets', 'total_items', 'total_misses', 'run_time', 'rowid', 'time_sample', 'rss', 'process','unique_col')	
+
+	systemstats <- fromJSON(file=paste("http://couchdb2.couchbaseqe.com:5984/experimental","/_design/data/_view/systemstats", sep=''))$rows
+	bb <- plyr::ldply(systemstats, unlist)
+	names(bb) <- c('id', 'build', 'ram', 'os', 'doc_id', 'test', 'test_time', 'test_name', 'max_items', 'min_value_size', 'total_gets', 'total_creates', 'total_sets', 'total_items', 'total_misses', 'run_time','rowid', 'time_sample', 'rss', 'process','cpu_time')		
+	bb <- transform(bb, build=paste(gsub("couchbase-", "", build)))
+	bb <- transform(bb, build=paste(gsub("membase-", "", build)))
+	bb <- transform(bb, rowid=as.numeric(rowid), ram=as.numeric(ram), test_time=as.numeric(test_time), time_sample=as.numeric(time_sample), rss=as.numeric(rss), unique_col=paste(substring(doc_id, 28, 32), '-', build))
+	bb <- bb[bb$test==tname,]
+	(temp_data_frame <- data_frame[FALSE, ])
+	builds = factor(bb$build)
+	for(a_build in levels(builds)) {
+		filtered <- bb[bb$build == a_build,]
+		max_time <- max(filtered$test_time)
+		print(max_time)
+		graphed <- bb[bb$build == a_build & bb$test_time == max_time & bb$process == 'beam.smp',]
+		temp_data_frame <- rbind(temp_data_frame,  graphed)
+	}
+	p <- ggplot(temp_data_frame, aes(rowid, rss, fill=build, label=rss))
+	#p  <-   p + stat_smooth(se = FALSE)
+	p <- p + geom_line(aes(rowid, rss, color=build))
+	p <- p + labs(y='Memory (in MB)', x="----time (every 10 secs) --->")
+	p <- p + opts(title=paste("beam.smp"))
+	p <- p + opts(panel.background = theme_rect(colour = 'black', fill = 'white', size = 1, linetype='solid'))
+    p <- p + opts(axis.ticks = theme_segment(colour = 'red', size = 1, linetype = 'dashed'))
+	print(p)
+	pushViewport(viewport())
+	y <- 0.75 
+	for (row in 1:7){
+		grid.text(values[row],gp=gpar(col="black"), x=0.71, y=y, check.overlap=TRUE, just="left")
+		y <- y + 0.026	
+	}
+	popViewport()
+	
+	# System Stats (Memory for Memcached)
+	data_frame <- data.frame()
+	data_frame <- data.frame(t(rep(NA, 21)))
+ 	names(data_frame)<- c('id', 'build', 'ram', 'os', 'doc_id', 'test', 'test_time', 'test_name', 'max_items', 'min_value_size', 'total_gets', 'total_creates', 'total_sets', 'total_items', 'total_misses', 'run_time', 'rowid', 'time_sample', 'rss', 'process','unique_col')	
+
+	systemstats <- fromJSON(file=paste("http://couchdb2.couchbaseqe.com:5984/experimental","/_design/data/_view/systemstats", sep=''))$rows
+	bb <- plyr::ldply(systemstats, unlist)
+	names(bb) <- c('id', 'build', 'ram', 'os', 'doc_id', 'test', 'test_time', 'test_name', 'max_items', 'min_value_size', 'total_gets', 'total_creates', 'total_sets', 'total_items', 'total_misses', 'run_time','rowid', 'time_sample', 'rss', 'process','cpu_time')
+	bb <- transform(bb, build=paste(gsub("couchbase-", "", build)))
+	bb <- transform(bb, build=paste(gsub("membase-", "", build)))
+	bb <- transform(bb, rowid=as.numeric(rowid), ram=as.numeric(ram), test_time=as.numeric(test_time), time_sample=as.numeric(time_sample), rss=as.numeric(rss), unique_col=paste(substring(doc_id, 28, 32), '-', build),cpu_time=as.numeric(cpu_time))
+	bb <- bb[bb$test==tname,]
+	(temp_data_frame <- data_frame[FALSE, ])
+	builds = factor(bb$build)
+	for(a_build in levels(builds)) {
+		filtered <- bb[bb$build == a_build,]
+		max_time <- max(filtered$test_time)
+		print(max_time)
+		graphed <- bb[bb$build == a_build & bb$test_time == max_time & bb$process == 'memcached',]
+		temp_data_frame <- rbind(temp_data_frame,  graphed)
+	}
+	p <- ggplot(temp_data_frame, aes(rowid, rss, fill=build, label=rss))
+	#p  <-   p + stat_smooth(se = FALSE)
+	p <- p + geom_line(aes(rowid, rss, color=build))
+	p <- p + labs(y='Memory (in MB)', x="----time (every 10 secs) --->")
+	p <- p + opts(title=paste("Memcached"))
+	p <- p + opts(panel.background = theme_rect(colour = 'black', fill = 'white', size = 1, linetype='solid'))
+    p <- p + opts(axis.ticks = theme_segment(colour = 'red', size = 1, linetype = 'dashed'))
+	print(p)
+    pushViewport(viewport())
+	y <- 0.75 
+	for (row in 1:7){
+		grid.text(values[row],gp=gpar(col="black"), x=0.73, y=y, check.overlap=TRUE, just="left")
+		y <- y + 0.026	
+	}
+    popViewport()
+    
 	# Does not import the null values if present in json 
 	cat("generating memcached cpu usage graphs")
 	systemstats <- fromJSON(file=paste("http://couchdb2.couchbaseqe.com:5984/experimental","/_design/data/_view/systemstats", sep=''))$rows
@@ -129,6 +204,13 @@ for(arg in args) {
 	p <- p + opts(panel.background = theme_rect(colour = 'black', fill = 'white', size = 1, linetype='solid'))
     p <- p + opts(axis.ticks = theme_segment(colour = 'red', size = 1, linetype = 'dashed'))	
     print(p)
+    pushViewport(viewport())
+	y <- 0.75 
+	for (row in 1:7){
+		grid.text(values[row],gp=gpar(col="black"), x=0.73, y=y, check.overlap=TRUE, just="left")
+		y <- y + 0.026	
+	}
+    popViewport()
     
     #cpu time for beam.smp
     cat("generating beam.smp cpu usage graphs")
@@ -158,7 +240,13 @@ for(arg in args) {
     p <- p + opts(axis.ticks = theme_segment(colour = 'red', size = 1, linetype = 'dashed'))	
     print(p)    
     
-	
+	pushViewport(viewport())
+	y <- 0.75 
+	for (row in 1:7){
+		grid.text(values[row],gp=gpar(col="black"), x=0.73, y=y, check.overlap=TRUE, just="left")
+		y <- y + 0.026	
+	}
+    popViewport()
 	
 dev.off()	
 	
