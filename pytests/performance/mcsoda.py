f574070a37d061c8e51ab1367c2fbb3bd4ba7c3d
Steve Yen <steve.yen@gmail.com>
2011-11-03 20:05:25 -0700

    Sometimes latency_cmd might be None on Control-C
    Change-Id: Ia45e17e3fe85cccba94fb0b31645ee5fc12bd42b
    Reviewed-on: http://review.couchbase.org/10609
    Reviewed-by: Farshid Ghods <farshid.ghods@gmail.com>
    Tested-by: Farshid Ghods <farshid.ghods@gmail.com>

--- pytests/performance/mcsoda.py
+++ pytests/performance/mcsoda.py
@@ -424,7 +424,8 @@ class StoreMemcachedBinary(Store):
                                    'start-time': latency_start,
                                    'end-time': latency_end })
 
-        self.add_timing_sample(latency_cmd, latency_end - latency_start)
+        if latency_cmd:
+           self.add_timing_sample(latency_cmd, latency_end - latency_start)
 
     def cmd_append(self, cmd, key_num, key_str, data, m, extra):
        if cmd[0] == 'g':
