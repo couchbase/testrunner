09a973935e58dd8ed91ec13153b23c505eb1728a
Steve Yen <steve.yen@gmail.com>
2011-11-15 14:23:31 -0800

    EPerf EPT-MIXED ratios corrected
    Change-Id: I087063ac19ab87f79511af3ecd13882d03d0aa2b
    Reviewed-on: http://review.couchbase.org/10922
    Tested-by: Karan Kumar <karan@couchbase.com>
    Reviewed-by: Karan Kumar <karan@couchbase.com>

--- pytests/performance/eperf.py
+++ pytests/performance/eperf.py
@@ -161,10 +161,10 @@ class EPerfMaster(perf.PerfBase):
         self.load_phase(self.parami("num_nodes", 10), items)
         notify = self.gated_start(self.input.clients)
         self.access_phase(items,
-                          ratio_sets     = self.paramf('ratio_sets', 0.8),
+                          ratio_sets     = self.paramf('ratio_sets', 0.5),
                           ratio_misses   = self.paramf('ratio_misses', 0.05),
-                          ratio_creates  = self.paramf('ratio_creates', 0.1875),
-                          ratio_deletes  = self.paramf('ratio_deletes', 0.0833),
+                          ratio_creates  = self.paramf('ratio_creates', 0.08),
+                          ratio_deletes  = self.paramf('ratio_deletes', 0.13),
                           ratio_hot      = self.paramf('ratio_hot', 0.2),
                           ratio_hot_gets = self.paramf('ratio_hot_gets', 0.95),
                           ratio_hot_sets = self.paramf('ratio_hot_sets', 0.95),
