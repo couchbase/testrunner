679b5e12044172c5aaa078483188048bc9d6cefd
Keith Batten <kbatten@gmail.com>
2012-01-06 16:37:21 -0800

    add script to generate testrunner ini files based on aws tags
    Change-Id: I9daf22b125204446dd823f576468d5c825159462
    Reviewed-on: http://review.couchbase.org/12128
    Tested-by: Keith Batten <kbatten@gmail.com>
    Reviewed-by: Farshid Ghods <farshid.ghods@gmail.com>
    Tested-by: Farshid Ghods <farshid.ghods@gmail.com>

---
+++ scripts/aws_ini.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import json
+
+from boto.ec2.connection import EC2Connection
+
+
+if len(sys.argv) != 2:
+    print "aws_ini.py <tagname>"
+    sys.exit()
+
+# use environment variables to set credentials
+# AWS_ACCESS_KEY_ID
+# AWS_SECRET_ACCESS_KEY
+
+tag_name = sys.argv[1]
+
+conn = EC2Connection()
+
+reservations = conn.get_all_instances()
+
+instances = []
+
+for reservation in reservations:
+    for instance in reservation.instances:
+        if u'Name' in instance.tags:
+            if instance.tags[u'Name'] == tag_name:
+                instances.append({"private_ip_address":instance.private_ip_address,
+                                  "public_dns_name":instance.public_dns_name})
+
+print '''[global]
+username:ec2-user
+ssh_key:/root/key.pem
+port:8091
+data_path:/mnt/ebs/
+
+[servers]'''
+i = 1
+for instance in instances:
+    print `i` + ":" + instance["private_ip_address"]
+    print "#public_dns_name:" + instance["public_dns_name"]
+    i += 1
+print '''
+[membase]
+rest_username:Administrator
+rest_password:password'''
