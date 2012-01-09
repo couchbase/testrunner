4e443d5785003f2918e5366cab8d7c8c7e124f24
farshid <farshid.ghods@gmail.com>
2012-01-09 15:44:56 -0800

    change test log level when running tests
    change the arg parsing to use optionparsing module
    and changed the logging method to create a dynamically generated
    logging configuration when running each test
    
    testrunner creates a logging.conf file under the tmp
    folder and uses logging.config.fileConfig to use that
    conf file
    
    Change-Id: I4152be938ef180e3a7a9a983da4dfeb617507bb1
    Reviewed-on: http://review.couchbase.org/12192
    Reviewed-by: Keith Batten <kbatten@gmail.com>
    Reviewed-by: Farshid Ghods <farshid.ghods@gmail.com>
    Tested-by: Farshid Ghods <farshid.ghods@gmail.com>

--- lib/logger.py
+++ lib/logger.py
@@ -1,7 +1,4 @@
 import logging
-from logging.handlers import RotatingFileHandler
-import os
-import uuid
 
 global _logger
 
@@ -15,64 +12,13 @@ class Logger:
 
     @staticmethod
     def get_logger():
-        if not Logger._logger:
-            Logger._logger = Logger.start_logger('console')
-        return Logger._logger
+        return logging.getLogger()
 
     @staticmethod
     def start_logger(name):
     # create logger
-        logger = logging.getLogger(name)
-        logger.setLevel(logging.INFO)
-
-        # create console handler and set level to debug
-        consoleHandler = logging.StreamHandler()
-        consoleHandler.setLevel(logging.DEBUG)
-
-        # create formatter
-        formatter = logging.Formatter("[%(asctime)s] - [%(module)s] [%(thread)d] - %(levelname)s - %(message)s")
-
-        max_size = 20 * 1024 * 1024 #max size is 50 megabytes
-
-        filename = "{0}.log".format(name)
-        if "TEMP-FOLDER" in os.environ:
-            filename = "{0}/{1}.log".format(os.environ["TEMP-FOLDER"], name)
-        fileHandler = RotatingFileHandler(filename, backupCount=2, maxBytes=max_size)
-
-        # add formatter to ch
-        consoleHandler.setFormatter(formatter)
-        fileHandler.setFormatter(formatter)
-
-        # add ch to logger
-        logger.addHandler(consoleHandler)
-        logger.addHandler(fileHandler)
-        Logger._logger = logger
-        print 'start logging to {0}'.format(filename)
-        print '  cat {0}'.format(filename)
-        return Logger._logger
+        return logging.getLogger()
 
 
 def new_logger(name):
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
-
-    # create console handler and set level to debug
-    consoleHandler = logging.StreamHandler()
-    consoleHandler.setLevel(logging.DEBUG)
-
-    # create formatter
-    formatter = logging.Formatter("[%(asctime)s] - [%(module)s] [%(thread)d] - %(levelname)s - %(message)s")
-
-    max_size = 20 * 1024 * 1024 #max size is 50 megabytes
-
-    filename = "{0}.log".format(name)
-    fileHandler = RotatingFileHandler(filename, backupCount=2, maxBytes=max_size)
-
-    # add formatter to ch
-    consoleHandler.setFormatter(formatter)
-    fileHandler.setFormatter(formatter)
-
-    # add ch to logger
-    logger.addHandler(consoleHandler)
-    logger.addHandler(fileHandler)
-    return logger
+    return logging.getLogger()
