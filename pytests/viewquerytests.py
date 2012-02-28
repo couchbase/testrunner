2f7346caa2f41837051c646052f75e1ada307134
Tommie McAfee <tmcafee@Tommies-MacBook-Pro.local>
2012-02-28 01:40:20 -0800

    fix syntax
    Change-Id: I308b8e694334ed699cb65b6014be425b6608b455
    Reviewed-on: http://review.couchbase.org/13648
    Reviewed-by: Tommie McAfee <tommie@couchbase.com>
    Tested-by: Tommie McAfee <tommie@couchbase.com>

--- pytests/viewquerytests.py
+++ pytests/viewquerytests.py
@@ -290,7 +290,7 @@ class View:
                         tc.assertEquals(num_keys, expected_num_docs, val)
                     except Exception as ex:
                         self.log.error(val)
-                        self.log.error("Last query result:\n\n%s\n\n" % (json.dumps(results, sort_keys=True, indent=4))
+                        self.log.error("Last query result:\n\n%s\n\n" % (json.dumps(results, sort_keys=True, indent=4)))
                         self.results.addFailure(tc, sys.exc_info())
             else:
                 # query without verification
