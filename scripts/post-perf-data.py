588ed5773a4531854a89d55830405147b3d68801
farshid <farshid.ghods@gmail.com>
2012-03-11 12:10:04 -0700

    ignore ns_server_system stats
    Change-Id: Ifa4d037d1109e4c3976edcdcf36702b595870fa2
    Reviewed-on: http://review.couchbase.org/13861
    Reviewed-by: Farshid Ghods <farshid@couchbase.com>
    Tested-by: Farshid Ghods <farshid@couchbase.com>

--- scripts/post-perf-data.py
+++ scripts/post-perf-data.py
@@ -121,6 +121,8 @@ if __name__ == "__main__":
                     row_dict = {}
                     row_dict['cpu_util'] = row['systemStats']['cpu_utilization_rate']
                     row_dict['swap_used'] = row['systemStats']['swap_used']
+                    if not 'vb_replica_curr_items' in row['interestingStats']:
+                       continue
                     row_dict['vb_replica_curr_items'] = row['interestingStats']['vb_replica_curr_items']
                     row_dict['curr_items_tot'] = row['interestingStats']['curr_items_tot']
                     row_dict['curr_items'] = row['interestingStats']['curr_items']
