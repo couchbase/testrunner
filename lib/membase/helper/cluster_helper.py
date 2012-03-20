0758ff8b18973a7f8f0529cce36ee63c5db36b82
Tommie McAfee <tmcafee@Tommies-MacBook-Pro.local>
2012-03-20 15:57:58 -0700

    ClusterOperationHelper should call RebalanceHelper
    first of larger refactoring where cluster_helper becomes
    gateway to all cluster ops such as rebalance, failover.
    Currently there is a lot of code duplication.
    
    Change-Id: I77901166497a723b24a3ec07ed59a35dbc05ccad
    Reviewed-on: http://review.couchbase.org/14128
    Reviewed-by: Srinivas Vadlamani <srinivas@couchbase.com>
    Tested-by: Tommie McAfee <tommie@couchbase.com>

--- lib/membase/helper/cluster_helper.py
+++ lib/membase/helper/cluster_helper.py
@@ -3,6 +3,7 @@ from memcached.helper.data_helper import MemcachedClientHelper
 from remote.remote_util import RemoteMachineShellConnection
 from mc_bin_client import MemcachedClient, MemcachedError
 from membase.api.exception import ServerAlreadyJoinedException
+from membase.helper.rebalance_helper import RebalanceHelper
 
 import logger
 import testconstants
@@ -278,57 +279,13 @@ class ClusterOperationHelper(object):
 
     @staticmethod
     def begin_rebalance_in(master, servers, timeout=5):
-        log = logger.Logger.get_logger()
-        rest = RestConnection(master)
-        otpNode = None
-
-        for server in servers:
-            if server == master:
-                continue
-            log.info("adding node {0}:{1} to cluster".format(server.ip, server.port))
-            try:
-                otpNode = rest.add_node(master.rest_username, master.rest_password, server.ip, server.port)
-                msg = "unable to add node {0}:{1} to the cluster"
-                assert otpNode, msg.format(server.ip, server.port)
-            except ServerAlreadyJoinedException:
-                log.info("server {0} already joined".format(server))
-        log.info("beginning rebalance in")
-        try:
-            rest.rebalance(otpNodes=[node.id for node in rest.node_statuses()], ejectedNodes=[])
-        except:
-            log.error("rebalance failed, trying again after {0} seconds".format(timeout))
-            time.sleep(timeout)
-            rest.rebalance(otpNodes=[node.id for node in rest.node_statuses()], ejectedNodes=[])
+        RebalanceHelper.begin_rebalance_in(master, servers, timeout)
 
     @staticmethod
     def begin_rebalance_out(master, servers, timeout=5):
-        log = logger.Logger.get_logger()
-        rest = RestConnection(master)
-
-        master_node = rest.get_nodes_self()
-
-        allNodes = []
-        ejectedNodes = []
-        nodes = rest.node_statuses()
-        for server in servers:
-            server_node = RestConnection(server).get_nodes_self()
-            if server_node == master_node:
-                continue
-            log.info("removing node {0}:{1} from cluster".format(server_node.ip, server_node.port))
-            for node in nodes:
-                if "{0}:{1}".format(node.ip, node.port) == "{0}:{1}".format(server_node.ip, server_node.port):
-                    ejectedNodes.append(node.id)
-        log.info("beginning rebalance out")
-        try:
-            rest.rebalance(otpNodes=[node.id for node in nodes], ejectedNodes=ejectedNodes)
-        except:
-            log.error("rebalance failed, trying again after {0} seconds".format(timeout))
-            time.sleep(timeout)
-            rest.rebalance(otpNodes=[node.id for node in nodes], ejectedNodes=ejectedNodes)
+        RebalanceHelper.begin_rebalance_out(master, servers, timeout)
 
     @staticmethod
     def end_rebalance(master):
-        log = logger.Logger.get_logger()
-        rest = RestConnection(master)
-        assert rest.monitorRebalance(), "rebalance operation failed after adding nodes"
-        log.info("rebalance finished")
+        RebalanceHelper.end_rebalance(master)
+
