9f74d0a3db8c26f822f78bb2763a1cbdd2f268dc
ronniedada <ronniedada@gmail.com>
2012-04-09 19:01:34 -0700

    Add scaled down version test_ept_write_original
    This is a temporary version of test_ept_write_original
    ops and number of items are divided by 2.
    
    Change-Id: I73269a783ec042a9c205ddef0799234ebab60271
    Reviewed-on: http://review.couchbase.org/14718
    Reviewed-by: Steve Yen <steve.yen@gmail.com>
    Tested-by: Steve Yen <steve.yen@gmail.com>

--- pytests/performance/eperf.py
+++ pytests/performance/eperf.py
@@ -446,6 +446,25 @@ class EPerfMaster(perf.PerfBase):
                           max_creates = self.parami("max_creates", 20000000))
         self.gated_finish(self.input.clients, notify)
 
+    # This is a scacled down version of test_ept_mixed_original with fewer creates.
+    def test_ept_write_original_scaled_down(self):
+        self.spec("EPT-WRITE-original_scaled_down")
+        items = self.parami("items", 22500000)
+        notify = self.gated_start(self.input.clients)
+        self.load_phase(self.parami("num_nodes", 10), items)
+        # Read:Insert:Update:Delete Ratio = 20:15:60:5.
+        self.access_phase(items,
+                          ratio_sets = self.paramf('ratio_sets', 0.8),
+                          ratio_misses = self.paramf('ratio_misses', 0.05),
+                          ratio_creates = self.paramf('ratio_creates', 0.1875),
+                          ratio_deletes = self.paramf('ratio_deletes', 0.0769),
+                          ratio_hot = self.paramf('ratio_hot', 0.2),
+                          ratio_hot_gets = self.paramf('ratio_hot_gets', 0.95),
+                          ratio_hot_sets = self.paramf('ratio_hot_sets', 0.95),
+                          ratio_expirations = self.paramf('ratio_expirations', 0.025),
+                          max_creates = self.parami("max_creates", 10000000))
+        self.gated_finish(self.input.clients, notify)
+
     def test_query_all_docs_mixed_original(self):
         self.spec("QEURY-ALLDOCS-MIXED-original")
         items = self.parami("items", 45000000)
