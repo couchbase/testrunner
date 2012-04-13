c9e0c9e8ea679d248674170c041dffb21f81aa9f
Karan Kumar <karan@couchbase.com>
2012-04-13 14:08:39 -0700

    Script to display masterEvents from ns_server
    Change-Id: Ie7245ec763777e40e795d8edab347e922aee59e9
    Reviewed-on: http://review.couchbase.org/14706
    Reviewed-by: Karan Kumar <karan@couchbase.com>
    Tested-by: Karan Kumar <karan@couchbase.com>

---
+++ scripts/stream_events.py
@@ -0,0 +1,37 @@
+from httplib import BadStatusLine
+import sys
+import urllib2
+import base64
+
+sys.path.append('.')
+sys.path.append('lib')
+
+from TestInput import TestInputParser
+
+def create_headers(username, password):
+    authorization = base64.encodestring('%s:%s' % (username, password))
+    return {'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic %s' % authorization,
+            'Accept': '*/*'}
+
+
+if __name__ == "__main__":
+    input = TestInputParser.get_test_input(sys.argv)
+    master = input.servers[0]
+    print "Streaming events from {0}".format(master.ip)
+    diag_url = "http://{0}:{1}/diag/masterEvents".format(master.ip, master.port)
+    print diag_url
+    try:
+        req = urllib2.Request(diag_url)
+        req.headers = create_headers(input.membase_settings.rest_username,
+                                     input.membase_settings.rest_password)
+        response = urllib2.urlopen(req)
+        for line in response:
+            print line.rstrip()
+
+    except urllib2.URLError as error:
+        print "unable to stream masterEvents from {0}".format(diag_url)
+    except BadStatusLine:
+        print "unable to stream masterEvents from {0}".format(diag_url)
+    except Exception:
+        print "unable to stream masterEvents from {0}".format(diag_url)
