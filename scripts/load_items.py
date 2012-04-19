56ea509f5bf46f294839a58ae8b43a72d3e5581a
Keith Batten <kbatten@gmail.com>
2012-04-18 18:16:42 -0700

    add data load and verification scripts
    Change-Id: I6e5d1a2e21213efb715f9bf25f895d8596e8d812
    Reviewed-on: http://review.couchbase.org/15004
    Tested-by: Thuan Nguyen <soccon@gmail.com>
    Reviewed-by: Keith Batten <kbatten@gmail.com>

---
+++ scripts/load_items.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+"""Load data into memcached and an on disk key value store."""
+
+import time
+import sys
+import getopt
+import pickle
+import uuid
+import random
+import re
+import mc_bin_client
+
+from memcached.helper.data_helper import VBucketAwareMemcached
+from membase.api.rest_client import RestConnection
+
+
+def usage(err=None):
+    err_code = 0
+    if err:
+        err_code = 1
+        print "Error:", err
+        print
+    print "./load_items.py -m <master> -p <prefix> -o <# of sets>:<# of mutations>:<# of deletes> -f <file>"
+    print ""
+    print " master             the master node rest interface"
+    print " prefix             prefix to use for key names"
+    print " operations         number of sets, mutations and deletes to do"
+    print " file               file to write out the kvstore to"
+    print ""
+    print "./load_items -m Administrator:password@10.1.2.99:8091 -p \"1-\" -o 1000:100:10 -f kvstore"
+    sys.exit(err_code)
+
+class KVStore(object):
+    """Simple key value store that handles sets and deletes, has the ability to read and write from a file"""
+
+    def __init__(self, filename=None):
+        if filename:
+            with open(filename) as f:
+                self.data = pickle.load(f)
+        else:
+            self.data = {}
+
+        self.index = 0
+
+    def save(self, filename):
+        """Write out the current kvstore to a pickle file"""
+        with open(filename, 'w') as f:
+            pickle.dump(self.data, f)
+
+    def set(self, key, exp, flags, val):
+        """Memcached set"""
+        if exp and exp <= 2592000:
+            exp += int(time.time())
+        self.data[key] = (exp, flags, val, True)
+
+    def delete(self, key):
+        """Delete an item, but don't remove it from the kvstore completely"""
+        if key in self.data:
+            self.data[key] = (0, 0, "", False)
+
+    def __iter__(self):
+        return self.data.__iter__()
+
+    def iteritems(self):
+        return self.data.iteritems()
+
+
+class Config(object):
+    def __init__(self, argv):
+        # defaults
+        self.master = None
+        self.prefix = ""
+        self.sets = 0
+        self.mutations = 0
+        self.deletes = 0
+        self.filename = None
+        self.bucket = 'default'
+
+        try:
+            (opts, args) = getopt.getopt(argv, 'hm:p:o:f:b:', ['help', 'master=', 'prefix=', 'operations=', 'file=', 'bucket='])
+        except IndexError:
+            usage()
+        except getopt.GetoptError, err:
+            usage(err)
+
+        for o, a in opts:
+            if o == "-h" or o == "--help":
+                usage()
+            if o == "-m" or o == "--master":
+                master_list = re.split('[:@]', a)
+                self.master = {}
+                self.master["username"] = master_list[0]
+                self.master["password"] = master_list[1]
+                self.master["ip"] = master_list[2]
+                self.master["port"] = master_list[3]
+            if o == "-p" or o == "--prefix":
+                self.prefix = a
+            if o == "-o" or o == "--operations":
+                operations = a.split(":")
+                self.sets = int(operations[0])
+                self.mutations = int(operations[1])
+                self.deletes = int(operations[2])
+            if o == "-f" or o == "--file":
+                self.filename = a
+            if o == "-b" or o == "--bucket":
+                self.bucket = a
+
+        if not self.master:
+            usage("missing master")
+        if not self.filename:
+            usage("missing file")
+
+
+def set_aware(awareness, key, exp, flags, val):
+    try:
+        awareness.memcached(key).set(key, exp, flags, value)
+    except mc_bin_client.MemcachedError as e:
+        if e.status == 7:
+            awareness.reset_vbucket(rest, key)
+            awareness.memcached(key).set(key, exp, flags, value)
+        else:
+            raise e
+
+def delete_aware(awareness, key):
+    try:
+        awareness.memcached(key).delete(key)
+    except mc_bin_client.MemcachedError as e:
+        if e.status == 7:
+            awareness.reset_vbucket(rest, key)
+            awareness.memcached(key).delete(key)
+        else:
+            raise e
+
+
+if __name__ == "__main__":
+
+    config = Config(sys.argv[1:])
+
+    kv = KVStore()
+
+    rest = RestConnection(config.master)
+    awareness = VBucketAwareMemcached(rest, config.bucket)
+
+
+    for i in range(config.sets):
+        key = config.prefix + str(i)
+        value = str(uuid.uuid4())
+        kv.set(key, 0, 0, value)
+        set_aware(awareness, key, 0, 0, value)
+
+    for i in range(config.mutations):
+        key = config.prefix + str(random.randint(0, config.sets))
+        value = str(uuid.uuid4())
+        kv.set(key, 0, 0, value)
+        set_aware(awareness, key, 0, 0, value)
+
+    for i in range(config.deletes):
+        key = config.prefix + str(i)
+        kv.delete(key)
+        delete_aware(awareness, key)
+
+    awareness.done()
+    kv.save(config.filename)
---
+++ scripts/verify_items.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+"""Verify items exist, or were deleted from couchbase using an on disk kvstore"""
+
+import time
+import sys
+import getopt
+import pickle
+import uuid
+import random
+import re
+import mc_bin_client
+
+from memcached.helper.data_helper import VBucketAwareMemcached
+from membase.api.rest_client import RestConnection
+
+
+def usage(err=None):
+    err_code = 0
+    if err:
+        err_code = 1
+        print "Error:", err
+        print
+    print "./verify_.py -m <master> -f <file>"
+    print ""
+    print " master             the master node rest interface"
+    print " file               file to write out the kvstore to"
+    print ""
+    print "./verify_items -m Administrator:password@10.1.2.99:8091 -f kvstore"
+    sys.exit(err_code)
+
+class KVStore(object):
+    """Simple key value store that handles sets and deletes, has the ability to read and write from a file"""
+
+    def __init__(self, filename=None):
+        if filename:
+            with open(filename) as f:
+                self.data = pickle.load(f)
+        else:
+            self.data = {}
+
+        self.index = 0
+
+    def save(self, filename):
+        """Write out the current kvstore to a pickle file"""
+        with open(filename, 'w') as f:
+            pickle.dump(self.data, f)
+
+    def set(self, key, exp, flags, val):
+        """Memcached set"""
+        if exp and exp <= 2592000:
+            exp += int(time.time())
+        self.data[key] = (exp, flags, val, True)
+
+    def delete(self, key):
+        """Delete an item, but don't remove it from the kvstore completely"""
+        if key in self.data:
+            self.data[key] = (0, 0, "", False)
+
+    def __iter__(self):
+        return self.data.__iter__()
+
+    def iteritems(self):
+        return self.data.iteritems()
+
+
+class Config(object):
+    def __init__(self, argv):
+        # defaults
+        self.master = None
+        self.filename = None
+        self.bucket = 'default'
+
+        try:
+            (opts, args) = getopt.getopt(argv, 'hm:f:b:', ['help', 'master=', 'file=', 'bucket='])
+        except IndexError:
+            usage()
+        except getopt.GetoptError, err:
+            usage(err)
+
+        for o, a in opts:
+            if o == "-h" or o == "--help":
+                usage()
+            if o == "-m" or o == "--master":
+                master_list = re.split('[:@]', a)
+                self.master = {}
+                self.master["username"] = master_list[0]
+                self.master["password"] = master_list[1]
+                self.master["ip"] = master_list[2]
+                self.master["port"] = master_list[3]
+            if o == "-f" or o == "--file":
+                self.filename = a
+            if o == "-b" or o == "--bucket":
+                self.bucket = a
+
+        if not self.master:
+            usage("missing master")
+        if not self.filename:
+            usage("missing file")
+
+
+def get_aware(awareness, key):
+    try:
+        val = awareness.memcached(key).get(key)
+    except mc_bin_client.MemcachedError as e:
+        if e.status == 7:
+            awareness.reset_vbucket(rest, key)
+            val = awareness.memcached(key).get(key)
+        else:
+            raise e
+    return val
+
+
+if __name__ == "__main__":
+
+    config = Config(sys.argv[1:])
+
+    kv = KVStore(config.filename)
+
+    rest = RestConnection(config.master)
+    awareness = VBucketAwareMemcached(rest, config.bucket)
+
+    undeleted = 0
+    missing = 0
+    badval = 0
+
+    for key, val_expected in kv.iteritems():
+        if val_expected[3]:
+            try:
+                val = get_aware(awareness, key)
+                if val[2] != val_expected[2]:
+                    badval += 1
+            except mc_bin_client.MemcachedError as e:
+                if e.status == 1:
+                    missing += 1
+                else:
+                    raise e
+        else:
+            try:
+                val = get_aware(awareness, key)
+                undeleted += 1
+            except mc_bin_client.MemcachedError as e:
+                if e.status != 1:
+                    undeleted += 1
+
+    awareness.done()
+
+    print "undeleted:", undeleted
+    print "missing:", missing
+    print "badval:", badval
