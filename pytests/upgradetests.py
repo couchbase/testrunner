73d30d83a1dc70e059ec261f4f9d9ab98261a907
Karan Kumar <karan@couchbase.com>
2012-05-16 13:51:54 -0700

    [CBQE-63] Fix upgrade tests for centos
    Change-Id: I4942fdcc45e59929611cd05ca6f82029c7cc717e
    Reviewed-on: http://review.couchbase.org/16075
    Tested-by: Karan Kumar <karan@couchbase.com>
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Keith Batten <kbatten@gmail.com>

--- pytests/upgradetests.py
+++ pytests/upgradetests.py
@@ -755,13 +755,16 @@ class MultipleNodeUpgradeTests(unittest.TestCase):
                 removed = helper.remove_nodes(knownNodes=allNodes, ejectedNodes=toBeEjectedNodes)
                 self.assertTrue(removed, msg="Unable to remove nodes {0}".format(toBeEjectedNodes))
                 remote = RemoteMachineShellConnection(server)
+                remote.download_build(appropriate_build)
                 # if initial version is 180
                 # Don't uninstall the server
                 if not initial_version.startswith('1.8.0'):
                     remote.membase_uninstall()
                     remote.couchbase_uninstall()
-                remote.download_build(appropriate_build)
-                remote.membase_install(appropriate_build)
+                    remote.membase_install(appropriate_build)
+                else:
+                    remote.membase_upgrade(appropriate_build)
+
                 RestHelper(rest).is_ns_server_running(testconstants.NS_SERVER_TIMEOUT)
                 log.info("sleep for 10 seconds to wait for membase-server to start...")
                 time.sleep(TIMEOUT_SECS)
