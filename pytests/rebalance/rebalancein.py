6e5672825cdd14bf0cd828b0f8e301235e7c3cfa
Andrei Baranouski <andrei.baranouski@gmail.com>
2012-06-04 22:18:13 -0700

    CBQE-167: create cases for rebalance in part according to test plan( use new approach)
    Change-Id: I699ae23d2b2d49f15a90300f93ec022faa14022d
    Reviewed-on: http://review.couchbase.org/16786
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Michael Wiederhold <mike@couchbase.com>

--- pytests/rebalance/rebalancein.py
+++ pytests/rebalance/rebalancein.py
@@ -11,6 +11,37 @@ class RebalanceInTests(RebalanceBaseTest):
     def tearDown(self):
         super(RebalanceInTests, self).tearDown()
 
+    """Rebalances nodes into a cluster while doing docs ops:create, delete, update.
+
+    This test begins by loading a given number of items into the cluster. It then
+    adds nodes_in nodes at a time and rebalances that nodes into the cluster.
+    During the rebalance we perform docs ops(add/remove/update/readd)
+    in the cluster( operate with a half of items that were loaded before).
+    Once the cluster has been rebalanced we recreate the deleted items,
+    wait for the disk queues to drain, and then verify that there has been no data loss.
+    Once all nodes have been rebalanced in the test is finished."""
+    def rebalance_in_with_ops(self):
+        gen_load = BlobGenerator('mike', 'mike-', self.value_size, end=self.num_items)
+        gen_update = BlobGenerator('mike', 'mike-', self.value_size, end=(self.num_items / 2 - 1))
+        gen_delete = BlobGenerator('mike', 'mike-', self.value_size, start=self.num_items / 2, end=self.num_items)
+        gen_create = BlobGenerator('mike', 'mike-', self.value_size, start=self.num_items / 2 + 1, end=self.num_items *3 / 2)
+        self._load_all_buckets(self.servers[0], gen_load, "create", 0)
+        servs_in=[self.servers[i+1] for i in range(self.nodes_in)]
+        rebalance = self.cluster.async_rebalance(self.servers[:1], servs_in, [])
+        if(self.doc_ops is not None):
+            if("update" in self.doc_ops):
+                self._load_all_buckets(self.servers[0], gen_update, "update", 0)
+            if("create" in self.doc_ops):
+                self._load_all_buckets(self.servers[0], gen_create, "create", 0)
+            if("delete" in self.doc_ops):
+                self._load_all_buckets(self.servers[0], gen_delete, "delete", 0)
+        rebalance.result()
+        self._wait_for_stats_all_buckets(self.servers[:self.nodes_in+1])
+        self._verify_all_buckets(self.servers[0])
+        self._verify_stats_all_buckets(self.servers[:self.nodes_in+1])
+
+
+
     """Rebalances nodes into a cluster while doing mutations.
 
     This test begins by loading a given number of items into the cluster. It then
