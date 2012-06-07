b6c47b2c760eb2a7ef46b41bcd71298589135fe2
Deepkaran <deepkaran.salooja@globallogic.com>
2012-06-07 18:51:39 +0530

    CBQE-175: Move common methods to BaseTestCase for Views
    Change-Id: I190b9c5484a378a5a20035be62a9cb023cc9a07c
    Reviewed-on: http://review.couchbase.org/16949
    Reviewed-by: Michael Wiederhold <mike@couchbase.com>
    Tested-by: buildbot <build@couchbase.com>

--- pytests/basetestcase.py
+++ pytests/basetestcase.py
@@ -1,5 +1,6 @@
 import logger
 import unittest
+import copy
 
 from couchbase.cluster import Cluster
 from TestInput import TestInputSingleton
@@ -97,3 +98,75 @@ class BaseTestCase(unittest.TestCase):
 
         for task in stats_tasks:
             task.result(60)
+
+
+    """Asynchronously applys load generation to all bucekts in the cluster.
+
+    Args:
+        server - A server in the cluster. (TestInputServer)
+        kv_gen - The generator to use to generate load. (DocumentGenerator)
+        op_type - "create", "read", "update", or "delete" (String)
+        exp - The expiration for the items if updated or created (int)
+        kv_store - The index of the bucket's kv_store to use. (int)
+
+    Returns:
+        A list of all of the tasks created.
+    """
+    def _async_load_all_buckets(self, server, kv_gen, op_type, exp, kv_store=1):
+        tasks = []
+        for bucket, kv_stores in self.buckets.items():
+            gen = copy.deepcopy(kv_gen)
+            tasks.append(self.cluster.async_load_gen_docs(server, bucket, gen,
+                                                          kv_stores[kv_store],
+                                                          op_type, exp))
+        return tasks
+
+    """Synchronously applys load generation to all bucekts in the cluster.
+
+    Args:
+        server - A server in the cluster. (TestInputServer)
+        kv_gen - The generator to use to generate load. (DocumentGenerator)
+        op_type - "create", "read", "update", or "delete" (String)
+        exp - The expiration for the items if updated or created (int)
+        kv_store - The index of the bucket's kv_store to use. (int)
+    """
+    def _load_all_buckets(self, server, kv_gen, op_type, exp, kv_store=1):
+        tasks = self._async_load_all_buckets(server, kv_gen, op_type, exp, kv_store)
+        for task in tasks:
+            task.result()
+
+    """Waits for queues to drain on all servers and buckets in a cluster.
+
+    A utility function that waits for all of the items loaded to be persisted
+    and replicated.
+
+    Args:
+        servers - A list of all of the servers in the cluster. ([TestInputServer])
+    """
+    def _wait_for_stats_all_buckets(self, servers):
+        tasks = []
+        for server in servers:
+            for bucket in self.buckets:
+                tasks.append(self.cluster.async_wait_for_stats([server], bucket, '',
+                                   'ep_queue_size', '==', 0))
+                tasks.append(self.cluster.async_wait_for_stats([server], bucket, '',
+                                   'ep_flusher_todo', '==', 0))
+        for task in tasks:
+            task.result()
+
+    """Verifies data on all of the nodes in a cluster.
+
+    Verifies all of the data in a specific kv_store index for all buckets in
+    the cluster.
+
+    Args:
+        server - A server in the cluster. (TestInputServer)
+        kv_store - The kv store index to check. (int)
+    """
+    def _verify_all_buckets(self, server, kv_store=1):
+        tasks = []
+        for bucket, kv_stores in self.buckets.items():
+            tasks.append(self.cluster.async_verify_data(server, bucket, kv_stores[kv_store]))
+        for task in tasks:
+            task.result()
+
--- pytests/rebalance/rebalance_base.py
+++ pytests/rebalance/rebalance_base.py
@@ -1,5 +1,3 @@
-import copy
-
 from basetestcase import BaseTestCase
 
 class RebalanceBaseTest(BaseTestCase):
@@ -16,72 +14,3 @@ class RebalanceBaseTest(BaseTestCase):
     def tearDown(self):
         super(RebalanceBaseTest, self).tearDown()
 
-    """Asynchronously applys load generation to all bucekts in the cluster.
-
-    Args:
-        server - A server in the cluster. (TestInputServer)
-        kv_gen - The generator to use to generate load. (DocumentGenerator)
-        op_type - "create", "read", "update", or "delete" (String)
-        exp - The expiration for the items if updated or created (int)
-        kv_store - The index of the bucket's kv_store to use. (int)
-
-    Returns:
-        A list of all of the tasks created.
-    """
-    def _async_load_all_buckets(self, server, kv_gen, op_type, exp, kv_store=1):
-        tasks = []
-        for bucket, kv_stores in self.buckets.items():
-            gen = copy.deepcopy(kv_gen)
-            tasks.append(self.cluster.async_load_gen_docs(server, bucket, gen,
-                                                          kv_stores[kv_store],
-                                                          op_type, exp))
-        return tasks
-
-    """Synchronously applys load generation to all bucekts in the cluster.
-
-    Args:
-        server - A server in the cluster. (TestInputServer)
-        kv_gen - The generator to use to generate load. (DocumentGenerator)
-        op_type - "create", "read", "update", or "delete" (String)
-        exp - The expiration for the items if updated or created (int)
-        kv_store - The index of the bucket's kv_store to use. (int)
-    """
-    def _load_all_buckets(self, server, kv_gen, op_type, exp, kv_store=1):
-        tasks = self._async_load_all_buckets(server, kv_gen, op_type, exp, kv_store)
-        for task in tasks:
-            task.result()
-
-    """Waits for queues to drain on all servers and buckets in a cluster.
-
-    A utility function that waits for all of the items loaded to be persisted
-    and replicated.
-
-    Args:
-        servers - A list of all of the servers in the cluster. ([TestInputServer])
-    """
-    def _wait_for_stats_all_buckets(self, servers):
-        tasks = []
-        for server in servers:
-            for bucket in self.buckets:
-                tasks.append(self.cluster.async_wait_for_stats([server], bucket, '',
-                                   'ep_queue_size', '==', 0))
-                tasks.append(self.cluster.async_wait_for_stats([server], bucket, '',
-                                   'ep_flusher_todo', '==', 0))
-        for task in tasks:
-            task.result()
-
-    """Verifies data on all of the nodes in a cluster.
-
-    Verifies all of the data in a specific kv_store index for all buckets in
-    the cluster.
-
-    Args:
-        server - A server in the cluster. (TestInputServer)
-        kv_store - The kv store index to check. (int)
-    """
-    def _verify_all_buckets(self, server, kv_store=1):
-        tasks = []
-        for bucket, kv_stores in self.buckets.items():
-            tasks.append(self.cluster.async_verify_data(server, bucket, kv_stores[kv_store]))
-        for task in tasks:
-            task.result()
