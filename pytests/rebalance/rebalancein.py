964ff613f3fcbde6d89dd2253ae3aae9bb2a032e
Andrei Baranouski <andrei.baranouski@gmail.com>
2012-06-19 17:51:12 -0700

    CBQE-254: implementation rebalancein with doc ops
    added tests: incremental_rebalance_in_with_ops &
    incremental_rebalance_out_in_with_mutation
    
    Change-Id: Id789c8a738e898bca0f033e63c82d32d158c1326
    Reviewed-on: http://review.couchbase.org/17426
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Karan Kumar <karan@couchbase.com>

--- pytests/rebalance/rebalancein.py
+++ pytests/rebalance/rebalancein.py
@@ -7,6 +7,12 @@ class RebalanceInTests(RebalanceBaseTest):
 
     def setUp(self):
         super(RebalanceInTests, self).setUp()
+        #gen_load data is used for upload before each test(1000 items by default)
+        self.gen_load = BlobGenerator('mike', 'mike-', self.value_size, end=self.num_items)
+        #gen_update is used for doing mutation for 1/2th of uploaded data
+        self.gen_update = BlobGenerator('mike', 'mike-', self.value_size, end=(self.num_items / 2 - 1))
+        #upload data before each test
+        self._load_all_buckets(self.servers[0], self.gen_load, "create", 0)
 
     def tearDown(self):
         super(RebalanceInTests, self).tearDown()
@@ -17,20 +23,19 @@ class RebalanceInTests(RebalanceBaseTest):
     adds nodes_in nodes at a time and rebalances that nodes into the cluster.
     During the rebalance we perform docs ops(add/remove/update/readd)
     in the cluster( operate with a half of items that were loaded before).
-    Once the cluster has been rebalanced we recreate the deleted items,
-    wait for the disk queues to drain, and then verify that there has been no data loss.
+    Once the cluster has been rebalanced we wait for the disk queues to drain,
+    and then verify that there has been no data loss.
     Once all nodes have been rebalanced in the test is finished."""
     def rebalance_in_with_ops(self):
-        gen_load = BlobGenerator('mike', 'mike-', self.value_size, end=self.num_items)
-        gen_update = BlobGenerator('mike', 'mike-', self.value_size, end=(self.num_items / 2 - 1))
         gen_delete = BlobGenerator('mike', 'mike-', self.value_size, start=self.num_items / 2, end=self.num_items)
-        gen_create = BlobGenerator('mike', 'mike-', self.value_size, start=self.num_items / 2 + 1, end=self.num_items *3 / 2)
-        self._load_all_buckets(self.servers[0], gen_load, "create", 0)
+        gen_create = BlobGenerator('mike', 'mike-', self.value_size, start=self.num_items + 1, end=self.num_items *3 / 2)
         servs_in=[self.servers[i+1] for i in range(self.nodes_in)]
         rebalance = self.cluster.async_rebalance(self.servers[:1], servs_in, [])
         if(self.doc_ops is not None):
+            # define which doc's ops will be performed during rebalancing
+            # allows multiple of them but one by one
             if("update" in self.doc_ops):
-                self._load_all_buckets(self.servers[0], gen_update, "update", 0)
+                self._load_all_buckets(self.servers[0], self.gen_update, "update", 0)
             if("create" in self.doc_ops):
                 self._load_all_buckets(self.servers[0], gen_create, "create", 0)
             if("delete" in self.doc_ops):
@@ -45,18 +50,29 @@ class RebalanceInTests(RebalanceBaseTest):
     """Rebalances nodes into a cluster while doing mutations.
 
     This test begins by loading a given number of items into the cluster. It then
-    adds one node at a time and rebalances that node into the cluster. During the
-    rebalance we update all of the items in the cluster. Once the cluster has been
-    rebalanced we recreate the deleted items, wait for the disk queues to drain, and
-    then verify that there has been no data loss. Once all nodes have been rebalanced
-    in the test is finished."""
-    def incremental_rebalance_in_with_mutation(self):
-        gen = BlobGenerator('mike', 'mike-', self.value_size, end=self.num_items)
-        self._load_all_buckets(self.servers[0], gen, "create", 0)
-
+    adds one node at a time and rebalances that node into the cluster. During the rebalance we
+    update(all of the items in the cluster)/delete( num_items/(num_servers -1) in each iteration)/
+    create(a half of initial items in each iteration). Once the cluster has been
+    rebalanced we wait for the disk queues to drain, and then verify that
+     there has been no data loss. Once all nodes have been rebalanced in the test is finished."""
+    def incremental_rebalance_in_with_ops(self):
         for i in range(self.num_servers)[1:]:
             rebalance = self.cluster.async_rebalance(self.servers[:i], [self.servers[i]], [])
-            self._load_all_buckets(self.servers[0], gen, "update", 0)
+            if self.doc_ops is not None:
+            # define which doc's operation will be performed during rebalancing
+            #only one type of ops can be passed
+                if("update" in self.doc_ops):
+                    # 1/2th of data will be updated in each iteration
+                    self._load_all_buckets(self.servers[0], self.gen_update, "update", 0)
+                elif("create" in self.doc_ops):
+                    # 1/2th of initial data will be added in each iteration
+                    gen_create = BlobGenerator('mike', 'mike-', self.value_size, start=self.num_items*(1 +i ) / 2.0 , end=self.num_items * (1 + i / 2.0))
+                    self._load_all_buckets(self.servers[0], gen_create, "create", 0)
+                elif("delete" in self.doc_ops):
+                    # 1/(num_servers) of initial data will be removed after each iteration
+                    # at the end we should get empty base( or couple items)
+                    gen_delete = BlobGenerator('mike', 'mike-', self.value_size, start=int(self.num_items * (1 - i / (self.num_servers -1.0))) + 1, end=int(self.num_items *( 1- (i - 1) / (self.num_servers -1.0))))
+                    self._load_all_buckets(self.servers[0], gen_delete, "delete", 0)
             rebalance.result()
             self._wait_for_stats_all_buckets(self.servers[:i+1])
             self._verify_all_buckets(self.servers[0])
@@ -71,18 +87,15 @@ class RebalanceInTests(RebalanceBaseTest):
     disk queues to drain, and then verify that there has been no data loss. Once all
     nodes have been rebalanced in the test is finished."""
     def incremental_rebalance_in_with_mutation_and_deletion(self):
-        gen = BlobGenerator('mike', 'mike-', self.value_size, end=self.num_items)
-        gen_1 = BlobGenerator('mike', 'mike-', self.value_size, end=(self.num_items / 2 - 1))
-        gen_2 = BlobGenerator('mike', 'mike-', self.value_size, start=self.num_items / 2,
+        gen_delete = BlobGenerator('mike', 'mike-', self.value_size, start=self.num_items / 2,
                               end=self.num_items)
-        self._load_all_buckets(self.servers[0], gen, "create", 0)
 
         for i in range(self.num_servers)[1:]:
             rebalance = self.cluster.async_rebalance(self.servers[:i], [self.servers[i]], [])
-            self._load_all_buckets(self.servers[0], gen_1, "update", 0)
-            self._load_all_buckets(self.servers[0], gen_2, "delete", 0)
+            self._load_all_buckets(self.servers[0], self.gen_update, "update", 0)
+            self._load_all_buckets(self.servers[0], gen_delete, "delete", 0)
             rebalance.result()
-            self._load_all_buckets(self.servers[0], gen_2, "create", 0)
+            self._load_all_buckets(self.servers[0], gen_delete, "create", 0)
             self._wait_for_stats_all_buckets(self.servers[:i+1])
             self._verify_all_buckets(self.servers[0])
             self._verify_stats_all_buckets(self.servers[:i+1])
@@ -97,15 +110,11 @@ class RebalanceInTests(RebalanceBaseTest):
     that there has been no data loss. Once all nodes have been rebalanced in the test
     is finished."""
     def incremental_rebalance_in_with_mutation_and_expiration(self):
-        gen = BlobGenerator('mike', 'mike-', self.value_size, end=self.num_items)
-        gen_1 = BlobGenerator('mike', 'mike-', self.value_size, end=(self.num_items / 2 - 1))
         gen_2 = BlobGenerator('mike', 'mike-', self.value_size, start=self.num_items / 2,
                               end=self.num_items)
-        self._load_all_buckets(self.servers[0], gen, "create", 0)
-
         for i in range(self.num_servers)[1:]:
             rebalance = self.cluster.async_rebalance(self.servers[:i], [self.servers[i]], [])
-            self._load_all_buckets(self.servers[0], gen_1, "update", 0)
+            self._load_all_buckets(self.servers[0], self.gen_update, "update", 0)
             self._load_all_buckets(self.servers[0], gen_2, "update", 5)
             time.sleep(5)
             rebalance.result()
--- pytests/rebalance/rebalanceinout.py
+++ pytests/rebalance/rebalanceinout.py
@@ -11,14 +11,14 @@ class RebalanceInOutTests(RebalanceBaseTest):
     def tearDown(self):
         super(RebalanceInOutTests, self).tearDown()
 
-    """Rebalances nodes into and out of the cluster while doing mutations.
+    """Rebalances nodes out and in of the cluster while doing mutations.
 
     This test begins by loading a given number of items into the cluster. It then
-    adds one node, rebalances that node into the cluster, and then rebalances it back
-    out. During the rebalancing we update all of the items in the cluster. Once the
+    removes one node, rebalances that node out the cluster, and then rebalances it back
+    in. During the rebalancing we update all of the items in the cluster. Once the
     node has been removed and added back we  wait for the disk queues to drain, and
     then verify that there has been no data loss. We then remove and add back two
-    nodes at at time and so on until we have reached the point where we are adding
+    nodes at a time and so on until we have reached the point where we are adding
     back and removing at least half of the nodes."""
     def incremental_rebalance_in_out_with_mutation(self):
         self.cluster.rebalance(self.servers[:self.num_servers],
@@ -39,6 +39,38 @@ class RebalanceInOutTests(RebalanceBaseTest):
             self._verify_all_buckets(self.servers[0])
             self._verify_stats_all_buckets(self.servers[:self.num_servers])
 
+
+    """Rebalances nodes in and out of the cluster while doing mutations.
+
+    This test begins by loading a initial number of nodes into the cluster.
+    It then adds one node, rebalances that node into the cluster,
+    and then rebalances it back out. During the rebalancing we update all  of
+    the items in the cluster. Once the nodes have been removed and added back we
+    wait for the disk queues to drain, and then verify that there has been no data loss.
+    We then add and remove back two nodes at a time and so on until we have reached
+    the point where we are adding back and removing at least half of the nodes."""
+    def incremental_rebalance_out_in_with_mutation(self):
+        init_num_nodes = self.input.param("init_num_nodes", 1)
+
+        self.cluster.rebalance(self.servers[:self.num_servers],
+                               self.servers[1:init_num_nodes], [])
+        gen = BlobGenerator('mike', 'mike-', self.value_size, end=self.num_items)
+        self._load_all_buckets(self.servers[0], gen, "create", 0)
+
+        for i in range(self.num_servers):
+            tasks = self._async_load_all_buckets(self.servers[0], gen, "update", 0)
+
+            self.cluster.rebalance(self.servers[:self.num_servers], self.servers[init_num_nodes:init_num_nodes + i + 1], [])
+            time.sleep(5)
+            self.cluster.rebalance(self.servers[:self.num_servers],
+                                   [], self.servers[init_num_nodes:init_num_nodes + i + 1])
+            for task in tasks:
+                task.result()
+            self._wait_for_stats_all_buckets(self.servers[:init_num_nodes])
+            self._verify_all_buckets(self.servers[0])
+            self._verify_stats_all_buckets(self.servers[:init_num_nodes])
+
+
     """Rebalances nodes into and out of the cluster while doing mutations and
     deletions.
 
@@ -47,7 +79,7 @@ class RebalanceInOutTests(RebalanceBaseTest):
     out. During the rebalancing we update half of the items in the cluster and delete
     the other half. Once the node has been removed and added back we recreate the
     deleted items, wait for the disk queues to drain, and then verify that there has
-    been no data loss. We then remove and add back two nodes at at time and so on
+    been no data loss. We then remove and add back two nodes at a time and so on
     until we have reached the point where we are adding back and removing at least
     half of the nodes."""
     def incremental_rebalance_in_out_with_mutation_and_deletion(self):
@@ -82,7 +114,7 @@ class RebalanceInOutTests(RebalanceBaseTest):
     out. During the rebalancing we update half of the items in the cluster and expire
     the other half. Once the node has been removed and added back we recreate the
     expired items, wait for the disk queues to drain, and then verify that there has
-    been no data loss. We then remove and add back two nodes at at time and so on
+    been no data loss. We then remove and add back two nodes at a time and so on
     until we have reached the point where we are adding back and removing at least
     half of the nodes."""
     def incremental_rebalance_in_out_with_mutation_and_expiration(self):
