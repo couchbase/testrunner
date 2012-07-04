679a6995604055da84d986781a68c4eae873d533
ronniedada <ronniedada@gmail.com>
2012-07-03 23:22:11 -0700

    CBD-155: abstract observer
    Change-Id: I6f2e6335789e4f0d9e1dd8f82f7516df60709453
    Reviewed-on: http://review.couchbase.org/17875
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Steve Yen <steve.yen@gmail.com>

---
+++ pytests/performance/libobserve/obs.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+from Queue import Queue
+
+from obs_def import ObserveKeyState
+
+class Observer:
+    #TODO: logging
+
+    _keys = Queue()     # keys to be observed, key = [:string]
+
+    def _observe_blocking(self, key_state):
+        if self._keys.empty():
+            print "<%s> empty keys" % self.__class__.__name__
+            return
+
+        print "<%s> self._keys %s" % (self.__class__.__name__, self._keys.queue)
+
+        self._send()
+        responses = self._recv()
+        self.cmp_rm_keys(responses, key_state)
+
+    def observe(self, key_state=ObserveKeyState.OBS_PERSISITED, blocking=True):
+        if blocking:
+            self._observe_blocking(key_state)
+        else:
+            raise NotImplementedError("<%s> unblocking observe has not been implemented"
+                % self.__class__.__name__ )
+
+    def load_keys(self, keys):
+        if not keys:
+            print "<%s> invalid argument key: %s" % (self.__class__.__name__, keys)
+            return
+
+        if self._keys.empty():
+            print "<%s> load_keys = %s" % (self.__class__.__name__, keys)
+            self._keys.put(keys)
+
+    def clear_keys(self):
+        with self._keys.mutex:
+            self._keys.queue.clear()
+
+    def cmp_rm_keys(self, responses, key_state):
+        """
+        remove keys based on (@param: responses),
+        using key_state as filter
+        """
+        for res in responses:
+            if res.__class__.__name__ != "ObserveResponse":
+                print "<%s> invalid response" % self.__class__.__name__
+                return False
+
+            if self._keys.empty():
+                return True
+
+            for obs_key in res.keys:
+                if obs_key.key_state == key_state and\
+                   obs_key.key in self._keys.queue:
+                    with self._keys.mutex:
+                        self._keys.queue.remove(obs_key.key)
+
+    def build_conns(self):
+        raise NotImplementedError(
+            "<%s> build_conns() has not been implemented" % self.__class__.__name__)
+
+    def _send(self):
+        """send packets to servers"""
+        raise NotImplementedError(
+            "<%s> send() has not been implemented" % self.__class__.__name__)
+
+    def _recv(self):
+        """receive packets from servers"""
+        raise NotImplementedError(
+            "<%s> recv() has not been implemented" % self.__class__.__name__)
+
