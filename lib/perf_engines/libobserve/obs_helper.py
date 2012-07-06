d3f8f48d218c0ab68aabd7ea1522035fb7df42cd
ronniedada <ronniedada@gmail.com>
2012-07-06 16:47:07 -0700

    CBD-155: sync dict
    A simple synchronized dict to replace Queue,
    in purpose of saving o(n) iteration.
    
    Change-Id: I886a281cfd5a7dbb651aa73b472de08213205e01
    Reviewed-on: http://review.couchbase.org/18039
    Reviewed-by: Ronnie Sun <ronnie@couchbase.com>
    Tested-by: Ronnie Sun <ronnie@couchbase.com>

--- lib/perf_engines/libobserve/obs_helper.py
+++ lib/perf_engines/libobserve/obs_helper.py
@@ -2,6 +2,11 @@
 
 from crc32 import crc32_hash
 
+try:
+    import threading as _threading
+except ImportError:
+    import dummy_threading as _threading
+
 class VbucketHelper:
 
     @staticmethod
@@ -10,3 +15,61 @@ class VbucketHelper:
         if num_vbuckets > 0:
             vbucketId = crc32_hash(key) & (num_vbuckets - 1)
         return vbucketId
+
+class SyncDict:
+    """
+    Synchronized dict wrapper with basic atomic operations.
+    """
+    def __init__(self):
+        self.mutex = _threading.Lock()
+        self.dict = {}
+
+    def __repr__(self):
+        return "<%s> dict: %s" % (self.__class__.__name__, self.dict)
+
+    def put(self, key, value):
+        with self.mutex:
+            self.dict[key] = value
+
+    def get(self, key):
+        with self.mutex:
+            try:
+                return self.dict[key]
+            except KeyError as e:
+                print "<%s> failed to get key %s : %s"\
+                    % (self.__class__.__name__, key, e)
+        return None
+
+    def pop(self, key):
+        with self.mutex:
+            try:
+                return self.dict.pop(key)
+            except KeyError as e:
+                print "<%s> failed to pop key %s : %s"\
+                    % (self.__class__.__name__, key, e)
+        return None
+
+    def remove(self, key):
+        with self.mutex:
+            try:
+                del self.dict[key]
+            except KeyError as e:
+                print "<%s> failed to delete key %s : %s"\
+                    % (self.__class__.__name__, key, e)
+                return False
+        return True
+
+    def clear(self):
+        with self.mutex:
+            self.dict.clear()
+
+    def empty(self):
+        with self.mutex:
+            return not self._size()
+
+    def size(self):
+        with self.mutex:
+            return self._size()
+
+    def _size(self):
+        return len(self.dict)
