e937ec6d672cffbf5556d0972af03767dd6d7627
Pavel.Paulau <pavel.paulau@gmail.com>
2012-07-06 14:25:50 -0700

    CBD-432: fixed import in cbkarma client
    Change-Id: I58d16e6e8a507aea78bfb456d2348040f5323fba
    Reviewed-on: http://review.couchbase.org/18023
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Steve Yen <steve.yen@gmail.com>

--- lib/cbkarma/rest_client.py
+++ lib/cbkarma/rest_client.py
@@ -1,11 +1,8 @@
-import sys
-sys.path.append('lib')
-
 import json
 import urllib
 
-from membase.api.rest_client import RestConnection
-from membase.api.exception import ServerUnavailableException
+from lib.membase.api.rest_client import RestConnection
+from lib.membase.api.exception import ServerUnavailableException
 
 class CbKarmaClient(RestConnection):
     """Performance dashboard (cbkarma) REST API"""
