a10ed9ffd3b8383c1688741152522ffac8aba5b9
Pavel.Paulau <pavel.paulau@gmail.com>
2012-07-19 14:36:18 -0700

    MB-100: move xdcr tests to iperf module
    Change-Id: I9fa776e12718c06be9ab1857317f3e608a558593
    Reviewed-on: http://review.couchbase.org/18614
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Steve Yen <steve.yen@gmail.com>

--- pytests/performance/iperf.py
+++ pytests/performance/iperf.py
@@ -1,7 +1,13 @@
+import copy
+import httplib2
+import socket
 import functools
 from multiprocessing import Process
 
+from membase.api.rest_client import RestConnection
+
 from eperf import EVPerfClient
+
 
 class PerfWrapper(object):
 
@@ -92,6 +98,54 @@ class PerfWrapper(object):
             return executors[-1]
         return wrapper
 
+    @staticmethod
+    def xperf(bidir=False):
+        def decorator(test):
+            @functools.wraps(test)
+            def wrapper(self, *args, **kargs):
+                """Define remote cluster and start replication (only before
+                load phase).
+                """
+                if (self.parami('prefix', -1) == 0 and
+                    self.parami('load_phase', 0) and
+                    not self.parami('hot_load_phase', 0) and
+                    not self.parami('disable_xdcr', 0)):
+                    master = self.input.clusters[0][0]
+                    slave = self.input.clusters[1][0]
+                    try:
+                        self.start_replication(master, slave, bidir=bidir)
+                    except Exception:
+                        pass
+
+                # Execute performance test
+                region = XPerfTests.get_ec2_region()
+                if region == 'east':
+                    self.input.servers = self.input.clusters[0]
+                    self.input.test_params['bucket'] = self.get_buckets()[0]
+                    return test(self, *args, **kargs)
+                elif region == 'west':
+                    self.input.servers = self.input.clusters[1]
+                    self.input.test_params['bucket'] = self.get_buckets(reversed=True)[0]
+                    return test(self, *args, **kargs)
+                else:
+                    # 1st test executor:
+                    self_copy = copy.copy(self)
+                    self_copy.input.servers = self_copy.input.clusters[0]
+                    self_copy.input.test_params['bucket'] = self_copy.get_buckets()[0]
+                    primary = Process(target=test, args=(self_copy, ))
+                    primary.start()
+
+                    # 2nd test executor:
+                    self.input.servers = self.input.clusters[1]
+                    self.input.test_params['bucket'] = self.get_buckets(reversed=True)[0]
+                    self.input.test_params['stats'] = 0
+                    secondary = test(self, *args, **kargs)
+
+                    primary.join()
+                    return secondary
+            return wrapper
+        return decorator
+
 
 class MultiClientTests(EVPerfClient):
 
@@ -116,3 +170,75 @@ class RampUpTests(EVPerfClient):
     @PerfWrapper.rampup
     def test_view_rampup_1(self):
         super(RampUpTests, self).test_vperf2()
+
+
+class XPerfTests(EVPerfClient):
+
+    """XDCR large-scale performance tests
+    """
+
+    def start_replication(self, master, slave, replication_type='continuous',
+                          buckets=None, bidir=False, suffix='A'):
+        """Add remote cluster and start replication"""
+
+        master_rest_conn = RestConnection(master)
+        remote_reference = 'remote_cluster_' + suffix
+
+        master_rest_conn.add_remote_cluster(slave.ip, slave.port,
+                                            slave.rest_username,
+                                            slave.rest_password,
+                                            remote_reference)
+
+        if not buckets:
+            buckets = self.get_buckets()
+        else:
+            buckets = self.get_buckets(reversed=True)
+
+        for bucket in buckets:
+            master_rest_conn.start_replication(replication_type, bucket,
+                                               remote_reference)
+
+        if self.parami('num_buckets', 1) > 1 and suffix == 'A':
+            self.start_replication(slave, master, replication_type, buckets,
+                                   suffix='B')
+
+        if bidir:
+            self.start_replication(slave, master, replication_type, buckets,
+                                   suffix='B')
+
+    def get_buckets(self, reversed=False):
+        """Return list of buckets to be replicated"""
+
+        num_buckets = self.parami('num_buckets', 1)
+        if num_buckets > 1:
+            num_replicated_buckets = self.parami('num_replicated_buckets',
+                                                 num_buckets)
+            buckets = ['bucket-{0}'.format(i) for i in range(num_buckets)]
+            if not reversed:
+                return buckets[:num_replicated_buckets]
+            else:
+                return buckets[-1:-1 - num_replicated_buckets:-1]
+        else:
+            return [self.param('bucket', 'default')]
+
+    @staticmethod
+    def get_ec2_region():
+        """Try to identify public hostname and return corresponding EC2 region.
+
+        Reference: http://bit.ly/instancedata
+        """
+
+        try:
+            uri = 'http://169.254.169.254/latest/meta-data/public-hostname'
+            http = httplib2.Http(timeout=5)
+            response, content = http.request(uri)
+            if 'west' in content:
+                return 'west'
+            else:
+                return 'east'
+        except socket.timeout:
+            return
+
+    @PerfWrapper.xperf()
+    def test_vperf_unidir(self):
+        super(XPerfTests, self).test_vperf2()
