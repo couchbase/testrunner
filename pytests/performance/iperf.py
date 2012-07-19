b76a44d2ef856267c23e2e1a8f8f65a8c572a935
Pavel.Paulau <pavel.paulau@gmail.com>
2012-07-19 13:57:36 -0700

    CBD-460: add ramp-up tests (sub-class and wrapper)
    Change-Id: Ifed119557df135fbf5ced4c8712c021916d32b73
    Reviewed-on: http://review.couchbase.org/18613
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Steve Yen <steve.yen@gmail.com>

--- pytests/performance/iperf.py
+++ pytests/performance/iperf.py
@@ -48,6 +48,50 @@ class PerfWrapper(object):
                 return executors[0]
         return wrapper
 
+    @staticmethod
+    def rampup(test):
+        @functools.wraps(test)
+        def wrapper(self, *args, **kargs):
+            """This wrapper launches two groups of processes:
+            -- constant background load (mainly memcached sets/gets)
+            -- ramping up load (mainly view queries)
+
+            Currenly these processes don't have synchronization points.
+            """
+            if self.parami('index_phase', 0) or self.parami('load_phase', 0):
+                return PerfWrapper.multiply(test(self, *args, **kargs))
+
+            total_bg_clients = self.parami('total_bg_clients', 1)
+            total_fg_clients = self.parami('total_fg_clients', 1)
+            total_clients = total_bg_clients + total_fg_clients
+            self.input.test_params['num_clients'] = total_clients
+
+            executors = list()
+
+            # Background load (memcached)
+            original_delay = self.parami('start_delay', 30)
+            self.input.test_params['start_delay'] = 5
+            for prefix in range(0, total_bg_clients):
+                self.input.test_params['prefix'] = prefix
+                executor = Process(target=test, args=(self, ))
+                executor.start()
+                executors.append(executor)
+
+            # Foreground load (memcached)
+            self.input.test_params['start_delay'] = original_delay
+            self.input.test_params['bg_max_ops_per_sec'] = 1
+            for prefix in range(total_bg_clients, total_clients):
+                self.input.test_params['prefix'] = prefix + total_bg_clients
+                executor = Process(target=test, args=(self, ))
+                executor.start()
+                executors.append(executor)
+
+            for executor in executors:
+                executor.join()
+
+            return executors[-1]
+        return wrapper
+
 
 class MultiClientTests(EVPerfClient):
 
@@ -62,3 +106,13 @@ class MultiClientTests(EVPerfClient):
     @PerfWrapper.multiply
     def test_vperf2(self):
         super(MultiClientTests, self).test_vperf2()
+
+
+class RampUpTests(EVPerfClient):
+
+    """Ramup-up load tests with increasing number of foreground workers.
+    """
+
+    @PerfWrapper.rampup
+    def test_view_rampup_1(self):
+        super(RampUpTests, self).test_vperf2()
