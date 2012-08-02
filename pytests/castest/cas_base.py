ed73864bf5d53adf120c4bdf5890a77b8251ac5e
Chisheng Hong <chisheng@couchbase.com>
2012-08-01 19:38:19 -0700

    CBQE-379 Create new test cases for CAS functional test
    CAS value manipulation by update, delete, expire test.
    We need to verify whether the memchached client operations
    set, get, and delete works properly and retun a valid CAS value.
    
    For delete(), after it is called, the item cas is always reset to zero.
    So we need to figure out a way to do verification about delete
    
    Change-Id: I15b5de1e0e82e78f924905f694b99914b284ed7a
    Reviewed-on: http://review.couchbase.org/19120
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Karan Kumar <karan@couchbase.com>

---
+++ pytests/castest/cas_base.py
@@ -0,0 +1,22 @@
+from basetestcase import BaseTestCase
+from memcached.helper.data_helper import MemcachedClientHelper
+from membase.api.rest_client import Bucket
+
+class CasBaseTest(BaseTestCase):
+    def setUp(self):
+        super(CasBaseTest, self).setUp()
+        self.value_size = self.input.param("value_size", 256)
+        self.doc_ops = self.input.param("doc_ops", None)
+        self.mutate_times = self.input.param("mutate_times", 10)
+        self.expire_time = self.input.param("expire_time", 5)
+        if self.doc_ops is not None:
+            self.doc_ops = self.doc_ops.split(";")
+        self.clients = {}
+        for bucket in self.buckets:
+            client = MemcachedClientHelper.direct_client(self.master, bucket.name)
+            self.clients[bucket.name]=client
+
+    def tearDown(self):
+        super(CasBaseTest, self).tearDown()
+        for bucket in self.buckets:
+            self.clients[bucket.name].close()
---
+++ pytests/castest/opschangecas.py
@@ -0,0 +1,82 @@
+import time
+import logger
+from castest.cas_base import CasBaseTest
+from couchbase.documentgenerator import BlobGenerator
+
+class OpsChangeCasTests(CasBaseTest):
+
+    def setUp(self):
+        super(OpsChangeCasTests, self).setUp()
+
+    def tearDown(self):
+        super(OpsChangeCasTests, self).tearDown()
+
+    def ops_change_cas(self):
+        """CAS value manipulation by update, delete, expire test.
+
+        We load a certain number of items. Then for half of them, we use
+        MemcachedClient cas() method to mutate those item values in order
+        to change CAS value of those items.
+        We use MemcachedClient set() method to set a quarter of the items expired.
+        We also use MemcachedClient delete() to delete a quarter of the items"""
+
+        gen_load_mysql = BlobGenerator('nosql', 'nosql-', self.value_size, end=self.num_items)
+        gen_update = BlobGenerator('nosql', 'nosql-', self.value_size, end=(self.num_items / 2 - 1))
+        gen_delete = BlobGenerator('nosql', 'nosql-', self.value_size, start=self.num_items / 2, end=(3* self.num_items / 4 - 1))
+        gen_expire = BlobGenerator('nosql', 'nosql-', self.value_size, start=3* self.num_items / 4, end=self.num_items)
+        self._load_all_buckets(self.master, gen_load_mysql, "create", 0)
+
+        if(self.doc_ops is not None):
+            if("update" in self.doc_ops):
+                self.verify_cas("update", gen_update)
+            if("delete" in self.doc_ops):
+                self.verify_cas("delete", gen_delete)
+            if("expire" in self.doc_ops):
+                self.verify_cas("expire", gen_expire)
+        self._wait_for_stats_all_buckets(self.servers[:self.num_servers])
+
+    def verify_cas(self, ops, generator):
+        """Verify CAS value manipulation
+
+        For update we use the latest CAS value return by set()
+        to do the mutation again to see if there is any exceptions.
+        We should be able to mutate that item with the latest CAS value.
+        For delete(), after it is called, the item cas is always reset to zero.
+        The cas() operation followed always succeeds.
+        For expire, We want to verify using the latest CAS value of that item
+        can not mutate it because it is expired already."""
+
+        for bucket in self.buckets:
+            gen = generator
+            cas_error_collection = []
+            data_error_collection = []
+            while gen.has_next():
+                key, value = gen.next()
+                if ops == "update":
+                    for x in range(self.mutate_times):
+                        o_old, cas_old, d_old = self.clients[bucket.name].get(key)
+                        self.clients[bucket.name].cas(key, 0, 0, cas_old, "{0}-{1}".format("mysql-new-value", x))
+                        o_new, cas_new, d_new = self.clients[bucket.name].get(key)
+                        if cas_old == cas_new:
+                            cas_error_collection.append(cas_old)
+                        if d_new != "{0}-{1}".format("mysql-new-value", x):
+                            data_error_collection.append((d_new,"{0}-{1}".format("mysql-new-value", x)))
+                elif ops == "delete":
+                    o, cas, d = self.clients[bucket.name].delete(key)
+                    self.clients[bucket.name].cas(key, 0, 0, cas, value)
+                    #There is no way to verify CAS value changed by delete until now.
+                elif ops == "expire":
+                    o, cas, d = self.clients[bucket.name].set(key, self.expire_time, 0, value)
+                    time.sleep(self.expire_time+1)
+                    self.clients[bucket.name].cas(key, 0, 0, cas, value)
+                    #It is expected to raise CAS exception becasue the key is expired.
+                    #It is a negative result test.
+
+            if len(cas_error_collection) > 0:
+                for cas_value in cas_error_collection:
+                    self.log.error("Set operation fails to modify the CAS {0}".format(cas_value))
+                raise Exception("set operation fails to modify the CAS value")
+            if len(data_error_collection) > 0:
+                for data_value in data_error_collection:
+                    self.log.error("Set operation fails. item-value is {0}, expected is {1}".format(data_value[0], data_value[1]))
+                raise Exception("Set operation fails to change item value")
