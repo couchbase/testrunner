7405cabcf90b6ba94af47e7771575b633aadbefd
ronniedada <ronniedada@gmail.com>
2012-08-21 10:40:30 -0700

    MB-100: private method to get ep engine int stats
    Change-Id: Iaed71a9d792a02e0de0ff7fcb34802cf101c2cd1
    Reviewed-on: http://review.couchbase.org/19963
    Reviewed-by: Steve Yen <steve.yen@gmail.com>
    Tested-by: Ronnie Sun <ronnie@couchbase.com>

--- pytests/performance/eperf.py
+++ pytests/performance/eperf.py
@@ -1693,7 +1693,7 @@ class ViewGen:
 
 class NRUMonitor(threading.Thread):
 
-     CMD = "/opt/couchbase/bin/cbstats localhost:11210 "\
+     CMD_NUM_RUNS = "/opt/couchbase/bin/cbstats localhost:11210 "\
            "all | grep ep_num_access_scanner_runs"
 
      def __init__(self, freq, reb_delay, eperf):
@@ -1738,27 +1738,37 @@ class NRUMonitor(threading.Thread):
 
      def get_nru_num(self):
          """Retrieve how many times nru access scanner has been run"""
-         output, error = self.shell.execute_command(NRUMonitor.CMD)
+         return self._get_shell_int(NRUMonitor.CMD_NUM_RUNS)
+
+     def _get_shell_int(self, cmd):
+         """
+         Fire a shell command and return output as integer
+         """
+         if not cmd:
+             print "<_get_shell_int> invalid cmd"
+             return -1
+
+         output, error = self.shell.execute_command(cmd)
 
          if error:
-             print "unable to get nru number from %s: %s" \
-                % (self.shell.ip, error)
+             print "<_get_shell_int> unable to execute cmd '%s' from %s: %s"\
+                % (cmd, self.shell.ip, error)
              return -1
 
          if not output:
-             print "unable to get nru number from %s: empty output" \
-                % self.shell.ip
+             print "<_get_shell_int> unable to execute cmd '%s' from %s: "\
+                "empty output" % (cmd, self.shell.ip)
              return -1
 
          try:
              num = int(output[0].split(":")[1])
          except  (AttributeError, IndexError, ValueError), e:
-             print "unable to get nru number from %s: output - %s, error - %s"\
-                % (self.shell.ip, output, e)
+             print "<_get_shell_int> unable to execute cmd '%s' from %s:"\
+                "output - %s, error - %s" % (cmd, self.shell.ip, output, e)
              return -1
 
          if num < 0:
-             print "invalid nru number: %d" % num
+             print "<_get_shell_int> invalid number: %d" % num
              return -1
 
          return num
