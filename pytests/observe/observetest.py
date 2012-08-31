7e5cbe976650c86dc1826eff9de47fc92035db62
rohitsinha1 <rohit.sinha1@globallogic.com>
2012-08-31 12:10:58 +0530

    CBQE-428 Test observe functionality Closing coding comments
    Change-Id: Ib978a93973aa611081237865d3621201d7748dae
    Reviewed-on: http://review.couchbase.org/20395
    Reviewed-by: Rohit Sinha1 <rohit.sinha1@globallogic.com>
    Tested-by: Rohit Sinha1 <rohit.sinha1@globallogic.com>

--- pytests/observe/observetest.py
+++ pytests/observe/observetest.py
@@ -21,13 +21,13 @@ class ObserveTests(ViewBaseTest):
     def tearDown(self):
         super(ObserveTests, self).tearDown()
 
-    def _load_doc_data_all_buckets(self, op_type='create', start=0, end=0):
+    def _load_doc_data_all_buckets(self, op_type='create', start=0, end=0, expiry=0):
         loaded = False
         count = 0
         gen_load = BlobGenerator('observe', 'observe', 1024, start=start, end=end)
         while not loaded and count < 60:
             try :
-                self._load_all_buckets(self.servers[0], gen_load, op_type, 0)
+                self._load_all_buckets(self.servers[0], gen_load, op_type, expiry)
                 loaded = True
             except MemcachedError as error:
                 if error.status == 134:
@@ -64,6 +64,7 @@ class ObserveTests(ViewBaseTest):
         self._load_doc_data_all_buckets('create', 0, self.num_items)
         rest = RestConnection(self.servers[0])
         self.servers = rest.get_nodes()
+        query_set = "true"
         #Persist all the loaded data item
         self._wait_for_stats_all_buckets(self.servers)
         self.cluster.create_view(
@@ -71,18 +72,27 @@ class ObserveTests(ViewBaseTest):
                     self.default_bucket_name, self.wait_timeout * 2)
         client = MemcachedClientHelper.direct_client(self.servers[0], self.default_bucket_name)
         observe_with = self.input.param("observe_with", "")
+        keys = ["observe%s" % (i) for i in range(0, self.num_items)]
+        for key in keys:
+            opaque, rep_time, persist_time, persisted = client.observe(key)
+            self.log.info("##########key:-%s################" % (key))
+            self.log.info("Persisted:- %s" % (persisted))
+            self.log.info("Persist_Time:- %s" % (rep_time))
+
+        query = {"stale" : "false", "full_set" : "true", "connection_timeout" : 60000}
+        self.cluster.query_view(self.master, "dev_Doc1", self.default_view.name, query, 10000, self.default_bucket_name)
+        self.log.info("Observe Validation:- view: %s in design doc dev_Doc1 and in bucket %s" % (self.default_view, self.default_bucket_name))
         # check whether observe has to run with delete and delete parallel with observe or not
         if len (observe_with) > 0 :
-            if observe_with == "Delete" :
+            if observe_with == "delete" :
                 self.log.info("Deleting 0- %s number of items" % (self.num_items / 2))
                 self._load_doc_data_all_buckets('delete', 0, self.num_items / 2)
                 query_set = "true"
-            elif observe_with == "Delete_Parallel":
+            elif observe_with == "delete_parallel":
                 self.log.info("Deleting Parallel 0- %s number of items" % (self.num_items / 2))
                 tasks = self._async_load_doc_data_all_buckets('delete', 0, self.num_items / 2)
                 query_set = "false"
 
-        keys = ["observe%s" % (i) for i in range(0, self.num_items)]
         for key in keys:
             opaque, rep_time, persist_time, persisted = client.observe(key)
             self.log.info("##########key:-%s################" % (key))
@@ -91,7 +101,14 @@ class ObserveTests(ViewBaseTest):
 
         for task in tasks:
             task.result()
+
         #verify the persistence of data by querying view
+        stale = self.input.param("stale", "ok")
+        if stale == "ok" :
+            query = {"stale" : "ok", "full_set" : query_set, "connection_timeout" : 60000}
+            self.cluster.query_view(self.master, "dev_Doc1", self.default_view.name, query, 10000, self.default_bucket_name)
+
         query = {"stale" : "false", "full_set" : query_set, "connection_timeout" : 60000}
         self.cluster.query_view(self.master, "dev_Doc1", self.default_view.name, query, 10000, self.default_bucket_name)
         self.log.info("Observe Validation:- view: %s in design doc dev_Doc1 and in bucket %s" % (self.default_view, self.default_bucket_name))
+
