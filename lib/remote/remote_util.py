ce4a9727b31aaaf1d3b49a1e6a9a4daa4390052c
Pavel.Paulau <pavel.paulau@gmail.com>
2012-09-11 10:18:28 -0700

    MB-100: rename mbenable_core_dumps to cbenable_core_dumps
    Change-Id: I46993761ffee58d0bd2b57f85d3896b8ea0f861e
    Reviewed-on: http://review.couchbase.org/20733
    Reviewed-by: Karan Kumar <karan@couchbase.com>
    Tested-by: buildbot <build@couchbase.com>

--- lib/remote/remote_util.py
+++ lib/remote/remote_util.py
@@ -712,7 +712,7 @@ bOpt2=0' > /cygdrive/c/automation/css_win2k8_64_install.iss"
                 output, error = self.execute_command('/opt/membase/bin/mbenable_core_dumps.sh  {0}'.format(path))
                 self.log_command_output(output, error)
             else:
-                output, error = self.execute_command('/opt/couchbase/bin/mbenable_core_dumps.sh  {0}'.format(path))
+                output, error = self.execute_command('/opt/couchbase/bin/cbenable_core_dumps.sh  {0}'.format(path))
                 self.log_command_output(output, error)
 
             if vbuckets and is_couchbase:
