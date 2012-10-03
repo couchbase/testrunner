3626da129d477c43de16724b7e44dfee2e11a045
Iryna <irynamironava@yandex.ru>
2012-10-03 01:43:32 -0700

    CBQE-619: Do not fail test while getting socket error
    View tests now fail once get any error, during warm up or failover socket errors or timeout errors are ok, test should not fail
    
    Change-Id: If784ee16cb1888a1cdc29b721c4c4f7a31cc1956
    Reviewed-on: http://review.couchbase.org/21299
    Reviewed-by: Deepkaran Salooja <deepkaran.salooja@globallogic.com>
    Tested-by: Iryna Mironava <irynamironava@yandex.ru>

--- pytests/viewtests.py
+++ pytests/viewtests.py
@@ -446,7 +446,8 @@ class ViewBaseTests(unittest.TestCase):
                 if invalid_results and ex.message.find('view_undefined') != -1:
                         raise ex
                 if ex.message.find('view_undefined') != -1 or ex.message.find('missing') != -1 or \
-                 ex.message.find('unable to reach') != -1 or ex.message.find('timeout') != -1:
+                 ex.message.find('unable to reach') != -1 or ex.message.find('timeout') != -1 or \
+                 ex.message.find('socket error') != -1 or ex.message.find('econnrefused') != -1 :
                     self.log.error(
                             "view_results not ready yet , try again in {1} seconds... , error {0}"
                             .format(ex, timeout))
