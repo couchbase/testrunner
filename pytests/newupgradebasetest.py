6b07bfe1855ab6394145e76de374330759005506
Chisheng Hong <chisheng@couchbase.com>
2012-10-16 18:29:16 -0700

    CBQE-700 Automate all single node upgrade test
    Test cases include upgrade from 180 to 200 and
    181 to 200 with/without data loading
    Multiple nodes upgrade is under implementing
    
    Change-Id: I87a589fdacdb3667b34e9755e70008316c5c1d09
    Reviewed-on: http://review.couchbase.org/21702
    Reviewed-by: Chisheng Hong <chisheng@couchbase.com>
    Tested-by: Chisheng Hong <chisheng@couchbase.com>

--- pytests/newupgradebasetest.py
+++ pytests/newupgradebasetest.py
@@ -1,40 +1,49 @@
+import re
 import time
+import testconstants
 from basetestcase import BaseTestCase
 from membase.api.rest_client import RestConnection, Bucket, RestHelper
 from remote.remote_util import RemoteMachineShellConnection
 from couchbase.documentgenerator import BlobGenerator
 from mc_bin_client import MemcachedError
-
+from scripts.install import InstallerJob
+from builds.build_query import BuildQuery
 
 class NewUpgradeBaseTest(BaseTestCase):
 
     def setUp(self):
         super(NewUpgradeBaseTest, self).setUp()
-        self.ops = []
-        self.product = 'couchbase-server-enterprise'
-        self.remote = RemoteMachineShellConnection(self.master)
-        self.rest = RestConnection(self.master)
-        self.info = self.remote.extract_remote_info()
+        self.product = self.input.param('product', 'couchbase-server')
+        self.initial_version = self.input.param('initial_version', '1.8.1-942-rel')
+        self.initial_vbuckets = self.input.param('initial_vbuckets', 64)
         self.rest_settings = self.input.membase_settings
+        self.rest = RestConnection(self.master)
         self.rest_helper = RestHelper(self.rest)
-        self.bucket_ddoc_map = {}
-        self.bucketnames = []
         self.sleep_time = 60
-        self.data_size = 1024
-        self.op_types = self.input.param("op_types", 'bucket')
-        if self.default_bucket:
-            self.bucketnames.append(self.default_bucket_name)
-        if self.sasl_buckets > 0:
-            for i in range(self.sasl_buckets):
-                self.bucketnames.append('bucket' + str(i))
-        if self.standard_buckets > 0:
-            for i in range(self.standard_buckets):
-                self.bucketnames.append('standard_bucket' + str(i))
+        self.data_size = self.input.param('data_size', 1024)
+        self.op_types = self.input.param('op_types', 'bucket')
+        self.item_flag = self.input.param('item_flag', 4042322160)
+        self.expire_time = self.input.param('expire_time', 0)
 
     def tearDown(self):
         super(NewUpgradeBaseTest, self).tearDown()
 
-    def operations(self):
+    def _install(self, servers):
+        params = {}
+        params['num_nodes'] = len(servers)
+        params['product'] = self.product
+        params['version'] = self.initial_version
+        params['vbuckets'] = [self.initial_vbuckets]
+        InstallerJob().parallel_install(servers, params)
+        if self.product in ["couchbase", "couchbase-server", "cb"]:
+            success = True
+            for server in servers:
+                success &= RemoteMachineShellConnection(server).is_couchbase_installed()
+                if not success:
+                    self.log.info("some nodes were not install successfully!")
+                    sys.exit(1)
+
+    def operations(self, multi_nodes=False):
         self.quota = self._initialize_nodes(self.cluster, self.servers, self.disabled_consistent_view)
         if self.total_buckets > 0:
             self.bucket_size = self._get_bucket_size(self.quota, self.total_buckets)
@@ -46,17 +55,20 @@ class NewUpgradeBaseTest(BaseTestCase):
 
         self._create_sasl_buckets(self.master, self.sasl_buckets)
         self._create_standard_buckets(self.master, self.standard_buckets)
-        time.sleep(self.sleep_time)
+        if multi_nodes:
+            servers_in = [self.servers[i+1] for i in range(self.num_servers-1)]
+            self.cluster.rebalance(self.servers[:1], servers_in, [])
         if self.op_types == "data":
-            self._load_doc_data_all_buckets("create")
+            self._load_data_all_buckets("create")
 
-    def _load_doc_data_all_buckets(self, op_type='create', start=0, expiry=0):
+    def _load_data_all_buckets(self, op_type='create', start=0):
         loaded = False
         count = 0
         gen_load = BlobGenerator('upgrade-', 'upgrade-', self.data_size, start=start, end=self.num_items)
         while not loaded and count < 60:
             try :
-                self._load_all_buckets(self.servers[0], gen_load, op_type, expiry)
+                self._load_all_buckets(self.master, gen_load, op_type, self.expire_time, 1,
+                                       self.item_flag, False, batch_size=20000, pause_secs=5, timeout_secs=180)
                 loaded = True
             except MemcachedError as error:
                 if error.status == 134:
@@ -65,19 +77,48 @@ class NewUpgradeBaseTest(BaseTestCase):
                     count += 1
                     time.sleep(self.sleep_time)
 
-    def verfication(self):
+    def _get_build(self, server, version, remote, is_amazon=False):
+        info = remote.extract_remote_info()
+        builds, changes = BuildQuery().get_all_builds()
+        self.log.info("finding build %s for machine %s" % (version, server))
+        result = re.search('r', version)
+
+        if result is None:
+            appropriate_build = BuildQuery().\
+                find_membase_release_build('%s-enterprise' % (self.product), info.deliverable_type,
+                                           info.architecture_type, version.strip(), is_amazon=is_amazon)
+        else:
+            appropriate_build = BuildQuery().\
+                find_membase_build(builds, '%s-enterprise' % (self.product), info.deliverable_type,
+                                   info.architecture_type, version.strip(), is_amazon=is_amazon)
+
+        return appropriate_build
+
+    def _upgrade(self, upgrade_version, server, remote):
+        appropriate_build = self._get_build(server, upgrade_version, remote)
+        self.assertTrue(appropriate_build.url, msg="unable to find build {0}".format(upgrade_version))
+        remote.download_build(appropriate_build)
+        remote.membase_upgrade(appropriate_build, save_upgrade_config=False)
+        self.rest_helper.is_ns_server_running(testconstants.NS_SERVER_TIMEOUT)
+        self.rest.init_cluster_port(self.rest_settings.rest_username, self.rest_settings.rest_password)
+        time.sleep(self.sleep_time)
+
+    def verfication(self, multi_nodes=False):
         for bucket in self.buckets:
-            bucketname = bucket.name
-            if self.op_types == "data":
-                bucketinfo = self.rest.get_bucket(bucketname)
-                self.log.info("bucket info :- %s" % bucketinfo)
-            if self.rest_helper.bucket_exists(bucketname):
+            if self.rest_helper.bucket_exists(bucket.name):
                 continue
             else:
-                raise Exception("bucket:- %s not found" % bucketname)
-            if self.op_types == "data":
-                bucketinfo = self.rest.get_bucket(bucketname)
+                raise Exception("bucket:- %s not found" % bucket.name)
+            if self.op_types == "bucket":
+                bucketinfo = self.rest.get_bucket(bucket.name)
                 self.log.info("bucket info :- %s" % bucketinfo)
 
-
-
+        if self.op_types == "data":
+            if multi_nodes:
+                self._wait_for_stats_all_buckets(self.servers[:self.num_servers])
+                self._verify_all_buckets(self.master, 1, self.wait_timeout*50, None, False, 1)
+                self._verify_stats_all_buckets(self.servers[:self.num_servers])
+            else:
+                self._wait_for_stats_all_buckets([self.master])
+                self._verify_all_buckets(self.master, 1, self.wait_timeout*50, None, False, 1)
+                self._verify_stats_all_buckets([self.master])
--- pytests/newupgradetests.py
+++ pytests/newupgradetests.py
@@ -1,8 +1,6 @@
-import re
 import time
-import testconstants
-from builds.build_query import BuildQuery
 from newupgradebasetest import NewUpgradeBaseTest
+from remote.remote_util import RemoteMachineShellConnection
 
 class SingleNodeUpgradeTests(NewUpgradeBaseTest):
     def setUp(self):
@@ -11,71 +9,46 @@ class SingleNodeUpgradeTests(NewUpgradeBaseTest):
     def tearDown(self):
         super(SingleNodeUpgradeTests, self).tearDown()
 
-    def _get_build(self, master, version, is_amazon=False):
-        #log = logger.Logger.get_logger()
-        #remote = RemoteMachineShellConnection(master)
-        info = self.remote.extract_remote_info()
-        #remote.disconnect()
-        builds, changes = BuildQuery().get_all_builds()
-        self.log.info("finding build {0} for machine {1}".format(version, master))
-        result = re.search('r', version)
-
-        if result is None:
-            appropriate_build = BuildQuery().\
-                find_membase_release_build(self.product, info.deliverable_type,
-                    info.architecture_type, version.strip(), is_amazon=is_amazon)
-        else:
-            appropriate_build = BuildQuery().\
-                find_membase_build(builds, self.product, info.deliverable_type,
-                    info.architecture_type, version.strip(), is_amazon=is_amazon)
-
-        return appropriate_build
-
-    def _upgrade(self, upgrade_version):
-        appropriate_build = self._get_build(self.servers[0], upgrade_version)
-        self.assertTrue(appropriate_build.url, msg="unable to find build {0}".format(upgrade_version))
-        self.remote.download_build(appropriate_build)
-        self.remote.membase_upgrade(appropriate_build, save_upgrade_config=False)
-        self.rest_helper.is_ns_server_running(testconstants.NS_SERVER_TIMEOUT)
-        self.rest.init_cluster_port(self.rest_settings.rest_username, self.rest_settings.rest_password)
-        time.sleep(self.sleep_time)
-
-    def _install(self, initial_version='1.8.1-942-rel'):
-        self.remote.couchbase_uninstall()
-        builds, changes = BuildQuery().get_all_builds()
-        # check to see if we are installing from latestbuilds or releases
-        if re.search('r', initial_version):
-            builds, changes = BuildQuery().get_all_builds()
-            older_build = BuildQuery().find_membase_build(builds, deliverable_type=self.info.deliverable_type,
-                                                          os_architecture=self.info.architecture_type,
-                                                          build_version=initial_version,
-                                                          product=self.product, is_amazon=False)
-        else:
-            older_build = BuildQuery().find_membase_release_build(deliverable_type=self.info.deliverable_type,
-                                                                  os_architecture=self.info.architecture_type,
-                                                                  build_version=initial_version,
-                                                                  product=self.product, is_amazon=False)
-        self.remote.download_build(older_build)
-        #Installation ?
-        self.remote.membase_install(older_build)
-        ns_running = self.rest_helper.is_ns_server_running(testconstants.NS_SERVER_TIMEOUT)
-        self.assertTrue(ns_running, "NS_Server is not running")
-        self.rest.init_cluster_port(self.rest_settings.rest_username, self.rest_settings.rest_password)
-
     def test_upgrade(self):
-        # singlenode paramaterized install
-        initial_version = self.input.param('initial_version', '1.8.1-942-rel')
-        if initial_version.startswith("1.6") or initial_version.startswith("1.7"):
-            self.product = 'membase-server-enterprise'
+        if self.initial_version.startswith("1.6") or self.initial_version.startswith("1.7"):
+            self.product = 'membase-server'
         else:
-            self.product = 'couchbase-server-enterprise'
-        self._install(initial_version=initial_version)
+            self.product = 'couchbase-server'
+        self._install([self.master])
         self.operations()
-        upgrade_versions = self.input.param('upgrade_version', '1.8.1-942-rel')
+        upgrade_versions = self.input.param('upgrade_version', '2.0.0-1856-rel')
         upgrade_versions = upgrade_versions.split(";")
         self.log.info("Installation done going to sleep for %s sec", self.sleep_time)
         time.sleep(self.sleep_time)
         for upgrade_version in upgrade_versions:
-            self._upgrade(upgrade_version)
+            remote = RemoteMachineShellConnection(self.master)
+            self._upgrade(upgrade_version, self.master, remote)
+            remote.disconnect()
+            time.sleep(self.expire_time+5)
             self.verfication()
-        self.remote.disconnect()
+
+class MultiNodesUpgradeTests(NewUpgradeBaseTest):
+    def setUp(self):
+        super(MultiNodesUpgradeTests, self).setUp()
+
+    def tearDown(self):
+        super(MultiNodesUpgradeTests, self).tearDown()
+
+    def test_upgrade(self):
+        if self.initial_version.startswith("1.6") or self.initial_version.startswith("1.7"):
+            self.product = 'membase-server'
+        else:
+            self.product = 'couchbase-server'
+        self._install(self.servers)
+        self.operations(multi_nodes=True)
+        upgrade_versions = self.input.param('upgrade_version', '2.0.0-1856-rel')
+        upgrade_versions = upgrade_versions.split(";")
+        self.log.info("Installation done going to sleep for %s sec", self.sleep_time)
+        time.sleep(self.sleep_time)
+        for upgrade_version in upgrade_versions:
+            for server in self.servers:
+                remote = RemoteMachineShellConnection(server)
+                self._upgrade(upgrade_version, server, remote)
+                remote.disconnect()
+            time.sleep(self.expire_time+5)
+            self.verfication(multi_nodes=True)
