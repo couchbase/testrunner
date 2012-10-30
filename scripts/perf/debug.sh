9acf95cbfd596738833c5cd2f439873856dc345c
Pavel.Paulau <pavel.paulau@gmail.com>
2012-10-30 10:48:30 -0700

    MB-100: fix debug script
    Change-Id: I9fb4b294c249eb6f15fdec70b6de07aa540e6d5f
    Reviewed-on: http://review.couchbase.org/22091
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>
    Tested-by: Pavel Paulau <pavel.paulau@gmail.com>

--- scripts/perf/debug.sh
+++ scripts/perf/debug.sh
@@ -1,9 +1,9 @@
-if [ -z ${ini_file} ] ; then
+if [ "x$1" != "x" ] ; then
     echo "Collecting btree stats..."
     easy_install btrc > /dev/null
-    hostname=awk -F: '/\[servers\]/ { getline; print $0 }' ${ini_file} | cut -c 3-
+    hostname=`awk -F: '/\[servers\]/ { getline; print $0 }' $1 | cut -c 3-`
     btrc -n ${hostname}:8091
 
     echo "Collecting server info..."
-    python scripts/collect_server_info.py -i ${ini_file}
+    python scripts/collect_server_info.py -i $1
 fi
