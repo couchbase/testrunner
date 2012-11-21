f411bf55e3da009e36c5e4dee8a6202b6f4e9de4
Pavel.Paulau <pavel.paulau@gmail.com>
2012-11-21 16:27:05 +0300

    MB-100: print couchbase log in debug script
    Change-Id: I19d30aec88feb389dc79fe86f3540863987e3160
    Reviewed-on: http://review.couchbase.org/22714
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>
    Tested-by: Pavel Paulau <pavel.paulau@gmail.com>

--- scripts/perf/debug.sh
+++ scripts/perf/debug.sh
@@ -2,11 +2,16 @@ if [ "x$1" != "x" ] ; then
     echo "Collecting btree stats..."
     easy_install -U btrc > /dev/null
     hostname=`awk -F: '/\[servers\]/ { getline; print $0 }' $1 | cut -c 3-`
-    btrc -n ${hostname}:8091
+    btrc -n $hostname:8091
 
     echo "Collecting server info..."
     python scripts/collect_server_info.py -i $1 > /dev/null
 
     echo "Collecting atop stats..."
     python -m scripts.perf.grab_atops -i $1
+
+    echo "Couchbase server log..."
+    username=`grep rest_username $1 | awk -F: '{print $2}'`
+    password=`grep rest_password $1 | awk -F: '{print $2}'`
+    curl -m 2 $username:$password@$hostname:8091/logs 2> /dev/null | python -mjson.tool
 fi
