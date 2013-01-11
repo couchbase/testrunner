e699cd202e566ef36095df7a25ae89813172c3eb
andreibara <andrei.baranouski@gmail.com>
2013-01-11 20:09:27 +0400

    CBQE-927: offline_cluster_upgrade with rm manifest
    Change-Id: Ibac0cd5a95a0486979f9305f70cc9beb1451c7d5
    Reviewed-on: http://review.couchbase.org/23877
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Andrei Baranouski <andrei.baranouski@gmail.com>

--- pytests/newupgradetests.py
+++ pytests/newupgradetests.py
@@ -63,6 +63,10 @@ class MultiNodesUpgradeTests(NewUpgradeBaseTest):
                 remote = RemoteMachineShellConnection(server)
                 remote.stop_server()
                 time.sleep(self.sleep_time)
+                if self.input.param('remove_config_files', False):
+                    for file in ['manifest.txt', 'manifest.xml','VERSION.txt']:
+                        output, error = remote.execute_command("rm -rf /opt/couchbase/{0}".format(file))
+                        remote.log_command_output(output, error)
                 remote.disconnect()
             upgrade_threads = self._async_update(upgrade_version, upgrade_nodes)
             for upgrade_thread in upgrade_threads:
