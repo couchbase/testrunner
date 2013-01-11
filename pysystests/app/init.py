4ee5062fa8cc445ac99b365038861f799a186abe
Chisheng Hong <chisheng@couchbase.com>
2013-01-10 18:33:08 -0800

    CBQE-962 Clean up previous data in seriesly db  when celery starts
    When celery starts, we need to clean up previous data stored
    in seriesly database from last run.
    
    Change-Id: I80e0daf23f56d6cdc49d70abb56a95020fde7d84
    Reviewed-on: http://review.couchbase.org/23761
    Reviewed-by: Chisheng Hong <chisheng@couchbase.com>
    Tested-by: Chisheng Hong <chisheng@couchbase.com>

--- pysystests/app/init.py
+++ pysystests/app/init.py
@@ -2,6 +2,7 @@ import os
 from rabbit_helper import RabbitHelper
 from cache import CacheHelper
 import testcfg as cfg
+from seriesly import Seriesly
 
 # make sure logdir exists
 os.system("mkdir -p "+cfg.LOGDIR)
@@ -15,6 +16,14 @@ CacheHelper.cacheClean()
 kill_procs=["sdkserver"]
 for proc in kill_procs:
     os.system("ps aux | grep %s | awk '{print $2}' | xargs kill" % proc)
+
+# cleaning up seriesly database (fast and slow created by cbtop)
+os.system("curl -X DELETE http://{0}:3133/fast".format(cfg.SERIESLY_IP))
+os.system("curl -X DELETE http://{0}:3133/slow".format(cfg.SERIESLY_IP))
+os.system("curl -X DELETE http://{0}:3133/event".format(cfg.SERIESLY_IP))
+
+seriesly = Seriesly(cfg.SERIESLY_IP, 3133)
+seriesly.create_db('event')
 
 # start sdk server
 os.system("python sdkserver.py  &")
