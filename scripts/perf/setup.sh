44b7e316e5cc5c9ac21f205c1db3b9d53a65d7a1
pavel-paulau <pavel.paulau@gmail.com>
2013-01-15 11:37:04 +0300

    MB-100: check erlang_threads in setup script
    Change-Id: I07c5a33a7a26de7223b815aeccd9048312ffc949
    Reviewed-on: http://review.couchbase.org/23937
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>
    Tested-by: Pavel Paulau <pavel.paulau@gmail.com>

--- scripts/perf/setup.sh
+++ scripts/perf/setup.sh
@@ -1,5 +1,12 @@
 #!/bin/sh -ex
+erlang_threads=`grep erlang_threads ${test_conf} | awk -F"=" '{print $2}'`
+if [ -z ${erlang_threads} ]; then
+    ./bin/install -i ${ini_file} -p product=cb,version=${version_number},vbuckets=1024,parallel=True
+else
+    ./bin/install -i ${ini_file} -p product=cb,version=${version_number},vbuckets=1024,parallel=True,erlang_threads=${erlang_threads}
+fi
+
 mem_quota=`grep mem_quota ${test_conf} | awk -F"=" '{print $2}'`
-./bin/install -i ${ini_file} -p product=cb,version=${version_number},vbuckets=1024,parallel=True
 ./bin/do_cluster -i ${ini_file} -c ${test_conf} -p num_nodes=${num_nodes},mem_quota=${mem_quota} setUp
+
 ./bin/ssh -i ${ini_file} "swapoff -a && swapon -a"
