87576f210b408a0fa805acfe7037487d7cc80dd5
andreibara <andrei.baranouski@gmail.com>
2013-01-15 19:53:51 +0300

    CBQE-927: skip upgrade teardown if test failed
    Change-Id: I529edc7561b160b7758266b4b5d2642f6e558147
    Reviewed-on: http://review.couchbase.org/23954
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Andrei Baranouski <andrei.baranouski@gmail.com>

--- pytests/newupgradebasetest.py
+++ pytests/newupgradebasetest.py
@@ -45,22 +45,26 @@ class NewUpgradeBaseTest(BaseTestCase):
             self.product = 'couchbase-server'
 
     def tearDown(self):
-        try:
-            #cleanup only nodes that are in cluster
-            #not all servers have been installed
-            if self.rest is None:
-                self.rest = RestConnection(self.master)
-                self.rest_helper = RestHelper(self.rest)
-            nodes = self.rest.get_nodes()
-            temp = []
-            for server in self.servers:
-                if server.ip in [node.ip for node in nodes]:
-                    temp.append(server)
-            self.servers = temp
-        except Exception, e:
-            self.cluster.shutdown()
-            self.fail(e)
-        super(NewUpgradeBaseTest, self).tearDown()
+        if (hasattr(self, '_resultForDoCleanups') and len(self._resultForDoCleanups.failures or self._resultForDoCleanups.errors)):
+                self.log.warn("CLEANUP WAS SKIPPED DUE TO FAILURES IN UPGRADE TEST")
+                self.cluster.shutdown()
+        else:
+            try:
+                #cleanup only nodes that are in cluster
+                #not all servers have been installed
+                if self.rest is None:
+                    self.rest = RestConnection(self.master)
+                    self.rest_helper = RestHelper(self.rest)
+                nodes = self.rest.get_nodes()
+                temp = []
+                for server in self.servers:
+                    if server.ip in [node.ip for node in nodes]:
+                        temp.append(server)
+                self.servers = temp
+            except Exception, e:
+                self.cluster.shutdown()
+                self.fail(e)
+            super(NewUpgradeBaseTest, self).tearDown()
 
     def _install(self, servers):
         params = {}
