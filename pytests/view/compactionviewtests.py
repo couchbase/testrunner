7ceba0bc7f22fe3094e008354f77c719633bf4b9
Iryna <irynamironava@yandex.ru>
2013-01-21 12:46:54 +0300

    CBQE-984: add compaction test
    and also deleted test_start_compaction: all views had 1 map fn, now it is changed by test_multiply_compaction,cycles-num=1,ddocs_num=1 with diff map fns
    
    Change-Id: Ic93a00a7169de0be858480b88a703c935e6e99cb
    Reviewed-on: http://review.couchbase.org/24093
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Deepkaran Salooja <deepkaran.salooja@globallogic.com>

--- pytests/view/compactionviewtests.py
+++ pytests/view/compactionviewtests.py
@@ -1,6 +1,6 @@
 from view.view_base import ViewBaseTest
 from couchbase.documentgenerator import BlobGenerator
-from couchbase.document import View
+from couchbase.document import DesignDocument, View
 import time
 from tasks.future import TimeoutError
 
@@ -10,21 +10,25 @@ class CompactionViewTests(ViewBaseTest):
         super(CompactionViewTests, self).setUp()
         self.value_size = self.input.param("value_size", 256)
         self.fragmentation_value = self.input.param("fragmentation_value", 80)
+        self.ddocs_num = self.input.param("ddocs_num", 1)
+        self.view_per_ddoc = self.input.param("view_per_ddoc", 2)
+        self.use_dev_views = self.input.param("use_dev_views", False)
+        self.ddocs = []
+        # disable auto compaction
+        self.disable_compaction()
 
     def tearDown(self):
         super(CompactionViewTests, self).tearDown()
 
-    """Trigger Compaction When specified Fragmentation is reached"""
-    def test_start_compaction(self):
 
-        # disable auto compaction
-        self.disable_compaction()
+        """Trigger Compaction When specified Fragmentation is reached"""
+    def test_multiply_compaction(self):
+
+        cycles_num = self.input.param("cycles-num", 3)
 
         # create ddoc and add views
-        ddoc_name = "ddoc1"
-        views = self.make_default_views("test_add_views", 3)
-        server = self.servers[0]
-        self.create_views(server, ddoc_name, views)
+        self.make_ddocs(self.ddocs_num, self.view_per_ddoc)
+        self.create_ddocs()
 
         # load initial documents
         gen_load = BlobGenerator('test_view_compaction',
@@ -32,29 +36,42 @@ class CompactionViewTests(ViewBaseTest):
                                  self.value_size,
                                  end=self.num_items)
 
-        self._load_all_buckets(server, gen_load, "create", 0)
+        self._load_all_buckets(self.master, gen_load, "create", 0)
 
-        # start fragmentation monitor
-        fragmentation_monitor = \
-            self.cluster.async_monitor_view_fragmentation(server,
-                                                          ddoc_name,
-                                                          self.fragmentation_value,
-                                                          timeout = 20)
+        for i in xrange(cycles_num):
 
-        # generate load until fragmentation reached
-        while fragmentation_monitor.state != "FINISHED":
+            for ddoc in self.ddocs:
+                # start fragmentation monitor
+                fragmentation_monitor = \
+                    self.cluster.async_monitor_view_fragmentation(self.master,
+                                                                  ddoc.name,
+                                                                  self.fragmentation_value)
 
-            # update docs to create fragmentation
-            self._load_all_buckets(server, gen_load, "update", 0)
-            for view in views:
+                # generate load until fragmentation reached
+                while fragmentation_monitor.state != "FINISHED":
+                    # update docs to create fragmentation
+                    self._load_all_buckets(self.master, gen_load, "update", 0)
+                    for view in ddoc.views:
+                        # run queries to create indexes
+                        self.cluster.query_view(self.master, ddoc.name, view.name, {})
+                fragmentation_monitor.result()
 
-                # run queries to create indexes
-                self.cluster.query_view(server, ddoc_name, view.name, {})
-        fragmentation_monitor.result()
+            for ddoc in self.ddocs:
+                result = self.cluster.compact_view(self.master, ddoc.name)
+                self.assertTrue(result)
 
+    def make_ddocs(self, ddocs_num, views_per_ddoc):
+        ddoc_name = "compaction_ddoc"
+        view_name = "compaction_view"
+        for i in xrange(ddocs_num):
+            views = self.make_default_views(view_name, views_per_ddoc, different_map=True)
+            self.ddocs.append(DesignDocument(ddoc_name + str(i), views))
 
-        # compact ddoc and make sure fragmentation is less than high_mark
-        # will throw exception if failed
-        result = self.cluster.compact_view(server, ddoc_name)
-
-        self.assertTrue(result)
+    def create_ddocs(self, ddocs=None, bucket=None):
+        bucket_views = bucket or self.buckets[0]
+        ddocs_to_create = ddocs or self.ddocs
+        for ddoc in ddocs_to_create:
+            if not ddoc.views:
+                self.cluster.create_view(self.master, ddoc.name, [], bucket=bucket_views)
+            for view in ddoc.views:
+                self.cluster.create_view(self.master, ddoc.name, view, bucket=bucket_views)
