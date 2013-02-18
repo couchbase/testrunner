b194506a3337f01b1fe8edb645d267eda73ef517
abhinavdangeti <abhinav@couchbase.com>
2013-02-18 14:06:13 -0800

    CBQE-791: Initialize nodes/buckets - systests
    - Correction in accepting parameters for
    the number of sasl_buckets, etc.
    	- Convert number to string for testrunner to
    	read them as expected
    
    Change-Id: I2e9a09cf878b614706e6238fcbefe64c2d94404c
    Reviewed-on: http://review.couchbase.org/24673
    Reviewed-by: abhinav dangeti <abhinav.dangeti@gmail.com>
    Tested-by: abhinav dangeti <abhinav.dangeti@gmail.com>

--- pysystests/app/init.py
+++ pysystests/app/init.py
@@ -21,11 +21,11 @@ if cfg.CLUSTER:
     if 'setitup' in cfg.CLUSTER and cfg.CLUSTER['setitup']:
         function = "setitup"
         if 'default_bucket' in cfg.CLUSTER and cfg.CLUSTER['default_bucket'] == False:
-            function = function + ",default_bucket=" + cfg.CLUSTER['default_bucket']
+            function = function + ",default_bucket=" + str(cfg.CLUSTER['default_bucket'])
         if 'sasl_buckets' in cfg.CLUSTER and cfg.CLUSTER['sasl_buckets'] > 0:
-            function = function + ",sasl_buckets=" + cfg.CLUSTER['sasl_buckets']
+            function = function + ",sasl_buckets=" + str(cfg.CLUSTER['sasl_buckets'])
         if 'standard_buckets' in cfg.CLUSTER and cfg.CLUSTER['standard_buckets'] > 0:
-            function = function + ",standard_buckets=" + cfg.CLUSTER['standard_buckets']
+            function = function + ",standard_buckets=" + str(cfg.CLUSTER['standard_buckets'])
 
         if 'xdcr' in cfg.CLUSTER and cfg.CLUSTER['xdcr']:
             function = function + ",xdcr=True"
