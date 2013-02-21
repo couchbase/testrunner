e538f832c6f79ba45f2d2f4919937620e5d3a64f
abhinavdangeti <abhinav@couchbase.com>
2013-02-20 17:40:17 -0800

    CBQE-1060: 2.0.1 views use case json spec
    Change-Id: Id722f50c6fafeba44a621a669784039e9881f619
    Reviewed-on: http://review.couchbase.org/24764
    Reviewed-by: abhinav dangeti <abhinav.dangeti@gmail.com>
    Tested-by: abhinav dangeti <abhinav.dangeti@gmail.com>

--- pysystests/tests/201views.js
+++ pysystests/tests/201views.js
@@ -1,6 +1,6 @@
 {
 	"name" : "orange",
-	"desc" : "views_201_160,
+	"desc" : "views_201_160",
 	"loop" : false,
 	"phases" :{
 		"0" :
@@ -8,49 +8,49 @@
 			"name" : "load_init",
 			"desc" : "load 30M items",
 			"workload" : [{"spec" : "s:100,ccq:default1keys,ops:10000",
-							"conditions" : "post:curr_items > 40000000"},
-							{"spec" : "b:saslbucket,pwd:password,s:100,ccq:sasl1keys,ops:10000",
-							"conditions" : "post:curr_items > 30000000"}]
+					"conditions" : "post:curr_items > 40000000"},
+					{"spec" : "b:saslbucket,pwd:password,s:100,ccq:sasl1keys,ops:10000",
+					"conditions" : "post:curr_items > 30000000"}]
 		},
 		"1" :
 		{
 			"name" : "drain_disks_1",
 			"desc" : "draining the disks",
 			"workload" : [{"bucket" : "default", "template" : "default",
-							"spec" : "g:100,coq:defaultph1keys,ops:100",
-							"conditions" : "post:ep_queue_size < 100"},
-							{"bucket" : "saslbucket", "template" : "default",
-							"spec" : "g:100,coq:sasl1keys,ops:100",
-							"conditions" : "post:ep_queue_size < 100"}]
+					"spec" : "g:100,coq:defaultph1keys,ops:100",
+					"conditions" : "post:ep_queue_size < 100"},
+					{"bucket" : "saslbucket", "template" : "default",
+					"spec" : "g:100,coq:sasl1keys,ops:100",
+					"conditions" : "post:ep_queue_size < 100"}]
 		},
 		"2" :
 		{
 			"name" : "load_dgm",
 			"desc" :  "load 10M items",
 			"workload" : [{"spec" : "s:100,ccq:default2keys,ops:15000",
-							"conditions" : "post:curr_items > 80000000"},
-							{"spec" : "b:saslbucket,pwd:password,s:100,ccq:sasl2keys,ops:15000",
-							"conditions" : "post:curr_items > 65000000"}]
+					"conditions" : "post:curr_items > 80000000"},
+					{"spec" : "b:saslbucket,pwd:password,s:100,ccq:sasl2keys,ops:15000",
+					"conditions" : "post:curr_items > 65000000"}]
 		},
 		"3" :
 		{
 			"name" : "drain_disks_2",
 			"desc" : "draining the disks",
 			"workload" : [{"bucket" : "default", "template" : "default",
-							"spec" : "g:100,coq:defaultph2keys,ops:100",
-							"conditions" : "post:ep_queue_size < 100"},
-							{"bucket" : "saslbucket", "template" : "default",
-							"spec" : "g:100,coq:sasl2keys,ops:100",
-							"conditions" : "post:ep_queue_size < 100"}]
+					"spec" : "g:100,coq:defaultph2keys,ops:100",
+					"conditions" : "post:ep_queue_size < 100"},
+					{"bucket" : "saslbucket", "template" : "default",
+					"spec" : "g:100,coq:sasl2keys,ops:100",
+					"conditions" : "post:ep_queue_size < 100"}]
 		},
 		"4" :
 		{
 			"name" : "access_phase",
 			"desc" :  "prepare to create cachemiss",
 			"workload" : [{"bucket" : "default",
-							"template" : "default",
-							"spec" : "s:10,u:10,g:75,d:5,ccq:default2keys,ops:30000"},
-							{"spec" : "b:saslbucket,pwd:password,u:20,g:80,ccq:sasl2keys,ops:30000"}],
+					"template" : "default",
+					"spec" : "s:10,u:10,g:75,d:5,ccq:default2keys,ops:30000"},
+					{"spec" : "b:saslbucket,pwd:password,u:20,g:80,ccq:sasl2keys,ops:30000"}],
 			"runtime" : 3600
 		},
 		"5" :
@@ -58,9 +58,9 @@
 			"name" : "rebalance_in_one",
 			"desc" :  "RB1",
 			"workload" : [{"bucket" : "default",
-							"template" : "default",
-							"spec" : "s:10,u:25,g:50,d:10,e:5,ttl:60000,ccq:default2keys,ops:10000"},
-							{"spec" : "b:saslbucket,pwd:password,s:10,u:35,g:40,d:10,e:5,ttl:60000,ccq:sasl2keys,ops:10000"}],
+					"template" : "default",
+					"spec" : "s:10,u:25,g:50,d:10,e:5,ttl:60000,ccq:default2keys,ops:10000"},
+					{"spec" : "b:saslbucket,pwd:password,s:10,u:35,g:40,d:10,e:5,ttl:60000,ccq:sasl2keys,ops:10000"}],
 			"runtime" : 32000
 		},
 		"6" :
@@ -68,7 +68,7 @@
 			"name" : "restart_all",
 			"desc" :  "CR-3",
 			"workload" : [{"spec" : "u:100,coq:default2keys,ops:10000"},
-							{"spec" : "b:saslbucket,pwd:password,u:100,coq:sasl2keys,ops:10000"}],
+					{"spec" : "b:saslbucket,pwd:password,u:100,coq:sasl2keys,ops:10000"}],
 			"runtime": 100
 		}
 	}
