e8e4fd4f8dba976e9e70dc5516bbbeeb93efc445
Iryna <irynamironava@yandex.ru>
2013-03-04 12:27:15 +0300

    CBQE-1042: add rebalance to connection test
    Change-Id: I480cc30390f5c1dd371f67dbe8fcd3f3e6729a31
    Reviewed-on: http://review.couchbase.org/24966
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Andrei Baranouski <andrei.baranouski@gmail.com>

--- pytests/connectionstests.py
+++ pytests/connectionstests.py
@@ -16,8 +16,12 @@ class ConnectionTests(BaseTestCase):
     def create_connections_test(self):
         num_connections = self.input.param('num_connections', 1000)
         mem_usage_delta = self.input.param('mem_usage_max_delta', 5)
+        servers_in = self.input.param('servers_in', 0)
         process = self.input.param('process', 'beam.smp')
 
+        if servers_in:
+            servs_in = self.servers[1:servers_in + 1]
+            rebalance = self.cluster.async_rebalance(self.master, servs_in, [])
         shell = RemoteMachineShellConnection(self.master)
         initial_rate = shell.get_mem_usage_by_process(process)
 
@@ -37,6 +41,8 @@ class ConnectionTests(BaseTestCase):
                                                                    mem_usage_delta))
 
             self.log.info("Delta is inside %s" % mem_usage_delta)
+            if servers_in:
+                rebalance.result()
         finally:
             for connection in connections:
                 connection.join()
