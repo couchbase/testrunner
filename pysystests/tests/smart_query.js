190df15ec178c8fc8fc05404d9a63ede07154301
Tommie McAfee <tommie@couchbase.com>
2013-03-18 18:26:35 -0400

    CBQE-966: update smart_query test case
    contains changes made query workload code.
    using new 'idx' param to specify index key
    
    Change-Id: Ieff65a18c0e59de397b24d2157a4c2a5089df68c
    Reviewed-on: http://review.couchbase.org/25218
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Tommie McAfee <tommie@couchbase.com>

--- pysystests/tests/smart_query.js
+++ pysystests/tests/smart_query.js
@@ -5,39 +5,64 @@
     "phases" : {
                 "0" :
                 {
+                    "name" : "create_bucket",
+                    "desc" :  "create bucket",
+                    "buckets" : {"default" : {"quota": "3082", "replicas": "1"} }
+                },
+                "1" :
+                {
+                    "name" : "create_views",
+                    "desc" : "create_views",
+                    "ddocs" : {"create" : [{"ddoc": "ddoc1", "view": "view1", "bucket": "default", "dev": false, "map": "function (doc) { emit(doc.st, null);}"},
+                                    {"ddoc": "ddoc1", "view": "view2", "bucket": "default", "dev": false, "map": "function (doc) { emit(doc.num, null);}"}]}
+                },
+                "2" :
+                {
                     "name" : "simple_load",
-                    "desc" :  "load items at 1k ops",
-                    "workload" : "s:100,ccq:simplekeys,ops:1000",
+                    "desc" :  "load items at 5k ops",
+                    "workload" : "s:100,ccq:simplekeys,ops:15000",
                     "template" : "default",
                     "runtime" : 20 },
-                "1" :
+                "3" :
                 {
                     "name" : "query_access",
                     "desc" :  "run 200 queries/sec while accessing keys",
-                    "workload" : "s:15,g:80,d:5,ccq:simplekeys,ops:1000",
-                    "query" : "ddoc:ddoc1,view:view1,qps:200,idx:st",
-                    "runtime" : 40 },
-                "2" :
+                    "workload" : "s:15,g:80,d:5,ccq:simplekeys,ops:15000",
+                    "query" : "ddoc:ddoc1,view:view1,qps:30,idx:st",
+                    "runtime" : 60 },
+                "4" :
+                {
+                    "name" : "wait_for_query",
+                    "desc" : "wait_for_queries_to_finish",
+                    "workload" : "post:couch_views_ops < 10"
+                },
+                "5" :
                 {
                     "name" : "query_with_docid_limit50",
                     "desc" :  "run 200 queries/sec while accessing keys",
-                    "workload" : "s:15,g:80,d:5,ccq:simplekeys,ops:1000",
-                    "query" : "ddoc:ddoc1,view:view1,qps:200,limit:50,include:startkey_docid endkey_docid,idx:st",
-                    "runtime" : 40 },
-                "3" :
+                    "workload" : "s:15,g:80,d:5,ccq:simplekeys,ops:15000",
+                    "query" : "ddoc:ddoc1,view:view1,qps:30,limit:50,include:startkey_docid endkey_docid,idx:st",
+                    "runtime" : 60 },
+                "6" :
                 {
                     "name" : "query_with_magicvals",
                     "desc" :  "run 200 queries/sec while accessing keys",
-                    "workload" : "s:15,g:80,d:5,ccq:simplekeys,ops:1000",
-                    "query" : "ddoc:ddoc1,view:view1,qps:200,limit:50,start:$str4,end:$str4",
-                    "runtime" : 40 },
-                "4" : {
+                    "workload" : "s:15,g:80,d:5,ccq:simplekeys,ops:15000",
+                    "query" : "ddoc:ddoc1,view:view1,qps:30,limit:50,start:$str4,end:$str4",
+                    "runtime" : 60 },
+                "7" :
+                {
+                    "name" : "wait_for_query",
+                    "desc" : "wait_for_queries_to_finish",
+                    "workload" : "post:couch_views_ops < 10"
+                },
+                "8" : {
                     "name" : "query_multi_view",
                     "desc" :  "run 200 queries/sec while accessing keys",
-                    "workload" : "s:15,g:80,d:5,ccq:simplekeys,ops:1000",
-                    "query" : ["ddoc:ddoc1,view:view1,qps:40,idx:st",
-                               "ddoc:ddoc1,view:view2,qps:50,idx:num"],
+                    "workload" : "s:15,g:80,d:5,ccq:simplekeys,ops:15000",
+                    "query" : ["ddoc:ddoc1,view:view1,qps:20,idx:st",
+                               "ddoc:ddoc1,view:view2,qps:20,idx:num"],
                     "template" : "default",
-                    "runtime" : 40 }
+                    "runtime" : 60 }
         }
 }
