5fe32047a735aa9dc734150e7c90cf7a5a12dfbd
pavel-paulau <pavel.paulau@gmail.com>
2013-03-27 12:24:57 +0300

    MB-100: fix formatting of graph poster script
    Change-Id: Ie2e1b7d3b83e64f2334dd933cbc96d0a4975baed
    Reviewed-on: http://review.couchbase.org/25351
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>
    Tested-by: Pavel Paulau <pavel.paulau@gmail.com>

--- scripts/perf/graph_poster.py
+++ scripts/perf/graph_poster.py
@@ -40,13 +40,13 @@ class DocGenerator:
         print self.to_string()
 
         doc = {"filename": self._filename,
-                "test": self._test,
-                "create_time": self._time,
-                "category": self._category,
-                "base_build": self._base,
-                "target_build": self._target,
-                "run_id": self._run_id,
-                "upload_time": self._unix_time}
+               "test": self._test,
+               "create_time": self._time,
+               "category": self._category,
+               "base_build": self._base,
+               "target_build": self._target,
+               "run_id": self._run_id,
+               "upload_time": self._unix_time}
 
         return doc
 
@@ -55,9 +55,10 @@ class DocGenerator:
 
     def to_string(self):
         return "filename: {0}, test: {1}, base_build = {2}, " \
-               "target_build = {3}, run_id = {4}, time = {5}, category = {6}"\
-                .format(self._filename, self._test, self._base,
-                        self._target, self._run_id, self._time, self._category)
+               "target_build = {3}, run_id = {4}, time = {5}, category = {6}" \
+               .format(self._filename, self._test, self._base,
+                       self._target, self._run_id, self._time, self._category)
+
 
 class GraphPoster:
 
@@ -72,24 +73,26 @@ class GraphPoster:
         if self.doc:
             self.res = self._db.save_doc(self.doc, force_update=True)
             if self.res:
-                print "posted pdf to couchdb : filename = {0}, doc_id = {1}, rev = {2} "\
+                print "posted pdf to couchdb: " + \
+                    "filename = {0}, doc_id = {1}, rev = {2} " \
                     .format(filename, self.res["id"], self.res["rev"])
 
             with open(filename, 'r') as f:
-                self._db.put_attachment(self.doc, f, filename, "application/pdf")
+                self._db.put_attachment(self.doc, f, filename,
+                                        "application/pdf")
 
 
 if __name__ == "__main__":
 
     parser = OptionParser()
-    parser.add_option("-n", "--node", dest="node", default="http://127.0.0.1:5984",
+    parser.add_option("-n", "--node", dest="node",
+                      default="http://127.0.0.1:5984",
                       help="couchdb ip:port , defaults to 127.0.0.1:5984")
-    parser.add_option("-d", "--database",
-                      dest="db", help="db name in couchdb",
-                      default="eperf-graphs")
-    parser.add_option("-f", "--filename",
-                      dest="filename", help="pdf file to be posted to couchdb",
-                      default="test.pdf")
+    parser.add_option("-d", "--database", dest="db",
+                      default="eperf-graphs", help="db name in couchdb")
+    parser.add_option("-f", "--filename", dest="filename",
+                      default="test.pdf",
+                      help="pdf file to be posted to couchdb")
     options, args = parser.parse_args()
 
     poster = GraphPoster(options.node, options.db)
