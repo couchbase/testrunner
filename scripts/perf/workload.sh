67397008c05f5a82ec68fd2558782756469c212e
pavel-paulau <pavel.paulau@gmail.com>
2013-04-09 11:55:59 +0300

    MB-100: disable stats collection during load and index phases
    Change-Id: Id5b53873ebfaac09b4c7bf3fbf97b64ec037000e
    Reviewed-on: http://review.couchbase.org/25559
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>
    Tested-by: Pavel Paulau <pavel.paulau@gmail.com>

--- scripts/perf/workload.sh
+++ scripts/perf/workload.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ex
-./bin/testrunner -i ${ini_file} -c ${test_conf} -p load_phase=1
-./bin/testrunner -i ${ini_file} -c ${test_conf} -p load_phase=1,hot_load_phase=1
-./bin/testrunner -i ${ini_file} -c ${test_conf} -p index_phase=1
+./bin/testrunner -i ${ini_file} -c ${test_conf} -p load_phase=1,stats=0
+./bin/testrunner -i ${ini_file} -c ${test_conf} -p load_phase=1,hot_load_phase=1,stats=0
+./bin/testrunner -i ${ini_file} -c ${test_conf} -p index_phase=1,stats=0
 ./bin/testrunner -i ${ini_file} -c ${test_conf} -p access_phase=1
 ./bin/testrunner -i ${ini_file} -c ${test_conf} -p incr_index_phase=1,stats=0
