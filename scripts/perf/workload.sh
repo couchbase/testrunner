d9ac603e26158c027c7c917a2e2c0175b56f6f06
pavel-paulau <pavel.paulau@gmail.com>
2013-04-09 12:11:39 +0300

    MB-100: explicitly define phase matrix
    Change-Id: I5375b8602bce9e7b4559a43dc101b1bbb82e6c28
    Reviewed-on: http://review.couchbase.org/25561
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>
    Tested-by: Pavel Paulau <pavel.paulau@gmail.com>

--- scripts/perf/workload.sh
+++ scripts/perf/workload.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -ex
-./bin/testrunner -i ${ini_file} -c ${test_conf} -p load_phase=1,stats=0
-./bin/testrunner -i ${ini_file} -c ${test_conf} -p load_phase=1,hot_load_phase=1,stats=0
-./bin/testrunner -i ${ini_file} -c ${test_conf} -p index_phase=1,stats=0
-./bin/testrunner -i ${ini_file} -c ${test_conf} -p access_phase=1
+./bin/testrunner -i ${ini_file} -c ${test_conf} -p load_phase=1,hot_load_phase=0,index_phase=0,access_phase=0,stats=0
+./bin/testrunner -i ${ini_file} -c ${test_conf} -p load_phase=1,hot_load_phase=1,index_phase=0,access_phase=0,stats=0
+./bin/testrunner -i ${ini_file} -c ${test_conf} -p load_phase=0,hot_load_phase=0,index_phase=1,access_phase=0,stats=0
+./bin/testrunner -i ${ini_file} -c ${test_conf} -p load_phase=0,hot_load_phase=0,index_phase=0,access_phase=1,stats=1
