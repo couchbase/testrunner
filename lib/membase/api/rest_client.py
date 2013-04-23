fc637cf7022b212a305cbdd3dffe7344283d01a5
Iryna <irynamironava@yandex.ru>
2013-04-23 09:24:04 -0700

    CBQE-0: adopt tests for dp4 builds
    In dp4 builds meta is stored in request content, not in header
    
    Change-Id: Ib7442c2e33f21fbcc18ec6a0cd1a8d076231924f
    Reviewed-on: http://review.couchbase.org/25846
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Andrei Baranouski <andrei.baranouski@gmail.com>

--- lib/membase/api/rest_client.py
+++ lib/membase/api/rest_client.py
@@ -562,8 +562,14 @@ class RestConnection(object):
         json_parsed = json.loads(content)
         meta_parsed = ""
         if status:
-            meta = header['x-couchbase-meta']
-            meta_parsed = json.loads(meta)
+            #in dp4 builds meta data is in content, not in header
+            if 'x-couchbase-meta' in header:
+                meta = header['x-couchbase-meta']
+                meta_parsed = json.loads(meta)
+            else:
+                meta_parsed = {}
+                meta_parsed["_rev"] = json_parsed["_rev"]
+                meta_parsed["_id"] = json_parsed["_id"]
         return status, json_parsed, meta_parsed
 
     def _delete_design_doc(self, bucket, name):
