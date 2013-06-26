4d3c39fb51d0e2c605bebe8565bd85b06f451ec6
Chisheng Hong <chisheng@couchbase.com>
2013-06-26 12:36:24 -0700

    CBQE-1343 Improve cbdocloader verification
    Change-Id: I9e961657446bd5ba143ce296ee932ad47eb7f07f
    Reviewed-on: http://review.couchbase.org/27148
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Chisheng Hong <chisheng@couchbase.com>

--- pytests/clitest/docloadertest.py
+++ pytests/clitest/docloadertest.py
@@ -2,6 +2,7 @@ from clitest.cli_base import CliBaseTest
 from membase.api.rest_client import RestConnection, Bucket
 from couchbase.cluster import Cluster
 import testconstants
+import json
 
 class docloaderTests(CliBaseTest):
 
@@ -9,6 +10,13 @@ class docloaderTests(CliBaseTest):
         super(docloaderTests, self).setUp()
         self.load_filename = self.input.param("filename", "gamesim-sample")
         self.memory_quota = self.input.param("memory_quota", 100)
+        info = self.shell.extract_remote_info()
+        type = info.type.lower()
+        self.os = 'linux'
+        if type == 'windows':
+            self.os = "windows"
+        if info.distribution_type.lower() == 'mac':
+            self.os = 'mac'
 
     def tearDown(self):
         super(docloaderTests, self).tearDown()
@@ -24,17 +32,22 @@ class docloaderTests(CliBaseTest):
         for bucket in self.buckets:
             output, error = self.shell.execute_cbdocloader(self.couchbase_usrname, self.couchbase_password,
                                                            bucket.name, self.memory_quota, self.load_filename)
-            info = self.shell.extract_remote_info()
-            type = info.type.lower()
-            if type != "windows" and info.distribution_type.lower() != 'mac':
-                if len(error) > 0:
-                    raise Exception("Command throw out error message. Please check the output of remote_util")
-                for output_line in output:
-                    if output_line.find("ERROR") >= 0 or output_line.find("error") >= 0:
-                        raise Exception("Command throw out error message. Please check the output of remote_util")
+
 
         self._wait_for_stats_all_buckets(self.servers[:self.num_servers])
+
+        self.shell.delete_files(self.load_filename)
+
+        if self.os != "windows":
+            command = "unzip %ssamples/%s.zip" % (testconstants.LINUX_CB_PATH, self.load_filename)
+            if self.os == 'mac':
+                command = "unzip %ssamples/%s.zip" % (testconstants.MAC_CB_PATH, self.load_filename)
+            output, error = self.shell.execute_command(command.format(command))
+            self.shell.log_command_output(output, error)
+
         self.verify_results(self.load_filename)
+        self.verify_ddoc(self.load_filename)
+        self.shell.delete_files(self.load_filename)
 
     def verify_results(self, file):
         stats_tasks = []
@@ -60,23 +73,7 @@ class docloaderTests(CliBaseTest):
             task.result(60)
 
     def get_number_of_files(self, file):
-        os = "linux"
-        zip_file = "%s.zip" % (file)
-        self.shell.delete_files(file)
-        info = self.shell.extract_remote_info()
-        type = info.type.lower()
-        if type == 'windows':
-            os = "windows"
-        if info.distribution_type.lower() == 'mac':
-            os = 'mac'
-
-        if os != "windows":
-            command = "unzip %ssamples/%s.zip" % (testconstants.LINUX_CB_PATH, file)
-            if os == 'mac':
-                command = "unzip %ssamples/%s.zip" % (testconstants.MAC_CB_PATH, file)
-            output, error = self.shell.execute_command(command.format(command))
-            self.shell.log_command_output(output, error)
-
+        if self.os != "windows":
             command = "find %s/ -name *.json | wc -l" % (file)
             output, error = self.shell.execute_command(command.format(command))
             self.shell.log_command_output(output, error)
@@ -95,13 +92,38 @@ class docloaderTests(CliBaseTest):
             else:
                 b = int(output[0])
             number_of_items = a - b  #design doc create views not items in cluster
-            self.shell.delete_files(file)
+
             return number_of_items
 
-        elif os == "windows":
+        elif self.os == "windows":
             if file == "gamesim-sample":
                 return 586
             elif file == "beer-sample":
                 return 7303
             else:
                 raise Exception("Sample file %s.zip doesn't exists" % (file))
+
+    def verify_ddoc(self, file):
+        rest = RestConnection(self.master)
+        ddoc_names = self.get_ddoc_names(file)
+        for bucket in self.buckets:
+            for ddoc_name in ddoc_names:
+                ddoc_json, header = rest.get_ddoc(bucket, ddoc_name)
+                if ddoc_json is not None:
+                    self.log.info('Database Document {0} details : {1}'.format(ddoc_name, json.dumps(ddoc_json)))
+                else:
+                    raise Exception("ddoc %s is not imported" % ddoc_name)
+
+    def get_ddoc_names(self, file):
+        if self.os == 'windows':
+            return []
+
+        ddoc_names = []
+        command = "find %s/design_docs/ -name *.json | cut -d \"/\" -f3" % (file)
+        output, error = self.shell.execute_command(command.format(command))
+        self.shell.log_command_output(output, error)
+
+        for line in output:
+            ddoc_names.append(line.split(".")[0])
+
+        return ddoc_names
