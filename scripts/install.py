0e70fabca822ad73353922c2c2d07467fac20021
Iryna <irynamironava@yandex.ru>
2013-07-05 08:51:05 -0700

    CBQE-0: change timeout for getting build
    Change-Id: If2a7e54a173a6f27503c1eed1ad6576b9cb602b9
    Reviewed-on: http://review.couchbase.org/27282
    Reviewed-by: Iryna Mironava <irynamironava@yandex.ru>
    Tested-by: Iryna Mironava <irynamironava@yandex.ru>

--- scripts/install.py
+++ scripts/install.py
@@ -175,7 +175,10 @@ class Installer(object):
         info = remote_client.extract_remote_info()
         remote_client.disconnect()
         if ok:
-            builds, changes = BuildQuery().get_all_builds()
+            timeout = None
+            if "timeout" in params:
+                timeout = int(params["timeout"])
+            builds, changes = BuildQuery().get_all_builds(timeout=timeout)
             releases_version = ["1.6.5.4", "1.7.0", "1.7.1", "1.7.1.1", "1.8.0"]
             for name in names:
                 if version in releases_version:
