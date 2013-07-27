24411061e8f4160ce33ab84502a038cf1c094909
pavel-paulau <pavel.paulau@gmail.com>
2013-07-27 21:04:34 +0300

    MB-100: add missing hostname variable
    Change-Id: I9fbc7afa6e99d2a7fd006dd89479f4de495656fa
    Reviewed-on: http://review.couchbase.org/27701
    Reviewed-by: Pavel Paulau <pavel.paulau@gmail.com>
    Tested-by: Pavel Paulau <pavel.paulau@gmail.com>

--- scripts/perf/debug.sh
+++ scripts/perf/debug.sh
@@ -6,6 +6,7 @@ if [ "x$1" != "x" ] ; then
     python -m scripts.perf.grab_atops -i $1
 
     echo "Couchbase server log..."
+    hostname=`awk -F: '/\[servers\]/ { getline; print $0 }' $1 | cut -c 3-`
     username=`grep rest_username $1 | awk -F: '{print $2}'`
     password=`grep rest_password $1 | awk -F: '{print $2}'`
     curl -m 2 $username:$password@$hostname:8091/logs 2> /dev/null | python -mjson.tool
