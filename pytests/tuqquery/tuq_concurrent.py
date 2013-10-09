82bfb7a87fac626c2375a2a2db1bc30274f595da
IrynaMironava <irynamironava@yandex.ru>
2013-10-09 15:56:34 +0300

    CBQE-1531: add concurrent queries test for tuq
    Change-Id: If5bba42ddaabc139d857179933ea788579ff5d02
    Reviewed-on: http://review.couchbase.org/29411
    Reviewed-by: Ketaki Gangal <ketakigangal@gmail.com>
    Tested-by: Iryna Mironava <irynamironava@yandex.ru>

---
+++ pytests/tuqquery/tuq_concurrent.py
@@ -0,0 +1,60 @@
+import threading
+from threading import Event
+from tuqquery.tuq import QueryTests
+from viewquerytests import StoppableThread
+
+class ConcurrentTests(QueryTests):
+    def setUp(self):
+        super(ConcurrentTests, self).setUp()
+        self.thread_crashed = Event()
+        self.thread_stopped = Event()
+        self.num_threads = self.input.param("num_threads", 4)
+        self.test_to_run = self.input.param("test_to_run", "test_simple_check")
+
+    def suite_setUp(self):
+        super(ConcurrentTests, self).suite_setUp()
+
+    def tearDown(self):
+        super(ConcurrentTests, self).tearDown()
+
+    def suite_tearDown(self):
+        super(ConcurrentTests, self).suite_tearDown()
+
+    def test_concurrent_queries(self):
+        query_threads = []
+        for n in xrange(self.num_threads):
+            t = StoppableThread(target=self.query_thread,
+                name="query-{0}".format(n),
+                args=(self.test_to_run,))
+            query_threads.append(t)
+            t.start()
+
+        while True:
+            if not query_threads:
+                break
+            self.thread_stopped.wait(60)
+            if self.thread_crashed.is_set():
+                self.log.error("Will stop all threads!")
+                for t in query_threads:
+                    t.stop()
+                    self.log.error("Thread %s stopped" % str(t))
+                break
+            else:
+                query_threads = [d for d in query_threads if d.is_alive()]
+                self.log.info("Current amount of threads %s" % len(query_threads))
+                self.thread_stopped.clear()
+        if self.thread_crashed.is_set():
+            self.fail("Test failed, see logs above!")
+
+    def query_thread(self, method_name):
+        try:
+            fn = getattr(self, method_name)
+            fn()
+        except Exception as ex:
+            self.log.error("***************ERROR*************\n" +\
+                           "At least one of query threads is crashed: {0}".format(ex))
+            self.thread_crashed.set()
+            raise ex
+        finally:
+            if not self.thread_stopped.is_set():
+                self.thread_stopped.set()
