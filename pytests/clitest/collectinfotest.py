4f4831ab010390f7a37b15169cf087df90e98cf0
Shashank Gupta1 <shashank.gupta1@globallogic.com>
2013-12-16 06:09:19 +0000

    Revert "CBQE-1432 - Automate test cases in cbcollectinfo test plan (xdcr and views)."
    This reverts commit d492c66c409fcc0a05a448771582f32c273da287.
    
    Change-Id: I8c4c6dd63c8f3f6ef6e6a9f20d7bfdb7ab6fbd32
    Reviewed-on: http://review.couchbase.org/31128
    Reviewed-by: Shashank Gupta1 <shashank.gupta1@globallogic.com>
    Tested-by: Shashank Gupta1 <shashank.gupta1@globallogic.com>

--- pytests/clitest/collectinfotest.py
+++ pytests/clitest/collectinfotest.py
@@ -4,7 +4,6 @@ from membase.api.rest_client import RestConnection, Bucket
 import testconstants
 import time
 import logger
-from couchbase.document import View
 
 LOG_FILE_NAME_LIST = ["couchbase.log", "diag.log", "ddocs.log", "ini.log", "syslog.tar.gz",
                       "ns_server.couchdb.log", "ns_server.debug.log", "ns_server.babysitter.log",
@@ -14,7 +13,6 @@ LOG_FILE_NAME_LIST = ["couchbase.log", "diag.log", "ddocs.log", "ini.log", "sysl
                       "ns_server.stats.log", "ns_server.xdcr_errors.log",
                       "ns_server.xdcr.log"]
 
-
 class CollectinfoTests(CliBaseTest):
 
     def setUp(self):
@@ -22,7 +20,6 @@ class CollectinfoTests(CliBaseTest):
         self.log_filename = self.input.param("filename", "info")
         self.doc_ops = self.input.param("doc_ops", None)
         self.expire_time = self.input.param("expire_time", 5)
-        self.value_size = self.input.param("value_size", 256)
         self.node_down = self.input.param("node_down", False)
         if self.doc_ops is not None:
             self.doc_ops = self.doc_ops.split(";")
@@ -56,7 +53,7 @@ class CollectinfoTests(CliBaseTest):
         self._wait_for_stats_all_buckets(self.servers[:self.num_servers])
 
         self.shell.delete_files("%s.zip" % (self.log_filename))
-        self.shell.delete_files("cbcollect_info*")  # This is the folder generated after unzip the log package
+        self.shell.delete_files("cbcollect_info*")  #This is the folder generated after unzip the log package
 
         if self.node_down:
             if self.os == 'linux':
@@ -71,7 +68,7 @@ class CollectinfoTests(CliBaseTest):
             for output_line in output:
                 if output_line.find("ERROR") >= 0 or output_line.find("Error") >= 0:
                     raise Exception("Command throw out error message. Please check the output of remote_util")
-        self.verify_results(self, self.log_filename)
+        self.verify_results(self.log_filename)
 
         if self.node_down:
             if self.os == 'linux':
@@ -80,98 +77,76 @@ class CollectinfoTests(CliBaseTest):
                 time.sleep(self.wait_timeout)
 
 
-    @staticmethod
-    def verify_results(self, output_file_name):
-        try:
-            os = "linux"
-            zip_file = "%s.zip" % (output_file_name)
-            info = self.shell.extract_remote_info()
-            type = info.type.lower()
-            if type == 'windows':
-                os = "windows"
+    def verify_results(self, file):
+        os = "linux"
+        zip_file = "%s.zip" % (file)
+        info = self.shell.extract_remote_info()
+        type = info.type.lower()
+        if type == 'windows':
+            os = "windows"
 
-            if os == "linux":
-                command = "unzip %s" % (zip_file)
-                output, error = self.shell.execute_command(command.format(command))
-                self.shell.log_command_output(output, error)
-                if len(error) > 0:
-                    raise Exception("unable to unzip the files. Check unzip command output for help")
+        if os == "linux":
+            command = "unzip %s" % (zip_file)
+            output, error = self.shell.execute_command(command.format(command))
+            self.shell.log_command_output(output, error)
+            if len(error) > 0:
+                raise Exception("uable to unzip the files. Check unzip command output for help")
 
-                command = "ls cbcollect_info*/"
-                output, error = self.shell.execute_command(command.format(command))
-                self.shell.log_command_output(output, error)
-                if len(error) > 0:
-                    raise Exception("unable to list the files. Check ls command output for help")
-                missing_logs = False
-                for x in LOG_FILE_NAME_LIST:
-                    find_log = False
-                    for output_line in output:
-                        if output_line.find(x) >= 0:
-                            find_log = True
-                    if not find_log:
-                        # missing syslog.tar.gz in mac as in ticket MB-9110
-                        # need to remove 3 lines below if it is fixed in 2.2.1
-                        # in mac os
-                        if x == "syslog.tar.gz" and info.distribution_type.lower() == "mac":
-                            missing_logs = False
-                        else:
-                            missing_logs = True
-                            self.log.error("The log zip file miss %s" % (x))
-
-                missing_buckets = False
-                if not self.node_down:
-                    for bucket in self.buckets:
-                        command = "grep %s cbcollect_info*/stats.log" % (bucket.name)
-                        output, error = self.shell.execute_command(command.format(command))
-                        self.shell.log_command_output(output, error)
-                        if len(error) > 0:
-                            raise Exception("unable to grep key words. Check grep command output for help")
-                        if len(output) == 0:
-                            missing_buckets = True
-                            self.log.error("%s stats are missed in stats.log" % (bucket.name))
-
-                command = "du -s cbcollect_info*/*"
-                output, error = self.shell.execute_command(command.format(command))
-                self.shell.log_command_output(output, error)
-                empty_logs = False
-                if len(error) > 0:
-                    raise Exception("unable to list file size. Check du command output for help")
+            command = "ls cbcollect_info*/"
+            output, error = self.shell.execute_command(command.format(command))
+            self.shell.log_command_output(output, error)
+            if len(error) > 0:
+                raise Exception("uable to list the files. Check ls command output for help")
+            missing_logs = False
+            for x in LOG_FILE_NAME_LIST:
+                find_log = False
                 for output_line in output:
-                    output_line = output_line.split()
-                    file_size = int(output_line[0])
-                    if file_size == 0:
-                        empty_logs = True
-                        self.log.error("%s is empty" % (output_line[1]))
+                    if output_line.find(x) >= 0:
+                        find_log = True
+                if not find_log:
+                    # missing syslog.tar.gz in mac as in ticket MB-9110
+                    # need to remove 3 lines below if it is fixed in 2.2.1
+                    # in mac os
+                    if x == "syslog.tar.gz" and info.distribution_type.lower() == "mac":
+                       missing_logs = False
+                    else:
+                       missing_logs = True
+                       self.log.error("The log zip file miss %s" % (x))
 
-                if missing_logs:
-                    raise Exception("Bad log file package generated. Missing logs")
-                if missing_buckets:
-                    raise Exception("Bad stats.log which miss some bucket information")
-                if empty_logs:
-                    raise Exception("Collect empty log files")
-            elif os == "windows":
-                # try to figure out what command works for windows for verification
-                pass
+            missing_buckets = False
+            if not self.node_down:
+                for bucket in self.buckets:
+                    command = "grep %s cbcollect_info*/stats.log" % (bucket.name)
+                    output, error = self.shell.execute_command(command.format(command))
+                    self.shell.log_command_output(output, error)
+                    if len(error) > 0:
+                        raise Exception("uable to grep key words. Check grep command output for help")
+                    if len(output) == 0:
+                        missing_buckets = True
+                        self.log.error("%s stats are missed in stats.log" % (bucket.name))
 
-        finally:
-            self.shell.delete_files(zip_file)
-            self.shell.delete_files("cbcollect_info*")
+            command = "du -s cbcollect_info*/*"
+            output, error = self.shell.execute_command(command.format(command))
+            self.shell.log_command_output(output, error)
+            empty_logs = False
+            if len(error) > 0:
+                raise Exception("uable to list file size. Check du command output for help")
+            for output_line in output:
+                output_line = output_line.split()
+                file_size = int(output_line[0])
+                if file_size == 0:
+                    empty_logs = True
+                    self.log.error("%s is empty" % (output_line[1]))
 
-    def collectinfo_test_for_views(self):
-        self.default_design_doc_name = "Doc1"
-        self.view_name = self.input.param("view_name", "View")
-        self.generate_map_reduce_error = self.input.param("map_reduce_error", False)
-        self.default_map_func = 'function (doc) { emit(doc.age, doc.first_name);}'
-        self.gen_load = BlobGenerator('couch', 'cb-', self.value_size, end=self.num_items)
-        self._load_all_buckets(self.master, self.gen_load, "create", 0)
-        self.reduce_fn = "_count"
-        if self.generate_map_reduce_error:
-            self.reduce_fn = "_sum"
-        view = View(self.view_name, self.default_map_func, self.reduce_fn, dev_view=False)
-        self.cluster.create_view(
-                    self.master, self.default_design_doc_name, view,
-                    'default', self.wait_timeout * 2)
-        query = {"stale": "false", "connection_timeout": 60000}
-        self.cluster.query_view(self.master, self.default_design_doc_name, self.view_name, query, self.num_items, 'default')
-        self.shell.execute_cbcollect_info("%s.zip" % (self.log_filename))
-        self.verify_results(self, self.log_filename)
+            if missing_logs:
+                raise Exception("Bad log file package generated. Missing logs")
+            if missing_buckets:
+                raise Exception("Bad stats.log which miss some bucket information")
+            if empty_logs:
+                raise Exception("Collect empty log files")
+        elif os == "windows":
+            # try to figure out what command works for windows for verification
+            pass
+
+        self.shell.delete_files(zip_file)
+        self.shell.delete_files("cbcollect_info*")
