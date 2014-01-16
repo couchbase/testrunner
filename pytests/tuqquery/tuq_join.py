ebd4ee10bf3ba3544c902a85d3779efcd42b6efb
IrynaMironava <irynamironava@yandex.ru>
2014-01-16 15:16:14 +0300

    CBQE-1843: tuq: add tests for left outer join
    Change-Id: I64097ef13dd0d204c5960abdbb732f2426e13572
    Reviewed-on: http://review.couchbase.org/32551
    Reviewed-by: Iryna Mironava <irynamironava@yandex.ru>
    Tested-by: Iryna Mironava <irynamironava@yandex.ru>

--- pytests/tuqquery/tuq_join.py
+++ pytests/tuqquery/tuq_join.py
@@ -4,14 +4,15 @@ from tuqquery.tuq import QueryTests
 from couchbase.documentgenerator import DocumentGenerator
 
 
-JOIN_INNER = "inner"
-JOIN_LEFT = "left"
-JOIN_RIGHT = "right"
+JOIN_INNER = "INNER"
+JOIN_LEFT = "LEFT"
+JOIN_RIGHT = "RIGHT"
 
 class JoinTests(QueryTests):
     def setUp(self):
         super(JoinTests, self).setUp()
         self.gens_tasks = self.generate_docs_tasks()
+        self.type_join = self.input.param("type_join", JOIN_INNER)
 
     def suite_setUp(self):
         super(JoinTests, self).suite_setUp()
@@ -26,85 +27,86 @@ class JoinTests(QueryTests):
     def test_simple_join_keys(self):
         for bucket in self.buckets:
             self.query = "SELECT employee.name, employee.tasks_ids, new_project " +\
-            "FROM %s as employee JOIN default.project as new_project " % bucket.name +\
+            "FROM %s as employee %s JOIN default.project as new_project " % (bucket.name, self.type_join) +\
             "KEYS employee.tasks_ids"
             actual_result = self.run_cbq_query()
-            actual_result = sorted(actual_result['resultset'], key=lambda doc:(
-                                                               doc['name'], doc['new_project']))
-            expected_result = self._generate_full_joined_docs_list()
-            expected_result = [{"name" : doc['name'], "tasks_ids" : doc['tasks_ids'],
-                                "new_project" : doc['project']}
-                               for doc in expected_result]
-            expected_result = sorted(expected_result, key=lambda doc:(
-                                                          doc['name'], doc['new_project']))
+            actual_result = sorted(actual_result['resultset'])
+            full_list = self._generate_full_joined_docs_list(join_type=self.type_join)
+            expected_result = [doc for doc in full_list if not doc]
+            expected_result.extend([{"name" : doc['name'], "tasks_ids" : doc['tasks_ids'],
+                                     "new_project" : doc['project']}
+                                    for doc in full_list if doc and 'project' in doc])
+            expected_result.extend([{"name" : doc['name'], "tasks_ids" : doc['tasks_ids']}
+                                    for doc in full_list if doc and not 'project' in doc])
+            expected_result = sorted(expected_result)
             self._verify_results(actual_result, expected_result)
 
     def test_join_several_keys(self):
         for bucket in self.buckets:
             self.query = "SELECT employee.name, employee.tasks_ids, new_task.project, new_task.task_name " +\
-            "FROM %s as employee JOIN default as new_task " % bucket.name +\
+            "FROM %s as employee %s JOIN default as new_task " % (bucket.name, self.type_join) +\
             "KEYS employee.tasks_ids"
             actual_result = self.run_cbq_query()
-            actual_result = sorted(actual_result['resultset'], key=lambda doc:(
-                                                               doc['name'],
-                                                               doc['new_project'], doc["task_name"]))
-            expected_result = self._generate_full_joined_docs_list()
-            expected_result = [{"name" : doc['name'], "tasks_ids" : doc['tasks_ids'],
-                                "project" : doc['project'], "task_name" : doc['task_name']}
-                               for doc in expected_result]
-            expected_result = sorted(expected_result, key=lambda doc:(
-                                                          doc['name'], doc['new_project'],
-                                                          doc["task_name"]))
+            actual_result = sorted(actual_result['resultset'])
+            full_list = self._generate_full_joined_docs_list(join_type=self.type_join)
+            expected_result = [doc for doc in full_list if not doc]
+            expected_result.extend([{"name" : doc['name'], "tasks_ids" : doc['tasks_ids'],
+                                     "project" : doc['project'], "task_name" : doc['task_name']}
+                                    for doc in full_list if doc and 'project' in doc])
+            expected_result.extend([{"name" : doc['name'], "tasks_ids" : doc['tasks_ids']}
+                                    for doc in full_list if doc and not 'project' in doc])
+            expected_result = sorted(expected_result)
             self._verify_results(actual_result, expected_result)
 
     def test_simple_join_key(self):
         for bucket in self.buckets:
             self.query = "SELECT employee.name, employee.tasks_ids, new_project " +\
-            "FROM %s as employee JOIN default.project as new_project " % bucket.name +\
+            "FROM %s as employee %s JOIN default.project as new_project " % (bucket.name, self.type_join) +\
             "KEY employee.tasks_ids[0]"
             actual_result = self.run_cbq_query()
-            actual_result = sorted(actual_result['resultset'], key=lambda doc:(
-                                                               doc['name'], doc['new_project']))
-            expected_result = self._generate_full_joined_docs_list(particular_key=0)
-            expected_result = [{"name" : doc['name'], "tasks_ids" : doc['tasks_ids'],
-                                "new_project" : doc['project']}
-                               for doc in expected_result]
-            expected_result = sorted(expected_result, key=lambda doc:(
-                                                          doc['name'], doc['new_project']))
+            actual_result = sorted(actual_result['resultset'])
+            full_list = self._generate_full_joined_docs_list(particular_key=0,
+                                                             join_type=self.type_join)
+            expected_result = [doc for doc in full_list if not doc]
+            expected_result.extend([{"name" : doc['name'], "tasks_ids" : doc['tasks_ids'],
+                                     "new_project" : doc['project']}
+                                    for doc in full_list if doc and 'project' in doc])
+            expected_result.extend([{"name" : doc['name'], "tasks_ids" : doc['tasks_ids']}
+                                    for doc in full_list if doc and not 'project' in doc])
+            expected_result = sorted(expected_result)
             self._verify_results(actual_result, expected_result)
 
     def test_join_several_key(self):
         for bucket in self.buckets:
             self.query = "SELECT employee.name, employee.tasks_ids, new_task.project, new_task.task_name " +\
-            "FROM %s as employee JOIN default as new_task " % bucket.name +\
+            "FROM %s as employee %s JOIN default as new_task " % (bucket.name, self.type_join) +\
             "KEY employee.tasks_ids[1]"
             actual_result = self.run_cbq_query()
-            actual_result = sorted(actual_result['resultset'], key=lambda doc:(
-                                                               doc['name'],
-                                                               doc['new_project'], doc["task_name"]))
-            expected_result = self._generate_full_joined_docs_list(particular_key=1)
-            expected_result = [{"name" : doc['name'], "tasks_ids" : doc['tasks_ids'],
-                                "project" : doc['project'], "task_name" : doc['task_name']}
-                               for doc in expected_result]
-            expected_result = sorted(expected_result, key=lambda doc:(
-                                                          doc['name'], doc['new_project'],
-                                                          doc["task_name"]))
+            actual_result = sorted(actual_result['resultset'])
+            full_list = self._generate_full_joined_docs_list(particular_key=1,
+                                                                   join_type=self.type_join)
+            expected_result = [doc for doc in full_list if not doc]
+            expected_result.extend([{"name" : doc['name'], "tasks_ids" : doc['tasks_ids'],
+                                     "project" : doc['project'], "task_name" : doc['task_name']}
+                                    for doc in full_list if doc and 'project' in doc])
+            expected_result.extend([{"name" : doc['name'], "tasks_ids" : doc['tasks_ids']}
+                                    for doc in full_list if doc and not 'project' in doc])
+            expected_result = sorted(expected_result)
             self._verify_results(actual_result, expected_result)
 
     def test_where_join_keys(self):
         for bucket in self.buckets:
             self.query = "SELECT employee.name, employee.tasks_ids, new_project " +\
-            "FROM %s as employee JOIN default.project as new_project " % bucket.name +\
+            "FROM %s as employee %s JOIN default.project as new_project " % (bucket.name, self.type_join) +\
             "KEYS employee.tasks_ids WHERE new_project == 'IT'"
             actual_result = self.run_cbq_query()
-            actual_result = sorted(actual_result['resultset'], key=lambda doc:(
-                                                               doc['name'], doc['new_project']))
-            expected_result = self._generate_full_joined_docs_list()
+            actual_result = sorted(actual_result['resultset'])
+            expected_result = self._generate_full_joined_docs_list(join_type=self.type_join)
             expected_result = [{"name" : doc['name'], "tasks_ids" : doc['tasks_ids'],
                                 "new_project" : doc['project']}
-                               for doc in expected_result if doc['project'] == 'IT']
-            expected_result = sorted(expected_result, key=lambda doc:(
-                                                          doc['name'], doc['new_project']))
+                               for doc in expected_result if doc and 'project' in doc and\
+                               doc['project'] == 'IT']
+            expected_result = sorted(expected_result)
             self._verify_results(actual_result, expected_result)
 
     def generate_docs(self, docs_per_day, start=0):
@@ -151,20 +153,30 @@ class JoinTests(QueryTests):
                                         particular_key=None):
         joined_list = []
         all_docs_list = self._generate_full_docs_list(self.gens_load)
-        if join_type == JOIN_INNER:
+        if join_type.upper() == JOIN_INNER:
             for item in all_docs_list:
                 keys = item["tasks_ids"]
-                if particular_key:
+                if particular_key is not None:
                     keys=[item["tasks_ids"][particular_key]]
                 tasks_items = self._generate_full_docs_list(self.gens_tasks, keys=keys)
                 for tasks_item in tasks_items:
                     item_to_add = copy.deepcopy(item)
                     item_to_add.update(tasks_item)
                     joined_list.append(item_to_add)
-        elif join_type == JOIN_LEFT:
-            raise Exception("Not implemented yet")
-        elif join_type == JOIN_RIGHT:
-            raise Exception("Not implemented yet")
+        elif join_type.upper() == JOIN_LEFT:
+            for item in all_docs_list:
+                keys = item["tasks_ids"]
+                if particular_key:
+                    keys=[item["tasks_ids"][particular_key]]
+                tasks_items = self._generate_full_docs_list(self.gens_tasks, keys=keys)
+                for key in keys:
+                    item_to_add = copy.deepcopy(item)
+                    if key in [doc["_id"] for doc in tasks_items]:
+                        item_to_add.update([doc for doc in tasks_items if key == doc['_id']][0])
+                    joined_list.append(item_to_add)
+            joined_list.extend([{}] * self.gens_tasks[-1].end)
+        elif join_type.upper() == JOIN_RIGHT:
+            raise Exception("RIGHT JOIN doen't exists in corrunt implementation")
         else:
             raise Exception("Unknown type of join")
         return joined_list
