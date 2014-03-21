28430bf4acf5519d9d6fc3ce5ba29ae713e158a9
Tommie McAfee <tommie@couchbase.com>
2014-03-21 04:52:09 -0400

    CBQE-2135: full kv-ejection test cases
    initial tests to verify that evicted items can be retrieved and also are purged when expired
    
    Change-Id: I509b4b48cebd5dcae4df9be1973f5f92f7ee521b
    Reviewed-on: http://review.couchbase.org/34791
    Reviewed-by: Andrei Baranouski <andrei.baranouski@gmail.com>
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Tommie McAfee <tommie@couchbase.com>

---
+++ pytests/eviction/evictionbase.py
@@ -0,0 +1,61 @@
+from basetestcase import BaseTestCase
+from couchbase.documentgenerator import BlobGenerator
+from membase.helper.cluster_helper import ClusterOperationHelper
+from couchbase.stats_tools import StatsCommon
+
+class EvictionBase(BaseTestCase):
+
+    def setUp(self):
+        super(EvictionBase, self).setUp()
+
+    def tearDown(self):
+        super(EvictionBase, self).tearDown()
+
+
+    def load_ejected_set(self, end=100):
+        gen_create = BlobGenerator('ejected', 'ejected-', 128, start=0, end=end)
+        self._load_all_buckets(self.master, gen_create, "create", 0)
+
+    def stat(self, key):
+        stats =  StatsCommon.get_stats([self.master], 'default', "", key)
+        val = stats.values()[0]
+        if val.isdigit():
+            val = int(val)
+        return val
+
+    def load_to_dgm(self, active = 75, ttl = 0):
+        """
+            decides how many items to load to enter active% dgm state
+            where active is an integer value between 0 and 100
+        """
+        doc_size = 1024
+        curr_active = self.stat('vb_active_perc_mem_resident')
+
+        # go into heavy dgm
+        while curr_active > active:
+            curr_items = self.stat('curr_items')
+            gen_create = BlobGenerator('dgmkv', 'dgmkv-', doc_size , start=curr_items + 1, end=curr_items+5000)
+            try:
+                self._load_all_buckets(self.master, gen_create, "create", ttl)
+            except:
+                pass
+            curr_active = self.stat('vb_active_perc_mem_resident')
+
+
+    def get_kv_store(self, index = 1):
+        bucket = [b for b in self.buckets if b.name == 'default'][0]
+        kv_store = bucket.kvs[index]
+        return kv_store
+
+
+    def ops_on_ejected_set(self, action, start = 0, end = 100,  ttl = 0):
+        kv_store = self.get_kv_store()
+        gen_reader = BlobGenerator('ejected', 'ejected-', 128, start=start, end=end)
+        self.cluster.load_gen_docs(self.master, 'default', gen_reader, kv_store, action, exp = ttl)
+
+    def run_expiry_pager(self, ts = 15):
+        ClusterOperationHelper.flushctl_set(self.master, "exp_pager_stime", ts)
+        self.log.info("wait for expiry pager to run on all these nodes")
+
+
+
---
+++ pytests/eviction/evictionkv.py
@@ -0,0 +1,85 @@
+import time
+from eviction.evictionbase import EvictionBase
+
+class EvictionKV(EvictionBase):
+
+
+    def test_verify_expiry(self):
+        """
+            heavy dgm purges expired items via compactor.
+            this is a smaller test to load items below compactor
+            threshold and check if items still expire via pager
+        """
+
+        self.load_ejected_set(100)
+        self.run_expiry_pager()
+        self.cluster.wait_for_stats([self.master],
+                                    "default", "",
+                                    "curr_items", "==", 0, timeout=30)
+
+
+    def test_eject_all_ops(self):
+        """
+            eject all items and ensure items can still be retrieved, deleted, and udpated
+            when fulleviction enabled
+        """
+        self.load_ejected_set(600)
+        self.load_to_dgm()
+
+        self.ops_on_ejected_set("read",   1,   200)
+        self.ops_on_ejected_set("delete", 201, 400)
+        self.ops_on_ejected_set("update", 401, 600)
+
+    def test_purge_ejected_docs(self):
+        """
+           Go into dgm with expired items and verify at end of load no docs expired docs remain
+           and ejected docs can still be deleted resulting in 0 docs left in cluster
+        """
+        num_ejected = 100
+        ttl = 240
+
+        self.load_ejected_set(num_ejected)
+        self.load_to_dgm(ttl=ttl)
+
+
+        while ttl > 0:
+            self.log.info("%s seconds until loaded docs expire" % ttl)
+            time.sleep(10)
+            ttl = ttl - 10
+
+
+        # compact to purge expiring docs
+        compacted = self.cluster.compact_bucket(self.master, 'default')
+        self.assertTrue(compacted, msg="unable compact_bucket")
+
+        iseq = self.cluster.wait_for_stats([self.master],
+                                           "default", "",
+                                           "curr_items", "==", num_ejected, timeout=120)
+        self.assertTrue(iseq, msg="curr_items != {0}".format(num_ejected))
+
+
+        # delete remaining non expiring docs
+        self.ops_on_ejected_set("delete", 0, num_ejected)
+        iseq = self.cluster.wait_for_stats([self.master],
+                                            "default", "",
+                                            "curr_items", "==", 0, timeout=30)
+        self.assertTrue(iseq, msg="curr_items != {0}".format(0))
+
+
+
+    def test_update_ejected_expiry_time(self):
+        """
+            eject all items set to expire
+        """
+
+        self.load_ejected_set(100)
+        self.load_to_dgm(ttl=30)
+        self.ops_on_ejected_set("update", ttl=30)
+
+        # run expiry pager
+        self.run_expiry_pager()
+
+        self.cluster.wait_for_stats([self.master],
+                                    "default", "",
+                                    "curr_items", "==", 0, timeout=30)
+
