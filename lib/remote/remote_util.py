9166297d6291e760d592635a837968cf6ac0dc4e
andreibara <andrei.baranouski@gmail.com>
2014-03-29 15:43:13 +0300

    CBQE-0: improve logs in log_command_output with track_words
    Change-Id: Icab00362be184fb14ecdf16360f745793994dc01
    Reviewed-on: http://review.couchbase.org/35069
    Reviewed-by: Andrei Baranouski <andrei.baranouski@gmail.com>
    Tested-by: Andrei Baranouski <andrei.baranouski@gmail.com>

--- lib/remote/remote_util.py
+++ lib/remote/remote_util.py
@@ -1395,7 +1395,7 @@ class RemoteMachineShellConnection:
             log.info(line)
             if any(s.lower() in line.lower() for s in track_words):
                 success = False
-                log.error('something wrong happened!!! error:{0}, track_words:{1}'.format(error, track_words))
+                log.error('something wrong happened on {0}!!! error:{1}, track_words:{2}'.format(self.ip, error, track_words))
         return success
 
     def execute_commands_inside(self, main_command, subcommands=[], min_output_size=0,
