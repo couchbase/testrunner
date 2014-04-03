8ef8ef52469843f9d4cf9684d375029fdd53698e
meenakshi25 <meenakshi.goel@globallogic.com>
2014-04-03 16:04:52 +0530

    CBQE 0 Fix timeout errors in views test
    Change-Id: I3e2f94da7ffc50fcc8bc2e48ef65849b31647b8f
    Reviewed-on: http://review.couchbase.org/35237
    Reviewed-by: Iryna Mironava <irynamironava@yandex.ru>
    Reviewed-by: Ketaki Gangal <ketakigangal@gmail.com>
    Reviewed-by: Meenakshi Goel <meenakshi.goel@globallogic.com>
    Tested-by: Meenakshi Goel <meenakshi.goel@globallogic.com>

--- pytests/view/createdeleteview.py
+++ pytests/view/createdeleteview.py
@@ -411,8 +411,8 @@ class CreateDeleteViewTests(BaseTestCase):
         max_verify = None
         if self.num_items > 500000:
             max_verify = 100000
-        self._verify_all_buckets(server=self.master, timeout=900, max_verify=max_verify)
-        self._verify_stats_all_buckets(self.servers[:self.nodes_in + 1])
+        self._verify_all_buckets(server=self.master, timeout=self.wait_timeout * 15 if not self.dgm_run else None, max_verify=max_verify)
+        self._verify_stats_all_buckets(self.servers[:self.nodes_in + 1], timeout=self.wait_timeout if not self.dgm_run else None)
         self._verify_ddoc_ops_all_buckets()
         if self.test_with_view:
             self._verify_ddoc_data_all_buckets()
@@ -441,8 +441,8 @@ class CreateDeleteViewTests(BaseTestCase):
             max_verify = None
             if self.num_items > 500000:
                 max_verify = 100000
-            self._verify_all_buckets(server=self.master, timeout=900, max_verify=max_verify)
-            self._verify_stats_all_buckets(self.servers[:i])
+            self._verify_all_buckets(server=self.master, timeout=self.wait_timeout * 15 if not self.dgm_run else None, max_verify=max_verify)
+            self._verify_stats_all_buckets(self.servers[:i], timeout=self.wait_timeout if not self.dgm_run else None)
             self._verify_ddoc_ops_all_buckets()
             if self.test_with_view:
                 self._verify_ddoc_data_all_buckets()
@@ -479,8 +479,8 @@ class CreateDeleteViewTests(BaseTestCase):
         max_verify = None
         if self.num_items > 500000:
             max_verify = 100000
-        self._verify_all_buckets(server=self.master, timeout=900, max_verify=max_verify)
-        self._verify_stats_all_buckets(servs_after_rebal)
+        self._verify_all_buckets(server=self.master, timeout=self.wait_timeout * 15 if not self.dgm_run else None, max_verify=max_verify)
+        self._verify_stats_all_buckets(servs_after_rebal, timeout=self.wait_timeout if not self.dgm_run else None)
         self._verify_ddoc_ops_all_buckets()
         if self.test_with_view:
             self._verify_ddoc_data_all_buckets()
