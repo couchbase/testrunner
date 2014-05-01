de126055ac5051d17af4adf17eb99b5f3c4e7c04
IrynaMironava <irynamironava@yandex.ru>
2014-05-01 16:19:44 +0300

    CBQE-2223: add aliases handles for tuq generator
    Change-Id: I16c3b558399eddfd80a0096e8ebf27a6cd952d80
    Reviewed-on: http://review.couchbase.org/36564
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Andrei Baranouski <andrei.baranouski@gmail.com>

--- pytests/tuqquery/tuq_generators.py
+++ pytests/tuqquery/tuq_generators.py
@@ -21,39 +21,83 @@ class TuqGenerators(object):
                             if isinstance(attr[1], list) and isinstance(attr[1][0], dict)]
         self.type_args['obj'] = [attr[0] for attr in full_set[0].iteritems()
                              if isinstance(attr[1], dict)]
+        for obj in self.type_args['obj']:
+            self.type_args['_obj%s_str' % (self.type_args['obj'].index(obj))] = [attr[0] for attr in full_set[0][obj].iteritems()
+                                                                                    if isinstance(attr[1], str)]
+            self.type_args['_obj%s_int'% (self.type_args['obj'].index(obj))] = [attr[0] for attr in full_set[0][obj].iteritems()
+                                                                                    if isinstance(attr[1], int)]
+        for obj in self.type_args['list_obj']:
+            self.type_args['_list_obj%s_str' % (self.type_args['list_obj'].index(obj))] = [attr[0] for attr in full_set[0][obj][0].iteritems()
+                                                                                    if isinstance(attr[1], str)]
+            self.type_args['_list_obj%s_int'% (self.type_args['list_obj'].index(obj))] = [attr[0] for attr in full_set[0][obj][0].iteritems()
+                                                                                    if isinstance(attr[1], int)]
         self.distict = False
         self.aggr_fns = {}
+        self.aliases = {}
 
     def generate_query(self, template):
         query = template
         for name_type, type_arg in self.type_args.iteritems():
             for attr_type_arg in type_arg:
                 query = query.replace('$%s%s' % (name_type, type_arg.index(attr_type_arg)), attr_type_arg)
-        for expr in [' where ', ' select ', ' from ', ' order by', ' limit ', ' offset ', ' count(']:
+        for expr in [' where ', ' select ', ' from ', ' order by', ' limit ', ' offset ', ' count(' , 'group by']:
             query = query.replace(expr, expr.upper())
         self.log.info("Generated query to be run: '''%s'''" % query)
         self.query = query
         return query
 
     def generate_expected_result(self):
+        self._create_alias_map()
         where_clause = self._format_where_clause()
         select_clause = self._format_select_clause()
+        from_clause = self._format_from_clause()
         result = self._filter_full_set(select_clause, where_clause)
         result = self._order_results(result)
         result = self._limit_and_offset(result)
         return result
 
+    def _create_alias_map(self):
+        query_dict = self.query.split()
+        for word in query_dict:
+            if word.upper() == 'AS':
+                self.aliases[query_dict[query_dict.index(word) + 1]] = query_dict[query_dict.index(word) - 1]
 
     def _format_where_clause(self):
         if self.query.find('WHERE') == -1:
             return None
-        clause = re.sub(r'.*ORDER BY', '', re.sub(r'.*WHERE', '', self.query)).strip()
-        attributes = [attr for group in self.type_args.itervalues() for attr in group]
+        clause = re.sub(r'ORDER BY.*', '', re.sub(r'.*WHERE', '', self.query))
+        attributes = [attr for name, group in self.type_args.iteritems()
+                      for attr in group if not name.startswith('_')]
         conditions = clause.replace('IS NULL', 'is None')
         conditions = conditions.replace('IS NOT NULL', 'is not None')
+        regex = re.compile("[\w']+\.[\w']+")
+        atts = regex.findall(conditions)
+        for att in atts:
+            parent, child = att.split('.')
+            if parent in attributes:
+                conditions = conditions.replace(' %s.%s ' % (parent, child),
+                                                ' doc["%s"]["%s"] ' % (parent, child))
+            else:
+                if parent not in self.aliases:
+                    conditions = conditions.replace(' %s.%s ' % (parent, child),
+                                                ' doc["%s"] ' % (child))
+                elif self.aliases[parent] in attributes:
+                    conditions = conditions.replace(' %s.%s ' % (parent, child),
+                                                    ' doc["%s"]["%s"] ' % (self.aliases[parent], child))
+                else:
+                    conditions = conditions.replace(' %s.%s ' % (parent, child),
+                                                    ' doc["%s"] ' % (child))
         for attr in attributes:
             conditions = conditions.replace(' %s ' % attr, ' doc["%s"] ')
         return conditions
+
+    def _format_from_clause(self):
+        clause = re.sub(r'ORDER BY.*', '', re.sub(r'.*FROM', '', self.query)).strip()
+        clause = re.sub(r'WHERE.*', '', re.sub(r'GROUP BY.*', '', clause)).strip()
+        clause = re.sub(r'SELECT.*', '', clause).strip()
+        if len(clause.split()) == 2:
+            self.aliases[clause.split()[1]] = clause.split()[0]
+        return clause
 
     def _format_select_clause(self):
         select_clause = re.sub(r'ORDER BY.*', '', re.sub(r'.*SELECT', '', self.query)).strip()
@@ -75,11 +119,21 @@ class TuqGenerators(object):
                 attr = attr[1:]
                 self.distict= True
             if len(attr) == 1:
-                condition += '"%s" : doc["%s"],' % (attr[0], attr[0])
+                if attr[0].find('.') != -1:
+                    condition += '"%s" : doc["%s"]["%s"],' % (attr[0], attr[0].split('.')[0], attr[0].split('.')[1])
+                else:
+                    condition += '"%s" : doc["%s"],' % (attr[0], attr[0])
             elif len(attr) == 2:
-                condition += '"%s" : doc["%s"],' % (attr[1], attr[0])
+                if attr[0].find('.') != -1:
+                    condition += '"%s" : doc["%s"]["%s"],' % (attr[1], attr[0].split('.')[0], attr[0].split('.')[1])
+                else:
+                    condition += '"%s" : doc["%s"],' % (attr[1], attr[0])
+                self.aliases[attr[1]] = attr[0]
             elif len(attr) == 3 and ('as' in attr or 'AS' in attr):
-                condition += '"%s" : doc["%s"],' % (attr[2], attr[0])
+                if attr[0].find('.') != -1:
+                    condition += '"%s" : doc["%s"]["%s"],' % (attr[2], attr[0].split('.')[0], attr[0].split('.')[1])
+                else:
+                    condition += '"%s" : doc["%s"],' % (attr[2], attr[0])
         condition += '}'
         return condition
 
@@ -108,10 +162,11 @@ class TuqGenerators(object):
             order_attrs = order_clause.split(',')
             for attr_s in order_attrs:
                 attr = attr_s.split()
-                if len(attr) == 1 or (len(attr) == 2 and attr[1].upper() == 'ASC'):
+                if attr[0].find('.') != -1:
+                    condition += 'doc["%s"]["%s"],' % attr[0].split('.')
+                else:
                     condition += 'doc["%s"],' % attr[0]
-                elif len(attr) == 2 and attr[1].upper() == 'DESC':
-                    condition += 'doc["%s"],' % attr[0]
+                if len(attr) == 2 and attr[1].upper() == 'DESC':
                     reverse = True
             key = lambda doc: eval(condition)
         result = sorted(result, key=key, reverse=reverse)
