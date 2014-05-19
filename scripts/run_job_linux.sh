0990740b35d622e768ea48c6ea13b3c705279f65
andreibara <andrei.baranouski@gmail.com>
2014-05-19 12:40:52 +0300

    CBQE-0: run_job_linux.sh with install_param
    new param to teplate job: install_param
    echo "install_param not set! Will use default value: product=cb,parallel=True";
    
    Change-Id: Idfc62d640e26c697145ef71f3c513e53fa8cbd59
    Reviewed-on: http://review.couchbase.org/37269
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Andrei Baranouski <andrei.baranouski@gmail.com>

--- scripts/run_job_linux.sh
+++ scripts/run_job_linux.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+
 if [ -n $BUILD_CAUSE ]
 then
   if [ "$BUILD_CAUSE" = "UPSTREAMTRIGGER" ]
   then
-    echo "!!!!!it's downstream project but will use own parameters for ini_file, config_file & test_params"
+    echo "!!!!!it's downstream project but will use own parameters for ini_file, config_file, test_params & install_params"
     sudo pip install python-jenkins
     export ini_file=`python -c 'import sys;import jenkins; import os;import json; j = jenkins.Jenkins(os.environ["HUDSON_URL"]); [sys.stdout.write(p["defaultParameterValue"]["value"]) for p in j.get_job_info(os.environ["JOB_NAME"])["actions"][1]["parameterDefinitions"] if p["name"] == "ini_file"]'`             
     export config_file=`python -c 'import sys;import jenkins; import os;import json; j = jenkins.Jenkins(os.environ["HUDSON_URL"]); [sys.stdout.write(p["defaultParameterValue"]["value"]) for p in j.get_job_info(os.environ["JOB_NAME"])["actions"][1]["parameterDefinitions"] if p["name"] == "config_file"]'`
     export test_params=`python -c 'import sys;import jenkins; import os;import json; j = jenkins.Jenkins(os.environ["HUDSON_URL"]); [sys.stdout.write(p["defaultParameterValue"]["value"]) for p in j.get_job_info(os.environ["JOB_NAME"])["actions"][1]["parameterDefinitions"] if p["name"] == "test_params"]'`
+    export install_params=`python -c 'import sys;import jenkins; import os;import json; j = jenkins.Jenkins(os.environ["HUDSON_URL"]); [sys.stdout.write(p["defaultParameterValue"]["value"]) for p in j.get_job_info(os.environ["JOB_NAME"])["actions"][1]["parameterDefinitions"] if p["name"] == "install_params"]'`
   elif [ "$BUILD_CAUSE" = "MANUALTRIGGER" ]
   then
     echo "WILL USE JOB's PARAMETERS:"
@@ -18,9 +20,26 @@ then
 else
   echo -e "BUILD_CAUSE not set\n"
 fi
+
+if [ -z "$install_params" ];
+then
+	echo "install_params not set! Will use default value: product=cb,parallel=True";
+	install_params="product=cb,parallel=True";
+fi
+
+
+
 export
 
 echo "ini_file to be used: " ${ini_file}
+
+if [ -z "$ini_file" ];
+then
+	echo "ini_file not set!!!";
+	exit;
+fi
+
+
 INI_NOT_FOUND=`cat ${ini_file} 2>&1| grep "No such file or directory"| wc -l`
 
 if [ ${INI_NOT_FOUND} -ge 1 ]
@@ -28,6 +47,7 @@ if [ ${INI_NOT_FOUND} -ge 1 ]
     echo "ini_file doesn't exist!: " ${ini_file}
     exit ${INI_NOT_FOUND}
 fi
+
 
 
 set +x
@@ -53,7 +73,7 @@ ulimit -a
 if [ ${run_install} = true ]
        then
        echo '---------------------------- INSTALLATION -----------------------'
-       python scripts/install.py -i ${ini_file} -p version=${version_number},product=cb,parallel=True,vbuckets=${vbuckets},upr=${upr}
+       python scripts/install.py -i ${ini_file} -p version=${version_number},vbuckets=${vbuckets},upr=${upr},${install_params}
 fi
 
 echo '---------------------------- TESTS RUN -----------------------'
