20e6423f5ac8748f8549401c19d9b651b1ec08b5
saigon <baolavn@gmail.com>
2014-05-27 12:37:10 -0700

    CBQE-0 make script compatible with python27
    Change-Id: I46265dcab44154c46f3adc59191a09004e41926a
    Reviewed-on: http://review.couchbase.org/37613
    Reviewed-by: Andrei Baranouski <andrei.baranouski@gmail.com>
    Tested-by: buildbot <build@couchbase.com>

--- scripts/run_job_linux.sh
+++ scripts/run_job_linux.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+python_exe = "python";
+grep "centos" /etc/issue -i -q
+if [ $? = '0' ];then
+python_exe = "python27"
+fi
 
 if [ -n $BUILD_CAUSE ]
 then
@@ -7,11 +12,11 @@ then
   then
     echo "!!!!!it's downstream project but will use own parameters for ini_file, config_file, test_params & install_params"
     sudo pip install python-jenkins
-    export ini_file=`python -c 'import sys;import jenkins; import os;import json; j = jenkins.Jenkins(os.environ["HUDSON_URL"]); [sys.stdout.write(p["defaultParameterValue"]["value"]) for p in j.get_job_info(os.environ["JOB_NAME"])["actions"][1]["parameterDefinitions"] if p["name"] == "ini_file"]'`             
-    export config_file=`python -c 'import sys;import jenkins; import os;import json; j = jenkins.Jenkins(os.environ["HUDSON_URL"]); [sys.stdout.write(p["defaultParameterValue"]["value"]) for p in j.get_job_info(os.environ["JOB_NAME"])["actions"][1]["parameterDefinitions"] if p["name"] == "config_file"]'`
-    export test_params=`python -c 'import sys;import jenkins; import os;import json; j = jenkins.Jenkins(os.environ["HUDSON_URL"]); [sys.stdout.write(p["defaultParameterValue"]["value"]) for p in j.get_job_info(os.environ["JOB_NAME"])["actions"][1]["parameterDefinitions"] if p["name"] == "test_params"]'`
-    export install_params=`python -c 'import sys;import jenkins; import os;import json; j = jenkins.Jenkins(os.environ["HUDSON_URL"]); [sys.stdout.write(p["defaultParameterValue"]["value"]) for p in j.get_job_info(os.environ["JOB_NAME"])["actions"][1]["parameterDefinitions"] if p["name"] == "install_params"]'`
-    export run_install=`python -c 'import sys;import jenkins; import os;import json; j = jenkins.Jenkins(os.environ["HUDSON_URL"]); [sys.stdout.write(p["defaultParameterValue"]["value"]) for p in j.get_job_info(os.environ["JOB_NAME"])["actions"][1]["parameterDefinitions"] if p["name"] == "run_install"]'`
+    export ini_file=`$python_exe -c 'import sys;import jenkins; import os;import json; j = jenkins.Jenkins(os.environ["HUDSON_URL"]); [sys.stdout.write(p["defaultParameterValue"]["value"]) for p in j.get_job_info(os.environ["JOB_NAME"])["actions"][1]["parameterDefinitions"] if p["name"] == "ini_file"]'`
+    export config_file=`$python_exe -c 'import sys;import jenkins; import os;import json; j = jenkins.Jenkins(os.environ["HUDSON_URL"]); [sys.stdout.write(p["defaultParameterValue"]["value"]) for p in j.get_job_info(os.environ["JOB_NAME"])["actions"][1]["parameterDefinitions"] if p["name"] == "config_file"]'`
+    export test_params=`$python_exe -c 'import sys;import jenkins; import os;import json; j = jenkins.Jenkins(os.environ["HUDSON_URL"]); [sys.stdout.write(p["defaultParameterValue"]["value"]) for p in j.get_job_info(os.environ["JOB_NAME"])["actions"][1]["parameterDefinitions"] if p["name"] == "test_params"]'`
+    export install_params=`$python_exe -c 'import sys;import jenkins; import os;import json; j = jenkins.Jenkins(os.environ["HUDSON_URL"]); [sys.stdout.write(p["defaultParameterValue"]["value"]) for p in j.get_job_info(os.environ["JOB_NAME"])["actions"][1]["parameterDefinitions"] if p["name"] == "install_params"]'`
+    export run_install=`$python_exe -c 'import sys;import jenkins; import os;import json; j = jenkins.Jenkins(os.environ["HUDSON_URL"]); [sys.stdout.write(p["defaultParameterValue"]["value"]) for p in j.get_job_info(os.environ["JOB_NAME"])["actions"][1]["parameterDefinitions"] if p["name"] == "run_install"]'`
   elif [ "$BUILD_CAUSE" = "MANUALTRIGGER" ]
   then
     echo "WILL USE JOB's PARAMETERS:"
@@ -54,7 +59,7 @@ fi
 set +x
 echo '---------------------------- PRE-SETUP VERIFICATION -----------------------'
 echo ${version_number}
-FAIL_CONNECTIONS=`python scripts/ssh.py -i ${ini_file} 'ls' 2>&1| grep 'No handlers' | wc -l`
+FAIL_CONNECTIONS=`$python_exe scripts/ssh.py -i ${ini_file} 'ls' 2>&1| grep 'No handlers' | wc -l`
 
 if [ ${FAIL_CONNECTIONS} -ge 1 ]
     then
@@ -62,29 +67,29 @@ if [ ${FAIL_CONNECTIONS} -ge 1 ]
     exit ${FAIL_CONNECTIONS}
 fi
 
-python scripts/ssh.py -i ${ini_file} "ntpdate ntp.ubuntu.com"
-python scripts/ssh.py -i ${ini_file} "date"
-python scripts/ssh.py -i ${ini_file} "cat /etc/*rele*"
-python scripts/ssh.py -i ${ini_file} "lscpu"
-python scripts/ssh.py -i ${ini_file} "find /tmp/core* -mtime +10 -exec rm {} \;"
-python scripts/ssh.py -i ${ini_file} "df"
-python scripts/ssh.py -i ${ini_file} "free"
-python scripts/ssh.py -i ${ini_file} "ls -la /tmp/"
+$python_exe scripts/ssh.py -i ${ini_file} "ntpdate ntp.ubuntu.com"
+$python_exe scripts/ssh.py -i ${ini_file} "date"
+$python_exe scripts/ssh.py -i ${ini_file} "cat /etc/*rele*"
+$python_exe scripts/ssh.py -i ${ini_file} "lscpu"
+$python_exe scripts/ssh.py -i ${ini_file} "find /tmp/core* -mtime +10 -exec rm {} \;"
+$python_exe scripts/ssh.py -i ${ini_file} "df"
+$python_exe scripts/ssh.py -i ${ini_file} "free"
+$python_exe scripts/ssh.py -i ${ini_file} "ls -la /tmp/"
 ulimit -a
 if [ ${run_install} = true ]
        then
        echo '---------------------------- INSTALLATION -----------------------'
-       python scripts/install.py -i ${ini_file} -p version=${version_number},vbuckets=${vbuckets},upr=${upr},${install_params}
+       $python_exe scripts/install.py -i ${ini_file} -p version=${version_number},vbuckets=${vbuckets},upr=${upr},${install_params}
 fi
 
 echo '---------------------------- TESTS RUN -----------------------'
-./testrunner -i ${ini_file} -c ${config_file} -p ${test_params}
+$python_exe ./testrunner -i ${ini_file} -c ${config_file} -p ${test_params}
 
 
 
 free
 df
 export
-python scripts/ssh.py -i ${ini_file} "ls -la /tmp/"
-bash -c "python scripts/getchanges.py ${version_number};exit 0;"
-bash -c "python scripts/getcoredumps.py	 -i ${ini_file};exit 0;"
+$python_exe scripts/ssh.py -i ${ini_file} "ls -la /tmp/"
+bash -c "$python_exe scripts/getchanges.py ${version_number};exit 0;"
+bash -c "$python_exe scripts/getcoredumps.py	 -i ${ini_file};exit 0;"
