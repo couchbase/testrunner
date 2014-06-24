54dad262d6a001591892a843ab8ca2b606802d6f
Tommie McAfee <tommie@couchbase.com>
2014-06-24 15:38:24 -0700

    CBQE-2297: upr rebalance scenarios for testrunner
    initial checkin of test cases that perform rebalance/failovers
    while verifying mutations using upr client
    
    Change-Id: Ic8b5f2a21929c9f27e43b3001eff839855367ad4
    Reviewed-on: http://review.couchbase.org/38773
    Reviewed-by: Tommie McAfee <tommie@couchbase.com>
    Tested-by: Tommie McAfee <tommie@couchbase.com>

---
+++ pytests/upr/rebalance.py
@@ -0,0 +1,206 @@
+import time
+import logger
+from upr.constants import *
+from uprbase import UPRBase
+from membase.api.rest_client import RestConnection, RestHelper
+from couchbase.documentgenerator import BlobGenerator
+from remote.remote_util import RemoteMachineShellConnection
+from lib.cluster_run_manager  import CRManager
+
+log = logger.Logger.get_logger()
+
+class UPRRebalanceTests(UPRBase):
+
+    def test_mutations_during_rebalance(self):
+
+        # start rebalance
+        task = self.cluster.async_rebalance(
+            [self.master],
+            self.servers[1:], [])
+
+        # load some data
+        vbucket = 0
+        self.load_docs(self.master, vbucket, self.num_items)
+        vb_uuid, seqno, high_seqno = self.vb_info(self.master,
+                                                  vbucket)
+        assert high_seqno == self.num_items
+
+        # stream
+        log.info("streaming vb {0} to seqno {1}".format(
+            vbucket, high_seqno))
+
+        upr_client = self.upr_client(self.master, PRODUCER, vbucket)
+        stream = upr_client.stream_req(
+            vbucket, 0, 0,
+            high_seqno, vb_uuid)
+
+        stream.run()
+        last_seqno = stream.last_by_seqno
+        assert last_seqno == high_seqno, last_seqno
+
+        # verify rebalance
+        assert task.result()
+
+    def test_failover_swap_rebalance(self):
+        """ add and failover node then perform swap rebalance """
+
+        assert len(self.servers) > 2, "not enough servers"
+        nodeA = self.servers[0]
+        nodeB = self.servers[1]
+        nodeC = self.servers[2]
+
+        gen_create = BlobGenerator('upr', 'upr-', 64, start=0, end=self.num_items)
+        self._load_all_buckets(nodeA, gen_create, "create", 0)
+
+        vbucket = 0
+        vb_uuid, seqno, high_seqno = self.vb_info(nodeA, vbucket)
+
+        # rebalance in nodeB
+        assert self.cluster.rebalance([nodeA], [nodeB], [])
+
+        # add nodeC
+        rest = RestConnection(nodeB)
+        rest.add_node(user=nodeC.rest_username,
+                      password=nodeC.rest_password,
+                      remoteIp=nodeC.ip,
+                      port=nodeC.port)
+
+        # stop and failover nodeA
+        assert self.stop_node(0)
+        assert self.cluster.failover([nodeB], [nodeA])
+        assert self.cluster.rebalance([nodeB], [], [])
+
+        # verify seqnos and stream mutations
+        rest = RestConnection(nodeB)
+        vbuckets = rest.get_vbuckets()
+        total_mutations = 0
+
+        for vb in vbuckets:
+            vbucket = vb.id
+            vb_uuid, seqno, high_seqno = self.vb_info(nodeB, vbucket)
+            total_mutations += high_seqno
+
+        assert total_mutations == self.num_items
+
+    def test_stream_req_during_failover(self):
+        """stream_req mutations before and after failover from state-changing vbucket"""
+
+        # start rebalance
+        self.cluster.rebalance([self.master], self.servers[1:], [])
+
+        vbucket = 0
+        mcd_client = self.mcd_client(self.master, vbucket)
+        mcd_client.set('key1', 0, 0, 'value', vbucket)
+
+        # failover node where key was set
+        rest = RestConnection(self.master)
+        index = self.vbucket_host_index(rest, vbucket)
+        fail_n = self.servers[index]
+        ready_n = filter(lambda n: n.ip != fail_n.ip or n.port != fail_n.port,
+                         self.servers)
+
+        assert self.stop_node(index)
+        assert self.cluster.failover(ready_n, [fail_n])
+        rebalance_t = self.cluster.async_rebalance(ready_n, [], [])
+
+        # vbucket has moved, set another key in new location
+        rest = RestConnection(ready_n[0])
+        index = self.vbucket_host_index(rest, vbucket)
+        new_master = ready_n[index]
+        mcd_client = self.mcd_client(new_master)
+        mcd_client.set('key2', 0, 0, 'value', vbucket)
+
+        # stream mutation
+        upr_client = self.upr_client(new_master, PRODUCER, vbucket)
+        stream = upr_client.stream_req(vbucket, 0, 0, 2, 0)
+
+        while stream.has_response():
+
+            response = stream.next_response(5)
+
+            assert response is not None,\
+                 "Timeout reading stream after failover"
+
+            if 'key' in response:
+                if response['by_seqno'] == 1:
+                    assert response['key'] == 'key1'
+                elif response['by_seqno'] == 2:
+                    assert response['key'] == 'key2'
+                else:
+                    assert False, "received unexpected mutation"
+            if response['opcode'] == 0x55: # end
+                break
+
+        assert stream.last_by_seqno == 2
+        assert rebalance_t.result()
+
+
+    def test_failover_log_table_updated(self):
+        """Verifies failover table entries are updated when vbucket ownership changes"""
+
+
+        # rebalance in nodeB
+        nodeA = self.servers[0]
+        nodeB = self.servers[1]
+
+        # load nodeA only
+        rest = RestConnection(nodeA)
+        vbuckets = rest.get_vbuckets()
+        for vb_info in vbuckets:
+            vbucket = vb_info.id
+            self.load_docs(nodeA, vbucket, self.num_items)
+
+        # get original failover table
+        mcd_client = self.mcd_client(nodeA)
+        orig_table = mcd_client.stats('failovers')
+
+        # add nodeB
+        self.cluster.rebalance([nodeA], [nodeB], [])
+
+        # stop nodeA and failover
+        assert self.stop_node(0)
+        assert self.cluster.failover([nodeB], [nodeA])
+        assert self.cluster.rebalance([nodeB], [], [])
+
+        # load nodeB only
+        rest = RestConnection(nodeB)
+        vbuckets = rest.get_vbuckets()
+        for vb_info in vbuckets:
+            vbucket = vb_info.id
+            self.load_docs(nodeB, vbucket, self.num_items)
+
+        # add nodeA back
+        assert self.start_node(0)
+        rest = RestHelper(RestConnection(nodeA))
+        assert rest.is_ns_server_running()
+        time.sleep(10)
+        self.cluster.rebalance([nodeB], [nodeA], [])
+
+        # stop nodeB and failover
+        assert self.stop_node(1)
+        assert self.cluster.failover([nodeA], [nodeB])
+        assert self.cluster.rebalance([nodeA], [], [])
+
+        # load nodeA only
+        rest = RestConnection(nodeA)
+        vbuckets = rest.get_vbuckets()
+        for vb_info in vbuckets:
+            vbucket = vb_info.id
+            self.load_docs(nodeA, vbucket, self.num_items)
+
+        # check failover table entries
+        mcd_client = self.mcd_client(nodeA)
+        stats = mcd_client.stats('failovers')
+        for vb_info in vbuckets:
+            vb = vb_info.id
+            assert long(stats['vb_'+str(vb)+':num_entries']) == 2
+
+            vb_uuid, _, _= self.vb_info(nodeA,
+                                                      vb)
+            upr_client = self.upr_client(nodeA, PRODUCER)
+            stream = upr_client.stream_req(
+                vb, 0, 0,
+                self.num_items*3, vb_uuid)
+
+            mutations = stream.run()
+            assert stream.last_by_seqno == self.num_items*3, stream.last_by_seqno
--- pytests/upr/uprbase.py
+++ pytests/upr/uprbase.py
@@ -1,69 +1,112 @@
+import time
 from upr.constants import *
 from upr_bin_client import UprClient
 from basetestcase import BaseTestCase
 from mc_bin_client import MemcachedClient
+from lib.cluster_run_manager  import CRManager
 from membase.api.rest_client import RestConnection
+from TestInput import TestInputSingleton
 
 class UPRBase(BaseTestCase):
 
+
+    def __init__(self, args):
+        super(UPRBase, self).__init__(args)
+        self.is_setup = False
+        self.crm = None
+        self.input = TestInputSingleton.input
+        self.use_cluster_run = self.input.param('dev', False)
+        self.doc_num = 0
+        if self.use_cluster_run:
+            num_nodes = self.input.param('num_nodes', 4)
+            self.crm = CRManager(num_nodes, 0)
+
     def setUp(self):
+        self.is_setup = True
+
+        if self.use_cluster_run:
+            assert self.crm.clean()
+            assert self.crm.start_nodes()
+            time.sleep(5)
+
         super(UPRBase, self).setUp()
-        rest = RestConnection(self.master)
+        self.is_setup = False
 
     def tearDown(self):
+        if self.use_cluster_run and not self.is_setup:
+            assert self.crm.stop_nodes()
+            self.cluster.shutdown(force=True)
+        else:
+            super(UPRBase, self).tearDown()
 
-        # undo any clustering
-        for rest in [RestConnection(node) for node in self.servers]:
-            nodes_out = rest.get_nodes()
-            if len(nodes_out) > 1:
-                self.cluster.rebalance([nodes_out[0]], [], nodes_out[1:])
-
-        super(UPRBase, self).tearDown()
-
-
-    def load_docs(self, node, vbucket, num_docs, exp = 0, flags = 0, unique = True):
+    def load_docs(self, node, vbucket, num_docs, exp = 0, flags = 0, update = False):
         """ using direct mcd client to control vbucket seqnos.
             keeps track of vbucket and keys stored """
 
-        mcd_client = self.mcd_client(node)
+        mcd_client = self.mcd_client(node, vbucket)
         for i in range(num_docs):
-            key = ("key", "key%s"%i)[unique]
+            key = "key%s"%self.doc_num
             rc = mcd_client.set(key, exp, flags, "val", vbucket)
+            if not update:
+                self.doc_num += 1
 
-    def upr_client(self, node, connection_type = PRODUCER, name = None):
+
+    def upr_client(self, node, connection_type = PRODUCER, vbucket = None, name = None):
 
         """ create an upr client from Node spec and opens connnection of specified type"""
-        client = self.client_helper(node, UPR)
+        client = self.client_helper(node, UPR, vbucket)
         assert connection_type in (PRODUCER, CONSUMER, NOTIFIER)
 
         name = name or DEFAULT_CONN_NAME
         if connection_type == PRODUCER:
-            client.open_producer(name)
+            response = client.open_producer(name)
         if connection_type == CONSUMER:
-            client.open_consumer(name)
+            response = client.open_consumer(name)
         if connection_type == NOTIFIER:
-            client.open_notifier(name)
+            response = client.open_notifier(name)
 
+        assert response['status'] == SUCCESS
         return client
 
-    def mcd_client(self, node):
+    def mcd_client(self, node, vbucket = None):
         """ create a mcd client from Node spec """
-        return self.client_helper(node, MCD)
+        return self.client_helper(node, MCD, vbucket)
 
-    def client_helper(self, node, type_):
-        client = None
+    def client_helper(self, node, type_, vbucket):
         assert type_ in (MCD, UPR)
 
+        client = None
+        ip = None
+        port = None
         rest = RestConnection(node)
-        client_node = rest.get_nodes_self()
-        if type_ == MCD:
-            client = MemcachedClient(client_node.ip, client_node.memcached)
-        else:
-            client = UprClient(client_node.ip, client_node.memcached)
 
-        assert client is not None, ENO_CLIENT
+        if vbucket is not None:
+            host = self.vbucket_host(rest, vbucket)
+            ip = host.split(':')[0]
+            port = int(host.split(':')[1])
+
+        else:
+            client_node = rest.get_nodes_self()
+            ip = client_node.ip
+            port = client_node.memcached
+
+        if type_ == MCD:
+            client = MemcachedClient(ip, port)
+        else:
+            client = UprClient(ip, port)
+
         return client
 
+    def vbucket_host(self, rest, vbucket):
+        info = rest.get_bucket_json()
+        return info['vBucketServerMap']['serverList']\
+            [info['vBucketServerMap']['vBucketMap'][vbucket][0]]
+
+    def vbucket_host_index(self, rest, vbucket):
+        info = rest.get_bucket_json()
+        host = self.vbucket_host(rest, vbucket)
+        return info['vBucketServerMap']['serverList'].index(host)
+        
     def flow_control_info(self, node, connection = None):
 
         connection = connection or DEFAULT_CONN_NAME
@@ -82,7 +125,7 @@ class UPRBase(BaseTestCase):
         return vb_uuid, seqno, high_seqno
 
     def vb_failover_entry(self, node, vbucket, table_entry = 0):
-        mcd_client = self.mcd_client(node)
+        mcd_client = self.mcd_client(node, vbucket)
         stats = mcd_client.stats(FAILOVER_STAT)
         assert len(stats) > vbucket,  ENO_STAT
         id_key = 'vb_{0}:{1}:id'.format(vbucket, table_entry)
@@ -90,8 +133,34 @@ class UPRBase(BaseTestCase):
         return long(stats[id_key]), long(stats[seq_key])
 
     def vb_seqno(self, node, vbucket):
-        mcd_client = self.mcd_client(node)
+        mcd_client = self.mcd_client(node, vbucket)
         stats = mcd_client.stats(VBSEQNO_STAT)
         assert len(stats) > vbucket, ENO_STAT
         id_key = 'vb_{0}:high_seqno'.format(vbucket)
         return long(stats[id_key])
+
+    def stop_node(self, index):
+        status = False
+        if self.use_cluster_run:
+            status = self.crm.stop(index)
+        elif index in self.servers:
+            node = self.servers[index]
+            shell = RemoteMachineShellConnection(node)
+            shell.stop_couchbase()
+            shell.disconnect()
+            status = True
+
+        return status
+
+    def start_node(self, index):
+        status = False
+        if self.use_cluster_run:
+            status = self.crm.start(index)
+        elif index in self.servers:
+            node = self.servers[index]
+            shell = RemoteMachineShellConnection(node)
+            shell.start_couchbase()
+            shell.disconnect()
+            status = True
+
+        return status
