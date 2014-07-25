777f3fcc55b45224070a86ac9cc76e408ff404e5
arunapiravi <arunapiravi86@gmail.com>
2014-07-25 14:04:00 -0700

    CBQE:0 Fix minor error in enabling xdcr trace logging in sys tests
    Change-Id:  I2c472be770e5341eaf155b5e80b1f791c7494a32
    Reviewed-on: http://review.couchbase.org/39892
    Reviewed-by: Aruna Piravi <aruna@couchbase.com>
    Tested-by: Aruna Piravi <aruna@couchbase.com>

--- pysystests/runsystest.py
+++ pysystests/runsystest.py
@@ -277,7 +277,7 @@ def install_couchbase():
         os.system("curl -d memoryQuota={0} \"http://{1}:{2}@{3}:8091/pools/default\"".
                   format(cfg.CLUSTER_RAM_QUOTA, cfg.COUCHBASE_USER,cfg.COUCHBASE_PWD,cfg.CLUSTER_IPS[0]))
     for ip in cfg.CLUSTER_IPS:
-        os.system("curl -X POST -d \'ale:set_loglevel(xdcr_trace, debug).\' \"http://{1}:{2}@{3}:8091/diag/eval\"".
+        os.system("curl -X POST -d \'ale:set_loglevel(xdcr_trace, debug).\' \"http://{0}:{1}@{2}:8091/diag/eval\"".
                   format(cfg.COUCHBASE_USER, cfg.COUCHBASE_PWD, ip))
 
 def warn_skip(task):
