4df5d7eca7a268e5ed400a19bf30c6814bab2fd2
Sangharsh Agarwal <sangharsh.agarwal@globallogic.com>
2014-08-02 15:19:31 +0530

    CBQE-2364: fixing test xdcr.xdcrMiscTests.test_verify_mb8825 to add stats from all the nodes for comparison instead of master nodes
    Change-Id: Ic29b8a14fcdb900ad9e976272d6a697413a6f5ab
    Reviewed-on: http://review.couchbase.org/40232
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Sangharsh Agarwal <sangharsh.agarwal@globallogic.com>
    Tested-by: Sangharsh Agarwal <sangharsh.agarwal@globallogic.com>

--- pytests/xdcr/xdcrMiscTests.py
+++ pytests/xdcr/xdcrMiscTests.py
@@ -57,19 +57,30 @@ class XdcrMiscTests(XDCRReplicationBaseTest):
         # Verify if no deletion performed at source node:
         src_master_buckets = self._get_cluster_buckets(self.src_master)
         for bucket in src_master_buckets:
-            src_stat_ep_num_ops_del_meta = int(StatsCommon.get_stats([self.src_master], bucket, '', 'ep_num_ops_del_meta')[self.src_master])
-            src_stat_ep_num_ops_set_meta = int(StatsCommon.get_stats([self.src_master], bucket, '', 'ep_num_ops_set_meta')[self.src_master])
+            src_stat_ep_num_ops_del_meta = 0
+            src_stat_ep_num_ops_set_meta = 0
+            src_stat_ep_num_ops_get_meta = 0
+            src_stat_ep_num_ops_del_meta_res_fail = 0
+            src_stat_ep_num_ops_set_meta_res_fail = 0
+            for src_node in self.src_nodes:
+                src_stat_ep_num_ops_del_meta += int(StatsCommon.get_stats([src_node], bucket, '', 'ep_num_ops_del_meta')[src_node])
+                src_stat_ep_num_ops_set_meta += int(StatsCommon.get_stats([src_node], bucket, '', 'ep_num_ops_set_meta')[src_node])
+                src_stat_ep_num_ops_get_meta += int(StatsCommon.get_stats([src_node], bucket, '', 'ep_num_ops_get_meta')[src_node])
+                src_stat_ep_num_ops_del_meta_res_fail += int(StatsCommon.get_stats([src_node], bucket, '', 'ep_num_ops_del_meta_res_fail')[src_node])
+                src_stat_ep_num_ops_set_meta_res_fail += int(StatsCommon.get_stats([src_node], bucket, '', 'ep_num_ops_set_meta_res_fail')[src_node])
+
             self.assertEqual(src_stat_ep_num_ops_set_meta, 0, "Number of set [%s] operation occurs at bucket = %s, while expected to 0" % (src_stat_ep_num_ops_set_meta, bucket))
             self.assertEqual(src_stat_ep_num_ops_del_meta, 0, "Number of delete [%s] operation occurs at bucket = %s, while expected to 0" % (src_stat_ep_num_ops_del_meta, bucket))
-            if self.rep_type == "xmem":
-                src_stat_ep_num_ops_del_meta_res_fail = int(StatsCommon.get_stats([self.src_master], bucket, '', 'ep_num_ops_del_meta_res_fail')[self.src_master])
-                src_stat_ep_num_ops_set_meta_res_fail = int(StatsCommon.get_stats([self.src_master], bucket, '', 'ep_num_ops_set_meta_res_fail')[self.src_master])
-                dest_stat_ep_num_ops_del_meta = int(StatsCommon.get_stats([self.dest_master], bucket, '', 'ep_num_ops_del_meta')[self.dest_master])
 
+            dest_stat_ep_num_ops_del_meta = 0
+            for dest_node in self.dest_nodes:
+                dest_stat_ep_num_ops_del_meta += int(StatsCommon.get_stats([dest_node], bucket, '', 'ep_num_ops_del_meta')[dest_node])
+
+            if self.rep_type == "xmem":
                 self.assertEqual(src_stat_ep_num_ops_del_meta_res_fail, dest_stat_ep_num_ops_del_meta, "Number of failed delete [%s] operation occurs at bucket = %s, while expected to %s" % (src_stat_ep_num_ops_del_meta_res_fail, bucket, dest_stat_ep_num_ops_del_meta))
                 self.assertTrue(src_stat_ep_num_ops_set_meta_res_fail > 0, "Number of failed set [%s] operation occurs at bucket = %s, while expected greater than 0" % (src_stat_ep_num_ops_set_meta_res_fail, bucket))
+
             elif self.rep_type == "capi":
-                src_stat_ep_num_ops_get_meta = int(StatsCommon.get_stats([self.src_master], bucket, '', 'ep_num_ops_get_meta')[self.src_master])
                 self.assertTrue(src_stat_ep_num_ops_get_meta > 0, "Number of get [%s] operation occurs at bucket = %s, while expected greater than 0" % (src_stat_ep_num_ops_get_meta, bucket))
 
     def test_diff_version_xdcr(self):
