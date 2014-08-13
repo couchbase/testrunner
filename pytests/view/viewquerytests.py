883ae472d129d723dca9f80a036a40af7941c411
meenakshi25 <meenakshi.goel@globallogic.com>
2014-08-13 16:56:39 +0530

    CBQE-0: Fix failover test in viewquery
    Change-Id: I5aae1d36766b45377c3ae45c695905e8503729a6
    Reviewed-on: http://review.couchbase.org/40567
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Iryna Mironava <irynamironava@yandex.ru>
    Reviewed-by: Meenakshi Goel <meenakshi.goel@globallogic.com>
    Tested-by: Meenakshi Goel <meenakshi.goel@globallogic.com>

--- pytests/view/viewquerytests.py
+++ pytests/view/viewquerytests.py
@@ -651,8 +651,8 @@ class ViewQueryTests(BaseTestCase):
             # incrementaly failover nodes and verify loaded data
             for i in xrange(failover_factor):
                 failover_node = self.servers[i : i + 1]
-                self.cluster.failover(self.servers, failover_nodes)
-                failover_nodes.add(failover_nodes)
+                self.cluster.failover(self.servers, failover_node)
+                failover_nodes.append(failover_node)
                 self.log.info("10 seconds sleep after failover before invoking rebalance...")
                 time.sleep(10)
 
@@ -969,8 +969,8 @@ class ViewQueryTests(BaseTestCase):
             # incrementaly failover nodes and verify loaded data
             for i in xrange(failover_factor):
                 failover_node = self.servers[i : i + 1]
-                self.cluster.failover(self.servers, failover_nodes)
-                failover_nodes.add(failover_nodes)
+                self.cluster.failover(self.servers, failover_node)
+                failover_nodes.append(failover_node)
                 self.log.info("10 seconds sleep after failover before invoking rebalance...")
                 time.sleep(10)
 
