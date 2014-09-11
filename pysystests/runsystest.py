78e314d3b8c04871c69248ba3d78c9b97ef5b9bc
Sangharsh Agarwal <sangharsh.agarwal@globallogic.com>
2014-09-11 16:15:23 +0530

    CBQE-0: added test_cfg file for KV_Windows_XDCR system test
    Change-Id: I7781dd6ae160fdc3166bf06eef5745776a10f846
    Reviewed-on: http://review.couchbase.org/41360
    Reviewed-by: Sangharsh Agarwal <sangharsh.agarwal@globallogic.com>
    Tested-by: Sangharsh Agarwal <sangharsh.agarwal@globallogic.com>

--- pysystests/runsystest.py
+++ pysystests/runsystest.py
@@ -1,33 +1,39 @@
 from librabbitmq import Connection
 import argparse
-import paramiko,sys
+import paramiko
+import sys
 import time
 import os
+import shutil
 
 python_exe = "python"
 if os.system("grep \'centos\' /etc/issue -i -q") == 0:
-    python_exe = "python27"
+    python_exe = "python2.7"
 
-def get_ssh_client(ip, timeout=10):
+def get_ssh_client(ip, username=None, password=None, timeout=10):
     client = None
     try:
         ip = ip.split(':')[0]
         client = paramiko.SSHClient()
         client.load_system_host_keys()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(ip, username=cfg.SSH_USER, password=cfg.SSH_PASSWORD, timeout=timeout)
+        username = username or cfg.SSH_USER
+        password = password or cfg.SSH_PASSWORD
+        client.connect(ip, username=username, password=password, timeout=timeout)
         print("Successfully SSHed to {0}".format(ip))
     except Exception as ex:
         print ex
         sys.exit(1)
     return client
 
-def get_sftp_client(ip):
+def get_sftp_client(ip, username=None, password=None,):
     try:
         ip = ip.split(':')[0]
         trans = paramiko.Transport((ip))
-        trans.connect(username=cfg.SSH_USER, password=cfg.SSH_PASSWORD)
-        print ("SFTPing to {}".format(ip))
+        username = username or cfg.SSH_USER
+        password = password or cfg.SSH_PASSWORD
+        trans.connect(username=username, password=password)
+        print ("SFTPing to {0}".format(ip))
         sftp_client = paramiko.SFTPClient.from_transport(trans)
         return sftp_client
     except Exception as ex:
@@ -46,14 +52,14 @@ def start_rabbitmq():
             continue
         rabbitmq_client.exec_command("sudo kill -9 {0}".format(pid))
     if cfg.RABBITMQ_LOG_LOCATION is not "":
-        print("Deleting RabbitMQ logs from {}".format(cfg.RABBITMQ_LOG_LOCATION))
-        rabbitmq_client.exec_command("rm -rf {}/*.*".format(cfg.RABBITMQ_LOG_LOCATION))
+        print("Deleting RabbitMQ logs from {0}".format(cfg.RABBITMQ_LOG_LOCATION))
+        rabbitmq_client.exec_command("rm -rf {0}/*.*".format(cfg.RABBITMQ_LOG_LOCATION))
     print ("Starting RabbitMQ ...")
     rabbitmq_client.exec_command("screen -dmS rabbitmq sh -c \'sudo rabbitmq-server start; exec bash;\'")
     time.sleep(20)
     _, stdout, _ = rabbitmq_client.exec_command("sudo rabbitmqctl status")
     for line in stdout.readlines():
-       sys.stdout.write(line)
+        sys.stdout.write(line)
     print("Rabbitmq has been restarted and is now running!")
     _, stdout, _ = rabbitmq_client.exec_command("sudo rabbitmqctl list_vhosts")
     for line in stdout.readlines():
@@ -78,7 +84,7 @@ def start_rabbitmq():
         except Exception as e:
             print e
             if tries <= 5:
-                print("Retrying connection {}/5 ...".format(tries))
+                print("Retrying connection {0}/5 ...".format(tries))
                 rabbitmq_client.exec_command("sudo rabbitmqctl delete_vhost {0}".format(cfg.CB_CLUSTER_TAG))
                 rabbitmq_client.exec_command("sudo rabbitmqctl add_vhost {0}".format(cfg.CB_CLUSTER_TAG))
                 rabbitmq_client.exec_command("sudo rabbitmqctl set_permissions -p {0} guest '.*' '.*' '.*'".format(cfg.CB_CLUSTER_TAG))
@@ -90,12 +96,16 @@ def start_rabbitmq():
 def start_worker(worker_ip):
     print("##### Setting up Celery Worker @ {0} #####".format(worker_ip))
     worker_client = get_ssh_client(worker_ip)
+
+    # Copy testcfg.py file to all workers
+    worker_client.open_sftp().put("./testcfg.py", os.path.join(cfg.WORKER_PYSYSTESTS_PATH, "testcfg.py"))
+
     # kill celery,remove screenlog
     _, stdout, _ = worker_client.exec_command("ps aux|grep celery|grep -v grep|awk \'{print $2}\'")
     for pid in stdout:
         if pid == "":
             continue
-        print ("Killing existing Celery process ...pid {}".format(pid))
+        print ("Killing existing Celery process ...pid {0}".format(pid))
         worker_client.exec_command("sudo kill -9 {0}".format(pid))
     worker_client.exec_command("screen -ls | grep \'celery\' | awk '{print $1}' | xargs -i screen -X -S {} quit")
     worker_client.exec_command("screen -wipe")
@@ -125,20 +135,20 @@ def start_seriesly():
     print("Killing seriesly ...")
     cbmonitor_client.exec_command("killall -9 seriesly")
     if cfg.SERIESLY_DB_LOCATION is not "":
-        print("Deleting old Seriesly db files from {}".format(cfg.SERIESLY_DB_LOCATION))
-        cbmonitor_client.exec_command("rm -rf {}/*.*".format(cfg.SERIESLY_DB_LOCATION))
+        print("Deleting old Seriesly db files from {0}".format(cfg.SERIESLY_DB_LOCATION))
+        cbmonitor_client.exec_command("rm -rf {0}/*.*".format(cfg.SERIESLY_DB_LOCATION))
     # kill all existing screens
     cbmonitor_client.exec_command("screen -ls | grep \'seriesly\' | awk \'{print $1}\' | xargs -i screen -X -S {} quit")
     cbmonitor_client.exec_command("screen -ls | grep \'webapp\' | awk \'{print $1}\' | xargs -i screen -X -S {} quit")
     cbmonitor_client.exec_command("screen -ls | grep \'ns_collector\' | awk \'{print $1}\' | xargs -i screen -X -S {} quit")
     cbmonitor_client.exec_command("screen -ls | grep \'atop_collector\' | awk \'{print $1}\' | xargs -i screen -X -S {} quit")
-    cbmonitor_client.exec_command("rm -rf {}/screenlog.0".format(cfg.CBMONITOR_HOME_DIR))
+    cbmonitor_client.exec_command("rm -rf {0}/screenlog.0".format(cfg.CBMONITOR_HOME_DIR))
     # screen 1 - start seriesly
     print ("Starting seriesly...")
     cbmonitor_client.exec_command("screen -dmS seriesly -L sh -c \'cd {0}; ./seriesly; exec bash;\'".
                                   format(cfg.SERIESLY_LOCATION))
     _, stdout, _ = cbmonitor_client.exec_command("pgrep seriesly")
-    print ("Seriesly is running with pid {}".format(stdout.readlines()[0]))
+    print ("Seriesly is running with pid {0}".format(stdout.readlines()[0]))
 
 def start_cbmonitor():
     print("\n##### Setting up CBMonitor @ {0} #####".format(cfg.SERIESLY_IP))
@@ -147,35 +157,35 @@ def start_cbmonitor():
     print ("Starting Django webserver...")
     _, stdout, _ = cbmonitor_client.exec_command("pgrep webapp")
     for pid in stdout.readlines():
-        cbmonitor_client.exec_command("kill -9 {}".format(pid))
+        cbmonitor_client.exec_command("kill -9 {0}".format(pid))
     cbmonitor_client.exec_command("cd {0}; screen -dmS webapp -L sh -c \'./bin/webapp add-user -S;./bin/webapp syncdb; \
-     ./bin/webapp runserver {1}:8000; exec bash;\'".format(cfg.CBMONITOR_HOME_DIR,cfg.SERIESLY_IP))
+     ./bin/webapp runserver {1}:8000; exec bash;\'".format(cfg.CBMONITOR_HOME_DIR, cfg.SERIESLY_IP))
     time.sleep(5)
     _, stdout, _ = cbmonitor_client.exec_command("pgrep webapp")
-    print ("Webserver is running with pid {}".format(stdout.readlines()[0]))
+    print ("Webserver is running with pid {0}".format(stdout.readlines()[0]))
     # screen 3 - start ns_collector
     print ("Starting ns_collector...")
     cbmonitor_client.exec_command("cd {0}; screen -dmS ns_collector -L sh -c \'./bin/ns_collector sample.cfg; exec bash;\'".
                                   format(cfg.CBMONITOR_HOME_DIR))
     time.sleep(5)
     _, stdout, _ = cbmonitor_client.exec_command("pgrep ns_collector")
-    print ("ns_collector is running with pid {}".format(stdout.readlines()[0]))
+    print ("ns_collector is running with pid {0}".format(stdout.readlines()[0]))
     # screen 4 - start atop_collector
     print ("Starting atop_collector...")
     cbmonitor_client.exec_command("cd {0}; screen -dmS atop_collector -L sh -c \'./bin/atop_collector sample.cfg; exec bash;\'".
                                   format(cfg.CBMONITOR_HOME_DIR))
     time.sleep(5)
     _, stdout, _ = cbmonitor_client.exec_command("pgrep atop_collector")
-    print ("atop_collector is running with pid {}".format(stdout.readlines()[0]))
+    print ("atop_collector is running with pid {0}".format(stdout.readlines()[0]))
     read_screenlog(cfg.SERIESLY_IP, cfg.CBMONITOR_HOME_DIR, stop_if_EOF=True, lines_to_read=100)
     cbmonitor_client.close()
 
-def read_screenlog(ip,screenlog_dir,retry=10, stop_if_EOF=False, lines_to_read=20000):
+def read_screenlog(ip, screenlog_dir, retry=10, stop_if_EOF=False, lines_to_read=20000):
     line = ""
     line_count = 0
     last_pos = 0
     transport_client = get_sftp_client(ip)
-    screen_log = "{0}/screenlog.0".format(screenlog_dir )
+    screen_log = "{0}/screenlog.0".format(screenlog_dir)
     op_file = transport_client.open(screen_log, 'r')
     while "Test Complete" not in line and line_count < lines_to_read:
         op_file.seek(last_pos)
@@ -202,12 +212,12 @@ def run_setup():
         temp = "{0} cbsystest.py import template {1}".format(python_exe, template)
         print temp
         _, stdout, _ = worker_client.exec_command("cd {0}; {1} cbsystest.py import template {2} --cluster {3}".
-                                                  format(cfg.WORKER_PYSYSTESTS_PATH,python_exe,template, cfg.CB_CLUSTER_TAG))
+                                                  format(cfg.WORKER_PYSYSTESTS_PATH, python_exe, template, cfg.CB_CLUSTER_TAG))
         for line in stdout.readlines():
             print line
     print ("Running test ...")
     _, stdout, _ = worker_client.exec_command("cd {0}; {1} cbsystest.py run test --cluster \'{2}\' --fromfile \'{3}\'".
-                                              format(cfg.WORKER_PYSYSTESTS_PATH,python_exe,cfg.CB_CLUSTER_TAG, cfg.SETUP_JSON))
+                                              format(cfg.WORKER_PYSYSTESTS_PATH, python_exe, cfg.CB_CLUSTER_TAG, cfg.SETUP_JSON))
     read_screenlog(cfg.WORKERS[0], cfg.WORKER_PYSYSTESTS_PATH)
     worker_client.close()
 
@@ -239,7 +249,10 @@ def pre_install_check():
         print("##### Pre-install inspection #####")
         print("Inspecting Couchbase server VMs ...")
         for vm_ip in cfg.CLUSTER_IPS:
-            vm_client = get_ssh_client(vm_ip)
+            if cfg.COUCHBASE_OS == "windows":
+                vm_client = get_ssh_client(vm_ip, cfg.COUCHBASE_SSH_USER, cfg.COUCHBASE_SSH_PASSWORD)
+            else:
+                vm_client = get_ssh_client(vm_ip)
             vm_client.close()
         print ("Inspecting RabbitMQ ...")
         rabbitmq = get_ssh_client(cfg.RABBITMQ_IP)
@@ -250,6 +263,7 @@ def pre_install_check():
         print ("Inspecting CBMonitor ...")
         cbmonitor = get_ssh_client(cfg.SERIESLY_IP)
         cbmonitor.close()
+        print("Inspection complete!")
     except Exception as e:
         print e
         sys.exit()
@@ -258,24 +272,24 @@ def upload_stats():
     print "\n##### Uploading stats to CBFS #####"
     worker_client = get_ssh_client(cfg.WORKERS[0])
     push_stats_cmd = "cd {0}; {1} tools/push_stats.py  --version {2} --build {3} --spec {4} \
-    --name {5} --cluster {6}".format(cfg.WORKER_PYSYSTESTS_PATH,python_exe, args['build'].split('-')[0],args['build'].split('-')[1],
-    cfg.TEST_JSON,  cfg.TEST_JSON[cfg.TEST_JSON.rfind('/')+1 : cfg.TEST_JSON.find('.')] , cfg.CB_CLUSTER_TAG)
-    print ("Executing {}".format(push_stats_cmd))
-    _, stdout, _  = worker_client.exec_command(push_stats_cmd)
+    --name {5} --cluster {6}".format(cfg.WORKER_PYSYSTESTS_PATH, python_exe, args['build'].split('-')[0], args['build'].split('-')[1],
+    cfg.TEST_JSON, cfg.TEST_JSON[cfg.TEST_JSON.rfind('/') + 1 : cfg.TEST_JSON.find('.')] , cfg.CB_CLUSTER_TAG)
+    print ("Executing {0}".format(push_stats_cmd))
+    _, stdout, _ = worker_client.exec_command(push_stats_cmd)
     time.sleep(30)
     for line in stdout.readlines():
         print line
     worker_client.close()
 
 def install_couchbase():
-    print("Inspection complete! \nInstalling version {0} Couchbase on servers ...".format(args['build']))
+    print("Installing version {0} Couchbase on servers ...".format(args['build']))
     install_cmd = "cd ..; {0} scripts/install.py -i {1} -p product=cb,version={2},parallel=true,{3}".\
                     format(python_exe, cfg.CLUSTER_INI, args['build'], args['params'])
-    print("Executing : {}".format(install_cmd))
+    print("Executing : {0}".format(install_cmd))
     os.system(install_cmd)
     if cfg.CLUSTER_RAM_QUOTA != "":
         os.system("curl -d memoryQuota={0} \"http://{1}:{2}@{3}:8091/pools/default\"".
-                  format(cfg.CLUSTER_RAM_QUOTA, cfg.COUCHBASE_USER,cfg.COUCHBASE_PWD,cfg.CLUSTER_IPS[0]))
+                  format(cfg.CLUSTER_RAM_QUOTA, cfg.COUCHBASE_USER, cfg.COUCHBASE_PWD, cfg.CLUSTER_IPS[0]))
     for ip in cfg.CLUSTER_IPS:
         os.system("curl -X POST -d \'ale:set_loglevel(xdcr_trace, debug).\' \"http://{0}:{1}@{2}:8091/diag/eval\"".
                   format(cfg.COUCHBASE_USER, cfg.COUCHBASE_PWD, ip))
@@ -288,32 +302,32 @@ def run(args):
     exlargs = args['exclude']
 
     # Pre-install check
-    "inspect" in exlargs and warn_skip("Inspection") or pre_install_check()
+    ("inspect" in exlargs) and warn_skip("Inspection") or pre_install_check()
 
     # Install Couchbase
-    "install" in exlargs and warn_skip("Installation") or install_couchbase()
+    ("install" in exlargs) and warn_skip("Installation") or install_couchbase()
 
     # Setup RabbitMQ
-    "rabbitmq" in exlargs and warn_skip("RabbitMQ") or start_rabbitmq()
+    ("rabbitmq" in exlargs) and warn_skip("RabbitMQ") or start_rabbitmq()
 
     # Setup Seriesly
-    "seriesly" in exlargs and warn_skip("Seriesly") or start_seriesly()
+    ("seriesly" in exlargs) and warn_skip("Seriesly") or start_seriesly()
 
     # Start workers
-    "worker" in exlargs and warn_skip("Celery Worker setup") or\
+    ("worker" in exlargs) and warn_skip("Celery Worker setup") or\
                                [start_worker(ip) for ip in cfg.WORKERS]
 
     # Cluster-setup/create buckets, set RAM quota
-    "setup" in exlargs and warn_skip("Cluster setup") or run_setup()
+    ("setup" in exlargs) and warn_skip("Cluster setup") or run_setup()
 
     # Start cbmonitor
-    "cbmonitor" in exlargs and warn_skip("CBMonitor") or start_cbmonitor()
+    ("cbmonitor" in exlargs) and warn_skip("CBMonitor") or start_cbmonitor()
 
     # Run test
-    "systest" in exlargs and warn_skip("System Test") or run_test()
+    ("systest" in exlargs) and warn_skip("System Test") or run_test()
 
     # Upload stats
-    "stats" in exlargs and warn_skip("Uploading Stats to CBFS") or upload_stats()
+    ("stats" in exlargs) and warn_skip("Uploading Stats to CBFS") or upload_stats()
 
     print("\n############################# Execution Complete! #################################")
 
@@ -334,17 +348,15 @@ if __name__ == "__main__":
                             help="optional param: inspect install rabbitmq seriesly worker cbmonitor setup systest stats",
                             required=False)
 
-    args = vars(parser.parse_args())
-    testcfg = args['testcfg']
     try:
-        os.system("cp -f {} testcfg.py".format(testcfg))
-        print "Copied {0} to {1} as testcfg.py".format(testcfg, os.getcwd())
+        args = vars(parser.parse_args())
+        testcfg = args['testcfg']
+        if os.path.basename(os.path.abspath(os.getcwd())) != 'pysystests':
+            raise Exception("Run script from testrunner/pysystests folder, current folder is: %s" % os.getcwd())
+        shutil.copy(testcfg, "./testcfg.py")
+        print "Copied {0} to {1}/testcfg.py".format(testcfg, os.getcwd())
         cfg = __import__("testcfg")
+        run(args)
     except Exception as e:
         print e
-        print "Recheck --testcfg param to contain relative path of the testcfg file inside tests/testcfg directory" \
-              "\nExample: --testcfg xdcr/testcfg_source.py if the file you are referring to is tests/testcfg/xdcr/testcfg_source.py"
-        sys.exit(1)
-
-    run(args)
-
+        raise
--- pysystests/tests/testcfg/xdcr/testcfg_win_source.py
+++ pysystests/tests/testcfg/xdcr/testcfg_win_source.py
@@ -1,56 +1,60 @@
-    #TODO: allow workers to pull this from cache
+#TODO: allow workers to pull this from cache
 
-RABBITMQ_IP = "127.0.0.1"
+RABBITMQ_IP = '172.23.106.224'
 RABBITMQ_LOG_LOCATION = ""
 CB_CLUSTER_TAG = "default"
 
-# Add location of cluster's .ini(from where this script is run) for installation
-CLUSTER_INI = ""
-CLUSTER_RAM_QUOTA = ""
+# Add location of cluster's .ini for installation
+CLUSTER_INI = "/tmp/kv_xdcr_win_source.ini"
+CLUSTER_RAM_QUOTA = "12500"
 
 # Add cluster setup json path, templates in triple quoted strings
-SETUP_JSON = "tests/simple_setup.js"
+SETUP_JSON = "/root/systest-worker/testrunner/pysystests/tests/xdcr/kv_xdcr_setup.js"
 SETUP_TEMPLATES = [r"""--name "default" --kvpair '"email":"$str10@couchbase.com" ' \
 '"city":"$str5"' '"list":["$int1","$str1","$fl o1"]' '"map":{"sample" : "$str3", "complex" : "$fl o1", "val" : "$int2"}' \
-'"num":"$int"' '"fl o":"$fl o"' '"st":"$str"' '"li":"$lis"' '"di":"$dic"' '"b":"$boo"' --size 128 256 512"""]
+'"num":"$int"' '"fl o":"$fl o"' '"st":"$str"' '"li":"$lis"' '"di":"$dic"' '"b":"$boo"' --size 128 256 512""",
+ r"""--name "template512" --kvpair '"email":"$str10@couchbase.com" ' '"city":"$str5"'  '"list":["$int1","$str1","$flo1"]' \
+ '"map":{"sample" : "$str3", "complex" : "$flo1", "val" : "$int2"}' '"num":"$int"' '"flo":"$flo"' '"st":"$str"' \
+  '"li":"$lis"' '"di":"$dic"' '"b":"$boo"' --size 512"""]
 
 # Add main system test json path and doc templates in triple quoted strings
-TEST_JSON = "tests/simple.js"
+TEST_JSON = "/root/systest-worker/testrunner/pysystests/tests/xdcr/kv_xdcr_source.js"
 TEST_TEMPLATES = [r"""--name "default" --kvpair '"email":"$str10@couchbase.com" ' \
 '"city":"$str5"' '"list":["$int1","$str1","$fl o1"]' '"map":{"sample" : "$str3", "complex" : "$fl o1", "val" : "$int2"}' \
-'"num":"$int"' '"fl o":"$fl o"' '"st":"$str"' '"li":"$lis"' '"di":"$dic"' '"b":"$boo"' --size 128 256 512"""]
+'"num":"$int"' '"fl o":"$fl o"' '"st":"$str"' '"li":"$lis"' '"di":"$dic"' '"b":"$boo"' --size 128 256 512""",
+ r"""--name "template512" --kvpair '"email":"$str10@couchbase.com" ' '"city":"$str5"'  '"list":["$int1","$str1","$flo1"]' \
+ '"map":{"sample" : "$str3", "complex" : "$flo1", "val" : "$int2"}' '"num":"$int"' '"flo":"$flo"' '"st":"$str"' \
+  '"li":"$lis"' '"di":"$dic"' '"b":"$boo"' --size 512"""]
 
-OBJECT_CACHE_IP = "127.0.0.1"
+OBJECT_CACHE_IP = "172.23.106.3"
 OBJECT_CACHE_PORT = "11911"
 
 # Add CBMonitor IP, absolute dir path and log location
-SERIESLY_IP = ''
+SERIESLY_IP = '172.23.107.16'
 SERIESLY_LOCATION = "/opt/gocode/bin"
 CBMONITOR_HOME_DIR = "/root/cbmonitor"
 SERIESLY_DB_LOCATION = "/root/db"
 
-# Must-fill params
-COUCHBASE_IP = '127.0.0.1'
+COUCHBASE_IP = '172.23.107.67'
 COUCHBASE_PORT = '8091'
 COUCHBASE_USER = "Administrator"
 COUCHBASE_PWD = "password"
-COUCHBASE_OS = "linux" # linux|windows|unix
-COUCHBASE_SSH_USER = "root"
-COUCHBASE_SSH_PASSWORD = "couchbase"
+COUCHBASE_OS = "windows" # linux|windows|unix
+COUCHBASE_SSH_USER = "Administrator"
+COUCHBASE_SSH_PASSWORD = "Membase123"
 
 SSH_USER = "root"
 SSH_PASSWORD = "couchbase"
-WORKERS = ['127.0.0.1']
+WORKERS = ['172.23.106.3']
 # valid configs ["kv","query","admin"] or ["all"]
 WORKER_CONFIGS = ["all"]
 WORKER_PYSYSTESTS_PATH = "/root/systest-worker/testrunner/pysystests"
 
 # CHANGE THIS! to match all clusters will be managed by this worker
 # ex. CLUSTER_IPS = ["127.0.0.1:9000","127.0.0.1:9001","127.0.0.1:9002","127.0.0.1:9003"]
-CLUSTER_IPS = ["127.0.0.1:8091"]
+CLUSTER_IPS = ['172.23.107.67', '172.23.107.68', '172.23.107.69', '172.23.107.70', '172.23.107.71', '172.23.107.72', '172.23.107.73', '172.23.107.74']
 
 # xdcr config
-
 """
 " pointer information to remote sites
 " remote1 = name for remote site
@@ -66,6 +70,11 @@ CLUSTER_IPS = ["127.0.0.1:8091"]
 "                             "COUCHBASE_IP" : "10.0.0.10",
 "                             "COUCHBASE_PORT" : "9000"}}
 """
+REMOTE_SITES = {"remote1" : {"RABBITMQ_IP" : "172.23.107.13",
+                             "CB_CLUSTER_TAG" : "default",
+                             "COUCHBASE_IP" : "172.23.107.75",
+                             "COUCHBASE_PORT" : "8091"}}
+
 
 LOGDIR = "logs"  # relative to current dir
 
