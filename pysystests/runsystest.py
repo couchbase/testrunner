c3d9a0ee73e5ff528f5cff9ff7efbf94cbd4acd4
Sangharsh Agarwal <sangharsh.agarwal@globallogic.com>
2014-10-28 14:31:00 +0530

    CBQE-0: fixing runsystest.py script, not to checkout testrunner on workers
    Change-Id: I52704f2ff3bcc3a6c11eff43079f5d541bdce608
    Reviewed-on: http://review.couchbase.org/42506
    Reviewed-by: Sangharsh Agarwal <sangharsh.agarwal@globallogic.com>
    Tested-by: Sangharsh Agarwal <sangharsh.agarwal@globallogic.com>

--- pysystests/runsystest.py
+++ pysystests/runsystest.py
@@ -117,10 +117,10 @@ def start_worker(worker_ip):
     print("##### Setting up Celery Worker @ {0} #####".format(worker_ip))
     worker_client = get_ssh_client(worker_ip)
 
-    # Update Worker's testrunner repository
-    repo_dir = os.path.dirname(os.path.dirname(cfg.WORKER_PYSYSTESTS_PATH))
-    worker_client.exec_command("rm -rf {0}; mkdir -p {0}".format(repo_dir))
-    worker_client.exec_command("cd {0}; git clone https://github.com/couchbase/testrunner.git".format(repo_dir))
+#    # Update Worker's testrunner repository
+#    repo_dir = os.path.dirname(os.path.dirname(cfg.WORKER_PYSYSTESTS_PATH))
+#    worker_client.exec_command("rm -rf {0}; mkdir -p {0}".format(repo_dir))
+#    worker_client.exec_command("cd {0}; git clone https://github.com/couchbase/testrunner.git".format(repo_dir))
 
     # Copy testcfg.py file to all workers
     worker_client.open_sftp().put("./testcfg.py", os.path.join(cfg.WORKER_PYSYSTESTS_PATH, "testcfg.py"))
