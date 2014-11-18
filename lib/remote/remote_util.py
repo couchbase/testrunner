f6a3cfbd2260b370ece8acb505090178e87bead5
saigon <baolavn@gmail.com>
2014-11-18 12:36:20 -0800

    CBQE-0 remove extra ip
    Change-Id: Ida50c8ca8dc32ad0affdb847aa67bb5920aa72ec
    Reviewed-on: http://review.couchbase.org/43378
    Reviewed-by: Thuan Nguyen <soccon@gmail.com>
    Tested-by: Thuan Nguyen <soccon@gmail.com>

--- lib/remote/remote_util.py
+++ lib/remote/remote_util.py
@@ -1362,8 +1362,7 @@ class RemoteMachineShellConnection:
                                                  .format(process_name))
             self.log_command_output(output, error)
             if output and process_name in output[0]:
-                self.sleep(5, "{0} wait for process ended!" \
-                           .format(self.ip))
+                self.sleep(5, "wait for process ended!")
                 process_running = True
             else:
                 if process_running:
