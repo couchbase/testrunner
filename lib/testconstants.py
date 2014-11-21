346c40a706705799f1dbabbd92cd5c9ef805aae3
saigon <baolavn@gmail.com>
2014-11-20 17:03:37 -0800

    CBQE-0 add support cb 2.5.2
    Change-Id: I3778710211f6e3add9bb2f2b43e8ad8473949d55
    Reviewed-on: http://review.couchbase.org/43461
    Reviewed-by: Thuan Nguyen <soccon@gmail.com>
    Tested-by: Thuan Nguyen <soccon@gmail.com>

--- lib/testconstants.py
+++ lib/testconstants.py
@@ -6,8 +6,9 @@ MEMBASE_VERSIONS = ["1.5.4", "1.6.5.4-win64", "1.7.0", "1.7.1", "1.7.1.1", "1.7.
 COUCHBASE_DATA_PATH = "/opt/couchbase/var/lib/couchbase/data/"
 # remember update WIN_REGISTER_ID also when update COUCHBASE_VERSION
 COUCHBASE_VERSIONS = ["1.8.0r", "1.8.0", "1.8.1", "2.0.0", "2.0.1", "2.0.2", "2.1.0", "2.1.1", "2.2.0",
-                      "2.2.1", "2.5.0", "2.5.1", "3.0.0", "3.0.1", "3.0.2", "3.5.0"]
-COUCHBASE_VERSION_2 = ["2.0.0", "2.0.1", "2.0.2", "2.1.0", "2.1.1", "2.2.0", "2.2.1", "2.5.0", "2.5.1"]
+                      "2.2.1", "2.5.0", "2.5.1", "2.5.2", "3.0.0", "3.0.1", "3.0.2", "3.5.0"]
+COUCHBASE_VERSION_2 = ["2.0.0", "2.0.1", "2.0.2", "2.1.0", "2.1.1", "2.2.0", "2.2.1", "2.5.0", "2.5.1",
+                       "2.5.2"]
 COUCHBASE_VERSION_3 = ["3.0.0", "3.0.1", "3.0.2", "3.5.0"]
 WIN_CB_VERSION_3 = ["3.0.0", "3.0.1", "3.0.2", "3.5.0"]
 WIN_MEMBASE_DATA_PATH = '/cygdrive/c/Program\ Files/Membase/Server/var/lib/membase/data/'
