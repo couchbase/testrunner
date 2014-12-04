402f3bbb8b6576ecefea11521dbc18e211851a3a
saigon <baolavn@gmail.com>
2014-12-03 23:21:48 -0800

    CBQE-0 fix collect log path on windows
    Change-Id: I2a194a9bc2782913bcf5427dac9c8cd52276ae96
    Reviewed-on: http://review.couchbase.org/43944
    Reviewed-by: Thuan Nguyen <soccon@gmail.com>
    Tested-by: Thuan Nguyen <soccon@gmail.com>

--- lib/testconstants.py
+++ lib/testconstants.py
@@ -58,7 +58,7 @@ LINUX_CONFIG_FILE = '/opt/couchbase/var/lib/couchbase/config/config.dat'
 LINUX_MOXI_PATH = '/opt/moxi/bin/'
 LINUX_CW_LOG_PATH = "/opt/couchbase/var/lib/couchbase/tmp/"
 MAC_CW_LOG_PATH = "/Applications/Couchbase\ Server.app/Contents/Resources/couchbase-core/var/lib/couchbase/tmp"
-WINDOWS_CW_LOG_PATH = "/cygdrive/c/Program\ Files/Couchbase/Server/var/lib/couchbase/tmp/"
+WINDOWS_CW_LOG_PATH = "c:/Program Files/Couchbase/Server/var/lib/couchbase/tmp/"
 CLI_COMMANDS = ["cbbackup", "cbbrowse_logs", "cbcollect_info", "cbcompact", "cbdump-config", "cbenable_core_dumps.sh", \
                 "cbepctl", "cbhealthchecker", "cbrecovery", "cbreset_password", "cbrestore", "cbsasladm", "cbstats", \
                 "cbtransfer", "cbvbucketctl", "cbworkloadgen", "couchbase-cli", "couchbase-server", "couch_compact", \
