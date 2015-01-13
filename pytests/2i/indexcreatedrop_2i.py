72a8bd8271168c45820daf4ad7bffe1613b3ce53
Parag Agarwal <parag@couchbase.com>
2015-01-13 14:09:35 -0800

    CBQE-0: remove suite methods from 2i tests
    Cleanup was required for suite methods which were removed from the base query test class for 2i
    
    Change-Id: Ia94407e9134406e5e490ac83de91b5e2fc028a29
    Reviewed-on: http://review.couchbase.org/45288
    Reviewed-by: Thuan Nguyen <soccon@gmail.com>
    Reviewed-by: Parag Agarwal <agarwal.parag@gmail.com>
    Tested-by: Parag Agarwal <agarwal.parag@gmail.com>

--- pytests/2i/indexcreatedrop_2i.py
+++ pytests/2i/indexcreatedrop_2i.py
@@ -5,14 +5,8 @@ class SecondaryIndexingCreateDropTests(BaseSecondaryIndexingTests):
     def setUp(self):
         super(SecondaryIndexingCreateDropTests, self).setUp()
 
-    def suite_setUp(self):
-        super(SecondaryIndexingCreateDropTests, self).suite_setUp()
-
     def tearDown(self):
         super(SecondaryIndexingCreateDropTests, self).tearDown()
-
-    def suite_tearDown(self):
-        super(SecondaryIndexingCreateDropTests, self).suite_tearDown()
 
     def test_multi_create_drop_index(self):
         self.log.info("test_multi_create_drop_index")
