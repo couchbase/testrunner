501c6a32b52e235ed945449b64e7eacc34874af0
Sangharsh Agarwal <sangharsh.agarwal@globallogic.com>
2015-01-14 16:54:19 +0530

    CBQE-0 Move pytests/xdcr/hostnamexdcrtest.py as per new base test pytests/xdcr/xdcrnewbasetests.py
    Change-Id: Ice313594487d80d3b724431fb3e4427aefb78b00
    Reviewed-on: http://review.couchbase.org/45316
    Reviewed-by: Sangharsh Agarwal <sangharsh.agarwal@globallogic.com>
    Tested-by: Sangharsh Agarwal <sangharsh.agarwal@globallogic.com>

--- pytests/xdcr/hostnamexdcrtest.py
+++ pytests/xdcr/hostnamexdcrtest.py
@@ -1,13 +1,19 @@
-from xdcrbasetests import XDCRReplicationBaseTest
-from couchbase_helper.documentgenerator import BlobGenerator
+from xdcrnewbasetests import XDCRNewBaseTest
+from xdcrnewbasetests import Utility
+from xdcrnewbasetests import BUCKET_NAME
 from hostname.hostnamemgmt_base import HostnameBaseTests
 
 
-#Assumption that at least 2 nodes on every cluster
-class HostnameXdcrTest(XDCRReplicationBaseTest, HostnameBaseTests):
+# Assumption that at least 2 nodes on every cluster
+class HostnameXdcrTest(XDCRNewBaseTest, HostnameBaseTests):
     def setUp(self):
         super(HostnameXdcrTest, self).setUp()
-        self.bidirection = self._input.param("bidirection", False)
+        self.src_cluster = self.get_cb_cluster_by_name('C1')
+        self.src_master = self.src_cluster.get_master_node()
+        self.dest_cluster = self.get_cb_cluster_by_name('C2')
+        self.dest_master = self.dest_cluster.get_master_node()
+        self.src_nodes = self.src_cluster.get_nodes()
+        self.dest_nodes = self.dest_cluster.get_nodes()
 
     def tearDown(self):
         self._deinitialize_servers(self.src_nodes)
@@ -15,45 +21,42 @@ class HostnameXdcrTest(XDCRReplicationBaseTest, HostnameBaseTests):
         super(HostnameXdcrTest, self).tearDown()
 
     def test_hostnames_xdcr(self):
-        gen_load = BlobGenerator('hostname', 'hostname', 1024, end=self.num_items)
-        self._load_all_buckets(self.src_master, gen_load, "create", 0)
-        self.merge_buckets(self.src_master, self.dest_master, bidirection=self.bidirection)
-        self.verify_referenced_by_names(self.src_nodes, self.hostnames)
-        self.verify_referenced_by_names(self.dest_nodes, self.hostnames)
-        for i in xrange(len(self.src_nodes)):
-            self.src_nodes[i].hostname = self.hostnames[self.src_nodes[i]]
-        for i in xrange(len(self.dest_nodes)):
-            self.dest_nodes[i].hostname = self.hostnames[self.dest_nodes[i]]
-        self.verify_xdcr_stats(self.src_nodes, self.dest_nodes)
+        self.setup_xdcr_and_load()
+        self.verify_referenced_by_names(self.src_nodes, self.src_cluster.get_host_names())
+        self.verify_referenced_by_names(self.dest_nodes, self.dest_cluster.get_host_names())
+        self.verify_results()
 
     def test_replication_with_view_queries(self):
-        self.verify_referenced_by_names(self.src_nodes, self.hostnames)
-        self.verify_referenced_by_names(self.dest_nodes, self.hostnames)
-        self._load_all_buckets(self.src_master, self.gen_create, "create", 0)
-        self.sleep(self.wait_timeout)
-        src_buckets = self._get_cluster_buckets(self.src_master)
-        dest_buckets = self._get_cluster_buckets(self.dest_master)
+        self.verify_referenced_by_names(self.src_nodes, self.src_cluster.get_host_names())
+        self.verify_referenced_by_names(self.dest_nodes, self.dest_cluster.get_host_names())
+
+        self.setup_xdcr_and_load()
+
+        num_views = self._input.param("num_views", 5)
+        is_dev_ddoc = self._input.param("is_dev_ddoc", True)
+        src_buckets = self.src_cluster.get_buckets()
+        dest_buckets = self.dest_cluster.get_buckets()
         for bucket in src_buckets:
-            views = self.make_default_views(bucket.name, self._num_views, self._is_dev_ddoc)
+            views = Utility.make_default_views(bucket.name, num_views, is_dev_ddoc)
         for bucket in dest_buckets:
-            views = self.make_default_views(bucket.name, self._num_views, self._is_dev_ddoc)
+            views = Utility.make_default_views(bucket.name, num_views, is_dev_ddoc)
         ddoc_name = "ddoc1"
-        prefix = ("", "dev_")[self._is_dev_ddoc]
+        prefix = ("", "dev_")[is_dev_ddoc]
         query = {"full_set" : "true", "stale" : "false"}
 
-        tasks = self.async_create_views(self.src_master, ddoc_name, views, self.default_bucket_name)
-        tasks += self.async_create_views(self.dest_master, ddoc_name, views, self.default_bucket_name)
+        tasks = self.src_cluster.async_create_views(ddoc_name, views, BUCKET_NAME.DEFAULT)
+        tasks += self.dest_cluster.async_create_views(ddoc_name, views, BUCKET_NAME.DEFAULT)
         for task in tasks:
             task.result(self._poll_timeout)
+
+        self.merge_all_buckets()
         tasks = []
         for view in views:
-            tasks.append(self.cluster.async_query_view(self.src_master, prefix + ddoc_name, view.name, query, self.num_items))
-            tasks.append(self.cluster.async_query_view(self.dest_master, prefix + ddoc_name, view.name, query, self.num_items))
+            tasks.append(self.src_cluster.async_query_view(prefix + ddoc_name, view.name, query, src_buckets[0].kvs[1].__len__()))
+            tasks.append(self.dest_cluster.async_query_view(prefix + ddoc_name, view.name, query, dest_buckets[0].kvs[1].__len__()))
         for task in tasks:
             task.result(self._poll_timeout)
 
-        self.merge_buckets(self.src_master, self.dest_master, bidirection=False)
-
-        self.verify_xdcr_stats(self.src_nodes, self.dest_nodes)
-        self.verify_referenced_by_names(self.src_nodes, self.hostnames)
-        self.verify_referenced_by_names(self.dest_nodes, self.hostnames)
+        self.verify_results()
+        self.verify_referenced_by_names(self.src_nodes, self.src_cluster.get_host_names())
+        self.verify_referenced_by_names(self.dest_nodes, self.dest_cluster.get_host_names())
