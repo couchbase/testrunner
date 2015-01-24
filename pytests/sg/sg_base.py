c7ecad54926a9e9e38328afd52eb0adabb50a571
Andrei Baranouski <andrei.baranouski@gmail.com>
2015-01-25 01:45:34 +0300

    CBQE-0: SG installation test. initial version
    Change-Id: If66c425b56811a242913fae7b4497429d387c229
    Reviewed-on: http://review.couchbase.org/45847
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Andrei Baranouski <andrei.baranouski@gmail.com>

---
+++ pytests/sg/sg_base.py
@@ -0,0 +1,98 @@
+import unittest
+import logging
+import logger
+from TestInput import TestInputSingleton
+from remote.remote_util import RemoteMachineShellConnection, RemoteUtilHelper
+import requests
+from requests.exceptions import ConnectionError
+
+
+class GatewayBaseTest(unittest.TestCase):
+
+    BUILDS = {
+        'http://packages.couchbase.com/builds/mobile/sync_gateway': (
+            '0.0.0/{0}/couchbase-sync-gateway_{0}_x86_64-community.rpm',
+            '1.0.0/{0}/couchbase-sync-gateway_{0}_x86_64.rpm',
+        ),
+        'http://packages.couchbase.com.s3.amazonaws.com/builds/mobile/sync_gateway': (
+            '0.0.0/{0}/couchbase-sync-gateway-community_{0}_x86_64.rpm',
+            '1.0.0/{0}/couchbase-sync-gateway-enterprise_{0}_x86_64.rpm',
+            '1.0.1/{0}/couchbase-sync-gateway-enterprise_{0}_x86_64.rpm',
+            '1.0.2/{0}/couchbase-sync-gateway-community_{0}_x86_64.rpm',
+            '1.0.3/{0}/couchbase-sync-gateway-community_{0}_x86_64.rpm',
+            '1.0.4/{0}/couchbase-sync-gateway-community_{0}_x86_64.rpm',
+            '1.1.0/{0}/couchbase-sync-gateway-community_{0}_x86_64.rpm',
+        ),
+        'http://cbfs-ext.hq.couchbase.com/builds': (
+            'couchbase-sync-gateway_{}_x86_64-community.rpm',
+        ),
+        'http://latestbuilds.hq.couchbase.com/couchbase-sync-gateway': (
+            '0.0.0/416/couchbase-sync-gateway-enterprise_0.0.0-326_x86_64.rpm',
+        ),
+    }
+
+    def setUp(self):
+        self.log = logger.Logger.get_logger()
+        self.input = TestInputSingleton.input
+        self.version = self.input.param("version", "0.0.0-422")
+        self.servers = self.input.servers
+        self.master = self.servers[0]
+
+
+    def find_package(self):
+        for filename, url in self.get_expected_locations():
+            try:
+                status_code = requests.head(url).status_code
+            except ConnectionError:
+                pass
+            else:
+                if status_code == 200:
+                    self.log.info('Found "{}"'.format(url))
+                    return filename, url
+        self.log.interrupt('Target build not found')
+
+    def get_expected_locations(self):
+        for location, patterns in self.BUILDS.items():
+            for pattern in patterns:
+                url = '{}/{}'.format(location, pattern.format(self.version))
+                filename = url.split('/')[-1]
+                yield filename, url
+
+    def kill_processes_gateway(self, shell):
+        self.log.info('Killing Sync Gateway')
+        shell.execute_command('killall -9 sync_gateway sgw_test_info.sh sar')  # ???? check command
+
+
+    def uninstall_gateway(self, shell):
+        self.log.info('Uninstalling Sync Gateway package')
+        output, error = shell.execute_command('yes | yum remove couchbase-sync-gateway')
+        shell.log_command_output(output, error)
+        # do we need any additional cleanup?
+
+    def install_gateway(self, shell):
+        filename, url = self.find_package()
+        self.log.info('Installing Sync Gateway package - {}'.format(filename))
+        output, error = shell.execute_command_raw('cd /tmp;wget -q -O {0} {1};cd /tmp;ls -lh'.format(filename, url))
+        shell.log_command_output(output, error)
+        output, error = shell.execute_command_raw('yes | rpm -i /tmp/{}'.format(filename))
+        shell.log_command_output(output, error)
+
+
+    def start_sync_gateways(self, shell):
+          self.log.info('Starting Sync Gateway instances')
+          output, error = shell.execute_command_raw('nohup /opt/couchbase-sync-gateway/bin/sync_gateway'
+                                                   '/root/gateway_config.json >/root/gateway.log 2>&1 &')
+          shell.log_command_output(output, error)
+
+
+    def install(self, shell):
+        self.kill_processes_gateway(shell)
+        self.uninstall_gateway(shell)
+        self.install_gateway(shell)
+        self.start_sync_gateways(shell)
+
+    def run_sync_gateway_service_install(self, shell, options=""):
+        output, error = shell.execute_command_raw('/opt/couchbase-sync-gateway/service/sync_gateway_service_install.sh ' + options)
+        shell.log_command_output(output, error)
+        return output, error
+
---
+++ pytests/sg/sginstalltests.py
@@ -0,0 +1,56 @@
+
+
+from basetestcase import BaseTestCase
+
+from TestInput import TestInputSingleton
+from sg.sg_base import GatewayBaseTest
+from remote.remote_util import RemoteMachineShellConnection
+
+help = ['This script creates an init service to run a sync_gateway instance.',
+        'If you want to install more than one service instance',
+        'create additional services with different names.',
+        '', 'sync_gateway_service_install.sh', '    -h --help',
+        '    --runas=<The user account to run sync_gateway as; default (sync_gateway)>',
+        '    --runbase=<The directory to run sync_gateway from; defaut (/home/sync_gateway)>',
+        '    --sgpath=<The path to the sync_gateway executable; default (/opt/couchbase-sync-gateway/bin/sync_gateway)>',
+        '    --cfgpath=<The path to the sync_gateway JSON config file; default (/home/sync_gateway/sync_gateway.json)>',
+        '    --logsdir=<The path to the log file direcotry; default (/home/sync_gateway/logs)>', '']
+
+
+class SGInstallerTest(GatewayBaseTest):
+    def setUp(self):
+        TestInputSingleton.input.test_params["default_bucket"] = False
+        super(SGInstallerTest, self).setUp()
+
+    def tearDown(self):
+        super(SGInstallerTest, self).tearDown()
+
+
+    def basicInstall(self):
+        for server in self.servers:
+            shell = RemoteMachineShellConnection(server)
+            self.install(shell)
+            self.kill_processes_gateway(shell)
+            self.uninstall_gateway(shell)
+            shell.disconnect()
+
+
+    def testSGServiceInstallHelp(self):
+        shell = RemoteMachineShellConnection(self.master)
+        self.kill_processes_gateway(shell)
+        self.uninstall_gateway(shell)
+        self.install_gateway(shell)
+        output, error = self.run_sync_gateway_service_install(shell, "-h")
+        self.assertEqual(error, [])
+        self.assertEqual(output, help)
+
+        output, error = self.run_sync_gateway_service_install(shell)
+        self.assertEqual(error[0], "The sync_gateway runtime user account does not exist \"sync_gateway\".")
+        self.assertEqual(output, [])
+
+        output, error = self.run_sync_gateway_service_install(shell, "bla-bla-bla")
+        temp_help = ["ERROR: unknown parameter \"bla-bla-bla\""]
+        temp_help.extend(help)
+
+        self.assertEqual(error, [])
+        self.assertEqual(output, temp_help)
