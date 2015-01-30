81e21aca55d2738358f3ac32953e9d9138af2767
arunapiravi <aruna@couchbase.com>
2015-01-30 12:44:35 -0800

    CBQE:0 size is now bucket_size after the last xdcrnewbasetests rewrite
    Change-Id: If3690eab568a6c765de56cb8c60cb1a0ff87be82
    Reviewed-on: http://review.couchbase.org/46147
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Aruna Piravi <aruna@couchbase.com>

--- pytests/xdcr/uniXDCR.py
+++ pytests/xdcr/uniXDCR.py
@@ -499,7 +499,7 @@ class unidirectional(XDCRNewBaseTest):
         bucket = self.dest_cluster.get_bucket_by_name(BUCKET_NAME.DEFAULT)
         self.dest_cluster.delete_bucket(BUCKET_NAME.DEFAULT)
 
-        self.dest_cluster.create_default_bucket(bucket.size, bucket.numReplicas, bucket.eviction_policy, bucket.bucket_priority)
+        self.dest_cluster.create_default_bucket(bucket.bucket_size)
 
         self.sleep(self._wait_timeout)
 
