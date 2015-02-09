0749710fe17da9380318bbddf830a69b4a63fd7e
IrynaMironava <irynamironava@yandex.ru>
2015-02-09 18:56:00 +0300

    forcefully kill all cbq engines after each suite
    Change-Id: I00fe965ebd14c18834e60c643a2f653a24009cbd
    Reviewed-on: http://review.couchbase.org/46595
    Reviewed-by: Ketaki Gangal <ketakigangal@gmail.com>
    Tested-by: buildbot <build@couchbase.com>

--- pytests/tuqquery/tuq.py
+++ pytests/tuqquery/tuq.py
@@ -78,8 +78,13 @@ class QueryTests(BaseTestCase):
     def suite_tearDown(self):
         if not self.input.param("skip_build_tuq", False):
             if hasattr(self, 'shell'):
-                self.shell.execute_command("killall cbq-engine")
-                self.shell.disconnect()
+               o = self.shell.execute_command("ps -aef| grep cbq-engine")
+               self.log.info(o)
+               if len(o):
+                   for cbq_engine in o[0]:
+                       if cbq_engine.find('grep') == -1:
+                           pid = [item for item in cbq_engine.split(' ') if item][1]
+                           self.shell.execute_command("kill -9 %s" % pid)
 
 
 ##############################################################################################
