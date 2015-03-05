b49b06ac5a87e646b3e45920d48bba77c58f9ff5
Parag Agarwal <parag@couchbase.com>
2015-03-05 15:15:40 -0800

    CBQE-0: Add robustness to load balancing tests, correct conf
    Change-Id: I7676f2e4ce0f6f8e5f9f04b7fd015e5f67ad6b90
    Reviewed-on: http://review.couchbase.org/47816
    Reviewed-by: Parag Agarwal <agarwal.parag@gmail.com>
    Tested-by: Parag Agarwal <agarwal.parag@gmail.com>

--- pytests/2i/index_load_balancing_2i.py
+++ pytests/2i/index_load_balancing_2i.py
@@ -11,16 +11,16 @@ class SecondaryIndexingLoadBalancingTests(BaseSecondaryIndexingTests):
 
     def test_index_create_sync(self):
         index_dist_factor = 2
-        #Create Index
-        servers = self.get_nodes_from_services_map(service_type = "index", get_all_nodes = True)
-        num_indexes=len(servers)*index_dist_factor
-        self.query_definitions = self._create_query_definitions(index_count = num_indexes)
-        self.run_multi_operations(buckets = self.buckets, query_definitions = self.query_definitions, create_index = True, drop_index = False)
-        indexList = [query_definition.index_name for query_definition in self.query_definitions]
-        #Verify that the indexes are equally distributed
-        expected_index_count_per_node =  len(indexList)/len(servers)
-        index_map = self.get_index_stats(perNode=True)
         try:
+            #Create Index
+            servers = self.get_nodes_from_services_map(service_type = "index", get_all_nodes = True)
+            num_indexes=len(servers)*index_dist_factor
+            self.query_definitions = self._create_query_definitions(index_count = num_indexes)
+            self.run_multi_operations(buckets = self.buckets, query_definitions = self.query_definitions, create_index = True, drop_index = False)
+            indexList = [query_definition.index_name for query_definition in self.query_definitions]
+            #Verify that the indexes are equally distributed
+            expected_index_count_per_node =  len(indexList)/len(servers)
+            index_map = self.get_index_stats(perNode=True)
             for node in index_map.keys():
                 total_indexes_per_node=0
                 self.log.info(" verifying node {0}".format(node))
@@ -36,20 +36,20 @@ class SecondaryIndexingLoadBalancingTests(BaseSecondaryIndexingTests):
 
     def test_index_create_async(self):
         index_dist_factor = 1
-        #Create Index
-        servers = self.get_nodes_from_services_map(service_type = "index", get_all_nodes = True)
-        num_indexes=len(servers)*index_dist_factor
-        self.query_definitions = self._create_query_definitions(index_count = num_indexes)
-        self.log.info(self.query_definitions)
-        tasks = self.async_run_multi_operations(buckets = self.buckets, query_definitions = self.query_definitions,
-         create_index = True, drop_index = False)
-        for task in tasks:
-            task.result()
-        indexList = [query_definition.index_name for query_definition in self.query_definitions]
-        #Verify that the indexes are equally distributed
-        expected_index_count_per_node =  len(indexList)/len(servers)
-        index_map = self.get_index_stats(perNode=True)
         try:
+            #Create Index
+            servers = self.get_nodes_from_services_map(service_type = "index", get_all_nodes = True)
+            num_indexes=len(servers)*index_dist_factor
+            self.query_definitions = self._create_query_definitions(index_count = num_indexes)
+            self.log.info(self.query_definitions)
+            tasks = self.async_run_multi_operations(buckets = self.buckets, query_definitions = self.query_definitions,
+             create_index = True, drop_index = False)
+            for task in tasks:
+                task.result()
+            indexList = [query_definition.index_name for query_definition in self.query_definitions]
+            #Verify that the indexes are equally distributed
+            expected_index_count_per_node =  len(indexList)/len(servers)
+            index_map = self.get_index_stats(perNode=True)
             for node in index_map.keys():
                 total_indexes_per_node=0
                 self.log.info(" verifying node {0}".format(node))
@@ -65,18 +65,18 @@ class SecondaryIndexingLoadBalancingTests(BaseSecondaryIndexingTests):
 
     def test_index_create_drop_sync(self):
         index_dist_factor = 4
-        #Create Index
-        servers = self.get_nodes_from_services_map(service_type = "index", get_all_nodes = True)
-        num_indexes=len(servers)*index_dist_factor
-        self.query_definitions = self._create_query_definitions(index_count = num_indexes)
-        self.run_multi_operations(buckets = self.buckets, query_definitions = self.query_definitions, create_index = True, drop_index = False)
-        indexList = [query_definition.index_name for query_definition in self.query_definitions]
-        #Verify that the indexes are equally distributed
-        expected_index_count_per_node =  len(indexList)/len(servers)
-        #drop the indexes
-        query_definitions = [self.query_definitions[x] for x in range(0,num_indexes/2)]
-        delete_query_definitions = [self.query_definitions[x] for x in range(num_indexes/2,num_indexes)]
         try:
+            #Create Index
+            servers = self.get_nodes_from_services_map(service_type = "index", get_all_nodes = True)
+            num_indexes=len(servers)*index_dist_factor
+            self.query_definitions = self._create_query_definitions(index_count = num_indexes)
+            self.run_multi_operations(buckets = self.buckets, query_definitions = self.query_definitions, create_index = True, drop_index = False)
+            indexList = [query_definition.index_name for query_definition in self.query_definitions]
+            #Verify that the indexes are equally distributed
+            expected_index_count_per_node =  len(indexList)/len(servers)
+            #drop the indexes
+            query_definitions = [self.query_definitions[x] for x in range(0,num_indexes/2)]
+            delete_query_definitions = [self.query_definitions[x] for x in range(num_indexes/2,num_indexes)]
             self.run_multi_operations(buckets = self.buckets, query_definitions = query_definitions, create_index = False, drop_index = True)
             index_map = self.get_index_stats(perNode=True)
             query_definitions = self._create_query_definitions(start= num_indexes, index_count=num_indexes/2)
@@ -98,13 +98,13 @@ class SecondaryIndexingLoadBalancingTests(BaseSecondaryIndexingTests):
 
     def test_query_using_index_sync(self):
         index_dist_factor = 1
-        #Create Index
-        servers = self.get_nodes_from_services_map(service_type = "index", get_all_nodes = True)
-        num_indexes=len(servers)*index_dist_factor
-        self.query_definitions = self._create_query_definitions(index_count = num_indexes)
-        self.run_multi_operations(buckets = self.buckets, query_definitions = self.query_definitions, create_index = True, query = True)
-        index_map = self.get_index_stats(perNode=True)
         try:
+            #Create Index
+            servers = self.get_nodes_from_services_map(service_type = "index", get_all_nodes = True)
+            num_indexes=len(servers)*index_dist_factor
+            self.query_definitions = self._create_query_definitions(index_count = num_indexes)
+            self.run_multi_operations(buckets = self.buckets, query_definitions = self.query_definitions, create_index = True, query = True)
+            index_map = self.get_index_stats(perNode=True)
             for node in index_map.keys():
                 self.log.info(" verifying node {0}".format(node))
                 for bucket_name in index_map[node].keys():
