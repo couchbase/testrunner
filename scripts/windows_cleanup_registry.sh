27385b268581b0045df81a257c55e3753d342422
saigon <baolavn@gmail.com>
2015-03-10 19:21:35 -0700

    CBQE-2633 fix typo when add comment
    Change-Id: I3181d451ebbbff5f578e5ffca949cc61b9cf21f4
    Reviewed-on: http://review.couchbase.org/48067
    Reviewed-by: Thuan Nguyen <soccon@gmail.com>
    Tested-by: Thuan Nguyen <soccon@gmail.com>

--- scripts/windows_cleanup_registry.sh
+++ scripts/windows_cleanup_registry.sh
@@ -2,9 +2,9 @@
 # How to run:
 # python scripts/ssh.py  -i your_bad_vms.ini -p script=scripts/windows_cleanup_registry.sh
 #
-echo -e "delete "HKEY_CLASSES_ROOT\Installer\UpgradeCodes\DAFE44492BF730D45B002C1133EA9A42 \n"
+echo -e "delete HKEY_CLASSES_ROOT\Installer\UpgradeCodes\DAFE44492BF730D45B002C1133EA9A42 \n"
 reg delete "HKEY_CLASSES_ROOT\Installer\UpgradeCodes\DAFE44492BF730D45B002C1133EA9A42" /f
-echo -e "delete HKEY_CLASSES_ROOT\Installer\Products\288F9D42C18440B25027002B37EC713B" \n"
+echo -e "delete HKEY_CLASSES_ROOT\Installer\Products\288F9D42C18440B25027002B37EC713B \n"
 reg delete "HKEY_CLASSES_ROOT\Installer\Products\288F9D42C18440B25027002B37EC713B" /f
 #
 echo -e "Start delete folder c/Program\ Files/Couchbase/Serve \n"
