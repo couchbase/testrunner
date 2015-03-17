60b1980f8c931a518bb95a7e43edfbab3acf6d22
saigon <baolavn@gmail.com>
2015-03-17 13:33:01 -0700

    CBQE-0 add remove sherlock registry in windows
    Change-Id: Id068984105b048ce2fd98156f63b9b378d923cb3
    Reviewed-on: http://review.couchbase.org/48418
    Reviewed-by: Thuan Nguyen <soccon@gmail.com>
    Tested-by: Thuan Nguyen <soccon@gmail.com>

--- scripts/windows_cleanup_registry.sh
+++ scripts/windows_cleanup_registry.sh
@@ -6,6 +6,8 @@ echo -e "delete HKEY_CLASSES_ROOT\Installer\UpgradeCodes\DAFE44492BF730D45B002C1
 reg delete "HKEY_CLASSES_ROOT\Installer\UpgradeCodes\DAFE44492BF730D45B002C1133EA9A42" /f
 echo -e "delete HKEY_CLASSES_ROOT\Installer\Products\288F9D42C18440B25027002B37EC713B \n"
 reg delete "HKEY_CLASSES_ROOT\Installer\Products\288F9D42C18440B25027002B37EC713B" /f
-#
+# for sherloc, delete key below
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\InstallShield_{24D9F882-481C-2B04-0572-00B273CE17B3}" /f
+##
 echo -e "Start delete folder c/Program\ Files/Couchbase/Serve \n"
 rm -rf /cygdrive/c/Program\ Files/Couchbase/Server
