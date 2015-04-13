953b229e8caa153ad821d0e731bebde5d8744ed4
Parag Agarwal <parag@couchbase.com>
2015-04-12 23:12:01 -0700

    CBQE-2759:  Implement support for testing without dependency on mysql
    - Define flag:: use_mysql=True/False to work against mysql or load data from file
    - Define mechanism to take snap-shot of mysql queries result runs
    - Define mechanism to take snap-shot of mysql data
    - Define mechanism to load from zip data to couchbase
    - Define mechanism to run test using a file which defines expected data, sql and mysql queries
    
    Change-Id: I382bae9d93949105374a62ad7acc97214aec95f4
    Reviewed-on: http://review.couchbase.org/49579
    Reviewed-by: Parag Agarwal <agarwal.parag@gmail.com>
    Tested-by: Parag Agarwal <agarwal.parag@gmail.com>

--- pytests/rqg/test_rqg.py
+++ pytests/rqg/test_rqg.py
@@ -1,5 +1,7 @@
 from basetestcase import BaseTestCase
 import json
+import os
+import zipfile
 from membase.helper.cluster_helper import ClusterOperationHelper
 import mc_bin_client
 from memcached.helper.data_helper import  VBucketAwareMemcached
@@ -9,12 +11,13 @@ from couchbase_helper.tuq_helper import N1QLHelper
 from couchbase_helper.query_helper import QueryHelper
 
 class RQGTests(BaseTestCase):
-    """ Class for defining tests for python sdk """
+    """ Class for defining tests for RQG base testing """
 
     def setUp(self):
         super(RQGTests, self).setUp()
         self.log.info("==============  RQGTests setup was finished for test #{0} {1} =============="\
                       .format(self.case_number, self._testMethodName))
+        self.initial_loading_to_cb= self.input.param("initial_loading_to_cb",True)
         self.database= self.input.param("database","flightstats")
         self.user_id= self.input.param("user_id","root")
         self.password= self.input.param("password","")
@@ -23,15 +26,15 @@ class RQGTests(BaseTestCase):
         self.gen_gsi_indexes= self.input.param("gen_gsi_indexes",True)
         self.n1ql_server = self.get_nodes_from_services_map(service_type = "n1ql")
         self.query_helper = QueryHelper()
-        self._initialize_mysql_client()
-        self._setup_and_load_buckets()
         self._initialize_n1ql_helper()
-        self._build_primary_indexes()
+        if self.initial_loading_to_cb:
+            self._initialize_cluster_setup()
 
     def tearDown(self):
         super(RQGTests, self).tearDown()
 
     def test_rqg_example(self):
+        self._initialize_mysql_client()
         sql_query = "SELECT a1.* FROM ( `ontime_mysiam`  AS a1 INNER JOIN `carriers`  AS a2 ON ( a1.`carrier` = a2.`code` ) )"
         n1ql_query = "SELECT a1.* FROM `ontime_mysiam`  AS a1 INNER JOIN `carriers`  AS a2 ON KEYS [ a1.`carrier` ]"
         # Run n1ql query
@@ -39,6 +42,7 @@ class RQGTests(BaseTestCase):
         self.assertTrue(check, msg)
 
     def test_rqg_from_list(self):
+        self._initialize_mysql_client()
         self.n1ql_file_path= self.input.param("n1ql_file_path","default")
         self.sql_file_path= self.input.param("sql_file_path","default")
         with open(self.n1ql_file_path) as f:
@@ -59,7 +63,6 @@ class RQGTests(BaseTestCase):
             sql_query = sql_query_list[i]
             i+=1
             # Run n1ql query
-            hints = self.query_helper._find_hints(n1ql_query)
             success, msg = self._run_queries_compare(n1ql_query = n1ql_query , sql_query = sql_query)
             total += 1
             check = check and success
@@ -69,6 +72,39 @@ class RQGTests(BaseTestCase):
                 fail_case +=  1
                 failure_map["Case :: "+str(i-1)] = { "sql_query":sql_query, "n1ql_query": n1ql_query, "reason for failure": msg}
             self.log.info(" <<<<<<<<<<<<<<<<<<<<<<<<<<<< END RUNNING QUERY CASE NUMBER {0} >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>".format(i-1))
+        self.log.info(" Total Queries Run = {0}, Pass = {1}, Fail = {2}".format(total, pass_case, fail_case))
+        self.assertTrue(check, failure_map)
+
+    def test_rqg_from_file(self):
+        self.n1ql_file_path= self.input.param("n1ql_file_path","default")
+        with open(self.n1ql_file_path) as f:
+            n1ql_query_list = f.readlines()
+        self._generate_secondary_indexes(n1ql_query_list)
+        i = 0
+        check = True
+        pass_case = 0
+        total =0
+        fail_case = 0
+        failure_map = {}
+        for n1ql_query_info in n1ql_query_list:
+            # Run n1ql query
+            data = json.loads(n1ql_query_info)
+            case_number = data["test case number"]
+            n1ql_query = data["n1ql_query"]
+            sql_query = data["sql_query"]
+            expected_result = data["expected_result"]
+            hints = self.query_helper._find_hints(n1ql_query)
+            self.log.info(" <<<<<<<<<<<<<<<<<<<<<<<<<<<< BEGIN RUNNING QUERY  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>".format(case_number))
+
+            success, msg = self._run_queries_from_file_and_compare(n1ql_query = n1ql_query , sql_query = sql_query, sql_result = expected_result)
+            total += 1
+            check = check and success
+            if success:
+                pass_case += 1
+            else:
+                fail_case +=  1
+                failure_map[case_number] = { "sql_query":sql_query, "n1ql_query": n1ql_query, "reason for failure": msg}
+            self.log.info(" <<<<<<<<<<<<<<<<<<<<<<<<<<<< END RUNNING QUERY CASE NUMBER {0} >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>".format(case_number))
         self.log.info(" Total Queries Run = {0}, Pass = {1}, Fail = {2}".format(total, pass_case, fail_case))
         self.assertTrue(check, failure_map)
 
@@ -89,6 +125,56 @@ class RQGTests(BaseTestCase):
                 failure_list.append({"n1ql_query":n1ql_query, "reason":ex})
         self.assertTrue(check, failure_list)
 
+    def test_bootstrap_with_data(self):
+        self.log.info(" Data has been bootstrapped !!")
+        self.skip_cleanup=True
+
+    def test_take_mysql_query_response_snap_shot(self):
+        self._initialize_mysql_client()
+        self.file_prefix= self.input.param("file_prefix","default_rqg_test")
+        self.data_dump_path= self.input.param("data_dump_path","/tmp")
+        self.queries_per_dump_file= self.input.param("queries_per_dump_file",10000)
+        self.n1ql_file_path= self.input.param("n1ql_file_path","default")
+        self.sql_file_path= self.input.param("sql_file_path","default")
+        with open(self.n1ql_file_path) as f:
+            n1ql_query_list = f.readlines()
+        with open(self.sql_file_path) as f:
+            sql_query_list = f.readlines()
+        self._generate_secondary_indexes(n1ql_query_list)
+        i = 0
+        queries =0
+        file_number=0
+        f = open(self.data_dump_path+"/"+self.file_prefix+"_"+str(file_number)+".txt","w")
+        for n1ql_query in n1ql_query_list:
+            n1ql_query = n1ql_query.replace("\n","")
+            sql_query = sql_query_list[i].replace("\n","")
+            hints = self.query_helper._find_hints(n1ql_query)
+            columns, rows = self.client._execute_query(query = sql_query)
+            sql_result = self.client._gen_json_from_results(columns, rows)
+            if hints == "FUN":
+                sql_result = self._convert_fun_result(sql_result)
+            dump_data = {
+              "test case number":(i+1),
+              "n1ql_query":n1ql_query,
+              "sql_query":sql_query,
+              "expected_result":sql_result
+               }
+            i+=1
+            queries += 1
+            f.write(json.dumps(dump_data)+"\n")
+            if queries > self.queries_per_dump_file:
+                queries = 0
+                file_number = 1
+                f.close()
+                f = open(self.data_dump_path+"/"+self.file_prefix+"_"+file_number+".txt","w")
+        f.close()
+
+    def test_take_snapshot_of_database(self):
+        self._take_snapshot_of_database()
+
+    def test_load_data_of_database(self):
+        self._setup_and_load_buckets_from_files()
+
     def _run_n1ql_queries(self, n1ql_query = None):
         # Run n1ql query
         actual_result = self.n1ql_helper.run_cbq_query(query = n1ql_query, server = self.n1ql_server)
@@ -100,10 +186,28 @@ class RQGTests(BaseTestCase):
         hints = self.query_helper._find_hints(n1ql_query)
         actual_result = self.n1ql_helper.run_cbq_query(query = n1ql_query, server = self.n1ql_server)
         n1ql_result = actual_result["results"]
-        self.log.info(actual_result)
+        #self.log.info(actual_result)
         # Run SQL Query
         columns, rows = self.client._execute_query(query = sql_query)
         sql_result = self.client._gen_json_from_results(columns, rows)
+        #self.log.info(sql_result)
+        self.log.info(" result from n1ql query returns {0} items".format(len(n1ql_result)))
+        self.log.info(" result from sql query returns {0} items".format(len(sql_result)))
+        try:
+            self.n1ql_helper._verify_results_rqg(sql_result = sql_result, n1ql_result = n1ql_result, hints = hints)
+        except Exception, ex:
+            self.log.info(ex)
+            return False, ex
+        return True, "Pass"
+
+    def _run_queries_from_file_and_compare(self, n1ql_query = None, sql_query = None, sql_result = None):
+        self.log.info(" SQL QUERY :: {0}".format(sql_query))
+        self.log.info(" N1QL QUERY :: {0}".format(n1ql_query))
+        # Run n1ql query
+        hints = self.query_helper._find_hints(n1ql_query)
+        actual_result = self.n1ql_helper.run_cbq_query(query = n1ql_query, server = self.n1ql_server)
+        n1ql_result = actual_result["results"]
+        self.log.info(actual_result)
         self.log.info(sql_result)
         self.log.info(" result from n1ql query returns {0} items".format(len(n1ql_result)))
         self.log.info(" result from sql query returns {0} items".format(len(sql_result)))
@@ -114,8 +218,21 @@ class RQGTests(BaseTestCase):
             return False, ex
         return True, "Pass"
 
-    def _build_primary_indexes(self, using_gsi= True, bucket_list = []):
-        self.n1ql_helper.create_primary_index(using_gsi = using_gsi,server = self.n1ql_server)
+    def _initialize_cluster_setup(self):
+        self.use_mysql= self.input.param("use_mysql",True)
+        if self.use_mysql:
+            self.log.info(" Will load directly from mysql")
+            self._initialize_mysql_client()
+            self._setup_and_load_buckets()
+        else:
+            self.log.info(" Will load directly from file snap-shot")
+            self._setup_and_load_buckets_from_files()
+        self._initialize_n1ql_helper()
+        self.sleep(10)
+        self._build_primary_indexes()
+
+    def _build_primary_indexes(self, using_gsi= True):
+        self.n1ql_helper.create_primary_index(using_gsi = using_gsi, server = self.n1ql_server)
 
     def _load_data_in_buckets_using_mc_bin_client(self, bucket, data_set):
         client = VBucketAwareMemcached(RestConnection(self.master), bucket)
@@ -126,15 +243,25 @@ class RQGTests(BaseTestCase):
             print 'WARN======================='
             print ex
 
-#    def _load_data_in_buckets(self, bucket_name, data_set):
-#        scheme = "couchbase"
-#        host=self.master.ip
-#        if self.master.ip == "127.0.0.1":
-#            scheme = "http"
-#            host="{0}:{1}".format(self.master.ip,self.master.port)
-#        client = SDKClient(scheme=scheme,hosts = [host], bucket = bucket_name)
-#        client.upsert_multi(data_set)
-#        client.close()
+    def _load_data_in_buckets_using_mc_bin_client_json(self, bucket, data_set):
+        client = VBucketAwareMemcached(RestConnection(self.master), bucket)
+        try:
+            for key in data_set.keys():
+                o, c, d = client.set(key.encode("utf8"), 0, 0, json.dumps(data_set[key]))
+        except Exception, ex:
+            print 'WARN======================='
+            print ex
+
+    def _load_data_in_buckets(self, bucket_name, data_set):
+        from sdk_client import SDKClient
+        scheme = "couchbase"
+        host=self.master.ip
+        if self.master.ip == "127.0.0.1":
+            scheme = "http"
+            host="{0}:{1}".format(self.master.ip,self.master.port)
+        client = SDKClient(scheme=scheme,hosts = [host], bucket = bucket_name)
+        client.upsert_multi(data_set)
+        client.close()
 
     def _initialize_n1ql_helper(self):
         self.n1ql_helper = N1QLHelper(version = "sherlock", shell = None,
@@ -146,6 +273,13 @@ class RQGTests(BaseTestCase):
     def _initialize_mysql_client(self):
         self.client = MySQLClient(database = self.database, host = self.mysql_url,
             user_id = self.user_id, password = self.password)
+
+    def _zipdir(self, path, zip_path):
+        self.log.info(zip_path)
+        zipf = zipfile.ZipFile(zip_path, 'w')
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                zipf.write(os.path.join(root, file))
 
     def _calculate_secondary_indexing_information(self, query_list = []):
         secondary_index_table_map = {}
@@ -208,6 +342,73 @@ class RQGTests(BaseTestCase):
                     self.log.info(ex)
                     raise
 
+    def _convert_fun_result(self, result_set):
+        list = []
+        for data in result_set:
+            map = {}
+            for key in data.keys():
+                val = data[key]
+                if val == None:
+                    val =0
+                if not isinstance(val, int):
+                    val = str(val)
+                    if val == "":
+                        val = 0
+                map[key] =  val
+            list.append(map)
+        return list
+
+    def _take_snapshot_of_database(self):
+        self.zip_path= self.input.param("zip_path","flightstats_data.zip")
+        self.data_dump_path= self.input.param("data_dump_path","b/resources/flightstats_mysql")
+        # Pull information about tables from mysql database and interpret them as no-sql dbs
+        table_key_map = self.client._get_primary_key_map_for_tables()
+        # Make a list of buckets that we want to create for querying
+        bucket_list = table_key_map.keys()
+        # Read Data from mysql database and populate the couchbase server
+        for bucket_name in bucket_list:
+            query = "select * from {0}".format(bucket_name)
+            columns, rows = self.client._execute_query(query = query)
+            dict = self.client._gen_json_from_results_with_primary_key(
+                columns, rows, table_key_map[bucket_name])
+            # Take snap-shot of Data in
+            f = open(self.data_dump_path+"/"+bucket_name+".txt",'w')
+            f.write(json.dumps(dict))
+            f.close()
+        self._zipdir(self.data_dump_path, self.zip_path)
+
+    def _setup_and_load_buckets_from_files(self):
+        bucket_list =[]
+        import shutil
+        self.data_dump_path= self.input.param("data_dump_path","b/resources/flightstats_mysql/data_set")
+        self.zip_path= self.input.param("zip_path","b/resources/flightstats_mysql/flightstats_data.zip")
+        shutil.rmtree(self.data_dump_path, ignore_errors=True)
+        #Unzip the files and get bucket list
+        with zipfile.ZipFile(self.zip_path, "r") as z:
+            z.extractall(".")
+        from os import listdir
+        from os.path import isfile, join
+        onlyfiles = [ f for f in listdir(self.data_dump_path) if isfile(join(self.data_dump_path,f))]
+        for file in onlyfiles:
+            bucket_list.append(file.split(".")[0])
+        # Remove any previous buckets
+        rest = RestConnection(self.master)
+        for bucket in self.buckets:
+            rest.delete_bucket(bucket.name)
+        self.buckets = []
+        # Create New Buckets
+        self._create_buckets(self.master, bucket_list, server_id=None, bucket_size=None)
+        # Wait till the buckets are up
+        self.sleep(15)
+        # Read Data from mysql database and populate the couchbase server
+        for bucket_name in bucket_list:
+             for bucket in self.buckets:
+                if bucket.name == bucket_name:
+                    file_path = self.data_dump_path+"/"+bucket_name+".txt"
+                    with open(file_path) as data_file:
+                        data = json.load(data_file)
+                        self._load_data_in_buckets_using_mc_bin_client_json(bucket, data)
+        shutil.rmtree(self.data_dump_path, ignore_errors=True)
 
     def _setup_and_load_buckets(self):
         # Remove any previous buckets
@@ -223,11 +424,6 @@ class RQGTests(BaseTestCase):
         self._create_buckets(self.master, bucket_list, server_id=None, bucket_size=None)
         # Wait till the buckets are up
         self.sleep(15)
-
-        # Create Schema in MYSQL Database - TBD : So far working with pre-populated Schema
-
-        # Load Data in MYSQL Database - TBD : So far working with pre-populated data
-
         # Read Data from mysql database and populate the couchbase server
         for bucket_name in bucket_list:
             query = "select * from {0}".format(bucket_name)
