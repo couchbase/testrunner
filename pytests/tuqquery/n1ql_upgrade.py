8ca5bd117fe4a15ddcb5375c1b35fc7eb469b479
IrynaMironava <irynamironava@yandex.ru>
2015-05-15 13:37:18 +0300

    CBQE-0: fix tearDown hung
    Change-Id: Iae1477357bcb473adb6800afe8e6bf847a7c1635
    Reviewed-on: http://review.couchbase.org/51086
    Reviewed-by: Iryna Mironava <irynamironava@yandex.ru>
    Tested-by: Iryna Mironava <irynamironava@yandex.ru>

--- pytests/tuqquery/n1ql_upgrade.py
+++ pytests/tuqquery/n1ql_upgrade.py
@@ -1,3 +1,4 @@
+import threading
 from tuq import QueryTests
 from newupgradebasetest import NewUpgradeBaseTest
 from remote.remote_util import RemoteMachineShellConnection
@@ -21,12 +22,17 @@ class QueriesUpgradeTests(QueryTests, NewUpgradeBaseTest):
 
     def tearDown(self):
         super(QueriesUpgradeTests, self).tearDown()
+        if self._testMethodName == 'suite_tearDown' and str(self.__call__).find('setUp') == -1:
+            for th in threading.enumerate():
+                if th != threading.current_thread():
+                    th._Thread__stop()
 
     def suite_tearDown(self):
         super(QueriesUpgradeTests, self).suite_tearDown()
 
     def test_mixed_cluster(self):
         self.assertTrue(len(self.servers) > 1, 'Test needs more than 1 server')
+        method_name = self.input.param('to_run', 'test_all_negative')
         self._install(self.servers[:2])
         self.bucket_size = 100
         self._bucket_creation()
@@ -36,11 +42,17 @@ class QueriesUpgradeTests(QueryTests, NewUpgradeBaseTest):
             upgrade_thread.join()
         self.cluster.rebalance(self.servers[:1], self.servers[1:2], [])
         self.shell = RemoteMachineShellConnection(self.servers[1])
+        o = self.shell.execute_command("ps -aef| grep cbq-engine")
+        if len(o):
+            for cbq_engine in o[0]:
+                if cbq_engine.find('grep') == -1:
+                    pid = [item for item in cbq_engine.split(' ') if item][1]
+                    self.shell.execute_command("kill -9 %s" % pid)
         self._start_command_line_query(self.servers[1])
         o = self.shell.execute_command("ps -aef| grep cbq-engine")
         print o
-        self.assertTrue(o[0][0].find('cbq-engine -datastore')!= -1,
-                        'N1ql couldn\'t be started')
+        self.master = self.servers[1]
+        getattr(self, method_name)()
 
     def test_upgrade(self):
         method_name = self.input.param('to_run', 'test_any')
