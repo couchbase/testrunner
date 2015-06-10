b82ce54162d0e42898b84b64d7cae7528e051032
Andrei Baranouski <andrei.baranouski@gmail.com>
2015-06-10 14:37:51 +0300

    CBQE-0: handle settings/saslauthdAuth in simple_requeststests for windows
    we should skip this verification on windows due to:
    This http API endpoint is only supported in enterprise edition running on GNU/Linux
    
    Change-Id: Id834d3c9b5606566f8f21ac95dc6300ec1466ea9
    Reviewed-on: http://review.couchbase.org/51904
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Ritam Sharma <ritamcouchbase@gmail.com>
    Tested-by: Ritam Sharma <ritamcouchbase@gmail.com>

--- pytests/ui/simple_requeststests.py
+++ pytests/ui/simple_requeststests.py
@@ -1,10 +1,19 @@
 from basetestcase import BaseTestCase
 from membase.api.rest_client import RestConnection
+from remote.remote_util import RemoteMachineShellConnection
 
 
 class SimpleRequests(BaseTestCase):
     def setUp(self):
         super(SimpleRequests, self).setUp()
+        shell = RemoteMachineShellConnection(self.master)
+        type = shell.extract_remote_info().distribution_type
+        shell.disconnect()
+        if type.lower() == 'windows':
+            self.is_linux = False
+        else:
+            self.is_linux = True
+
 
     def test_simple_ui_request(self):
         rest = RestConnection(self.master)
@@ -28,6 +37,9 @@ class SimpleRequests(BaseTestCase):
             self.log.info("GET " + url)
             status, content, header = rest._http_request(url)
             self.log.info(header)
+            if self.is_linux == False and api == "settings/saslauthdAuth":
+                #This http API endpoint is only supported in enterprise edition running on GNU/Linux
+                continue
             if not status:
                 self.log.info("wrong status for {0} GET request {1}".format(url, header))
                 passed = False
@@ -38,8 +50,3 @@ class SimpleRequests(BaseTestCase):
         if foundIndex != -1:
             self.log.info(content[foundIndex - 1000: foundIndex + 1000])
             self.assertFalse(foundIndex, "some web request failed in server logs. See logs above")
-
-
-
-
-
