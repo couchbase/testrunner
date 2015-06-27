b1339deb5c0ab396859e6ca50de08ecd427da7a7
ashvinder <ashvinder@couchbase.com>
2015-06-26 17:39:56 -0700

    added rebalance tests for mobile upgrade tests
    Change-Id: I010c5119063f175aec33baad0835589d51a73ac0
    Reviewed-on: http://review.couchbase.org/52618
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Ashvinder Singh <ashvinder@couchbase.com>
    Tested-by: Ashvinder Singh <ashvinder@couchbase.com>

---
+++ scripts/cli_rebalance.py
@@ -0,0 +1,80 @@
+
+import getopt
+import copy
+import logging
+import sys
+from threading import Thread
+from datetime import datetime
+import socket
+import Queue
+
+sys.path = [".", "lib"] + sys.path
+import testconstants
+import time
+from builds.build_query import BuildQuery
+import logging.config
+from membase.api.exception import ServerUnavailableException
+from membase.api.rest_client import RestConnection, RestHelper
+from remote.remote_util import RemoteMachineShellConnection, RemoteUtilHelper
+from membase.helper.cluster_helper import ClusterOperationHelper
+from testconstants import MV_LATESTBUILD_REPO
+from testconstants import SHERLOCK_BUILD_REPO
+from testconstants import COUCHBASE_REPO
+import TestInput
+
+
+logging.config.fileConfig("scripts.logging.conf")
+log = logging.getLogger()
+
+def usage():
+    print "Please provide ini file"
+
+def main():
+    log_install_failed = "some nodes were not install successfully!"
+    try:
+        (opts, args) = getopt.getopt(sys.argv[1:], 'hi:p:', [])
+        for o, a in opts:
+            if o == "-h":
+                usage()
+
+        if len(sys.argv) <= 1:
+            usage()
+
+        input = TestInput.TestInputParser.get_test_input(sys.argv)
+        if not input.servers:
+            usage("ERROR: no servers specified. Please use the -i parameter.")
+    except IndexError:
+        usage()
+    except getopt.GetoptError, err:
+        usage("ERROR: " + str(err))
+    print input
+
+    cli_command = "server-info"
+
+    # add upgraded nodes in the cluster
+    # Assumption 4 nodes in ini file and add nodes from last node upwards
+    cli_command = "rebalance"
+    remote_client = RemoteMachineShellConnection(input.servers[0])
+    for server in input.servers[2:]:
+        print server.ip
+        options = "--server-add={0}:8091".format(server.ip) + " --server-add-username=Administrator --server-add-password=password"
+        output, error = remote_client.execute_couchbase_cli(cli_command, options=options, cluster_host=input.servers[0].ip, cluster_port=server.port, user=server.rest_username, password=server.rest_password)
+        print output, error
+        time.sleep(5)
+
+    time.sleep(5)
+    # remove old build nodes out from the cluster
+    # Assumption 4 nodes and remove nodes from the top
+    cli_command = "rebalance"
+    remote_client = RemoteMachineShellConnection(input.servers[2])
+    for server in input.servers[:2]:
+        print server.ip
+        options = "--server-remove={0}:8091".format(server.ip) + " --server-add-username=Administrator --server-add-password=password"
+        output, error = remote_client.execute_couchbase_cli(cli_command, options=options, cluster_host=input.servers[2].ip, cluster_port=server.port, user=server.rest_username, password=server.rest_password)
+        print output, error
+        time.sleep(5)
+
+
+
+if __name__ == "__main__":
+    main()
