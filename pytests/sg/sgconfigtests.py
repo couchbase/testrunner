4e1f88a5eb5f654906d95351cc33300d1cb448f6
Andrei Baranouski <andrei.baranouski@gmail.com>
2015-07-12 00:19:00 +0300

    CBQE-0: some fixes in sgconfigtests.py
    Change-Id: I8374577866b1631243deda3263984a63771871f9
    Reviewed-on: http://review.couchbase.org/53103
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Andrei Baranouski <andrei.baranouski@gmail.com>

--- pytests/sg/sgconfigtests.py
+++ pytests/sg/sgconfigtests.py
@@ -5,20 +5,20 @@ import shutil
 from couchbase_helper.cluster import Cluster
 
 help_string = ['Usage of /opt/couchbase-sync-gateway/bin/sync_gateway:',
-'  -adminInterface="127.0.0.1:4985": Address to bind admin interface to',
-'  -bucket="sync_gateway": Name of bucket',
-'  -configServer="": URL of server that can return database configs',
-'  -dbname="": Name of Couchbase Server database (defaults to name of bucket)',
-'  -deploymentID="": Customer/project identifier for stats reporting',
-'  -interface=":4984": Address to bind to',
-'  -log="": Log keywords, comma separated',
-'  -logFilePath="": Path to log file',
-'  -personaOrigin="": Base URL that clients use to connect to the server',
-'  -pool="default": Name of pool',
-'  -pretty=false: Pretty-print JSON responses',
-'  -profileInterface="": Address to bind profile interface to',
-'  -url="walrus:": Address of Couchbase server',
-'  -verbose=false: Log more info about requests']
+               '  -adminInterface="127.0.0.1:4985": Address to bind admin interface to',
+               '  -bucket="sync_gateway": Name of bucket',
+               '  -configServer="": URL of server that can return database configs',
+               '  -dbname="": Name of Couchbase Server database (defaults to name of bucket)',
+               '  -deploymentID="": Customer/project identifier for stats reporting',
+               '  -interface=":4984": Address to bind to',
+               '  -log="": Log keywords, comma separated',
+               '  -logFilePath="": Path to log file',
+               '  -personaOrigin="": Base URL that clients use to connect to the server',
+               '  -pool="default": Name of pool',
+               '  -pretty=false: Pretty-print JSON responses',
+               '  -profileInterface="": Address to bind profile interface to',
+               '  -url="walrus:": Address of Couchbase server',
+               '  -verbose=false: Log more info about requests']
 
 
 class SGConfigTests(GatewayConfigBaseTest):
@@ -28,10 +28,11 @@ class SGConfigTests(GatewayConfigBaseTest):
             if self.case_number == 1:
                 with open('pytests/sg/resources/gateway_config_walrus_template.json', 'r') as file:
                     filedata = file.read()
-                    filedata=filedata.replace('LOCAL_IP', server.ip)
+                    filedata = filedata.replace('LOCAL_IP', server.ip)
                 with open('pytests/sg/resources/gateway_config_walrus.json', 'w') as file:
-                        file.write(filedata)
+                    file.write(filedata)
                 shell = RemoteMachineShellConnection(server)
+                shell.execute_command("rm -rf {0}/tmp/*".format(self.folder_prefix))
                 shell.copy_files_local_to_remote('pytests/sg/resources', '/tmp')
                 # will install sg only the first time
                 self.install(shell)
@@ -40,45 +41,20 @@ class SGConfigTests(GatewayConfigBaseTest):
                 exist = shell.file_exists('/tmp/', 'gateway.log')
                 self.assertTrue(exist)
                 shell.disconnect()
-        if self.case_number==1:
+        if self.case_number == 1:
+            shutil.copy2('pytests/sg/resources/gateway_config_backup.json', 'pytests/sg/resources/gateway_config.json')
             BucketOperationHelper.delete_all_buckets_or_assert(self.servers, self)
             self.cluster = Cluster()
             self.cluster.create_default_bucket(self.master, 150)
-
-            self.cluster.async_create_sasl_bucket(self.master, 'test_%E-.5', 'password', 150, 1)
-            self.cluster.async_create_standard_bucket(self.master, 'db', 11219, 150, 1)
+            task = self.cluster.async_create_sasl_bucket(self.master, 'test_%E-.5', 'password', 150, 1)
+            task.result()
+            task = self.cluster.async_create_standard_bucket(self.master, 'db', 11219, 150, 1)
+            task.result()
 
     def tearDown(self):
         super(SGConfigTests, self).tearDown()
-        if self.case_number==1:
+        if self.case_number == 1:
             self.cluster.shutdown(force=True)
-
-    def configCBS(self):
-        for server in self.servers:
-            shell = RemoteMachineShellConnection(server)
-            self.assertTrue(self.start_sync_gateway_template(shell, self.template))
-            if not self.expected_error:
-                success, revision = self.create_doc(shell)
-                self.assertTrue(success)
-                self.assertTrue(self.delete_doc(shell, revision))
-            self.assertTrue(self.check_message_in_gatewaylog(shell, self.expected_log))
-            shell.disconnect()
-
-    def configStartSgw(self):
-        for server in self.servers:
-            shell = RemoteMachineShellConnection(server)
-            shutil.copy2('pytests/sg/resources/gateway_config_backup.json', 'pytests/sg/resources/gateway_config.json')
-            shell.copy_files_local_to_remote('pytests/sg/resources', '/tmp')
-            self.assertTrue(self.start_sync_gateway(shell))
-            self.assertTrue(self.check_message_in_gatewaylog(shell, self.expected_log))
-            if not self.expected_error:
-                if self.admin_port:
-                    self.assertTrue(self.get_users(shell))
-                if self.sync_port:
-                    success, revision = self.create_doc(shell)
-                    self.assertTrue(success)
-                    self.assertTrue(self.delete_doc(shell, revision))
-            shell.disconnect()
 
     def configHelp(self):
         for server in self.servers:
@@ -86,7 +62,9 @@ class SGConfigTests(GatewayConfigBaseTest):
             output, error = shell.execute_command_raw('/opt/couchbase-sync-gateway/bin/sync_gateway -help')
             for index, str in enumerate(help_string):
                 if index != help_string[index]:
-                    self.log.info('configHelp found unmatched help text. error({0}), help({1})'.format(error[index], help_string[index]))
+                    self.log.info('configHelp found unmatched help text. error({0}), help({1})'.format(error[index],
+                                                                                                       help_string[
+                                                                                                           index]))
                 self.assertEqual(error[index], help_string[index])
             shell.disconnect()
 
@@ -155,4 +133,32 @@ class SGConfigTests(GatewayConfigBaseTest):
                 self.assertTrue(success)
                 self.assertTrue(self.get_all_docs(shell))
                 self.assertTrue(self.delete_doc(shell, revision))
+            shell.disconnect()
+
+    def configCBS(self):
+        for server in self.servers:
+            shell = RemoteMachineShellConnection(server)
+            shutil.copy2('pytests/sg/resources/gateway_config_backup.json', 'pytests/sg/resources/gateway_config.json')
+            self.assertTrue(self.start_sync_gateway_template(shell, self.template))
+            if not self.expected_error:
+                success, revision = self.create_doc(shell)
+                self.assertTrue(success)
+                self.assertTrue(self.delete_doc(shell, revision))
+            self.assertTrue(self.check_message_in_gatewaylog(shell, self.expected_log))
+            shell.disconnect()
+
+    def configStartSgw(self):
+        for server in self.servers:
+            shell = RemoteMachineShellConnection(server)
+            shutil.copy2('pytests/sg/resources/gateway_config_backup.json', 'pytests/sg/resources/gateway_config.json')
+            shell.copy_files_local_to_remote('pytests/sg/resources', '/tmp')
+            self.assertFalse(self.start_sync_gateway(shell))
+            self.assertTrue(self.check_message_in_gatewaylog(shell, self.expected_log))
+            if not self.expected_error:
+                if self.admin_port:
+                    self.assertTrue(self.get_users(shell))
+                if self.sync_port:
+                    success, revision = self.create_doc(shell)
+                    self.assertTrue(success)
+                    self.assertTrue(self.delete_doc(shell, revision))
             shell.disconnect()
