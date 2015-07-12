bbe3a111d462be6c7a6913deedadba93da0a1cc3
Andrei Baranouski <andrei.baranouski@gmail.com>
2015-07-12 12:20:47 +0300

    CBQE-0: add sleep after run SG with CB references
    Change-Id: Ida9e27c1a6e149e2ae9ed6de223194d384e9ed5e
    Reviewed-on: http://review.couchbase.org/53106
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Andrei Baranouski <andrei.baranouski@gmail.com>

--- pytests/sg/sginstalltests.py
+++ pytests/sg/sginstalltests.py
@@ -1,15 +1,16 @@
 from sg.sg_base import GatewayBaseTest
 from remote.remote_util import RemoteMachineShellConnection
+import time
 
 help_string = ['This script creates an init service to run a sync_gateway instance.',
-        'If you want to install more than one service instance',
-        'create additional services with different names.',
-        '', 'sync_gateway_service_install.sh', '    -h --help',
-        '    --runas=<The user account to run sync_gateway as; default (sync_gateway)>',
-        '    --runbase=<The directory to run sync_gateway from; defaut (/home/sync_gateway)>',
-        '    --sgpath=<The path to the sync_gateway executable; default (/opt/couchbase-sync-gateway/bin/sync_gateway)>',
-        '    --cfgpath=<The path to the sync_gateway JSON config file; default (/home/sync_gateway/sync_gateway.json)>',
-        '    --logsdir=<The path to the log file direcotry; default (/home/sync_gateway/logs)>', '']
+               'If you want to install more than one service instance',
+               'create additional services with different names.',
+               '', 'sync_gateway_service_install.sh', '    -h --help',
+               '    --runas=<The user account to run sync_gateway as; default (sync_gateway)>',
+               '    --runbase=<The directory to run sync_gateway from; defaut (/home/sync_gateway)>',
+               '    --sgpath=<The path to the sync_gateway executable; default (/opt/couchbase-sync-gateway/bin/sync_gateway)>',
+               '    --cfgpath=<The path to the sync_gateway JSON config file; default (/home/sync_gateway/sync_gateway.json)>',
+               '    --logsdir=<The path to the log file direcotry; default (/home/sync_gateway/logs)>', '']
 
 
 class SGInstallerTest(GatewayBaseTest):
@@ -55,22 +56,21 @@ class SGInstallerTest(GatewayBaseTest):
             self.assertTrue(self.install_gateway(shell))
             shell.execute_command_raw(
                 'rm -rf {0}/* {1}/* {2}/sync_gateway.json {3}/tmp/test*; mkdir {3}/tmp/test {3}/tmp/test2'.
-                format(self.logsdir, self.datadir, self.configdir, self.folder_prefix ))
+                    format(self.logsdir, self.datadir, self.configdir, self.folder_prefix))
             output, error = self.run_sync_gateway_service_install(shell, self.extra_param)
             self.check_normal_error_output(shell, output, error)
             self.assertTrue(self.is_sync_gateway_service_running(shell))
             self.assertTrue(self.is_sync_gateway_process_running(shell))
             if not "--runbase" in self.extra_param:
                 # hardcoded for services LOGS_TEMPLATE_VAR=${RUNBASE_TEMPLATE_VAR}/logs
-                self.datadir='/home/sync_gateway'
+                self.datadir = '/home/sync_gateway'
             if not "--logsdir" in self.extra_param:
-                self.logsdir='/home/sync_gateway/logs'
+                self.logsdir = '/home/sync_gateway/logs'
             if not "--cfgpath" in self.extra_param:
-                self.configdir='/home/sync_gateway'
+                self.configdir = '/home/sync_gateway'
             self.assertTrue(shell.file_exists(self.logsdir, 'sync_gateway_error.log'))
             self.assertTrue(shell.file_exists(self.datadir, 'data'))
             self.assertTrue(shell.file_exists(self.configdir, self.configfile))
-
 
     def serviceInstallSGPath(self):
         for server in self.servers:
@@ -78,7 +78,8 @@ class SGInstallerTest(GatewayBaseTest):
             self.assertTrue(self.service_clean(shell))
             self.assertTrue(self.install_gateway(shell))
             shell.execute_command_raw('mv /opt/couchbase-sync-gateway/bin /opt/couchbase-sync-gateway/bin2 ')
-            output, error = self.run_sync_gateway_service_install(shell, '--sgpath=/opt/couchbase-sync-gateway/bin2/sync_gateway')
+            output, error = self.run_sync_gateway_service_install(shell,
+                                                                  '--sgpath=/opt/couchbase-sync-gateway/bin2/sync_gateway')
             self.check_normal_error_output(shell, output, error)
             self.assertTrue(self.is_sync_gateway_service_running(shell))
             self.assertTrue(self.is_sync_gateway_process_running(shell))
@@ -166,9 +167,10 @@ class SGInstallerTest(GatewayBaseTest):
             self.assertTrue(self.install_gateway(shell))
             output, error = self.run_sync_gateway_service_install(shell, self.extra_param)
             self.assertTrue(error[0].startswith(self.expected_error))
+            time.sleep(3)
             # self.assertEqual(output, [])
             # self.assertFalse(shell.file_exists("/tmp/sync_gateway", 'logs'))
-            #self.assertFalse(shell.file_exists("/tmp/sync_gateway", 'data'))
+            # self.assertFalse(shell.file_exists("/tmp/sync_gateway", 'data'))
             self.assertFalse(shell.file_exists("/tmp/sync_gateway", 'sync_gateway.json'))
             self.assertFalse(self.is_sync_gateway_service_running(shell))
             self.assertFalse(self.is_sync_gateway_process_running(shell))
