f024cc74c07bf78d5574e59e2b2ec090510386fe
Arunkumar Senthilnathan <arun.couchbase@yahoo.com>
2015-08-10 11:01:54 +0530

    CBQE-3002: Addressing review comments Change-Id: Ia2181ed747be8950a78f81da937c8338bcf639b1 Reviewed-on: http://review.couchbase.org/54280 Reviewed-by: Aruna Piravi <aruna@couchbase.com> Tested-by: buildbot <build@couchbase.com>

--- pytests/view/xdcrview.py
+++ pytests/view/xdcrview.py
@@ -1,9 +1,9 @@
-from xdcr.xdcrbasetests import XDCRReplicationBaseTest
-from couchbase_helper.documentgenerator import BlobGenerator
+from xdcr.xdcrnewbasetests import XDCRNewBaseTest
 from createdeleteview import CreateDeleteViewTests
 from membase.api.exception import QueryViewException
+from couchbase_helper.document import View
 
-class XDCRViewTests(XDCRReplicationBaseTest, CreateDeleteViewTests):
+class XDCRViewTests(XDCRNewBaseTest, CreateDeleteViewTests):
     def setUp(self):
         super(XDCRViewTests, self).setUp()
         self.bucket_ddoc_map = {}
@@ -14,13 +14,15 @@ class XDCRViewTests(XDCRReplicationBaseTest, CreateDeleteViewTests):
         self.num_ddocs = self._input.param("num_ddocs", 1)
         self.test_with_view = True
         self.updated_map_func = 'function (doc) { emit(null, doc);}'
-        if self._replication_direction_str in "bidirection":
-            self.gen_create2 = BlobGenerator('LoadTwo', 'LoadTwo', self._value_size, end=self.num_items)
-            self.gen_delete2 = BlobGenerator('LoadTwo', 'LoadTwo-', self._value_size,
-                start=int((self.num_items) * (float)(100 - self._percent_delete) / 100), end=self.num_items)
-            self.gen_update2 = BlobGenerator('LoadTwo', 'LoadTwo-', self._value_size, start=0,
-                end=int(self.num_items * (float)(self._percent_update) / 100))
-
+        self.src_cluster = self.get_cb_cluster_by_name('C1')
+        self.src_master = self.src_cluster.get_master_node()
+        self.dest_cluster = self.get_cb_cluster_by_name('C2')
+        self.dest_master = self.dest_cluster.get_master_node()
+        self.defaul_map_func = "function (doc) {\n  emit(doc._id, doc);\n}"
+        self.default_view_name = "default_view"
+        self.default_view = View(self.default_view_name, self.defaul_map_func, None)
+        self.cluster = self.src_cluster.get_cluster()
+            
     def tearDown(self):
         super(XDCRViewTests, self).tearDown()
 
@@ -34,10 +36,10 @@ class XDCRViewTests(XDCRReplicationBaseTest, CreateDeleteViewTests):
                 except QueryViewException:
                     self.fail("Error occurred querying view")
 
-    def _run_ddoc_ops(self, server, view_ops):
+    def _run_ddoc_ops(self, cluster, view_ops):
         tasks = []
-        self.master = server
-        buckets = self._get_cluster_buckets(self.master)
+        master = cluster.get_master_node();
+        buckets = cluster.get_buckets();
         if view_ops in ["update", "delete"]:
             for bucket in buckets:
                 tasks.extend(self._async_execute_ddoc_ops(view_ops, self.test_with_view, self.num_ddocs / 2,
@@ -46,9 +48,9 @@ class XDCRViewTests(XDCRReplicationBaseTest, CreateDeleteViewTests):
             if self.stale_param in ["false", "ok", "update_after"]:
                 query = {"stale" : self.stale_param, "full_set" : "true"}
             for bucket, self.ddoc_view_map in self.bucket_ddoc_map.items():
-                for ddoc_name, view_list in self.ddoc_view_map.items():
+                for ddoc_name, view_list in self.ddoc_view_map.items():                                                                      
                     for view in view_list:
-                        tasks.append(self.cluster.async_query_view(server, ddoc_name, view.name, query))
+                        tasks.append(self.cluster.async_query_view(master, ddoc_name, view.name, query))
         return tasks
 
     def _verify_views(self, verify_src):
@@ -62,12 +64,11 @@ class XDCRViewTests(XDCRReplicationBaseTest, CreateDeleteViewTests):
 
     """CBQE-1867,1868,1869 : Delete,Update and Query Views during Replication"""
     def test_views_with_xdcr(self):
-        verify_src = False
-        self._load_all_buckets(self.src_master, self.gen_create, "create", 0)
-        if self._replication_direction_str in "bidirection":
-            self._load_all_buckets(self.dest_master, self.gen_create2, "create", 0)
-        for self.master in [self.src_master, self.dest_master]:
-            buckets = self._get_cluster_buckets(self.master)
+        self.setup_xdcr_and_load()
+        for cluster in [self.src_cluster, self.dest_cluster]:
+            buckets = cluster.get_buckets()
+            self.master = cluster.get_master_node()
+            self.wait_timeout = self._wait_timeout;
             for bucket in buckets:
                 self._execute_ddoc_ops("create", self.test_with_view, self.num_ddocs, self.num_views_per_ddoc, "dev_test", "v1", bucket=bucket)
             self._query_view()
@@ -75,39 +76,15 @@ class XDCRViewTests(XDCRReplicationBaseTest, CreateDeleteViewTests):
 
         tasks = []
         if self.ddoc_ops is not None:
-            tasks = self._run_ddoc_ops(self.src_master, self.ddoc_ops)
+            tasks = self._run_ddoc_ops(self.src_cluster, self.ddoc_ops)
         if self.ddoc_ops_dest is not None:
-            tasks += self._run_ddoc_ops(self.dest_master, self.ddoc_ops_dest)
-        tasks_ops = []
-        if self._doc_ops is not None:
-            if "update" in self._doc_ops:
-                tasks_ops.extend(self._async_load_all_buckets(self.src_master, self.gen_update, "update", self._expires))
-            if "delete" in self._doc_ops:
-                tasks_ops.extend(self._async_load_all_buckets(self.src_master, self.gen_delete, "delete", 0))
-            self.sleep(5)
-        if self._doc_ops_dest is not None:
-            if "update" in self._doc_ops_dest:
-                tasks_ops.extend(self._async_load_all_buckets(self.dest_master, self.gen_update2, "update", self._expires))
-            if "delete" in self._doc_ops_dest:
-                tasks_ops.extend(self._async_load_all_buckets(self.dest_master, self.gen_delete2, "delete", 0))
-            self.sleep(5)
+            tasks += self._run_ddoc_ops(self.dest_cluster, self.ddoc_ops_dest)
         for task in tasks:
-                task.result(self._poll_timeout)
-        max_retry = 20
-        retry = 0
-        while retry < max_retry and set([task.state for task in tasks_ops]) != set(["FINISHED"]):
-            self._query_view()
-            retry += 1
-
-        if self._replication_direction_str in "unidirection":
-            self.merge_buckets(self.src_master, self.dest_master, bidirection=False)
-        elif self._replication_direction_str in "bidirection":
-            self.merge_buckets(self.src_master, self.dest_master, bidirection=True)
-            verify_src = True
+            task.result(self._poll_timeout)
+            
+        self.async_perform_update_delete()
 
         self.verify_results()
         self.sleep(self.wait_timeout / 2)
-        for bucket in buckets:
-            self.num_items = sum([len(kv_store) for kv_store in bucket.kvs.values()])
-            break
-        self._verify_views(verify_src=verify_src)
+        
+        self._verify_views(True)
