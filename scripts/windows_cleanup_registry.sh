40815ed44b31ae1e0c7df11080599f818778c4b7
saigon <baolavn@gmail.com>
2015-11-17 14:23:30 -0800

    CBQE-0 delete install shield registry for 4.5.0 version
    Change-Id: I036a2eea71d365b7601574446845e0c67d02565c
    Reviewed-on: http://review.couchbase.org/57128
    Tested-by: Thuan Nguyen <soccon@gmail.com>
    Reviewed-by: Thuan Nguyen <soccon@gmail.com>

--- scripts/windows_cleanup_registry.sh
+++ scripts/windows_cleanup_registry.sh
@@ -11,8 +11,9 @@ reg delete "HKEY_CLASSES_ROOT\Installer\UpgradeCodes\DAFE44492BF730D45B002C1133E
 echo -e "delete HKEY_CLASSES_ROOT\Installer\Products\288F9D42C18440B25027002B37EC713B \n"
 reg delete "HKEY_CLASSES_ROOT\Installer\Products\288F9D42C18440B25027002B37EC713B" /f
 # for sherloc, delete key below
-reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\InstallShield_{24D9F882-481C-2B04-0572-00B273CE17B3}" /f
-reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\InstallShield_{898C4818-1F6D-C554-1163-6DF5C0F1F7D8}" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\InstallShield_{24D9F882-481C-2B04-0572-00B273CE17B3}" /f  # 4.0.0
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\InstallShield_{898C4818-1F6D-C554-1163-6DF5C0F1F7D8}" /f  # 4.1.0
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\InstallShield_{A4BB2687-E63E-F424-F9F3-18D739053798}" /f  # 4.5.0
 reg delete "HKEY_CLASSES_ROOT\Installer\Products\5E5D7293-AC1D-3424-E583-0644411FDA20" /f	# 3.1.0
 reg delete "HKEY_CLASSES_ROOT\Installer\Products\24D9F882-481C-2B04-0572-00B273CE17B3" /f        # 4.0
 reg delete "HKEY_CLASSES_ROOT\Installer\Products\41276A8D-2A65-88D4-BDCC-8C4FE109F4B8" /f         # 3.1.1
