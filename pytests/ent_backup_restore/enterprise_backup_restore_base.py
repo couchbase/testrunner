98c1cfbe8833ddc07d139a918188b9bc6e1f1fe6
Arunkumar Senthilnathan <arun.couchbase@yahoo.com>
2015-12-15 11:27:14 +0530

    Minor fix in backup_restore_validate

--- pytests/ent_backup_restore/enterprise_backup_restore_base.py
+++ pytests/ent_backup_restore/enterprise_backup_restore_base.py
@@ -205,7 +205,7 @@ class EnterpriseBackupRestoreBase(BaseTestCase):
 
     def backup_restore_validate(self, compare_uuid=False, seqno_compare_function="==", replicas=False, mode="memory"):
         output, error = self.backup_restore()
-        if output or "Transfer plan finished successfully" not in error[-1]:
+        if error or "Transfer plan finished successfully" not in output:
             self.fail("Restoring backup failed.")
         self.log.info("Finished restoring backup")
         current_vseqno = self.get_vbucket_seqnos(self.cluster_to_restore, self.buckets)
