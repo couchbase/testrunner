5f76aababf74104d1d7d5df8755d7cfac1762626
Ketaki <ketakigangal@gmail.com>
2015-12-15 17:55:17 -0800

    cbqe 3270: add tests for subdoc exists and replace
    Change-Id: I0db08ad9dfe2e2cbc217e4db6b0d0a124d4e61b5
    Reviewed-on: http://review.couchbase.org/57832
    Well-Formed: buildbot <build@couchbase.com>
    Reviewed-by: Ketaki Gangal <ketaki@couchbase.com>
    Tested-by: Ketaki Gangal <ketaki@couchbase.com>

--- pytests/subdoc/subdoc_sanity.py
+++ pytests/subdoc/subdoc_sanity.py
@@ -47,7 +47,6 @@ class SubdocSanityTests(unittest.TestCase):
         inserted_keys = data_set.load()
 
         ''' Randomly generate 1000 long string to replace existing path strings '''
-
         replace_string = self.generate_string(1000)
 
         data_set.upsert_all_docs(inserted_keys, replace_string, path='isDict')
@@ -70,14 +69,53 @@ class SubdocSanityTests(unittest.TestCase):
         data_set.remove_all_docs(inserted_keys, path='array[0]')
         data_set.remove_all_docs(inserted_keys, path='array[-1]')
 
-    def test_deeply_nested_dataset_get(self):
+    def test_simple_dataset_exists(self):
+        num_docs = self.helper.input.param("num-docs")
+        self.log.info("description : Issue simple exists sub doc single path "
+                      "dataset with {0} docs".format(num_docs))
+
+        data_set = SimpleDataSet(self.helper, num_docs)
+        inserted_keys = data_set.load()
+
+        ''' add test code to accept Bool values and not error out '''
+        data_set.exists_all_docs(inserted_keys, path='isDict')
+        data_set.exists_all_docs(inserted_keys, path='geometry.coordinates[0]')
+        data_set.exists_all_docs(inserted_keys, path='dict_value.name')
+        data_set.exists_all_docs(inserted_keys, path='array[0]')
+        data_set.exists_all_docs(inserted_keys, path='array[-1]')
+
+    def test_simple_dataset_replace(self):
+        num_docs = self.helper.input.param("num-docs")
+        self.log.info("description : Issue simple replace sub doc single path "
+                      "dataset with {0} docs".format(num_docs))
+
+        data_set = SimpleDataSet(self.helper, num_docs)
+        inserted_keys = data_set.load()
+
+        ''' Randomly generate 1000 long string to replace existing path strings '''
+        replace_string = self.generate_string(10)
+
+        data_set.replace_all_docs(inserted_keys, replace_string, path='isDict')
+        data_set.replace_all_docs(inserted_keys, replace_string, path='geometry.coordinates[0]')
+        data_set.replace_all_docs(inserted_keys, replace_string, path='dict_value.name')
+        data_set.replace_all_docs(inserted_keys, "999", path='height')
+        data_set.replace_all_docs(inserted_keys, replace_string, path='array[-1]')
+
+    def test_simple_dataset_append(self):
+        pass
+
+    def test_simple_dataset_prepend(self):
+        pass
+
+    def test_simple_dataset_counter(self):
+        pass
+
+    def test_simple_dataset_array_add_unqiue(self):
         pass
 
     def generate_string(self, range_val=100):
         long_string = ''.join(chr(97 + randint(0, 25)) for i in range(range_val))
         return '"' + long_string + '"'
-
-
 
 class SimpleDataSet:
     def __init__(self, helper, num_docs):
@@ -121,8 +159,27 @@ class SimpleDataSet:
                     "Unable to remove value for key {0} for path {1} after {2} tries"
                     .format(in_key, path, num_tries))
 
-    def replace_all_paths(self):
-        pass
+    def exists_all_docs(self, inserted_keys, path):
+        for in_key in inserted_keys:
+            num_tries = 1
+            try:
+                opaque, cas, data = self.helper.client.exists_in(in_key, path)
+            except Exception as e:
+                print '[ERROR] {}'.format(e)
+                self.helper.testcase.fail(
+                    "Unable to validate value for key {0} for path {1} after {2} tries"
+                    .format(in_key, path, num_tries))
+
+    def replace_all_docs(self, inserted_keys, long_string, path):
+        for in_key in inserted_keys:
+            num_tries = 1
+            try:
+                opaque, cas, data = self.helper.client.replace_in(in_key, path ,long_string)
+            except Exception as e:
+                print '[ERROR] {}'.format(e)
+                self.helper.testcase.fail(
+                    "Unable to replace for key {0} for path {1} after {2} tries"
+                    .format(in_key, path, num_tries))
 
     def append_all_paths(self):
         pass
@@ -136,7 +193,6 @@ class SimpleDataSet:
     def multi_mutation_all_paths(self):
         pass
 
-
 class DeeplyNestedDataSet:
     def __init__(self, helper, num_docs):
         self.helper = helper
@@ -149,6 +205,9 @@ class DeeplyNestedDataSet:
 
     def get_all_docs(self):
             pass
+
+    def upsert_all_docs(self):
+        pass
 
 
 
