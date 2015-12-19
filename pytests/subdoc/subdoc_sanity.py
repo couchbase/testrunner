15d3e985eaac26190576d9ac492e1b29a134d773
Ketaki <ketakigangal@gmail.com>
2015-12-18 17:32:44 -0800

    CBQE-3275:new tests for subdoc simpledataset
    Change-Id: Iac7b280cc60fa22b37ec2ef7b0477c0910207465
    Reviewed-on: http://review.couchbase.org/57976
    Reviewed-by: Ketaki Gangal <ketaki@couchbase.com>
    Tested-by: Ketaki Gangal <ketaki@couchbase.com>

--- pytests/subdoc/subdoc_sanity.py
+++ pytests/subdoc/subdoc_sanity.py
@@ -38,9 +38,70 @@ class SubdocSanityTests(unittest.TestCase):
         #self.assertFalse(data_set.get_all_docs(inserted_keys, path='array[-5]'))
         #self.assertFalse(data_set.get_all_docs(inserted_keys, path='  '))
 
-    def test_simple_dataset_upsert(self):
+    def test_deep_nested_dataset_get_dict(self):
         num_docs = self.helper.input.param("num-docs")
-        self.log.info("description : Issue simple upsert sub doc single path "
+        self.log.info("description : Issue  get sub doc on deep nested single path on dictionaries "
+                      "dataset with {0} docs".format(num_docs))
+
+        data_set = DeeplyNestedDataSet(self.helper, num_docs)
+        inserted_keys, levels = data_set.load()
+
+        '''Top level element'''
+        #data_set.get_all_docs(inserted_keys, path = 'number', check_data=levels)
+        data_set.get_all_docs(inserted_keys, path = 'array')
+        data_set.get_all_docs(inserted_keys, path = 'array[0]')
+
+        '''Last element Dictionary'''
+        self.log.info('Testing last element dictionary')
+        data_set.get_all_docs(inserted_keys, path = self._get_path('child', levels-1))
+
+        '''Last element Dict.Array'''
+        self.log.info('Testing Dict.Array')
+        data_set.get_all_docs(inserted_keys, path = self._get_path('child', levels-2)+'.array[0]')
+
+        '''Intermediate element Dict.Array'''
+        self.log.info('Testing Intermediate element Dict. Array')
+        data_set.get_all_docs(inserted_keys, path = self._get_path('child', levels/2)+'.array[0]')
+
+    def test_deep_nested_dataset_get_array(self):
+        num_docs = self.helper.input.param("num-docs")
+        self.log.info("description : Issue  get sub doc on deep nested single path on dictionaries "
+                      "dataset with {0} docs".format(num_docs))
+
+        data_set = DeeplyNestedDataSet(self.helper, num_docs)
+        inserted_keys, levels = data_set.load()
+
+        '''Top level element'''
+        data_set.get_all_docs(inserted_keys, path = 'number')
+        data_set.get_all_docs(inserted_keys, path = 'array')
+        data_set.get_all_docs(inserted_keys, path = 'array[0]')
+
+        '''Last element Array'''
+        last_path ='child'
+        for i in xrange(levels-1):
+            last_path +='.child'
+        print last_path
+        data_set.get_all_docs(inserted_keys, path = last_path)
+
+        '''Last element Array of Array'''
+        last_path ='child'
+        for i in xrange(levels-3):
+            last_path +='.child'
+        last_path +='.array[-1][-1][-1]'
+        print last_path
+        data_set.get_all_docs(inserted_keys, path = last_path)
+
+        '''Intermediate element Array'''
+        last_path ='child'
+        for i in xrange(levels/2):
+            last_path +='.child'
+        last_path +='.array[0][-1]'
+        print last_path
+        data_set.get_all_docs(inserted_keys, path = last_path)
+
+    def test_simple_dataset_dict_upsert(self):
+        num_docs = self.helper.input.param("num-docs")
+        self.log.info("description : Issue simple upsert dict sub doc single path "
                       "dataset with {0} docs".format(num_docs))
 
         data_set = SimpleDataSet(self.helper, num_docs)
@@ -54,6 +115,23 @@ class SubdocSanityTests(unittest.TestCase):
         data_set.upsert_all_docs(inserted_keys, replace_string, path='dict_value.name')
         data_set.upsert_all_docs(inserted_keys, "999", path='height')
         data_set.upsert_all_docs(inserted_keys, replace_string, path='array[-1]')
+
+    def test_simple_dataset_dict_add(self):
+        num_docs = self.helper.input.param("num-docs")
+        self.log.info("description : Issue simple add dict sub doc single path "
+                      "dataset with {0} docs".format(num_docs))
+
+        data_set = SimpleDataSet(self.helper, num_docs)
+        inserted_keys = data_set.load()
+
+        ''' Randomly generate 1000 long string to replace existing path strings '''
+        replace_string = self.generate_string(1000)
+
+        #data_set.add_all_docs(inserted_keys, replace_string, path='isDict')
+        #data_set.add_all_docs(inserted_keys, replace_string, path='geometry.coordinates[0]')
+        data_set.add_all_docs(inserted_keys, replace_string, path='dict_value')
+        #data_set.add_all_docs(inserted_keys, "999", path='height')
+        #data_set.add_all_docs(inserted_keys, replace_string, path='array[-1]')
 
     def test_simple_dataset_remove(self):
         num_docs = self.helper.input.param("num-docs")
@@ -101,27 +179,101 @@ class SubdocSanityTests(unittest.TestCase):
         data_set.replace_all_docs(inserted_keys, "999", path='height')
         data_set.replace_all_docs(inserted_keys, replace_string, path='array[-1]')
 
-    def test_simple_dataset_append(self):
-        pass
+    def test_simple_dataset_array_push_last(self):
+        num_docs = self.helper.input.param("num-docs")
+        self.log.info("description : Issue simple array_push_last sub doc single path "
+                      "dataset with {0} docs".format(num_docs))
 
-    def test_simple_dataset_prepend(self):
-        pass
+        data_set = SimpleDataSet(self.helper, num_docs)
+        inserted_keys = data_set.load()
+
+        ''' Randomly generate 1000 long string to replace existing path strings '''
+        replace_string = self.generate_string(10)
+
+        #Should be a negative testcase below.
+        #data_set.array_push_last(inserted_keys, replace_string, path='isDict')
+        data_set.array_push_last(inserted_keys, replace_string, path='geometry.coordinates')
+        #data_set.array_push_last(inserted_keys, replace_string, path='dict_value.name')
+        #data_set.array_push_last(inserted_keys, "999", path='height')
+        data_set.array_push_last(inserted_keys, replace_string, path='array')
+
+    def test_simple_dataset_array_push_first(self):
+        num_docs = self.helper.input.param("num-docs")
+        self.log.info("description : Issue simple array_push_first sub doc single path "
+                      "dataset with {0} docs".format(num_docs))
+
+        data_set = SimpleDataSet(self.helper, num_docs)
+        inserted_keys = data_set.load()
+
+        ''' Randomly generate 1000 long string to replace existing path strings '''
+        replace_string = self.generate_string(10)
+
+        #Should be a negative testcase below.
+        #data_set.array_push_last(inserted_keys, replace_string, path='isDict')
+        data_set.array_push_first(inserted_keys, replace_string, path='geometry.coordinates')
+        #data_set.array_push_last(inserted_keys, replace_string, path='dict_value.name')
+        #data_set.array_push_last(inserted_keys, "999", path='height')
+        data_set.array_push_first(inserted_keys, replace_string, path='array')
 
     def test_simple_dataset_counter(self):
-        pass
+        num_docs = self.helper.input.param("num-docs")
+        self.log.info("description : Issue simple counter sub doc single path "
+                      "dataset with {0} docs".format(num_docs))
+
+        data_set = SimpleDataSet(self.helper, num_docs)
+        inserted_keys = data_set.load()
+
+        ''' Randomly generate 1000 long string to replace existing path strings '''
+        replace_string = self.generate_string(10)
+
+        #Should be a negative testcase below.
+        #data_set.array_push_last(inserted_keys, replace_string, path='isDict')
+        #data_set.counter_all_paths(inserted_keys, replace_string, path='geometry.coordinates')
+        #data_set.array_push_last(inserted_keys, replace_string, path='dict_value.name')
+        data_set.counter_all_paths(inserted_keys, 1, path='height')
+        #data_set.counter_all_paths(inserted_keys, replace_string, path='array')
+
 
     def test_simple_dataset_array_add_unqiue(self):
+        num_docs = self.helper.input.param("num-docs")
+        self.log.info("description : Issue simple add array unique sub doc single path "
+                      "dataset with {0} docs".format(num_docs))
+
+        data_set = SimpleDataSet(self.helper, num_docs)
+        inserted_keys = data_set.load()
+
+        ''' Randomly generate 1000 long string to replace existing path strings '''
+        replace_string = self.generate_string(10)
+
+        #Should be a negative testcase below.
+        #data_set.array_push_last(inserted_keys, replace_string, path='isDict')
+        data_set.array_add_unique(inserted_keys, replace_string, path='geometry.coordinates')
+        #data_set.array_push_last(inserted_keys, replace_string, path='dict_value.name')
+        #data_set.counter_all_paths(inserted_keys, 1, path='height')
+        #data_set.counter_all_paths(inserted_keys, replace_string, path='array')
+
+    def test_simple_dataset_multi_mutation(self):
+        pass
+
+    def test_simple_dataset_multi_lookup(self):
         pass
 
     def generate_string(self, range_val=100):
         long_string = ''.join(chr(97 + randint(0, 25)) for i in range(range_val))
         return '"' + long_string + '"'
 
-class SimpleDataSet:
+    def _get_path(self, subdoc_elt=None, levels=None):
+        subdoc_path = subdoc_elt
+        for i in xrange(levels-1):
+            subdoc_path +='.'+subdoc_elt
+        return subdoc_path
+
+class SimpleDataSet(SubdocSanityTests):
     def __init__(self, helper, num_docs):
         self.helper = helper
         self.num_docs = num_docs
         self.name = "simple_dataset"
+        self.log = logger.Logger.get_logger()
 
     def load(self):
         inserted_keys = self.helper.insert_docs(self.num_docs, self.name)
@@ -131,7 +283,7 @@ class SimpleDataSet:
             for in_key in inserted_keys:
                 num_tries = 1
                 try:
-                    opaque, cas, data = self.helper.client.get_in(in_key, path)
+                    opaque, cas, data = self.helper.client.get_sd(in_key, path)
                 except Exception as e:
                     self.helper.testcase.fail(
                         "Unable to get key {0} for path {1} after {2} tries"
@@ -141,7 +293,18 @@ class SimpleDataSet:
         for in_key in inserted_keys:
             num_tries = 1
             try:
-                opaque, cas, data = self.helper.client.upsert_in(in_key, path ,long_string)
+                opaque, cas, data = self.helper.client.dict_upsert_sd(in_key, path ,long_string)
+            except Exception as e:
+                print '[ERROR] {}'.format(e)
+                self.helper.testcase.fail(
+                    "Unable to upsert key {0} for path {1} after {2} tries"
+                    .format(in_key, path, num_tries))
+
+    def add_all_docs(self, inserted_keys, long_string, path):
+        for in_key in inserted_keys:
+            num_tries = 1
+            try:
+                opaque, cas, data = self.helper.client.dict_add_sd(in_key, path ,long_string)
             except Exception as e:
                 print '[ERROR] {}'.format(e)
                 self.helper.testcase.fail(
@@ -152,7 +315,7 @@ class SimpleDataSet:
         for in_key in inserted_keys:
             num_tries = 1
             try:
-                opaque, cas, data = self.helper.client.remove_in(in_key, path)
+                opaque, cas, data = self.helper.client.delete_sd(in_key, path)
             except Exception as e:
                 print '[ERROR] {}'.format(e)
                 self.helper.testcase.fail(
@@ -163,7 +326,7 @@ class SimpleDataSet:
         for in_key in inserted_keys:
             num_tries = 1
             try:
-                opaque, cas, data = self.helper.client.exists_in(in_key, path)
+                opaque, cas, data = self.helper.client.exists_sd(in_key, path)
             except Exception as e:
                 print '[ERROR] {}'.format(e)
                 self.helper.testcase.fail(
@@ -174,37 +337,84 @@ class SimpleDataSet:
         for in_key in inserted_keys:
             num_tries = 1
             try:
-                opaque, cas, data = self.helper.client.replace_in(in_key, path ,long_string)
+                opaque, cas, data = self.helper.client.replace_sd(in_key, path ,long_string)
             except Exception as e:
                 print '[ERROR] {}'.format(e)
                 self.helper.testcase.fail(
                     "Unable to replace for key {0} for path {1} after {2} tries"
                     .format(in_key, path, num_tries))
 
-    def append_all_paths(self):
-        pass
+    def array_push_last(self, inserted_keys, long_string, path):
+        for in_key in inserted_keys:
+            num_tries = 1
+            try:
+                opaque, cas, data = self.helper.client.array_push_last_sd(in_key, path ,long_string)
+            except Exception as e:
+                print '[ERROR] {}'.format(e)
+                self.helper.testcase.fail(
+                    "Unable to  array push last for key {0} for path {1} after {2} tries"
+                    .format(in_key, path, num_tries))
 
-    def prepend_all_paths(self):
-        pass
+    def array_push_first(self, inserted_keys, long_string, path):
+        for in_key in inserted_keys:
+            num_tries = 1
+            try:
+                opaque, cas, data = self.helper.client.array_push_first_sd(in_key, path ,long_string)
+            except Exception as e:
+                print '[ERROR] {}'.format(e)
+                self.helper.testcase.fail(
+                    "Unable to  array push first for key {0} for path {1} after {2} tries"
+                    .format(in_key, path, num_tries))
 
-    def counter_all_paths(self):
-        pass
+    def counter_all_paths(self, inserted_keys, long_string, path):
+        for in_key in inserted_keys:
+            num_tries = 1
+            try:
+                opaque, cas, data = self.helper.client.counter_sd(in_key, path ,long_string)
+            except Exception as e:
+                print '[ERROR] {}'.format(e)
+                self.helper.testcase.fail(
+                    "Unable to  counter incr/decr for key {0} for path {1} after {2} tries"
+                    .format(in_key, path, num_tries))
+
+    def array_add_unique(self, inserted_keys, long_string, path):
+        for in_key in inserted_keys:
+            num_tries = 1
+            try:
+                opaque, cas, data = self.helper.client.array_add_unique_sd(in_key, path ,long_string)
+            except Exception as e:
+                print '[ERROR] {}'.format(e)
+                self.helper.testcase.fail(
+                    "Unable to  add array_unique key {0} for path {1} after {2} tries"
+                    .format(in_key, path, num_tries))
 
     def multi_mutation_all_paths(self):
         pass
 
-class DeeplyNestedDataSet:
+class DeeplyNestedDataSet(SubdocSanityTests):
     def __init__(self, helper, num_docs):
         self.helper = helper
         self.num_docs = num_docs
         self.name = "deeplynested_dataset"
+        self.levels = 30
+        self.log = logger.Logger.get_logger()
 
     def load(self):
-        inserted_keys = self.helper.insert_docs(self.num_docs, self.name)
-        return inserted_keys
+        inserted_keys = self.helper.insert_nested_docs(self.num_docs, self.name, self.levels)
+        return inserted_keys, self.levels
 
-    def get_all_docs(self):
-            pass
+    def get_all_docs(self, inserted_keys, path):
+        for in_key in inserted_keys:
+            num_tries = 1
+            try:
+                opaque, cas, data = self.helper.client.get_sd(in_key, path)
+                #self.log.info(data)
+                #assert data == check_data
+            except Exception as e:
+                self.log.info(e)
+                self.helper.testcase.fail(
+                    "Unable to get key {0} for path {1} after {2} tries"
+                    .format(in_key, path, num_tries))
 
     def upsert_all_docs(self):
         pass
