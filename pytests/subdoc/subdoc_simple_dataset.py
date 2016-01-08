14564b236e698ced701ba71fbcdd0e404cfc8c41
Parag Agarwal <parag@couchbase.com>
2016-01-08 13:44:53 -0800

    CBQE:SUBDOC - Add more tests for get - simple data set
    Change-Id: If5bc3ffbb5c1ef99c250ef651a398f9be9541cc3
    Reviewed-on: http://review.couchbase.org/58408
    Reviewed-by: Parag Agarwal <agarwal.parag@gmail.com>
    Tested-by: Parag Agarwal <agarwal.parag@gmail.com>

--- pytests/subdoc/subdoc_simple_dataset.py
+++ pytests/subdoc/subdoc_simple_dataset.py
@@ -26,6 +26,21 @@ class SubdocSimpleDataset(SubdocBaseTest):
     		result = result and logic
     	self.assertTrue(result, dict)
 
+    def test_get_boolean(self):
+        dict = {}
+        result = True
+        self.key = "simple_dataset_boolean"
+        self.json =  self.generate_simple_data_boolean()
+        jsonDump = json.dumps(self.json)
+        self.client.set(self.key, 0, 0, jsonDump)
+        for key in self.json.keys():
+            value = self.json[key]
+            logic, data_return  =  self.get_string_and_verify_return( self.client, key = self.key, path = key)
+            if not logic:
+                dict[key] = {"expected": self.json[key], "actual": data_return}
+            result = result and logic
+        self.assertTrue(result, dict)
+
     def test_get_array_numbers(self):
     	dict = {}
     	result = True
@@ -41,10 +56,10 @@ class SubdocSimpleDataset(SubdocBaseTest):
     		result = result and logic
     	self.assertTrue(result, dict)
 
-    def test_get_strings(self):
+    def test_get_array_strings(self):
     	dict = {}
     	result = True
-    	self.key = "generate_simple_data_strings"
+    	self.key = "generate_simple_data_array_strings"
     	self.json =  self.generate_simple_data_strings()
         jsonDump = json.dumps(self.json)
         self.client.set(self.key, 0, 0, jsonDump)
@@ -55,6 +70,34 @@ class SubdocSimpleDataset(SubdocBaseTest):
     			dict[key] = {"expected":self.json[key], "actual":data_return}
     		result = result and logic
     	self.assertTrue(result, dict)
+
+    def test_get_mix_arrays(self):
+        dict = {}
+        result = True
+        self.key = "generate_simple_data_mix_arrays"
+        self.json =  self.generate_simple_data_mix_arrays()
+        jsonDump = json.dumps(self.json)
+        self.client.set(self.key, 0, 0, jsonDump)
+        for key in self.json.keys():
+            value = self.json[key]
+            logic, data_return  =  self.get_string_and_verify_return(self.client, key = self.key, path = key)
+            if not logic:
+                dict[key] = {"expected":self.json[key], "actual":data_return}
+            result = result and logic
+        self.assertTrue(result, dict)
+
+    def test_get_element_arrays(self):
+        dict = {}
+        result = True
+        self.key = "element_arrays"
+        self.json =  self.generate_simple_arrays()
+        jsonDump = json.dumps(self.json)
+        self.client.set(self.key, 0, 0, jsonDump)
+        key_single_dimension_path = "single_dimension_array[0]"
+        self.get_and_verify_with_value(self.client, self.key, key_single_dimension_path, str(self.json["single_dimension_array"][0]))
+        key_two_dimension_path = "two_dimension_array[0][0]"
+        self.get_and_verify_with_value(self.client, self.key, key_two_dimension_path, str(self.json["two_dimension_array"][0][0]))
+        self.assertTrue(result, dict)
 
     def test_get_numbers_boundary(self):
     	dict = {}
@@ -71,7 +114,6 @@ class SubdocSimpleDataset(SubdocBaseTest):
     		result = result and logic
     	self.assertTrue(result, dict)
 
-
     def get_and_verify(self, client, key = '', path = ''):
         try:
             opaque, cas, data = self.client.get_sd(key, path)
@@ -79,9 +121,18 @@ class SubdocSimpleDataset(SubdocBaseTest):
             self.fail("Unable to get key {0} for path {1} after {2} tries".format(key, path, 1))
         self.assertTrue(str(data) == str(self.json[path]), msg="data not returned correctly")
 
+    def get_and_verify_with_value(self, client, key = '', path = '', value = ''):
+        try:
+            opaque, cas, data = self.client.get_sd(key, path)
+            data = json.loads(data)
+        except Exception as e:
+            self.fail("Unable to get key {0} for path {1} after {2} tries".format(key, path, 1))
+        self.assertTrue(str(data) == value, msg="data not returned correctly")
+
     def get_string_and_verify_return(self, client, key = '', path = ''):
         try:
             opaque, cas, data = self.client.get_sd(key, path)
+            data = json.loads(data)
         except Exception as e:
             self.fail("Unable to get key {0} for path {1} after {2} tries".format(key, path, 1))
         return data == self.json[path], data
