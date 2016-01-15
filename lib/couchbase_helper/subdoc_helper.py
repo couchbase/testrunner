08644e93ec94f2aa1d9f4f8ec15dc1abd249e0d5
Parag Agarwal <parag@couchbase.com>
2016-01-14 17:29:45 -0800

    CBQE-0: SUBDOC : test infrastructure api
    Change-Id: I93c26314a3c6a60d8940193f628575b5423865d3
    Reviewed-on: http://review.couchbase.org/58667
    Reviewed-by: Parag Agarwal <agarwal.parag@gmail.com>
    Tested-by: Parag Agarwal <agarwal.parag@gmail.com>

---
+++ lib/couchbase_helper/subdoc_helper.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import copy
+
+class SubdocHelper():
+    def __init__(self):
+    	print "Initialize SubdocHelper"
+
+    def find_json_paths(self, json = {}, path  = "", pairs = {}):
+    	for key in json.keys():
+    		prefix = ""
+    		if path != "":
+    			prefix = path+"."
+    		if isinstance(json[key], dict):
+    			self.find_json_paths(json[key], prefix+key, pairs)
+    		if isinstance(json[key], list):
+    			self.find_array_paths(json[key], prefix+key, pairs)
+    		pairs[prefix+key] = json[key]
+
+    def find_array_paths(self, array = [], path  = "", pairs = {}):
+    	index = 0
+    	for element in array:
+    		if isinstance(element, dict):
+    			self.find_json_paths(element, copy.deepcopy(path)+"["+str(index)+"]", pairs)
+    		if isinstance(element, list):
+    			self.find_array_paths(element, copy.deepcopy(path)+"["+str(index)+"]", pairs)
+    		pairs[path+"["+str(index)+"]"] =element
+    		index += 1
+
+    def show_all_paths(self, pairs):
+    	for path in pairs.keys():
+    		print "PATH = {0} || VALUE = {1} ".format(path, pairs[path])
+
+if __name__=="__main__":
+    helper = SubdocHelper()
+    pairs= {}
+    nest_json = {"string":"data", "integer":3, "simple_json":{"string":"data", "integer":3, "array":[0,1,2, {"field":"value", "array":[0,1,2,3]}]}, "array":[0,1,2]}
+    json_document = {"string":"data", "integer":3,"array":[1,2,3, [0, 1, 2, [3]]], "json":nest_json}
+    helper.find_json_paths(json_document,"", pairs)
+    helper.show_all_paths(pairs)
+
