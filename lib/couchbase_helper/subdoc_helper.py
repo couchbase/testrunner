13fb8b1b8ced73805b41c2c8b5fd43fc07b53675
Parag Agarwal <parag@couchbase.com>
2016-01-14 18:09:51 -0800

    CBQE-0: SUBDOC : generalize json parsing
    Change-Id: I13c4f007152b22fc93d6d64f9075ae58364128b8
    Reviewed-on: http://review.couchbase.org/58672
    Reviewed-by: Parag Agarwal <agarwal.parag@gmail.com>
    Tested-by: Parag Agarwal <agarwal.parag@gmail.com>

--- lib/couchbase_helper/subdoc_helper.py
+++ lib/couchbase_helper/subdoc_helper.py
@@ -23,9 +23,30 @@ class SubdocHelper():
     			self.find_json_paths(element, path + "["+str(index)+"]", pairs)
     		elif isinstance(element, list):
     			self.find_array_paths(element, path + "["+str(index)+"]", pairs)
-    		else:
-    			pairs[path+"["+str(index)+"]"] =element
+    		pairs[path+"["+str(index)+"]"] =element
     		index += 1
+
+    def find_pairs(self, data_set = {}, path  = "", pairs = {}):
+    	if isinstance(data_set, dict):
+    		for key in data_set.keys():
+    			prefix = ""
+    			if path != "":
+    				prefix = path+"."
+    			if isinstance(data_set[key], dict):
+    				self.find_pairs(data_set[key], prefix + key, pairs)
+    			elif isinstance(data_set[key], list):
+    				self.find_pairs(data_set[key], prefix + key, pairs)
+    			pairs[prefix+key] = data_set[key]
+    	elif  isinstance(data_set, list):
+    		index = 0
+    		for element in data_set:
+    			if isinstance(element, dict):
+    				self.find_pairs(element, path + "["+str(index)+"]", pairs)
+    			elif isinstance(element, list):
+    				self.find_pairs(element, path + "["+str(index)+"]", pairs)
+    			pairs[path+"["+str(index)+"]"] =element
+    			index += 1
+
 
     def show_all_paths(self, pairs):
     	for path in pairs.keys():
@@ -36,6 +57,11 @@ if __name__=="__main__":
     pairs= {}
     nest_json = {"string":"data", "integer":3, "simple_json":{"string":"data", "integer":3, "array":[0,1,2, {"field":"value", "array":[0,1,2,3]}]}, "array":[0,1,2]}
     json_document = {"string":"data", "integer":3,"array":[1,2,3, [0, 1, 2, [3]]], "json":nest_json}
-    helper.find_json_paths(json_document,"", pairs)
+    helper.find_pairs(json_document,"", pairs)
+    helper.show_all_paths(pairs)
+    pairs= {}
+    print  "++++++++++++++++++++++++++"
+    nest_json = [{"field_1":1}, {"field_2":2}, 3]
+    helper.find_pairs(nest_json,"", pairs)
     helper.show_all_paths(pairs)
 
