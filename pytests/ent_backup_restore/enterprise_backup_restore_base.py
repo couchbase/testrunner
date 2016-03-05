b744efb150b8923b85bab865c8f49fdb8779b058
Arunkumar Senthilnathan <arun.couchbase@yahoo.com>
2016-03-05 21:27:29 +0530

    CBQE-0: enterprise backup and recovery - fixing bucket creation during restore for sasl and standard buckets Change-Id: Iaf003f2b841f22a4f258ebaf4f09f91a1b57c37a Reviewed-on: http://review.couchbase.org/60984 Reviewed-by: Arunkumar Senthilnathan <arun.couchbase@yahoo.com> Tested-by: Arunkumar Senthilnathan <arun.couchbase@yahoo.com>

--- pytests/ent_backup_restore/enterprise_backup_restore_base.py
+++ pytests/ent_backup_restore/enterprise_backup_restore_base.py
@@ -253,14 +253,18 @@ class EnterpriseBackupRestoreBase(BaseTestCase):
             args += " --force-updates"
         if self.no_progress_bar:
             args += " --no-progress-bar"
-	rest_conn = RestConnection(self.backupset.restore_cluster_host)
+	    rest_conn = RestConnection(self.backupset.restore_cluster_host)
         rest_helper = RestHelper(rest_conn)
         for bucket in self.buckets:
             if not rest_helper.bucket_exists(bucket.name):
                 self.log.info("Creating bucket {0} in restore host {1}".format(bucket.name,
                                                                                self.backupset.restore_cluster_host.ip))
-                rest_conn.create_bucket(bucket=bucket.name, ramQuotaMB=512)
-	remote_client = RemoteMachineShellConnection(self.backupset.backup_host)
+                rest_conn.create_bucket(bucket=bucket.name,
+                                        ramQuotaMB=512,
+                                        authType=bucket.authType if bucket.authType else 'none',
+                                        proxyPort=bucket.port,
+                                        saslPassword=bucket.saslPassword)
+	    remote_client = RemoteMachineShellConnection(self.backupset.backup_host)
         command = "{0}/cbbackupmgr {1}".format(self.cli_command_location, args)
         output, error = remote_client.execute_command(command)
         remote_client.log_command_output(output, error)
