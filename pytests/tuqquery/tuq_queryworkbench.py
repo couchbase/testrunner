bf74834d35c1177befa4bfcce410c401cd7ad1c3
PRERNA MANAKTALA <prerna.manaktala@couchbase.com>
2016-03-30 11:46:13 -0700

    changes for rqg.
    Change-Id: I34b50c0c8504b2f7d481f5a8e8833cafba6a0c68
    Reviewed-on: http://review.couchbase.org/62188
    Reviewed-by: Prerna Manaktala <prerna.manaktala@couchbase.com>
    Tested-by: Prerna Manaktala <prerna.manaktala@couchbase.com>

--- pytests/tuqquery/tuq_queryworkbench.py
+++ pytests/tuqquery/tuq_queryworkbench.py
@@ -21,7 +21,7 @@ class QueryWorkbenchTests(BaseTestCase):
         "NEW_ORDER": 100,
         "ORDERS": 100,
         "ORDER_LINE": 100,
-        "default:": 100
+        #"default:": 100
         }
 
     def setUp(self):
@@ -30,7 +30,7 @@ class QueryWorkbenchTests(BaseTestCase):
         if self.input.tuq_client and "client" in self.input.tuq_client:
             server = self.tuq_client
         self.rest = RestConnection(server)
-        self.rest.delete_bucket("default")
+        #self.rest.delete_bucket("default")
         time.sleep(20)
         # drop and recreate buckets
         for i, bucket_name in enumerate(self.buckets_ram.keys()):
@@ -38,14 +38,17 @@ class QueryWorkbenchTests(BaseTestCase):
                                    ramQuotaMB=int(self.buckets_ram[bucket_name]),
                                    replicaNumber=0,
                                    proxyPort=11218+i)
+            print self.servers[0]
+            #bucket = self.src_cluster.get_bucket_by_name(bucket_name)
         time.sleep(20)
-        self.rest.create_bucket(bucket="default",
-                                   ramQuotaMB=int(self.buckets_ram["default"]),
-                                   replicaNumber=0,
-                                   proxyPort=11218)
+        #self.rest.create_bucket(bucket="default",
+                                   #ramQuotaMB=int(self.buckets_ram["default"]),
+                                   #replicaNumber=0,
+                                   #proxyPort=11218)
+        self._load_all_buckets(self,self.servers[0], self.gen_create, "create", 0)
+        #time.sleep(20)
 
-        time.sleep(20)
-        self._async_load_all_buckets(self.master, self.gen_create, "create", 0)
+
 
 
 
@@ -58,5 +61,5 @@ class QueryWorkbenchTests(BaseTestCase):
         for bucket_name in self.rest.get_buckets():
             query = "infer %s" % bucket_name
             print query
-            result = self.rest.query_tool(query, self.n1ql_port,describe=True)
+            result = self.rest.query_tool(query, self.n1ql_port)
             print result
