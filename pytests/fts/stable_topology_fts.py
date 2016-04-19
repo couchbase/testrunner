0f07836a1a636f38a73d9005848acc2d60c295aa
Aruna Piravi <arunapiravi86@gmail.com>
2016-04-18 17:01:10 -0700

    CBQE-3067 Add dgm query tests
    Change-Id: Iee7ecdbdaf5d4625b86bc990578f3fbc1f4c6d94
    Reviewed-on: http://review.couchbase.org/62998
    Reviewed-by: Aruna Piravi <aruna@couchbase.com>
    Tested-by: Aruna Piravi <aruna@couchbase.com>

--- pytests/fts/stable_topology_fts.py
+++ pytests/fts/stable_topology_fts.py
@@ -31,6 +31,12 @@ class StableTopFTS(FTSBaseTest):
         self.wait_for_indexing_complete()
         self.validate_index_count(equal_bucket_doc_count=True)
 
+    def query_in_dgm(self):
+        self.create_simple_default_index()
+        for index in self._cb_cluster.get_indexes():
+            self.generate_random_queries(index, self.num_queries, self.query_types)
+            self.run_query_and_compare(index)
+
     def run_default_index_query(self, query=None, expected_hits=None):
         self.create_simple_default_index()
         zero_results_ok = True
