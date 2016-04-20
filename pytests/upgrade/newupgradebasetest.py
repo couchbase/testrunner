e3022b3df6730678863f283e23561b3e7813396f
saigon <baolavn@gmail.com>
2016-04-19 23:13:35 -0700

    CBQE-0 update cb release version
    Change-Id: I7c613cf286fce73574b1db2f859d83d8766173fb
    Reviewed-on: http://review.couchbase.org/63075
    Tested-by: Thuan Nguyen <soccon@gmail.com>
    Reviewed-by: Thuan Nguyen <soccon@gmail.com>

--- pytests/upgrade/newupgradebasetest.py
+++ pytests/upgrade/newupgradebasetest.py
@@ -35,7 +35,8 @@ class NewUpgradeBaseTest(BaseTestCase):
         super(NewUpgradeBaseTest, self).setUp()
         self.released_versions = ["2.0.0-1976-rel", "2.0.1", "2.5.0", "2.5.1",
                                   "2.5.2", "3.0.0", "3.0.1",
-                                  "3.0.1-1444", "3.0.2", "3.0.2-1603", "3.0.3"]
+                                  "3.0.1-1444", "3.0.2", "3.0.2-1603", "3.0.3",
+                                  "3.1.0", "3.1.1", "3.1.2", "3.1.3"]
         self.use_hostnames = self.input.param("use_hostnames", False)
         self.product = self.input.param('product', 'couchbase-server')
         self.initial_version = self.input.param('initial_version', '2.5.1-1083')
