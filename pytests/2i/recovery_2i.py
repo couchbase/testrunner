0ed7a71c7239321db9eff161d86bcddf65a7eae7
prasanna135 <prasanna.gholap@yahoo.co.in>
2016-05-12 20:18:53 +0530

    2i Recovery Test add sleep
    Change-Id: I0a948a978f2b019003f70937e7510eb1e0a8ebc3
    Reviewed-on: http://review.couchbase.org/63978
    Reviewed-by: Prasanna Gholap <prasanna.gholap@yahoo.co.in>
    Tested-by: Prasanna Gholap <prasanna.gholap@yahoo.co.in>

--- pytests/2i/recovery_2i.py
+++ pytests/2i/recovery_2i.py
@@ -85,6 +85,7 @@ class SecondaryIndexingRecoveryTests(BaseSecondaryIndexingTests):
             in_between_index_ops = self._run_in_between_tasks()
             rebalance.result()
             self._run_tasks([kvOps_tasks, before_index_ops, in_between_index_ops])
+            self.sleep(30)
             self._run_after_index_tasks()
         except Exception, ex:
             raise
