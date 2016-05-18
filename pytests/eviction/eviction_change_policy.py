bbc5cc938ad8f3f7de9d89b53c47ff42dd2218f4
Eric <ericcouchbase@gmail.com>
2016-05-18 14:22:14 -0700

    CBQE-0: Automated test for MB-19371
    Change-Id: I75ac4c34734259f448763a03cb397333a3fc81f1
    Reviewed-on: http://review.couchbase.org/64178
    Reviewed-by: Eric Cooper <ericcouchbase@gmail.com>
    Tested-by: Eric Cooper <ericcouchbase@gmail.com>

--- pytests/eviction/eviction_change_policy.py
+++ pytests/eviction/eviction_change_policy.py
@@ -4,6 +4,14 @@ from eviction.evictionbase import EvictionBase
 from membase.helper.cluster_helper import ClusterOperationHelper
 from remote.remote_util import RemoteMachineShellConnection
 
+from membase.api.rest_client import RestConnection, RestHelper
+from memcached.helper.data_helper import VBucketAwareMemcached, MemcachedClientHelper
+
+from mc_bin_client import MemcachedError
+
+
+import json
+
 class EvictionChangePolicy(EvictionBase):
 
     def test_reproducer_MB_11698(self):
@@ -45,3 +53,41 @@ class EvictionChangePolicy(EvictionBase):
         self._load_all_buckets(self.master, gen_create2, "create", 0)
         self._wait_for_stats_all_buckets(self.servers[:self.nodes_init], timeout=self.wait_timeout * 5)
         self._verify_stats_all_buckets(self.servers[:self.nodes_init])
+
+
+    # MB-19371
+    def test_full_eviction_changed_to_value_eviction(self):
+
+        KEY_NAME = 'key1'
+
+        gen_create = BlobGenerator('eviction', 'eviction-', self.value_size, end=self.num_items)
+        gen_create2 = BlobGenerator('eviction2', 'eviction2-', self.value_size, end=self.num_items)
+        self._load_all_buckets(self.master, gen_create, "create", 0)
+
+        self._wait_for_stats_all_buckets(self.servers[:self.nodes_init])
+        self._verify_stats_all_buckets(self.servers[:self.nodes_init])
+        remote = RemoteMachineShellConnection(self.master)
+        for bucket in self.buckets:
+            output, _ = remote.execute_couchbase_cli(cli_command='bucket-edit',
+                                                         cluster_host="localhost",
+                                                         user=self.master.rest_username,
+                                                         password=self.master.rest_password,
+                                                         options='--bucket=%s --bucket-eviction-policy=valueOnly' % bucket.name)
+            self.assertTrue(' '.join(output).find('SUCCESS') != -1, 'Eviction policy wasn\'t changed')
+        ClusterOperationHelper.wait_for_ns_servers_or_assert(
+                                            self.servers[:self.nodes_init], self,
+                                            wait_time=self.wait_timeout, wait_if_warmup=True)
+        self.sleep(10, 'Wait some time before next load')
+        #self._load_all_buckets(self.master, gen_create2, "create", 0)
+        #import pdb;pdb.set_trace()
+
+
+        rest = RestConnection(self.master)
+        client = VBucketAwareMemcached(rest, 'default')
+        mcd = client.memcached(KEY_NAME)
+        try:
+            rc = mcd.set(KEY_NAME, 0,0, json.dumps({'value':'value2'}))
+            self.fail('Bucket is incorrectly functional')
+        except MemcachedError, e:
+            pass   # this is the exception we are hoping for
+
