978275cf53abd0a02ee5b4aad61928c74099047f
Mihir Kamdar <mihir.kamdar@couchbase.com>
2016-08-03 16:41:50 +0530

    CBQE-0 : Fixed an invalid argument name in fts_base.py to create ES index map
    Change-Id: I869362487cafeeb794c66ba2d88d8238f999e703
    Reviewed-on: http://review.couchbase.org/66447
    Reviewed-by: Mihir Kamdar <mihir.kamdar@couchbase.com>
    Tested-by: Mihir Kamdar <mihir.kamdar@couchbase.com>

--- pytests/fts/fts_base.py
+++ pytests/fts/fts_base.py
@@ -2759,9 +2759,10 @@ class FTSBaseTest(unittest.TestCase):
     def create_es_index_mapping(self, es_mapping, fts_mapping):
         if not (self.num_custom_analyzers > 0):
             self.es.create_index_mapping(index_name="es_index",
-                                     mapping=es_mapping,fts_mapping=None)
+                                    es_mapping=es_mapping,fts_mapping=None)
         else:
-            self.es.create_index_mapping(index_name="es_index",es_mapping=es_mapping,fts_mapping=fts_mapping)
+            self.es.create_index_mapping(index_name="es_index",
+                                    es_mapping=es_mapping,fts_mapping=fts_mapping)
 
     def load_data_es_from_generator(self, generator,
                                     index_name="es_index"):
