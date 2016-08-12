5f1a2ea5a79bfa922aafd1e381248f945bbf1319
bharath-gp <gpbharath@yahoo.in>
2016-08-12 20:39:30 +0530

    CBQE-3556: Fix rebalanceinout_P1 test cases for failures Check the exception as string
    Change-Id: I8e7d5e1739a83353e488f43eaf55a7cc2456fc51
    Reviewed-on: http://review.couchbase.org/66732
    Reviewed-by: Bharath G P <bharath.gp@couchbase.com>
    Tested-by: Bharath G P <bharath.gp@couchbase.com>

--- pytests/rebalance/rebalanceinout.py
+++ pytests/rebalance/rebalanceinout.py
@@ -109,7 +109,7 @@ class RebalanceInOutTests(RebalanceBaseTest):
         try:
             self.shuffle_nodes_between_zones_and_rebalance(servs_out)
         except Exception, e:
-            if "deltaRecoveryNotPossible" not in e:
+            if "deltaRecoveryNotPossible" not in e.__str__():
                 self.fail("Rebalance did not fail. Rebalance has to fail since no delta recovery should be possible"
                           " while adding nodes too")
 
