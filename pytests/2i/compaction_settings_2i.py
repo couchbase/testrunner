a2c3ec43235247e2a974e12ff1217a0dad02d3a9
prasanna135 <prasanna.gholap@yahoo.co.in>
2016-09-26 18:56:11 +0530

    2i Fix for Circular Compaction timeout issue
    Change-Id: I66a2c79f369310057065f5746742e6af8ac93d2c
    Reviewed-on: http://review.couchbase.org/67998
    Reviewed-by: Prasanna Gholap <prasanna.gholap@yahoo.co.in>
    Tested-by: Prasanna Gholap <prasanna.gholap@yahoo.co.in>

--- pytests/2i/compaction_settings_2i.py
+++ pytests/2i/compaction_settings_2i.py
@@ -116,13 +116,13 @@ class SecondaryIndexingCompactionTests(BaseSecondaryIndexingTests):
         #Trust Me this works
         dayOfWeek = (date.weekday() + (date.hour+((date.minute+5)/60))/24)%7
         status, content, header = rest.set_indexer_compaction(indexDayOfWeek=DAYS[dayOfWeek],
-                                              indexFromHour=date.hour+((date.minute+1)/60),
-                                              indexFromMinute=(date.minute+1)%60,
-                                              indexToHour=date.hour+((date.minute+2)/60),
-                                              indexToMinute=(date.minute+2)%60,
+                                              indexFromHour=date.hour+((date.minute+2)/60),
+                                              indexFromMinute=(date.minute+2)%60,
+                                              indexToHour=date.hour+((date.minute+3)/60),
+                                              indexToMinute=(date.minute+3)%60,
                                               abortOutside=True)
         self.assertTrue(status, "Error in setting Circular Compaction... {0}".format(content))
-        self.sleep(180)
+        self.sleep(300)
         final_index_map = rest.get_index_stats()
         self.check_compaction_number(initial_index_map, final_index_map)
         self._validate_compaction_for_abort_or_complete()
