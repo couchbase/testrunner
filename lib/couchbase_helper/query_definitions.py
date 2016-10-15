e7abf1f1d3fba0f4d874e612d9abced96cd064d5
prasanna135 <prasanna.gholap@yahoo.co.in>
2016-10-15 20:04:58 +0530

    Fix for CBQE-3661 Update array index document generator and query definition generator
    Change-Id: Id40d565105d36ba9b881c414d540c83d76ea64ac
    Reviewed-on: http://review.couchbase.org/68781
    Tested-by: Ritam Sharma <ritamcouchbase@gmail.com>
    Reviewed-by: Ritam Sharma <ritamcouchbase@gmail.com>
    Reviewed-by: Mihir Kamdar <mihir.kamdar@couchbase.com>

--- lib/couchbase_helper/query_definitions.py
+++ lib/couchbase_helper/query_definitions.py
@@ -1,4 +1,6 @@
 import uuid
+import random
+
 FULL_SCAN_TEMPLATE = "SELECT {0} FROM %s"
 RANGE_SCAN_TEMPLATE = "SELECT {0} FROM %s WHERE {1}"
 FULL_SCAN_GROUP_BY_TEMPLATE = "SELECT {0} FROM %s GROUP by {2}"
@@ -11,6 +13,10 @@ RANGE_SCAN_JOIN_TEMPLATE = "SELECT s1.{0},s2.{1} FROM %s as s1 JOIN %s as s2"
 INDEX_CREATION_TEMPLATE =  "CREATE INDEX %s ON %s(%s)"
 INDEX_DROP_TEMPLATE = "DROP INDEX %s.%s"
 SIMPLE_INDEX="simple"
+SIMPLE_ARRAY="simple_array"
+ARRAY="array"
+DUPLICATE_ARRAY="duplicate_array"
+DISTINCT_ARRAY="distinct_array"
 COMPOSITE_INDEX="composite"
 GROUP_BY="groupby"
 ORDER_BY="orderby"
@@ -24,6 +30,7 @@ GREATER_THAN="greater_than"
 LESS_THAN="less_than"
 AND = "and"
 OR = "or"
+
 class QueryDefinition(object):
 	def __init__(self, name = "default", index_name = "Random", index_fields = [], index_creation_template = INDEX_CREATION_TEMPLATE,
 		index_drop_template = INDEX_DROP_TEMPLATE, query_template = "", groups = [], index_where_clause = None, gsi_type = None):
@@ -56,6 +63,24 @@ class QueryDefinition(object):
 			query += " WITH " + str(deployment_plan)
 		return query
 
+	def generate_gsi_index_create_query_using_rest(self, bucket="default", deploy_node_info=None, defer_build=None,
+												   index_where_clause=None, gsi_type="forestdb", expr_type="N1QL"):
+		deployment_plan = {}
+		ind_content = {}
+		ind_content["name"] = self.index_name
+		ind_content["bucket"] = "{0}".format(bucket)
+		ind_content["secExprs"] = self.index_fields
+		ind_content["using"] = gsi_type
+		ind_content["exprType"] = "{0}".format(expr_type)
+		if index_where_clause:
+			ind_content["whereExpr"] = index_where_clause
+		if deploy_node_info  != None:
+			deployment_plan["nodes"] = deploy_node_info
+		if defer_build != None:
+			deployment_plan["defer_build"] = defer_build
+		ind_content["with"] = str(deployment_plan)
+		return ind_content
+
 	def generate_index_drop_query(self, bucket = "default", use_gsi_for_secondary = True, use_gsi_for_primary = True):
 		if "primary" in self.index_name:
 			query =  "DROP PRIMARY INDEX ON {0}".format(bucket)
@@ -65,7 +90,7 @@ class QueryDefinition(object):
 			query += " USING GSI"
 		elif use_gsi_for_primary and "primary" in self.index_name:
 			query += " USING GSI"
-                if not use_gsi_for_secondary:
+		if not use_gsi_for_secondary:
 			query += " USING VIEW "
 		return query
 
@@ -309,7 +334,197 @@ class SQLDefinitionGenerator:
 				groups = [SIMPLE_INDEX,RANGE_SCAN, NO_ORDERBY_GROUPBY, LESS_THAN,"employee"], index_where_clause = " join_yr < 2014 "))
 		return definitions_list
 
-	def filter_by_group(self, groups = [], query_definitions = []):
+	def generate_airlines_data_query_definitions(self):
+		definitions_list = []
+
+		#emit_fields = "name, job_title, join_yr, join_mo, join_day"
+		emit_fields = "*"
+		and_conditions = ["job_title = \"Sales\"","job_title != \"Sales\""]
+		#Primary Index
+		index_name_prefix = "airlines_" + str(random.randint(100000, 999999))
+		definitions_list.append(
+			QueryDefinition(index_name=index_name_prefix + "_primary_index",
+							index_fields=[], query_template="SELECT * FROM %s",
+							groups=["full_data_set","primary"],
+							index_where_clause=""))
+		#simple index on string
+		index_name_prefix = "airlines_" + str(random.randint(100000, 999999))
+		definitions_list.append(
+			QueryDefinition(index_name=index_name_prefix + "_name",
+							index_fields=["name"],
+							query_template=RANGE_SCAN_ORDER_BY_TEMPLATE.format(
+								emit_fields, "name IS NOT NULL","name,_id"),
+							groups=["all", SIMPLE_INDEX, FULL_SCAN, ORDER_BY, "airlines", "isnotnull"],
+							index_where_clause=" name IS NOT NULL "))
+		#simple index on int
+		index_name_prefix = "airlines_" + str(random.randint(100000, 999999))
+		definitions_list.append(
+			QueryDefinition(index_name=index_name_prefix + "_age",
+							 index_fields=["age"],
+							 query_template=RANGE_SCAN_TEMPLATE.format(emit_fields," %s " % "age = 40"),
+							 groups=["all", SIMPLE_INDEX, NO_ORDERBY_GROUPBY, EQUALS],
+							index_where_clause=" age IS NOT NULL "))
+		#simple index on boolean
+		index_name_prefix = "airlines_" + str(random.randint(100000, 999999))
+		definitions_list.append(
+			QueryDefinition(index_name=index_name_prefix + "_premium_customer",
+							 index_fields=["premium_customer"],
+							 query_template=RANGE_SCAN_TEMPLATE.format(emit_fields," %s " % "premium_customer = True"),
+							 groups=["all", SIMPLE_INDEX, NO_ORDERBY_GROUPBY, EQUALS],
+							index_where_clause=" premium_customer IS NOT NULL "))
+		#array duplicate index on strings
+		index_name_prefix = "airlines_" + str(random.randint(100000, 999999))
+		definitions_list.append(
+			QueryDefinition(index_name=index_name_prefix + "_travel_history_duplicate",
+							index_fields=["ALL ARRAY t FOR t in `travel_history` END"],
+							query_template=RANGE_SCAN_TEMPLATE.format(emit_fields," %s " %
+										   "ANY t IN travel_history SATISFIES t = \"India\" END ORDER BY _id"),
+							groups=["all", ARRAY, SIMPLE_ARRAY, DUPLICATE_ARRAY, ORDER_BY, EQUALS,"airlines"],
+							index_where_clause=" travel_history IS NOT NULL "))
+		#array distinct index on strings
+		index_name_prefix = "airlines_" + str(random.randint(100000, 999999))
+		definitions_list.append(
+			QueryDefinition(index_name=index_name_prefix + "_travel_history_distinct",
+							index_fields=["DISTINCT ARRAY t FOR t in `travel_history` END"],
+							query_template=RANGE_SCAN_TEMPLATE.format(emit_fields," %s " %
+										   "ANY t IN travel_history SATISFIES t = \"India\" END ORDER BY _id"),
+							groups=["all", ARRAY, SIMPLE_INDEX, SIMPLE_ARRAY, DISTINCT_ARRAY,
+									ORDER_BY, EQUALS, "airlines"],
+							index_where_clause=" travel_history IS NOT NULL "))
+		#array duplicate index on alphanumeric
+		index_name_prefix = "airlines_" + str(random.randint(100000, 999999))
+		definitions_list.append(
+			QueryDefinition(index_name=index_name_prefix + "_travel_history_code_duplicate",
+							index_fields=["ALL ARRAY t FOR t in `travel_history_code` END"],
+							query_template=RANGE_SCAN_TEMPLATE.format(emit_fields," %s " %
+										   "ANY t IN travel_history_code SATISFIES t = \"Ind123\" END ORDER BY _id"),
+							groups=["all", ARRAY, SIMPLE_ARRAY, DUPLICATE_ARRAY, ORDER_BY, EQUALS,"airlines"],
+							index_where_clause=" travel_history_code IS NOT NULL "))
+		#array distinct index on alphanumeric
+		index_name_prefix = "airlines_" + str(random.randint(100000, 999999))
+		definitions_list.append(
+			QueryDefinition(index_name=index_name_prefix + "_travel_history_code_distinct",
+							index_fields=["DISTINCT ARRAY t FOR t in `travel_history_code` END"],
+							query_template=RANGE_SCAN_TEMPLATE.format(emit_fields," %s " %
+										   "ANY t IN travel_history_code SATISFIES t = \"Ind123\" END ORDER BY _id"),
+							groups=["all", ARRAY, SIMPLE_INDEX, SIMPLE_ARRAY, DISTINCT_ARRAY,
+									ORDER_BY, EQUALS, "airlines"],
+							index_where_clause=" travel_history_code IS NOT NULL "))
+		#array duplicate index on numbers
+		index_name_prefix = "airlines_" + str(random.randint(100000, 999999))
+		definitions_list.append(
+			QueryDefinition(index_name=index_name_prefix + "_credit_cards_duplicate",
+							index_fields=["ALL ARRAY t FOR t in `credit_cards` END"],
+							query_template=RANGE_SCAN_TEMPLATE.format(emit_fields," %s " %
+										   "ANY t IN credit_cards SATISFIES t > 5000000 END ORDER BY _id"),
+							groups=["all", ARRAY, SIMPLE_ARRAY, DUPLICATE_ARRAY, RANGE_SCAN, ORDER_BY,"airlines"],
+							index_where_clause=" credit_cards IS NOT NULL "))
+		#array distinct index on numbers
+		index_name_prefix = "airlines_" + str(random.randint(100000, 999999))
+		definitions_list.append(
+			QueryDefinition(index_name=index_name_prefix + "_credit_cards_distinct",
+							index_fields=["DISTINCT ARRAY t FOR t in `credit_cards` END"],
+							query_template=RANGE_SCAN_TEMPLATE.format(emit_fields," %s " %
+										   "ANY t IN credit_cards SATISFIES t > 5000000 END ORDER BY _id"),
+							groups=["all", ARRAY, SIMPLE_INDEX, SIMPLE_ARRAY, DISTINCT_ARRAY,
+									RANGE_SCAN, ORDER_BY, "airlines"],
+							index_where_clause=" credit_cards IS NOT NULL "))
+		#Duplcate array on boolean array
+		index_name_prefix = "airlines_" + str(random.randint(100000, 999999))
+		definitions_list.append(
+			QueryDefinition(index_name=index_name_prefix + "_question_values_duplicate",
+							index_fields=["ALL ARRAY t FOR t in `question_values` END"],
+							query_template=RANGE_SCAN_TEMPLATE.format(emit_fields," %s "
+										   % "ANY t IN question_values SATISFIES t = True END ORDER BY _id"),
+							groups=["all", ARRAY, SIMPLE_ARRAY, DUPLICATE_ARRAY, RANGE_SCAN, ORDER_BY, "airlines"],
+							index_where_clause=" question_values IS NOT NULL "))
+		#Distinct array on boolean array
+		index_name_prefix = "airlines_" + str(random.randint(100000, 999999))
+		definitions_list.append(
+			QueryDefinition(index_name=index_name_prefix + "_question_values_duplicate",
+							index_fields=["DISTINCT ARRAY t FOR t in `question_values` END"],
+							query_template=RANGE_SCAN_TEMPLATE.format(emit_fields," %s " %
+										   "ANY t IN question_values SATISFIES t = True END ORDER BY _id"),
+							groups=["all", ARRAY, SIMPLE_INDEX, SIMPLE_ARRAY, DISTINCT_ARRAY,
+									RANGE_SCAN, ORDER_BY, "airlines"],
+							index_where_clause=" question_values IS NOT NULL "))
+		#array distinct index on mixed data type
+		index_name_prefix = "airlines_" + str(random.randint(100000, 999999))
+		definitions_list.append(
+			QueryDefinition(index_name=index_name_prefix + "_secret_combination",
+							index_fields=["DISTINCT ARRAY t FOR t in `secret_combination` END"],
+							query_template=RANGE_SCAN_TEMPLATE.format(emit_fields," %s " %
+										   "ANY t IN secret_combination SATISFIES t > \"a\" OR t > 1 END ORDER BY _id"),
+							groups=["all", ARRAY, SIMPLE_INDEX, SIMPLE_ARRAY, DISTINCT_ARRAY,
+									RANGE_SCAN, ORDER_BY, OR, "airlines"],
+							index_where_clause=" secret_combination IS NOT NULL "))
+		#array index on items if object
+		index_name_prefix = "airlines_" + str(random.randint(100000, 999999))
+		definitions_list.append(
+			QueryDefinition(index_name=index_name_prefix + "_booking_duplicate",
+							index_fields=["ALL ARRAY t FOR t in TO_ARRAY(`booking`) END"],
+							query_template=RANGE_SCAN_TEMPLATE.format(emit_fields," %s " %
+										   "ANY t IN booking SATISFIES t.source = \"India\" END ORDER BY _id"),
+							groups=["all", ARRAY, SIMPLE_ARRAY, DUPLICATE_ARRAY, RANGE_SCAN,
+									ORDER_BY, EQUALS, "airlines"],
+							index_where_clause=" booking IS NOT NULL "))
+		#array index on items if object
+		index_name_prefix = "airlines_" + str(random.randint(100000, 999999))
+		definitions_list.append(
+			QueryDefinition(index_name=index_name_prefix + "_booking_distinct",
+							index_fields=["DISTINCT ARRAY t FOR t in TO_ARRAY(`booking`) END"],
+							query_template=RANGE_SCAN_TEMPLATE.format(emit_fields," %s " %
+										   "ANY t IN booking SATISFIES t.source = \"India\" END ORDER BY _id"),
+							groups=["all", ARRAY, SIMPLE_ARRAY, DISTINCT_ARRAY, RANGE_SCAN,
+									ORDER_BY, EQUALS, "airlines"],
+							index_where_clause=" booking IS NOT NULL "))
+		#Composite array distinct index
+		index_name_prefix = "airlines_" + str(random.randint(100000, 999999))
+		definitions_list.append(
+			QueryDefinition(index_name=index_name_prefix + "_travel_history_name_age",
+							index_fields=["DISTINCT ARRAY t FOR t in `travel_history` END", "name", "age"],
+							query_template=RANGE_SCAN_TEMPLATE.format(emit_fields," %s " %
+							   "ANY t IN travel_history SATISFIES t = \"India\" END AND name IS NOT NULL ORDER BY _id"),
+							groups=["all", ARRAY, COMPOSITE_INDEX, DISTINCT_ARRAY, RANGE_SCAN,
+									ORDER_BY, AND, EQUALS, "airlines"],
+							index_where_clause=" travel_history IS NOT NULL "))
+		#Simple array on scalar
+		index_name_prefix = "airlines_" + str(random.randint(100000, 999999))
+		definitions_list.append(
+			QueryDefinition(index_name=index_name_prefix + "_name_array",
+							index_fields=["DISTINCT ARRAY t FOR t in TO_ARRAY(`name`) END"],
+							query_template=RANGE_SCAN_TEMPLATE.format(emit_fields," %s " %
+										   "ANY t IN name SATISFIES t = \"Ciara\" END ORDER BY _id"),
+							groups=["all", ARRAY, SIMPLE_ARRAY, DISTINCT_ARRAY, RANGE_SCAN,
+									ORDER_BY, EQUALS, "airlines"],
+							index_where_clause=" name IS NOT NULL "))
+		#Duplicate array Index on Array of objects
+		index_name_prefix = "airlines_" + str(random.randint(100000, 999999))
+		definitions_list.append(
+			QueryDefinition(index_name=index_name_prefix + "_travel_details_duplicate",
+							index_fields=["ALL ARRAY t FOR t in `travel_details` END"],
+							query_template=RANGE_SCAN_TEMPLATE.format(emit_fields," %s " %
+										   "ANY t IN travel_details SATISFIES t.country = \"India\" END ORDER BY _id"),
+							groups=["all", ARRAY, SIMPLE_ARRAY, DUPLICATE_ARRAY, RANGE_SCAN,
+									ORDER_BY, EQUALS, "airlines"],
+							index_where_clause=" travel_details IS NOT NULL "))
+		#Distinct array Index on Array of objects
+		index_name_prefix = "airlines_" + str(random.randint(100000, 999999))
+		definitions_list.append(
+			QueryDefinition(index_name=index_name_prefix + "_travel_details_distinct",
+							index_fields=["DISTINCT ARRAY t FOR t in `travel_details` END"],
+							query_template=RANGE_SCAN_TEMPLATE.format(emit_fields," %s " %
+										   "ANY t IN travel_details SATISFIES t.country = \"India\" END ORDER BY _id"),
+							groups=["all", ARRAY, SIMPLE_ARRAY, SIMPLE_INDEX, DISTINCT_ARRAY,
+									RANGE_SCAN, ORDER_BY, EQUALS, "airlines"],
+							index_where_clause=" travel_details IS NOT NULL "))
+		return definitions_list
+
+	def filter_by_group(self, groups = None, query_definitions = None):
+		if not groups:
+			groups = []
+		if not query_definitions:
+			query_definitions = []
 		new_query_definitions = {}
 		for query_definition in query_definitions:
 			count = 0
