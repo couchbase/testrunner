4ea02e3a7dcce5f8b121554d1ad4d6726d27e822
prasanna135 <prasanna.gholap@yahoo.co.in>
2016-10-24 12:11:54 +0530

    Fix for CBQE-3668
    Date-time functions tests
    
    Change-Id: I1ffd3fa24e3b597aa9a33063b704970b6cb1456a
    Reviewed-on: http://review.couchbase.org/69115
    Reviewed-by: Mihir Kamdar <mihir.kamdar@couchbase.com>
    Tested-by: Prasanna Gholap <prasanna.gholap@yahoo.co.in>

--- pytests/tuqquery/date_time_functions.py
+++ pytests/tuqquery/date_time_functions.py
@@ -4,21 +4,19 @@ import time
 
 from datetime import datetime
 from dateutil.parser import parse
+from threading import Thread
 from tuq import QueryTests
 
 log = logging.getLogger(__name__)
 
-FORMATS = ["2006-01-02T15:04:05.999Z07:00",
-           "2006-01-02T15:04:05Z07:00",
+FORMATS = ["2006-01-02T15:04:05.999+07:00",
            "2006-01-02T15:04:05.999",
            "2006-01-02T15:04:05",
-           "2006-01-02 15:04:05.999Z07:00",
-           "2006-01-02 15:04:05Z07:00",
+           "2006-01-02 15:04:05.999+07:00",
            "2006-01-02 15:04:05.999",
            "2006-01-02 15:04:05",
            "2006-01-02",
-           "15:04:05.999Z07:00",
-           "15:04:05Z07:00",
+           "15:04:05.999+07:00",
            "15:04:05.999",
            "15:04:05"]
 
@@ -97,10 +95,91 @@ class DateTimeFunctionClass(QueryTests):
             for expected_format in FORMATS:
                 query = self._generate_date_format_str_query(expression, expected_format)
                 actual_result = self.run_cbq_query(query)
-                query = 'SELECT MILLIS_TO_STR(MILLIS("{0}"), "{1}")'.format(expression, expected_format)
+                query = 'SELECT MILLIS_TO_UTC(MILLIS("{0}"), "{1}")'.format(expression, expected_format)
                 expected_result = self.run_cbq_query(query)
                 self.assertEqual(actual_result["results"][0]["$1"], expected_result["results"][0]["$1"],
-                         "Resulting format {0} doesn't match with expected {1}".format(actual_result["results"][0]["$1"], expected_result["results"][0]["$1"]))
+                         "Resulting format {0} doesn't match with expected {1}".format(
+                             actual_result["results"][0]["$1"], expected_result["results"][0]["$1"]))
+
+    def test_array_date_range(self):
+        error_query = []
+        local_parts = ["millennium",
+                       "century",
+                       "decade",
+                       "year",
+                       "quarter",
+                       "month",
+                       "week",
+                       "day",
+                       "hour",
+                       "minute",
+                       "second",
+                       "millisecond"]
+        count = 3
+        for first_expression in FORMATS:
+            expect_null_result = 0
+            for part in local_parts:
+                query = 'SELECT DATE_ADD_STR("{0}", {1}, "{2}")'.format(first_expression, count, part)
+                expected_result = self.run_cbq_query(query)
+                temp_expression = expected_result["results"][0]["$1"]
+                self.assertIsNotNone(temp_expression, "result is {0} for query {1}".format(expected_result, query))
+                query = self._generate_date_format_str_query(temp_expression, first_expression)
+                result = self.run_cbq_query(query)
+                second_expression = result["results"][0]["$1"]
+                if part in local_parts[:8]:
+                    if not (self._is_date_part_present(first_expression) and
+                                self._is_date_part_present(second_expression)):
+                        expect_null_result = 1
+                else:
+                    if not (self._is_time_part_present(first_expression) and
+                                self._is_time_part_present(second_expression)):
+                        expect_null_result = 1
+                query = self._generate_array_date_range_query(first_expression, second_expression, part)
+                log.info(query)
+                try:
+                    actual_result = self.run_cbq_query(query)
+                except Exception:
+                    error_query.append(query)
+                else:
+                    lst = actual_result["results"][0]["$1"]
+                    if not expect_null_result and not lst:
+                        error_query.append(query)
+                    elif lst:
+                        if len(lst) != count+1:
+                            error_query.append(query)
+        self.assertFalse(error_query, "Queries Failed are: {0}".format(error_query))
+
+    def test_array_date_range_for_intervals(self):
+        #Set Interval
+        intervals = [0, 2, 10, -1]
+        start_date = "2006-01-02T15:04:05"
+        end_date = "2006-01-10T15:04:05"
+        for interval in intervals:
+            query = self._generate_array_date_range_query(start_date, end_date, "day", interval)
+            actual_result = self.run_cbq_query(query)
+            if interval < 0:
+                self.assertIsNone(actual_result["results"][0]["$1"],
+                                  "{0}  Failed. Result {1}".format(query, actual_result))
+            lst = actual_result["results"][0]["$1"]
+            if interval == 0:
+                self.asserEqual(len(lst), 0, "Query {1} Failed".format(query))
+            self.asserEqual(len(lst), 8/interval, "Query {1} Failed".format(query))
+
+    def test_new_functions(self):
+        local_formats = ["2006-01-02"]
+        for expression in local_formats:
+            query = 'SELECT STR_TO_UTC(CLOCK_STR("{0}"))'.format(expression)
+            expected_result = self.run_cbq_query(query)
+            query = 'SELECT CLOCK_UTC("{0}")'.format(expression)
+            actual_result = self.run_cbq_query(query)
+            self.assertEqual(actual_result["results"][0]["$1"], expected_result["results"][0]["$1"],
+                             "{0} failed ".format(query))
+            query = 'SELECT STR_TO_UTC(NOW_STR("{0}"))'.format(expression)
+            expected_result = self.run_cbq_query(query)
+            query = 'SELECT NOW_UTC("{0}")'.format(expression)
+            actual_result = self.run_cbq_query(query)
+            self.assertEqual(actual_result["results"][0]["$1"], expected_result["results"][0]["$1"],
+                             "{0} failed ".format(query))
 
     def _generate_date_part_millis_query(self, expression, part, timezone=None):
         if not timezone:
@@ -132,8 +211,14 @@ class DateTimeFunctionClass(QueryTests):
         return query
 
     def _generate_array_date_range_query(self, initial_date, final_date, part, increment=None):
-        if not increment:
+        if increment is None:
             query = 'SELECT ARRAY_DATE_RANGE("{0}", "{1}", "{2}")'.format(initial_date, final_date, part)
         else:
             query = 'SELECT ARRAY_DATE_RANGE("{0}", "{1}", "{2}", {3})'.format(initial_date, final_date, part, increment)
         return query
+
+    def _is_date_part_present(self, expression):
+        return (len(expression.split("-")) > 1)
+
+    def _is_time_part_present(self, expression):
+        return (len(expression.split(":")) > 1)
