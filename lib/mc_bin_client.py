e8e983c97003a88d3f878508039288c8fed640f1
Eric <ericcouchbase@gmail.com>
2017-01-03 12:11:32 -0800

    CBQE-0: fix the del with meta command
    Change-Id: Ia0e063baa6615245412c1b176df9b0df6a498808
    Reviewed-on: http://review.couchbase.org/71521
    Reviewed-by: Eric Cooper <ericcouchbase@gmail.com>
    Tested-by: Eric Cooper <ericcouchbase@gmail.com>

--- lib/mc_bin_client.py
+++ lib/mc_bin_client.py
@@ -332,6 +332,7 @@ class MemcachedClient(object):
 
 
     def del_with_meta(self, key, exp, flags, seqno, cas, vbucket= -1,
+                      options=0,
                       add_extended_meta_data=False,
                       adjusted_time=0, conflict_resolution_mode=0):
         """Set a value in the memcached server."""
@@ -339,7 +340,7 @@ class MemcachedClient(object):
 
         resp = self._doCmd(memcacheConstants.CMD_DEL_WITH_META, key, '',
                        struct.pack(memcacheConstants.EXTENDED_META_CMD_FMT, flags,
-                                   exp, seqno, cas, memcacheConstants.FORCE_ACCEPT_WITH_META_OPS) )
+                                   exp, seqno, cas, options, 0) )
         return resp
 
 
