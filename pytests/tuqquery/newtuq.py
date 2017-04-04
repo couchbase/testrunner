b612cb6f299e28956721be3f06e5271b1dbd7d77
PRERNA MANAKTALA <prerna.manaktala@couchbase.com>
2017-04-04 12:56:03 -0700

    Fix test_order_by_alias_aggr_fn.
    Change-Id: Id2e7b8cb7e2d33010a5a655440cdcde67c00df24
    Reviewed-on: http://review.couchbase.org/76280
    Reviewed-by: Prerna Manaktala <prerna.manaktala@couchbase.com>
    Tested-by: Prerna Manaktala <prerna.manaktala@couchbase.com>

--- pytests/tuqquery/newtuq.py
+++ pytests/tuqquery/newtuq.py
@@ -475,7 +475,7 @@ class QueryTests(BaseTestCase):
                                                                             bucket.name) +\
             ' WHERE $int1 >7 GROUP BY $int0, $int1 ORDER BY emp_per_month, $int1, $int0'  
             actual_result, expected_result = self.run_query_from_template(query_template)
-            self.assertEquals(actual_result['results'], [])
+            self.assertTrue(len(actual_result['results'])== 0)
 
     def test_order_by_aggr_fn(self):
         for bucket in self.buckets:
