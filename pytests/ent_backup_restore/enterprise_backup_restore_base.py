b5bca790981343c6c9af2ac29146efd14ab7e830
saigon <baolavn@gmail.com>
2017-04-19 11:42:49 -0700

    CBQE-4038 fix test broken in backup/restore sanity
    Change-Id: Ia6975bdd4d59316f746f4532d28a600d9addd448
    Reviewed-on: http://review.couchbase.org/77065
    Reviewed-by: Thuan Nguyen <soccon@gmail.com>
    Tested-by: Thuan Nguyen <soccon@gmail.com>

--- pytests/ent_backup_restore/enterprise_backup_restore_base.py
+++ pytests/ent_backup_restore/enterprise_backup_restore_base.py
@@ -296,35 +296,31 @@ class EnterpriseBackupRestoreBase(BaseTestCase):
             count = 0
             buckets = []
             for bucket in self.buckets:
-                if not rest_helper.bucket_exists(bucket.name):
+                bucket_name = bucket.name
+                if not rest_helper.bucket_exists(bucket_name):
                     if self.backupset.map_buckets is None:
                         self.log.info("Creating bucket {0} in restore host {1}"
-                                                    .format(bucket.name,
-                                                     self.backupset.restore_cluster_host.ip))
-                        rest_conn.create_bucket(bucket=bucket.name,
-                                        ramQuotaMB=512,
-                                        authType=bucket.authType if bucket.authType else 'none',
-                                        proxyPort=bucket.port,
-                                        saslPassword=bucket.saslPassword,
-                                        lww=self.lww_new)
-                        bucket_ready = rest_helper.vbucket_map_ready(bucket.name)
-                        if not bucket_ready:
-                            self.fail("Bucket %s not created after 120 seconds." % bucket.name)
+                                                .format(bucket_name,
+                                                 self.backupset.restore_cluster_host.ip))
                     elif self.backupset.map_buckets:
                         self.log.info("Create new bucket name to restore to this bucket")
+                        bucket_maps = ""
                         bucket_name = bucket.name + "_" + str(count)
-                        rest_conn.create_bucket(bucket=bucket_name,
-                                        ramQuotaMB=512,
-                                        authType=bucket.authType if bucket.authType else 'none',
-                                        proxyPort=bucket.port,
-                                        saslPassword=bucket.saslPassword,
-                                        lww=self.lww_new)
-                        bucket_ready = rest_helper.vbucket_map_ready(bucket_name)
-                        if not bucket_ready:
-                            self.fail("Bucket %s not created after 120 seconds." % bucket_name)
-                    buckets.append("%s=%s" % (bucket.name, bucket_name))
+                    self.log.info("Creating bucket {0} in restore host {1}"
+                                                .format(bucket_name,
+                                                 self.backupset.restore_cluster_host.ip))
+                    rest_conn.create_bucket(bucket=bucket_name,
+                                    ramQuotaMB=512,
+                                    authType=bucket.authType if bucket.authType else 'none',
+                                    proxyPort=bucket.port,
+                                    saslPassword=bucket.saslPassword,
+                                    lww=self.lww_new)
+                    bucket_ready = rest_helper.vbucket_map_ready(bucket_name)
+                    if not bucket_ready:
+                        self.fail("Bucket %s not created after 120 seconds." % bucket_name)
+                buckets.append("%s=%s" % (bucket.name, bucket_name))
                 count +=1
-                bucket_maps = ",".join(buckets)
+            bucket_maps = ",".join(buckets)
         if self.backupset.map_buckets:
             args += " --map-buckets %s " % bucket_maps
         remote_client = RemoteMachineShellConnection(self.backupset.backup_host)
