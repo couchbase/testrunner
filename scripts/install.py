a64d50ce3592eb7e48d0c9514462aa658a813e30
prasanna135 <prasanna.gholap@yahoo.co.in>
2017-08-16 12:29:27 +0530

    Add Indexer Storage Mode in install.py
    Use "storage_mode" parameter in command line
    It takes 3 values:
    1. plasma
    2. memory_optimized
    3. forestdb
    By default, the storage mode will be set to Plasma
    
    Change-Id: Ib564ca78ce899557055e37624028894ecffb4f2f
    Reviewed-on: http://review.couchbase.org/82347
    Reviewed-by: Ajay Bhullar <ajay.bhullar@couchbase.com>
    Reviewed-by: Arunkumar Senthilnathan <arun.couchbase@yahoo.com>
    Tested-by: Ajay Bhullar <ajay.bhullar@couchbase.com>

--- scripts/install.py
+++ scripts/install.py
@@ -60,6 +60,7 @@ Available keys:
  xdcr_upr=                  Enable UPR for XDCR (temporary param until XDCR with UPR is stable), values: None | True | False
  fts_query_limit=1000000    Set a limit for the max results to be returned by fts for any query
  change_indexer_ports=false Sets indexer ports values to non-default ports
+ storage_mode=plasma        Sets indexer storage mode
 
 
 Examples:
@@ -541,7 +542,11 @@ class CouchbaseServerInstaller(Installer):
                                                 password=server.rest_password,
                                                         services=set_services)
                     if "index" in set_services:
-                        rest.set_indexer_storage_mode()
+                        if "storage_mode" in params:
+                            storageMode = params["storage_mode"]
+                        else:
+                            storageMode = "plasma"
+                        rest.set_indexer_storage_mode(storageMode=storageMode)
                     rest.init_cluster(username=server.rest_username,
                                          password=server.rest_password)
 
