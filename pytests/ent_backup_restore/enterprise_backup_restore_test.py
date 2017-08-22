42a8ee5f9b1c3a1b04b6a9aae02c2b1e7b8f55d3
saigon <baolavn@gmail.com>
2017-08-21 23:53:11 -0700

    CBQE-4286 modify printout error message
    Change-Id: I4d4d453932994b0f3081badba2a240331f7dc076
    Reviewed-on: http://review.couchbase.org/82556
    Reviewed-by: Thuan Nguyen <soccon@gmail.com>
    Tested-by: Thuan Nguyen <soccon@gmail.com>

--- pytests/ent_backup_restore/enterprise_backup_restore_test.py
+++ pytests/ent_backup_restore/enterprise_backup_restore_test.py
@@ -1020,8 +1020,11 @@ class EnterpriseBackupRestoreTest(EnterpriseBackupRestoreBase, NewUpgradeBaseTes
                 if fail_msg not in output[0]:
                     self.fail("cbbackupmgr failed to block user to backup")
             if self.cluster_new_role == "views_admin[*]":
-                self.assertTrue("Error backing up cluster: Invalid permissions" in output[0],
+                self.assertTrue("Error backing up cluster: Unable to find" in output[0],
                                 "Expected error message not thrown")
+                # remove comment when ticket MB-25767 is fixed.
+                #self.assertTrue("Error backing up cluster: Invalid permissions" in output[0],
+                #                "Expected error message not thrown")
             status, _, message = self.backup_list()
             if not status:
                 self.fail(message)
@@ -1044,7 +1047,8 @@ class EnterpriseBackupRestoreTest(EnterpriseBackupRestoreBase, NewUpgradeBaseTes
                 error_messages = ["Error backing up cluster: Forbidden",
                                   "Could not find file shard_0.fdb",
                                   "Error backing up cluster: Invalid permissions",
-                                  "Database file is empty"]
+                                  "Database file is empty",
+                                  "Error backing up cluster: Unable to find the latest vbucket"]
                 if self.do_verify:
                     if str(e) in error_messages:
                         error_found = True
