91cf044249d85f6b25c338e0d2efd56288de5d78
Hemant <Hemant.rajput@couchbase.com>
2017-08-31 16:36:05 +0530

    CBQE 4282:
    Fixed test cases'replicate_correct_data_after_rollback' in dcp.rollback.DCPRollBack by changing the way to add back failover node.
    
    Change-Id: Id0be817384098baed6be4ec3a9c81a96094067a5
    Reviewed-on: http://review.couchbase.org/82960
    Reviewed-by: Mihir Kamdar <mihir.kamdar@couchbase.com>
    Tested-by: Hemant Kumar Rajput <Hemant.rajput@couchbase.com>

--- pytests/dcp/rollback.py
+++ pytests/dcp/rollback.py
@@ -111,8 +111,13 @@ class DCPRollBack(DCPBase):
 
         # need to rebalance the node back into the cluster
         # def rebalance(self, servers, to_add, to_remove, timeout=None, use_hostnames=False, services = None):
+        nodes_all = self.rest.node_statuses()
+        for node in nodes_all:
+            if node.ip == self.server[1].ip:
+                break
+
         rest_obj = RestConnection(self.servers[0])
-        node_id_for_recovery = "ns_1@" + self.servers[1].ip
+        node_id_for_recovery = node.id
         status = rest_obj.add_back_node(node_id_for_recovery)
         if status:
             rest_obj.set_recovery_type(node_id_for_recovery,
