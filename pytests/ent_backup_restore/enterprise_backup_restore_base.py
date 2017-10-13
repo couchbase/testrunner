c6d235522a360e319c4cd6269f911607db82443e
saigon <baolavn@gmail.com>
2017-10-13 14:06:32 -0700

    CBQE-1898 set restore cluster services to the same as backup cluster. Delete correct directory after each tests.
    Change-Id: I48b3af1b3d19a2a429dcbb764dab466b98019597
    Reviewed-on: http://review.couchbase.org/84377
    Reviewed-by: Thuan Nguyen <soccon@gmail.com>
    Tested-by: Thuan Nguyen <soccon@gmail.com>
    Reviewed-on: http://review.couchbase.org/84379

--- pytests/ent_backup_restore/enterprise_backup_restore_base.py
+++ pytests/ent_backup_restore/enterprise_backup_restore_base.py
@@ -213,6 +213,7 @@ class EnterpriseBackupRestoreBase(BaseTestCase):
                 backup_directory = WIN_TMP_PATH_RAW + "entbackup"
             else:
                 raise Exception("OS not supported.")
+            backup_directory += self.master.ip
             validation_files_location = "%sbackuprestore" % self.tmp_path + self.master.ip
             if info == 'linux':
                 command = "rm -rf {0}".format(backup_directory)
@@ -221,7 +222,7 @@ class EnterpriseBackupRestoreBase(BaseTestCase):
             elif info == 'windows':
                 remote_client.remove_directory_recursive(backup_directory)
             if info == 'linux':
-                command = "rm -rf /cbqe3043/entbackup".format(backup_directory)
+                command = "rm -rf /cbqe3043/entbackup"
                 output, error = remote_client.execute_command(command)
                 remote_client.log_command_output(output, error)
             if self.input.clusters:
