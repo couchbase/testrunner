30672d980f4104e03c7521cfd0eb16f2a3957870
bkumaran <balakumaran.gopal@couchbase.com>
2017-11-17 15:38:22 +0530

    CBQE-4333: make expiry local variable
    Change-Id: I635b044d088ae20781f48a1d4e1fa5d87a93e4fd
    Reviewed-on: http://review.couchbase.org/85682
    Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>
    Tested-by: Balakumaran G <balakumaran.gopal@couchbase.com>

--- pytests/eventing/handler_code/multiple_timer_operations_on_update.js
+++ pytests/eventing/handler_code/multiple_timer_operations_on_update.js
@@ -1,5 +1,5 @@
 function OnUpdate(doc,meta) {
-    expiry = Math.round((new Date()).getTime() / 1000) + 5;
+    var expiry = Math.round((new Date()).getTime() / 1000) + 5;
     docTimer(timerCallback, meta.id, expiry);
     cronTimer(NDtimerCallback, meta.id, expiry);
 }
--- pytests/eventing/handler_code/n1ql_insert_with_cron_timer.js
+++ pytests/eventing/handler_code/n1ql_insert_with_cron_timer.js
@@ -1,5 +1,5 @@
 function OnUpdate(doc,meta) {
-    expiry = Math.round((new Date()).getTime() / 1000) + 5;
+    var expiry = Math.round((new Date()).getTime() / 1000) + 5;
     cronTimer(NDtimerCallback, meta.id, expiry);
 }
 function NDtimerCallback(docid, expiry) {
