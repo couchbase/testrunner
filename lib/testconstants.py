42fe1f0fbff3270c65f14299a9cf3d1c352a83ce
Arunkumar Senthilnathan <arun.couchbase@yahoo.com>
2017-12-19 18:00:55 -0800

    CBQE-0: changing repo names after build renaming
    Change-Id: I89bdfb10e77b88f85d76738b8aa7ca23b12511ae
    Reviewed-on: http://review.couchbase.org/87065
    Tested-by: Arunkumar Senthilnathan <arun.couchbase@yahoo.com>
    Reviewed-by: Jaekwon Park <jaekwon.park@couchbase.com>

--- lib/testconstants.py
+++ lib/testconstants.py
@@ -54,7 +54,7 @@ WIN_CB_VERSION_3 = ["3.0.0", "3.0.1", "3.0.2", "3.0.3", "3.1.0", "3.1.1","3.1.2"
 SHERLOCK_VERSION = ["4.0.0", "4.0.1", "4.0", "4.1.0", "4.1", "4.1.1", "4.1.2"]
 WATSON_VERSION = ["4.5.0", "4.5.1", "4.6.0", "4.6.1", "4.6.2", "4.6.3", "4.6.4"]
 CB_VERSION_NAME = {"4.0":"sherlock", "4.1":"sherlock", "4.5":"watson", "4.6":"watson",
-                   "4.7":"spock", "5.0":"spock", "5.1":"vulcan"}
+                   "4.7":"spock", "5.0":"spock", "5.1":"spock", "5.5":"vulcan"}
 WIN_MEMBASE_DATA_PATH = '/cygdrive/c/Program\ Files/Membase/Server/var/lib/membase/data/'
 WIN_COUCHBASE_DATA_PATH = '/cygdrive/c/Program\ Files/Couchbase/Server/var/lib/couchbase/data/'
 WIN_CB_PATH = "/cygdrive/c/Program Files/Couchbase/Server/"
