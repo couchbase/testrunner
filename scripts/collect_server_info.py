718facb012cb1a471b559330c491c27d1cf0062c
Aruna Piravi <aruna@couchbase.com>
2018-02-07 15:07:38 -0800

    CBQE-4378 Fix cbcollect collection for raw IPv6 addresses - remove square brackets from filename
    Change-Id: I1ec0a32a55c28da005ea8a716fe03f235f6035dc
    Reviewed-on: http://review.couchbase.org/89035
    Tested-by: Aruna Piravi <aruna@couchbase.com>
    Reviewed-by: Thuan Nguyen <soccon@gmail.com>

--- scripts/collect_server_info.py
+++ scripts/collect_server_info.py
@@ -48,7 +48,7 @@ class couch_dbinfo_Runner(object):
         self.local = local
 
     def run(self):
-        file_name = "%s-%s-couch-dbinfo.txt" % (self.server.ip, time_stamp())
+        file_name = "%s-%s-couch-dbinfo.txt" % (self.server.ip.replace('[', '').replace(']', ''), time_stamp())
         if not self.local:
             from lib.remote.remote_util import RemoteMachineShellConnection
             remote_client = RemoteMachineShellConnection(self.server)
@@ -86,7 +86,7 @@ class cbcollectRunner(object):
         self.local = local
 
     def run(self):
-        file_name = "%s-%s-diag.zip" % (self.server.ip, time_stamp())
+        file_name = "%s-%s-diag.zip" % (self.server.ip.replace('[', '').replace(']', ''), time_stamp())
         if not self.local:
             from lib.remote.remote_util import RemoteMachineShellConnection
             remote_client = RemoteMachineShellConnection(self.server)
