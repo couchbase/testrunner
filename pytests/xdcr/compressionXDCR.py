3bf00815cdddca10f3d14f764c1a90743257628b
Arunkumar Senthilnathan <arun.couchbase@yahoo.com>
2018-02-13 14:59:45 -0800

    CBQE-4553: P0(sanity)s for XDCR compression
    Change-Id: Id8153e4318befe9631bbab07e06745657a17b86b
    Reviewed-on: http://review.couchbase.org/89368
    Tested-by: Arunkumar Senthilnathan <arun.couchbase@yahoo.com>
    Reviewed-by: Korrigan Clark <korrigan.clark@couchbase.com>
    Reviewed-by: Thuan Nguyen <soccon@gmail.com>

---
+++ pytests/xdcr/compressionXDCR.py
@@ -0,0 +1,164 @@
+from xdcrnewbasetests import XDCRNewBaseTest, NodeHelper
+from remote.remote_util import RemoteMachineShellConnection
+from couchbase_helper.documentgenerator import BlobGenerator
+import json
+
+class compression(XDCRNewBaseTest):
+    def setUp(self):
+        super(compression, self).setUp()
+        self.src_cluster = self.get_cb_cluster_by_name('C1')
+        self.src_master = self.src_cluster.get_master_node()
+        self.dest_cluster = self.get_cb_cluster_by_name('C2')
+        self.dest_master = self.dest_cluster.get_master_node()
+
+    def tearDown(self):
+        super(compression, self).tearDown()
+
+    def _set_compression_type(self, cluster, bucket_name, compression_type="None"):
+        repls = cluster.get_remote_clusters()[0].get_replications()
+        for repl in repls:
+            if bucket_name in str(repl):
+                repl_id = repl.get_repl_id()
+        shell = RemoteMachineShellConnection(cluster.get_master_node())
+        repl_id = str(repl_id).replace('/','%2F')
+        base_url = "http://" + cluster.get_master_node().ip + ":8091/settings/replications/" + repl_id
+        command = "curl -X POST -u Administrator:password " + base_url + " -d compressionType=" + str(compression_type)
+        output, error = shell.execute_command(command)
+        shell.log_command_output(output, error)
+        shell.disconnect()
+
+    def _verify_compression(self, cluster, compr_bucket_name="", uncompr_bucket_name="", compression_type="None"):
+        repls = cluster.get_remote_clusters()[0].get_replications()
+        for repl in repls:
+            if compr_bucket_name in str(repl):
+                compr_repl_id = repl.get_repl_id()
+            elif uncompr_bucket_name in str(repl):
+                uncompr_repl_id = repl.get_repl_id()
+
+        compr_repl_id = str(compr_repl_id).replace('/', '%2F')
+        uncompr_repl_id = str(uncompr_repl_id).replace('/', '%2F')
+
+        base_url = "http://" + cluster.get_master_node().ip + ":8091/settings/replications/" + compr_repl_id
+        shell = RemoteMachineShellConnection(cluster.get_master_node())
+        command = "curl -u Administrator:password " + base_url
+        output, error = shell.execute_command(command)
+        shell.log_command_output(output, error)
+        self.assertTrue('"compressionType":"Snappy"' in output[0],
+                        "Compression Type for replication " + compr_repl_id + " is not Snappy")
+        self.log.info("Compression Type for replication " + compr_repl_id + " is Snappy")
+
+        base_url = "http://" + cluster.get_master_node().ip + ":8091/pools/default/buckets/" + compr_bucket_name + \
+                   "/stats/replications%2F" + compr_repl_id + "%2Fdata_replicated"
+        command = "curl -u Administrator:password " + base_url
+        output, error = shell.execute_command(command)
+        shell.log_command_output(output, error)
+        output = json.loads(output[0])
+        compressed_data_replicated = output["nodeStats"]["{0}:8091".format(cluster.get_master_node().ip)][-1]
+        self.log.info("Compressed data for replication {0} is {1}".format(compr_repl_id, compressed_data_replicated))
+
+        base_url = "http://" + cluster.get_master_node().ip + ":8091/pools/default/buckets/" + uncompr_bucket_name + \
+                   "/stats/replications%2F" + uncompr_repl_id + "%2Fdata_replicated"
+        command = "curl -u Administrator:password " + base_url
+        output, error = shell.execute_command(command)
+        shell.log_command_output(output, error)
+        output = json.loads(output[0])
+        uncompressed_data_replicated = output["nodeStats"]['{0}:8091'.format(cluster.get_master_node().ip)][-1]
+        self.log.info("Uncompressed data for replication {0} is {1}".format(uncompr_repl_id, uncompressed_data_replicated))
+
+        self.assertTrue(uncompressed_data_replicated > compressed_data_replicated,
+                        "Compression did not work as expected")
+        self.log.info("Compression worked as expected")
+
+        shell.disconnect()
+
+    def test_compression_with_unixdcr_incr_load(self):
+        self.setup_xdcr()
+        self.sleep(60)
+        compression_type = self._input.param("compression_type", "Snappy")
+        self._set_compression_type(self.src_cluster, "standard_bucket_1", compression_type)
+
+        gen_create = BlobGenerator('comprOne-', 'comprOne-', self._value_size, end=self._num_items)
+        self.src_cluster.load_all_buckets_from_generator(kv_gen=gen_create)
+
+        self.perform_update_delete()
+
+        self._verify_compression(cluster=self.src_cluster,
+                                 compr_bucket_name="standard_bucket_1",
+                                 uncompr_bucket_name="standard_bucket_2",
+                                 compression_type=compression_type)
+        self.verify_results()
+
+    def test_compression_with_unixdcr_backfill_load(self):
+        self.setup_xdcr()
+        self.sleep(60)
+        compression_type = self._input.param("compression_type", "Snappy")
+        self._set_compression_type(self.src_cluster, "standard_bucket_1", compression_type)
+
+        self.src_cluster.pause_all_replications()
+
+        gen_create = BlobGenerator('comprOne-', 'comprOne-', self._value_size, end=self._num_items)
+        self.src_cluster.load_all_buckets_from_generator(kv_gen=gen_create)
+
+        self.src_cluster.resume_all_replications()
+
+        self.perform_update_delete()
+
+        self._verify_compression(cluster=self.src_cluster,
+                                 compr_bucket_name="standard_bucket_1",
+                                 uncompr_bucket_name="standard_bucket_2",
+                                 compression_type=compression_type)
+        self.verify_results()
+
+    def test_compression_with_bixdcr_incr_load(self):
+        self.setup_xdcr()
+        self.sleep(60)
+        compression_type = self._input.param("compression_type", "Snappy")
+        self._set_compression_type(self.src_cluster, "standard_bucket_1", compression_type)
+        self._set_compression_type(self.dest_cluster, "standard_bucket_1", compression_type)
+
+        gen_create = BlobGenerator('comprOne-', 'comprOne-', self._value_size, end=self._num_items)
+        self.src_cluster.load_all_buckets_from_generator(kv_gen=gen_create)
+        gen_create = BlobGenerator('comprTwo-', 'comprTwo-', self._value_size, end=self._num_items)
+        self.dest_cluster.load_all_buckets_from_generator(kv_gen=gen_create)
+
+        self.perform_update_delete()
+
+        self._verify_compression(cluster=self.src_cluster,
+                                 compr_bucket_name="standard_bucket_1",
+                                 uncompr_bucket_name="standard_bucket_2",
+                                 compression_type=compression_type)
+        self._verify_compression(cluster=self.dest_cluster,
+                                 compr_bucket_name="standard_bucket_1",
+                                 uncompr_bucket_name="standard_bucket_2",
+                                 compression_type=compression_type)
+        self.verify_results()
+
+    def test_compression_with_bixdcr_backfill_load(self):
+        self.setup_xdcr()
+        self.sleep(60)
+        compression_type = self._input.param("compression_type", "Snappy")
+        self._set_compression_type(self.src_cluster, "standard_bucket_1", compression_type)
+        self._set_compression_type(self.dest_cluster, "standard_bucket_1", compression_type)
+
+        self.src_cluster.pause_all_replications()
+        self.dest_cluster.pause_all_replications()
+
+        gen_create = BlobGenerator('comprOne-', 'comprOne-', self._value_size, end=self._num_items)
+        self.src_cluster.load_all_buckets_from_generator(kv_gen=gen_create)
+        gen_create = BlobGenerator('comprTwo-', 'comprTwo-', self._value_size, end=self._num_items)
+        self.dest_cluster.load_all_buckets_from_generator(kv_gen=gen_create)
+
+        self.src_cluster.resume_all_replications()
+        self.dest_cluster.resume_all_replications()
+
+        self.perform_update_delete()
+
+        self._verify_compression(cluster=self.src_cluster,
+                                 compr_bucket_name="standard_bucket_1",
+                                 uncompr_bucket_name="standard_bucket_2",
+                                 compression_type=compression_type)
+        self._verify_compression(cluster=self.dest_cluster,
+                                 compr_bucket_name="standard_bucket_1",
+                                 uncompr_bucket_name="standard_bucket_2",
+                                 compression_type=compression_type)
+        self.verify_results()
