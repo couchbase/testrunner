10816b816ee6f6be05bf6be7e9569601e470224d
bkumaran <balakumaran.gopal@couchbase.com>
2018-04-12 18:28:26 +0530

    CBQE-4333: Remove depcfg for eventing structure
    Change-Id: If50439f9cfd1b2ebd97fbbe8abd7ce9cf98eb5d1
    Reviewed-on: http://review.couchbase.org/92627
    Reviewed-by: Mihir Kamdar <mihir.kamdar@couchbase.com>
    Tested-by: Balakumaran G <balakumaran.gopal@couchbase.com>

--- pytests/eventing/eventing_base.py
+++ pytests/eventing/eventing_base.py
@@ -72,13 +72,18 @@ class EventingBaseTest(QueryHelperTests, BaseTestCase):
         fh = open(abs_file_path, "r")
         body['appcode'] = fh.read()
         fh.close()
-        body['depcfg'] = {}
-        body['depcfg']['buckets'] = []
-        body['depcfg']['buckets'].append({"alias": self.dst_bucket_name, "bucket_name": self.dst_bucket_name})
+        body['version'] = "1.0.0"
+        body['id'] = 0
+        body['bindings'] = []
+        body['bindings'].append({"binding_type": "alias",
+                                 "binding_name": self.dst_bucket_name,
+                                 "binding_value": {"alias": self.dst_bucket_name}})
         if multi_dst_bucket:
-            body['depcfg']['buckets'].append({"alias": self.dst_bucket_name1, "bucket_name": self.dst_bucket_name1})
-        body['depcfg']['metadata_bucket'] = self.metadata_bucket_name
-        body['depcfg']['source_bucket'] = self.src_bucket_name
+            body['bindings'].append({"binding_type": "alias",
+                                     "binding_name": self.dst_bucket_name1,
+                                     "binding_value": {"alias": self.dst_bucket_name1}})
+        body['metadata_bucket'] = self.metadata_bucket_name
+        body['source_bucket'] = self.src_bucket_name
         body['settings'] = {}
         body['settings']['checkpoint_interval'] = checkpoint_interval
         body['settings']['cleanup_timers'] = cleanup_timers
