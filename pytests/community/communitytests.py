62bc9978be5dc45bdf43218b02a2067b1d9c3dfe
saigon <baolavn@gmail.com>
2018-06-21 11:53:14 -0700

    CBQE-4661 fix typo in services order.
    Change-Id: If91b0e15c9ce9b503108d84859afe89132076a64
    Reviewed-on: http://review.couchbase.org/95967
    Tested-by: Thuan Nguyen <soccon@gmail.com>
    Reviewed-by: Aruna Piravi <aruna@couchbase.com>

--- pytests/community/communitytests.py
+++ pytests/community/communitytests.py
@@ -207,7 +207,7 @@ class CommunityTests(CommunityBaseTest):
                         self.fail("maybe bug in add node")
                 elif self.version in COUCHBASE_FROM_WATSON:
                     if self.start_node_services in ["kv", "index,kv,n1ql",
-                         "index,kv,n1ql,fts"] and self.add_node_services not in \
+                         "fts,index,kv,n1ql"] and self.add_node_services not in \
                                     ["kv", "index,kv,n1ql", "fts,index,kv,n1ql"]:
                         self.log.info("services are enforced in CE")
                     elif self.start_node_services not in ["kv", "index,kv,n1ql",
