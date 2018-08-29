d0bc9432c231657ce34eb70c488925ccd2dea04f
Korrigan Clark <korrigan.clark@couchbase.com>
2018-08-29 12:34:31 -0700

    port bug automation
    Change-Id: Ia1f953b61f3278e055e874811c5cf2064cbf92c8
    Reviewed-on: http://review.couchbase.org/98965
    Reviewed-by: Ajay Bhullar <ajay.bhullar@couchbase.com>
    Tested-by: Korrigan Clark <korrigan.clark@couchbase.com>

--- pytests/tuqquery/tuq_misc.py
+++ pytests/tuqquery/tuq_misc.py
@@ -134,3 +134,100 @@ class QueryMiscTests(QueryTests):
                     self.wait_for_index_drop("temp_bucket", index, createdIndexes[index], self.gsi_type)
                 if createdBucket:
                     self.cluster.bucket_delete(self.master, "temp_bucket")
+
+    def test_intersectscan_thread_growth(self):
+        createdIndexes = {}
+        createdBucket = False
+        try:
+            temp_bucket_params = self._create_bucket_params(server=self.master, size=self.bucket_size,
+                                                            replicas=self.num_replicas, bucket_type=self.bucket_type,
+                                                            enable_replica_index=self.enable_replica_index,
+                                                            eviction_policy=self.eviction_policy, lww=self.lww)
+            self.cluster.create_standard_bucket("temp_bucket", 11222, temp_bucket_params)
+            createdBucket = True
+
+            self.query = 'CREATE primary index ON temp_bucket'
+            self.run_cbq_query()
+            self._wait_for_index_online("temp_bucket", "#primary")
+            createdIndexes["#primary"] = []
+
+            self.query = 'CREATE INDEX ix1 ON `temp_bucket`(a)'
+            self.run_cbq_query()
+            self._wait_for_index_online("temp_bucket", "ix1")
+            createdIndexes["ix1"] = ['a']
+
+            self.query = 'CREATE INDEX ix2 ON `temp_bucket`(b)'
+            self.run_cbq_query()
+            self._wait_for_index_online("temp_bucket", "ix2")
+            createdIndexes["ix2"] = ['b']
+
+            for i in range(0, 20000):
+                self.query = 'INSERT INTO `temp_bucket` (KEY, VALUE) VALUES ("'+str(i)+'", {"a":'+str(i)+', "b":'+str(i)+'})'
+                self.run_cbq_query()
+            self.query = 'select * from temp_bucket'
+            res = self.run_cbq_query()
+            self.assertEqual(len(res['results']), 20000)
+
+            self.query = 'explain select * from `temp_bucket` where a > 0 and b > 0 limit 1000'
+            res = self.run_cbq_query()
+            self.assertTrue("IntersectScan" in str(res['results'][0]['plan']))
+            self.assertTrue("ix1" in str(res['results'][0]['plan']))
+            self.assertTrue("ix2" in str(res['results'][0]['plan']))
+
+            self.log.info("priming query engine")
+            self.query = 'select * from `temp_bucket` where a > 0 and b > 0 limit 1000'
+            for i in range(0, 10):
+                res = self.run_cbq_query()
+                self.assertEqual(len(res['results']), 1000)
+            port_for_version = "8093"
+            cbversion = self.cb_version.split(".")
+            major_version = cbversion[0]
+            minor_version = cbversion[1]
+            if (int(major_version) < 5) or (int(major_version) == 5 and int(minor_version) < 5):
+                port_for_version = "6060"
+            curl_cmd = "curl -u Administrator:password http://localhost:"+port_for_version+"/debug/pprof/goroutine?debug=2"
+            curl_output = self.shell.execute_command(curl_cmd)
+            pprof_list = curl_output[0]
+            count_intersect = 0
+            for item in pprof_list:
+                if "Intersect" in str(item) or "intersect" in str(item):
+                    count_intersect = count_intersect + 1
+            self.log.info("number of intersect threads after primed: "+str(count_intersect))
+
+            self.sleep(10)
+
+            # run query 1000 times now
+            for i in range(0, 1000):
+                res = self.run_cbq_query()
+                self.assertEqual(len(res['results']), 1000)
+
+            curl_output = self.shell.execute_command(curl_cmd)
+            pprof_list = curl_output[0]
+            count_intersect_a = 0
+            for item in pprof_list:
+                if "Intersect" in str(item) or "intersect" in str(item):
+                    count_intersect_a = count_intersect_a + 1
+
+            self.log.info("number of intersect threads A: "+str(count_intersect_a))
+            self.assertEqual(count_intersect_a, 0)
+
+            self.sleep(60)
+
+            curl_output = self.shell.execute_command(curl_cmd)
+            pprof_list = curl_output[0]
+            count_intersect_b = 0
+            for item in pprof_list:
+                if "Intersect" in str(item) or "intersect" in str(item):
+                    count_intersect_b = count_intersect_b + 1
+            self.log.info("number of intersect threads B: "+str(count_intersect_b))
+            self.assertEqual(count_intersect_b, 0)
+        finally:
+            for index in createdIndexes.keys():
+                if index == "#primary":
+                    self.query = "DROP primary index on temp_bucket"
+                else:
+                    self.query = 'DROP INDEX temp_bucket.'+str(index)
+                self.run_cbq_query()
+                self.wait_for_index_drop("temp_bucket", index, createdIndexes[index], self.gsi_type)
+            if createdBucket:
+                self.cluster.bucket_delete(self.master, "temp_bucket")
