5c3cf474df71ae364b1f2faf2aed781aeea477d5
saigon <baolavn@gmail.com>
2018-09-26 11:21:57 -0700

    Remove unuse import.
    Change-Id: Iefb410c8dd1a8eb06822b9f23521a29fdbd258b5
    Reviewed-on: http://review.couchbase.org/99997
    Tested-by: Thuan Nguyen <soccon@gmail.com>
    Reviewed-by: Korrigan Clark <korrigan.clark@couchbase.com>

--- pytests/newupgradebasetest.py
+++ pytests/newupgradebasetest.py
@@ -6,7 +6,6 @@ import sys
 import traceback
 import Queue
 from threading import Thread
-from basetestcase import BaseTestCase
 from mc_bin_client import MemcachedError
 from memcached.helper.data_helper import VBucketAwareMemcached, MemcachedClientHelper
 from membase.helper.bucket_helper import BucketOperationHelper
