1765ba856ac480e88e6f1f1f0d39a5492d83f113
vikas chaudhary <vikas.chaudhary@couchbase.com>
2018-10-09 14:34:42 +0530

    Eventing: Fixing test issues
    Change-Id: I2c4114d9dae39059d05caf7b70ffdf2eab65f197
    Reviewed-on: http://review.couchbase.org/100393
    Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>
    Tested-by: Balakumaran G <balakumaran.gopal@couchbase.com>

--- pytests/eventing/eventing_rebalance.py
+++ pytests/eventing/eventing_rebalance.py
@@ -439,6 +439,7 @@ class EventingRebalance(EventingBaseTest):
         # do a recovery and rebalance
         self.rest.set_recovery_type('ns_1@' + kv_server.ip, recovery_type)
         self.rest.add_back_node('ns_1@' + kv_server.ip)
+        self.sleep(30)
         task.result()
         rebalance = self.cluster.rebalance(self.servers[:self.nodes_init], [], [])
         if rebalance:
