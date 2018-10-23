27908b5859a16e85eabe8045be36f705851dad63
saigon <baolavn@gmail.com>
2018-10-23 10:11:42 -0700

    correct erlang proccess name in windows to be terminated.
    Change-Id: I66d447ef0b15fb7065a34eae9fae584e5bf396ec
    Reviewed-on: http://review.couchbase.org/100927
    Tested-by: Thuan Nguyen <soccon@gmail.com>
    Reviewed-by: Korrigan Clark <korrigan.clark@couchbase.com>

--- lib/remote/remote_util.py
+++ lib/remote/remote_util.py
@@ -536,20 +536,24 @@ class RemoteMachineShellConnection:
         if os == "windows":
             o, r = self.execute_command("taskkill /F /T /IM epmd.exe*")
             self.log_command_output(o, r)
-            o, r = self.execute_command("taskkill /F /T /IM erl*")
+            o, r = self.execute_command("taskkill /F /T /IM erl.exe*")
             self.log_command_output(o, r)
             o, r = self.execute_command("tasklist | grep erl.exe")
             kill_all = False
             count = 0
-            while len(o) >= 1 and not kill_all and count < 10:
-                self.execute_command("taskkill /F /T /IM erl*")
-                o, r = self.execute_command("tasklist | grep erl.exe")
+            while len(o) >= 1 and not kill_all:
+                if o and "erl.exe" in o[0]:
+                    self.execute_command("taskkill /F /T /IM erl.exe*")
+                    self.sleep(1)
+                    o, r = self.execute_command("tasklist | grep erl.exe")
                 if len(o) == 0:
                     kill_all = True
                     log.info("all erlang processes were killed")
-                count+=1
-            if not kill_all:
-                raise Exception("Could not kill erlang process")
+                else:
+                    count += 1
+                if count == 5:
+                    log.error("erlang process is not killed")
+                    break
         else:
             o, r = self.execute_command("killall -9 beam.smp")
             self.log_command_output(o, r)
