0ce99d53c00499a054bc6c6e473a6c5f553e6b14
Evgeny Makarenko <evgeny.makarenko@couchbase.com>
2018-11-01 08:53:25 -0700

    Some new functions added: ignored jobs list, downloading of core panic dump file, minor bugfix
    Change-Id: Ifbad7a8d406ff944f2bb01a99009f48d79792518
    Reviewed-on: http://review.couchbase.org/101274
    Tested-by: <evgeny.makarenko@couchbase.com>
    Reviewed-by: Mihir Kamdar <mihir.kamdar@couchbase.com>

--- scripts/triage.py
+++ scripts/triage.py
@@ -8,6 +8,9 @@ import sys
 from string import maketrans
 import re
 import pdb
+import copy
+import os
+import paramiko
 
 
 def usage(err=None):
@@ -21,7 +24,9 @@ Options
     --user, -u Username for coachbase instance
     --password, -p Password for couchbase instance
     --job, -j Job name (optional), for example centos-query_covering-2_moi
-    --format, -f, Output format (optional)\n \tc - config file\n \ts - stack trace\n \td - cmd line (pretty long operation)
+    --format, -f, Output format (optional)\n \tc - config file\n \ts - stack trace\n \td - cmd line, ini file, core panic dump file (pretty long operation)
+    --ignore, -i, Igrore jobs, comma-separated list (optional)\n
+
     --help Show this help
 
 Examples:
@@ -30,34 +35,42 @@ Examples:
     sys.exit(0)
 
 
-def triage(os, build, component, username, password, job, format):
+def triage(os, build, component, username, password, job, format, ignore_list):
     fmt = format
     if format == '' or format is None:
         fmt = 'csd'
-
-    connection_error = ['ServerUnavailableException', 'unable to reach the host']
-    cbq_error = ['No such prepared statement', 'Create index cannot proceed', 'No index available', 'RebalanceFailedException']
-    assertion_error = ['AssertionError']
-    dictionary_error = ["TypeError:", "KeyError:", "IndexError"]
-    general_cbq_error = ['CBQError']
-    json_error = ['No JSON object could be decoded']
     job_name = ''
+    ignore_jobs = ''
+
+    connection_error = ['ServerUnavailableException',
+                        'unable to reach the host',
+                        'This test requires buckets',
+                        'ValueError: No JSON object could be decoded']
+
     if job is not None and job != '':
-        job_name = " and lower(name)='"+job.lower()+"'"
+        job_name = " and lower(name)='"+job.lower()+"' "
+    if ignore_list is not None and ignore_list !='':
+        ignore_arr = ignore_list.split(",")
+        ignore_jobs_list = ''
+        for j in ignore_arr:
+            ignore_jobs_list += ("'"+j.lower()+"'"+',')
+
+        ignore_jobs = ' and lower(name) not in ['+ignore_jobs_list[:len(ignore_jobs_list)-1]+'] '
 
 
     cluster = Cluster('couchbase://172.23.99.54')
     authenticator = PasswordAuthenticator(username, password)
     cluster.authenticate(authenticator)
     cb = cluster.open_bucket('server')
-    query = N1QLQuery("select * from server where os='"+os.upper()+"' and `build`='"+build+"' and result='UNSTABLE' and component='"+component.upper()+"' "+job_name)
+    query = N1QLQuery("select * from server where os='"+os.upper()+"' and `build`='"+build+"' and result='UNSTABLE' and component='"+component.upper()+"' "+job_name+ignore_jobs)
     # centos-query_covering_tokens centos-query_monitoring_moi centos-query_vset-ro-moi centos-query_clusterops_moi centos-query_tokens
 
     '''
         main data structure to store all jobs fails info. Format is:
         failers[
                 {
-                 'name': job name
+                 'name': job name,
+                 'ini_file': ini file name,
                  'fail_reasons': {
                                     'name': 'connection',
                                     'cases':[
@@ -66,6 +79,7 @@ def triage(os, build, component, username, password, job, format):
                                                 conf_file: config file name,
                                                 stack_trace: stack trace,
                                                 cmd: run command 
+                                                dump_file: core panic downloaded file name
                                              }        
                                             ]
                                     },
@@ -128,8 +142,13 @@ def triage(os, build, component, username, password, job, format):
 
         '''Load full text logs for last job call'''
         job_logs = []
+        full_job_logs = []
         if 'd' in fmt:
-            job_logs = load_job_logs(build_id)
+            full_job_logs = download_job_logs(build_id)
+
+            job_logs = extract_cmd_lines(full_job_logs)
+            ini_filename = extract_ini_filename(full_job_logs)
+            job_dict['ini_file'] = ini_filename
 
         for suite in suites:
             test_cases = suite['cases']
@@ -149,10 +168,16 @@ def triage(os, build, component, username, password, job, format):
                     raw_stack_trace = str(case['errorStackTrace'])
                     new_stack_trace = format_stack_trace(raw_stack_trace)
 
+
+
                     '''Collecting case data'''
                     if 'd' in fmt:
-                        case_cmd = extract_cmd(case_name, job_logs)
+                        json_params = collect_json_params(case_attrs)
+                        case_cmd = extract_cmd(case_name, json_params, job_logs)
                         case_data['cmd_line'] = case_cmd
+                        if 'new core dump(s) were found and collected. Check testrunner logs folder.' in str(case['errorStackTrace']):
+                            core_dump_filename = find_and_download_core_dump(case_cmd, full_job_logs)
+                            case_data['dump_file'] = core_dump_filename
 
                     case_data['name'] = case_name
                     case_data['conf_file'] = case_conf_file
@@ -173,22 +198,98 @@ def triage(os, build, component, username, password, job, format):
 
     print_report(failers, os, build, component, fmt)
 
+def collect_json_params(case_attrs):
+    result = {}
+    for attr in case_attrs:
+        if ':' in attr:
+            key = attr[:attr.find(':')]
+            val = attr[attr.find(':')+1:]
+            result[key] = val
+    return result
 
-def load_job_logs(build_id):
-    lines = []
+def find_and_download_core_dump(case_cmd, job_logs):
+    #print "CASE NAME ::"+case_cmd+"::"
+    case_log_started = False
+    crashes_line = ''
+
+    for line in job_logs:
+        if './testrunner' in line and case_cmd in line:
+            case_log_started = True
+        if './testrunner' in line and not case_cmd in line:
+            if case_log_started:
+                break
+        if case_log_started:
+            if 'put all crashes on' in line:
+                crashes_line = line
+                break
+    vm_ip = crashes_line[len('put all crashes on '):]
+    vm_ip = vm_ip[:vm_ip.find(' ')]
+    logs_folder = crashes_line[crashes_line.find('in backup folder: '):].replace('in backup folder: ', '').replace('\n', '')
+    local_file_path = download_core_dump(vm_ip, logs_folder)
+    return local_file_path
+
+def download_core_dump(vm_ip, logs_folder):
+    transport = paramiko.Transport((vm_ip, 22))
+    transport.connect(username='root', password='couchbase')
+    sftp = paramiko.SFTPClient.from_transport(transport)
+    files = sftp.listdir(path=logs_folder)
+    if files and len(files)>0:
+        sftp.get(logs_folder+'/'+files[0], files[0])
+    return files[0]
+
+
+
+def download_job_logs(build_id):
+    logs = []
     console_page_url = "http://qa.sc.couchbase.com/job/test_suite_executor/" + str(build_id) + "/consoleText"
     console_response = urllib.urlopen(console_page_url)
     for line in console_response.readlines():
+        logs.append(line)
+    return logs
+
+def extract_ini_filename(logs):
+    ini_filename = ''
+    for line in logs:
+        if line.startswith("the ini file is"):
+            ini_filename = line[len("the ini file is "):]
+            break
+    return ini_filename
+
+def extract_cmd_lines(logs):
+    lines = []
+    for line in logs:
         if line.startswith("./testrunner"):
             lines.append(line)
     return lines
 
-
-def extract_cmd(case_name, lines):
+def extract_cmd(case_name, json_params, lines):
     for line in lines:
         if line.find(case_name) > -1:
-            cmd = line
-            return cmd[:len(cmd)-1]
+            rest_of_line = line[line.find(case_name)+len(case_name):]
+            if len(rest_of_line) == 0 or rest_of_line[:1] in [' ',',']:
+                params_line = copy.copy(line)
+                params_line = params_line[params_line.find(' -p '):]
+                params_line = params_line.replace(' -p ', '').replace(' -t ', ',').replace('\n','')
+                params = params_line.split(',')
+                params_dict = {}
+                for param in params:
+                    if '=' in param:
+                        key = param[:param.find('=')]
+                        val = param[param.find('=')+1:]
+                        params_dict[key] = val
+                if compare_json_and_cmd_params(json_params, params_dict):
+                    cmd = line
+                    return cmd[:len(cmd)-1]
+
+def compare_json_and_cmd_params(json_params={}, cmd_params={}):
+    #print("JSON ::"+str(json_params)+"::")
+    #print("CMD ::"+str(cmd_params)+"::")
+    for cmd_key in cmd_params.keys():
+        if cmd_key in json_params.keys():
+            if cmd_params[cmd_key] != json_params[cmd_key]:
+                if cmd_key!='doc-per-day':
+                    return False
+    return True
 
 
 def print_report(failers, os, build, component, fmt):
@@ -197,14 +298,19 @@ def print_report(failers, os, build, component, fmt):
     no_logs_failers = []
     all_the_rest_failers = []
     n=1
+    connection_jobs_container = ""
 
     for job in failers:
+
         if len(job['fail_reasons']['connection']['cases']) > 0:
             connection_failers.append(job['name'])
         if len(job['fail_reasons']['no_logs']['cases']) > 0:
             no_logs_failers.append(job['name'])
         if len(job['fail_reasons']['all_the_rest']['cases']) > 0:
             job_data = {}
+            if 'ini_file' in job.keys():
+                job_data['ini_file'] = job['ini_file']
+
             job_data['name'] = job['name']
             job_data['cases'] = []
             for case in job['fail_reasons']['all_the_rest']['cases']:
@@ -236,21 +342,30 @@ def print_report(failers, os, build, component, fmt):
         print str(n)+". There are failed jobs because of different errors."
         for job in all_the_rest_failers:
             print("="*100)
-            print "JOB - "+job['name']
+            print "JOB - "+str(job['name'])
+            if 'ini_file' in job.keys():
+                print(".ini file - "+str(job['ini_file']))
             print
             for case in job['cases']:
-                print("     CASE NAME - "+case['name'])
+                print("     CASE NAME - "+str(case['name']))
                 print
                 if 'c' in fmt:
-                    print("         config file - "+case['conf_file'])
+                    print("         config file - "+str(case['conf_file']))
                     print
                 if 's' in fmt:
                     print("         stack trace - ")
-                    print(""+case['stack_trace'])
+                    print(""+str(case['stack_trace']))
                     print("-"*100)
                 if 'd' in fmt:
                     print
-                    print("         cmd line - "+case['cmd_line'])
+                    if str(case['cmd_line']) == 'None':
+                        print('         cmd line - None. If you see this, I could not identify cmd line for this test case.')
+                        print('         Please send email to evgeny.makarenko@couchbase.com with subject \'Triage script - cmd error\',')
+                        print('         containing component name, job name, build version, and test case name. I\'ll fix it immediately.')
+                    else:
+                        print("         cmd line - "+str(case['cmd_line']))
+                    if 'dump_file' in case.keys():
+                        print("         dump file - "+str(case['dump_file']))
                 print
             print("=" * 100)
         n = n+1
@@ -281,7 +396,10 @@ if __name__ == "__main__":
     parser.add_argument('--user', '-u', help='Username', type=str)
     parser.add_argument('--password', '-p', help='Password', type=str)
     parser.add_argument('--job', '-j', help='Job name (optional)', type=str)
-    parser.add_argument('--format', '-f', help='Output format (optional)\n c - config file\n s - stack trace\n d - cmd line (pretty long operation)', type=str)
+    parser.add_argument('--format', '-f', help='Output format (optional)\n c - config file\n s - stack trace\n d - cmd line, ini file, core panic dump file (pretty long operation)', type=str)
+    parser.add_argument('--ignore', '-i', help='Igrore jobs, comma-separated list (optional)\n ', type=str)
+    #parser.add_argument('--restart' ,'-r', help='Restart jobs automatically, comma-separated list.\n'
+    #                                            ' Empty list means that only connection issue jobs will be restarted (optional)', type=str)
 
     args = parser.parse_args()
     os = args.os
@@ -291,9 +409,14 @@ if __name__ == "__main__":
     password = args.password
     job = args.job
     format = args.format
+    ignore_list = args.ignore
+    #restart = args.restart
 
     if os is None or os == '' or build is None or build == '' or component is None or component == '' \
             or username is None or username == '' or password is None or password == '':
         usage()
 
-    triage(os, build, component, username, password, job, format)
+    triage(os, build, component, username, password, job, format, ignore_list)
+
+
+
