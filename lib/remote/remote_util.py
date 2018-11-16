ad15451da2785ce2133b6b5ac2efb98f6238feae
saigon <baolavn@gmail.com>
2018-11-16 12:20:00 -0800

    correct string format when using .format
    Change-Id: I97afe24cc37de8c02f8973fbb97f71321cab7f75
    Reviewed-on: http://review.couchbase.org/101858
    Tested-by: Thuan Nguyen <soccon@gmail.com>
    Reviewed-by: Arunkumar Senthilnathan <arun.couchbase@yahoo.com>
    Reviewed-on: http://review.couchbase.org/101859

--- lib/remote/remote_util.py
+++ lib/remote/remote_util.py
@@ -2049,7 +2049,8 @@ class RemoteMachineShellConnection:
                 startserver = True
 
             if enable_ipv6:
-                if build.product_version[:5] == "6.0.0":
+                cb_version = build.product_version[:5]
+                if cb_version == "6.0.0" or cb_version < "5.5.3":
                     output, error = \
                         self.execute_command("sed -i '/ipv6, /c \\{ipv6, true\}'. %s"
                             % testconstants.LINUX_STATIC_CONFIG)
@@ -2057,8 +2058,8 @@ class RemoteMachineShellConnection:
                     """ dist_cfg contains {dist_type,inet_tcp}. in IPv4.
                         We need to change it to {dist_type,inet6_tcp}. in IPv6 server
                     """
-                    output, error = self.execute_command("echo \{dist_type,inet6_tcp\}. > {0}"\
-                                                                   .format(LINUX_DIST_CONFIG))
+                    output, error = self.execute_command("echo '{{dist_type,inet6_tcp}}.' > {0}"\
+                                                                      .format(LINUX_DIST_CONFIG))
                 success &= self.log_command_output(output, error, track_words)
                 startserver = True
 
