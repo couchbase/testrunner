fee6b8ab1c0cf8c0470ca506d54a11d59f9586a0
vikas chaudhary <vikas.chaudhary@couchbase.com>
2019-04-25 14:00:49 +0530

    Eventing: Retry on untill succeeds
    Change-Id: Ic27041318004e1c4aef19ccb24cb034bc0da9c55
    Reviewed-on: http://review.couchbase.org/108251
    Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>
    Tested-by: Balakumaran G <balakumaran.gopal@couchbase.com>

--- pytests/eventing/handler_code/n1ql_op_without_timers.js
+++ pytests/eventing/handler_code/n1ql_op_without_timers.js
@@ -19,19 +19,14 @@ function OnDelete(meta) {
     var docID = meta.id;
     // Adding these extra comments to validate MB-30240
     log('Before Deleting document', docID);
+    while (true) {
     try {
         var query = DELETE FROM dst_bucket where meta().id = $docID;
+        break;
     } catch (e) {
         log(e);
-        var obj = JSON.parse(e.message);
-        var code=obj['errors'][0]['code'];
-        log(code);
-        if(code == 12011){
-            log('retry');
-            query = DELETE FROM dst_bucket where meta().id = $docID;
         }
     }
-
     // Adding these extra comments to validate MB-30240
     log('After Deleting document', docID);
 }
