50ae4fed06b2b44d864f5ba9db9a893920359d44
vikas chaudhary <vikas.chaudhary@couchbase.com>
2019-06-04 16:31:53 +0530

    Eventing: Adding delay for n1ql
    Change-Id: Id62b3a4fe7c49cc6ef915805107006774af50751
    Reviewed-on: http://review.couchbase.org/110109
    Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>
    Tested-by: vikas chaudhary <vikas.chaudhary@couchbase.com>

--- pytests/eventing/handler_code/n1ql_op_without_timers.js
+++ pytests/eventing/handler_code/n1ql_op_without_timers.js
@@ -4,7 +4,15 @@ function OnUpdate(doc, meta) {
     log('Before Inserting document', doc);
     log('Before Inserting document', doc);
     log('Before Inserting document', doc);
+    sleep(30);
+    while (true) {
+    try {
     var query = INSERT INTO dst_bucket ( KEY, VALUE ) VALUES ( $docID ,'N1QL op');
+     break;
+    } catch (e) {
+        log(e);
+        }
+    }
     // Adding these extra comments to validate MB-30240
     log('After Inserting document', doc);
     log('After Inserting document', doc);
@@ -14,8 +22,22 @@ function OnUpdate(doc, meta) {
 function OnDelete(meta) {
     var docID = meta.id;
     // Adding these extra comments to validate MB-30240
-    log('Deleting document', docID);
-    var query = DELETE FROM dst_bucket where meta().id = $docID;
+    log('Before Deleting document', docID);
+    sleep(30);
+    while (true) {
+    try {
+        var query = DELETE FROM dst_bucket where meta().id = $docID;
+        break;
+    } catch (e) {
+        log(e);
+        }
+    }
     // Adding these extra comments to validate MB-30240
     log('Deleting document', docID);
+}
+
+function sleep(seconds) {
+  var start = new Date().getTime();
+  while(new Date().getTime() < start + seconds*1000){
+  }
 }
