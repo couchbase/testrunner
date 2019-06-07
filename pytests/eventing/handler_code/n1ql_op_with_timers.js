ab4333bdf4202192321a80082446f28f399238e3
vikas chaudhary <vikas.chaudhary@couchbase.com>
2019-06-07 05:38:55 +0000

    Revert "Eventing: Adding delay for n1ql"
    This reverts commit 50ae4fed06b2b44d864f5ba9db9a893920359d44.
    
    Change-Id: I0544f9eed3019b8eb5b996874806ddaaaccf49cb
    Reviewed-on: http://review.couchbase.org/110334
    Reviewed-by: Ashwin <ashwintrojan+github@gmail.com>
    Tested-by: vikas chaudhary <vikas.chaudhary@couchbase.com>

--- pytests/eventing/handler_code/n1ql_op_with_timers.js
+++ pytests/eventing/handler_code/n1ql_op_with_timers.js
@@ -1,45 +1,24 @@
 function OnUpdate(doc, meta) {
     var expiry = new Date();
     expiry.setSeconds(expiry.getSeconds() + 300);
-    sleep(10);
+
     var context = {docID : meta.id, random_text : "e6cZZGHuh0R7Aumoe6cZZGHuh0R7Aumoe6cZZGHuh0R7Aumoe6cZZGHuh0R7Aumoe6cZZGHuh0R7Aumoe6cZZGHuh0R7Aumoe6cZZGHuh0R7Aumoe6cZZGHuh0R7Aumoe6cZZGHuh0R7Aumoe6cZZGHuh0R7Aumoe6cZZGHuh0R7Aumoe6cZZGHuh0R7Aumoe6cZZGHuh0R7Aumoe6cZZGHuh07Aumoe6cZZGHuh07Aumoe6cZZGHuh07Aumoe6"};
     createTimer(timerCallback,  expiry, meta.id, context);
 }
 function OnDelete(meta) {
     var expiry = new Date();
     expiry.setSeconds(expiry.getSeconds() + 300);
-    sleep(10);
+
     var context = {docID : meta.id};
     createTimer(NDtimerCallback,  expiry, meta.id, context);
 }
 function NDtimerCallback(context) {
     var docID = context.docID;
-    sleep(10);
-    while (true) {
-    try {
-        var query = DELETE FROM dst_bucket where meta().id = $docID;
-        break;
-    } catch (e) {
-        log(e);
-        }
-    }
+    var query = DELETE FROM dst_bucket where meta().id = $docID;
+//    query.execQuery();
 }
-
 function timerCallback(context) {
     var docID = context.docID;
-    sleep(10);
-    while (true) {
-    try {
-        var query = INSERT INTO dst_bucket ( KEY, VALUE ) VALUES ( $docID ,'timerCallback');
-        break;
-    } catch (e) {
-        log(e);
-        }
-    }
-}
-
-function sleep(seconds) {
-  var start = new Date().getTime();
-  while(new Date().getTime() < start + seconds*1000){
-  }
+    var query = INSERT INTO dst_bucket ( KEY, VALUE ) VALUES ( $docID ,'timerCallback');
+//    query.execQuery();
 }
--- pytests/eventing/handler_code/n1ql_op_without_timers.js
+++ pytests/eventing/handler_code/n1ql_op_without_timers.js
@@ -4,15 +4,7 @@ function OnUpdate(doc, meta) {
     log('Before Inserting document', doc);
     log('Before Inserting document', doc);
     log('Before Inserting document', doc);
-    sleep(30);
-    while (true) {
-    try {
     var query = INSERT INTO dst_bucket ( KEY, VALUE ) VALUES ( $docID ,'N1QL op');
-     break;
-    } catch (e) {
-        log(e);
-        }
-    }
     // Adding these extra comments to validate MB-30240
     log('After Inserting document', doc);
     log('After Inserting document', doc);
@@ -22,22 +14,8 @@ function OnUpdate(doc, meta) {
 function OnDelete(meta) {
     var docID = meta.id;
     // Adding these extra comments to validate MB-30240
-    log('Before Deleting document', docID);
-    sleep(30);
-    while (true) {
-    try {
-        var query = DELETE FROM dst_bucket where meta().id = $docID;
-        break;
-    } catch (e) {
-        log(e);
-        }
-    }
+    log('Deleting document', docID);
+    var query = DELETE FROM dst_bucket where meta().id = $docID;
     // Adding these extra comments to validate MB-30240
     log('Deleting document', docID);
-}
-
-function sleep(seconds) {
-  var start = new Date().getTime();
-  while(new Date().getTime() < start + seconds*1000){
-  }
 }
