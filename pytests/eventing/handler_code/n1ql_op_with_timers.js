91334082ae92358d442afd2cdce77060e122295a
vikas chaudhary <vikas.chaudhary@couchbase.com>
2019-06-24 12:47:26 +0530

    Updating n1ql handlers
    Change-Id: Ifd1c78fb6f50d326d4d66b3ce68fa707159db00f
    Reviewed-on: http://review.couchbase.org/111074
    Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>
    Tested-by: vikas chaudhary <vikas.chaudhary@couchbase.com>

--- pytests/eventing/handler_code/n1ql_op_with_timers.js
+++ pytests/eventing/handler_code/n1ql_op_with_timers.js
@@ -1,20 +1,17 @@
 function OnUpdate(doc, meta) {
     var expiry = new Date();
     expiry.setSeconds(expiry.getSeconds() + 300);
-    sleep(10);
     var context = {docID : meta.id, random_text : "e6cZZGHuh0R7Aumoe6cZZGHuh0R7Aumoe6cZZGHuh0R7Aumoe6cZZGHuh0R7Aumoe6cZZGHuh0R7Aumoe6cZZGHuh0R7Aumoe6cZZGHuh0R7Aumoe6cZZGHuh0R7Aumoe6cZZGHuh0R7Aumoe6cZZGHuh0R7Aumoe6cZZGHuh0R7Aumoe6cZZGHuh0R7Aumoe6cZZGHuh0R7Aumoe6cZZGHuh07Aumoe6cZZGHuh07Aumoe6cZZGHuh07Aumoe6"};
     createTimer(timerCallback,  expiry, meta.id, context);
 }
 function OnDelete(meta) {
     var expiry = new Date();
     expiry.setSeconds(expiry.getSeconds() + 300);
-    sleep(10);
     var context = {docID : meta.id};
     createTimer(NDtimerCallback,  expiry, meta.id, context);
 }
 function NDtimerCallback(context) {
     var docID = context.docID;
-    sleep(10);
     while (true) {
     try {
         var query = DELETE FROM dst_bucket where meta().id = $docID;
@@ -27,7 +24,6 @@ function NDtimerCallback(context) {
 
 function timerCallback(context) {
     var docID = context.docID;
-    sleep(10);
     while (true) {
     try {
         var query = INSERT INTO dst_bucket ( KEY, VALUE ) VALUES ( $docID ,'timerCallback');
@@ -36,10 +32,4 @@ function timerCallback(context) {
         log(e);
         }
     }
-}
-
-function sleep(seconds) {
-  var start = new Date().getTime();
-  while(new Date().getTime() < start + seconds*1000){
-  }
 }
