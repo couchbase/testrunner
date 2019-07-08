d7fc8cfe8540f660766e0f6201f4f9d02a4dee05
vikas chaudhary <vikas.chaudhary@couchbase.com>
2019-07-08 13:31:16 +0530

    Eventing: remove exec query
    Change-Id: If31ab4aed74906b973d5102f41c349dcba18f27b
    Reviewed-on: http://review.couchbase.org/111680
    Reviewed-by: Gautham B A <gautham.bangalore@gmail.com>
    Tested-by: vikas chaudhary <vikas.chaudhary@couchbase.com>

--- pytests/eventing/handler_code/n1ql_temp.js
+++ pytests/eventing/handler_code/n1ql_temp.js
@@ -1,7 +1,9 @@
 function OnUpdate(doc, meta) {
     try{
         var query=$n1ql
-        query.execQuery();
+        //query.execQuery();
+        for(var raw of query){
+        }
         var key = Date.now()+Math.random();
         dst_bucket[key]={'passed_query' : query};
     }
