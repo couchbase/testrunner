3c69665ec99f82882e9ce044ee196ddc9eba6242
vikas chaudhary <vikas.chaudhary@couchbase.com>
2019-07-31 22:11:36 +0530

    Eventing: updating handler
    Change-Id: Ie2310f4dd1305317c0085ad4277de903d761e75d
    Reviewed-on: http://review.couchbase.org/112727
    Reviewed-by: Ashwin <ashwintrojan+github@gmail.com>
    Reviewed-by: Ritam Sharma <ritam@couchbase.com>
    Tested-by: vikas chaudhary <vikas.chaudhary@couchbase.com>

--- pytests/eventing/handler_code/curl/bucket_op_curl_delete.js
+++ pytests/eventing/handler_code/curl/bucket_op_curl_delete.js
@@ -15,6 +15,9 @@ function OnUpdate(doc, meta) {
     	if(response.status == 200){
     	    dst_bucket[meta.id]=response.body;
     	}
+    	else{
+    	    dst_bucket[meta.id]=response.status;
+    	}
     }
     catch (e) {
     	log('error:', e);
@@ -38,6 +41,9 @@ function OnDelete(meta) {
     	log('response status received from server:', response.status);
     	var res= new Uint8Array(response.body);
     	if(response.status == 200){
+    	    delete dst_bucket[meta.id];
+    	}
+    	else{
     	    delete dst_bucket[meta.id];
     	}
     }
