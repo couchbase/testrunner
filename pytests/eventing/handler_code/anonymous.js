87fd8a01bd5c965acda728e43b48c3a02368d042
vikas chaudhary <vikas.chaudhary@couchbase.com>
2019-08-06 13:08:03 +0530

    Eventing: fixing n1ql tests
    Change-Id: I5477802fa3e400d65be526ebc48b5a50bdbbc3bc
    Reviewed-on: http://review.couchbase.org/112952
    Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>
    Tested-by: Balakumaran G <balakumaran.gopal@couchbase.com>

--- pytests/eventing/handler_code/anonymous.js
+++ pytests/eventing/handler_code/anonymous.js
@@ -1,12 +1,16 @@
 function OnUpdate(doc, meta) {
     var anon = function () {
          var sel = SELECT  *  from src_bucket where mutated=0 limit 1;
-         dst_bucket['select'] = sel.execQuery();
+         for(var row of sel){
+         dst_bucket['select'] = row;
+         }
    };
     anon();
 
      (function () {
          var sel = SELECT  *  from src_bucket where mutated=0 limit 1;
-         dst_bucket['select2'] = sel.execQuery();
+         for(var row of sel){
+         dst_bucket['select2'] = row;
+         }
    })();
 }
