c9b09e03d2609f860067be6f7fd8e9b2b973f868
Ritam Sharma <ritam@couchbase.com>
2019-10-23 11:02:00 +0530

    Minor fix for x509 tests
    Change-Id: I8dbf347569e582f9fe9c9ab1e3c2fded44ae8520
    Reviewed-on: http://review.couchbase.org/116818
    Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>
    Tested-by: Balakumaran G <balakumaran.gopal@couchbase.com>

--- pytests/security/x509tests.py
+++ pytests/security/x509tests.py
@@ -153,10 +153,13 @@ class x509tests(BaseTestCase):
             return False
 
     def _sdk_connection(self, root_ca_path=x509main.CACERTFILEPATH + x509main.CACERTFILE, bucket='default', host_ip=None):
-        self.sleep(30)
+        self.sleep(10)
         result = False
         self.add_built_in_server_user([{'id': bucket, 'name': bucket, 'password': 'password'}], \
                                       [{'id': bucket, 'name': bucket, 'roles': 'admin'}], self.master)
+        self.add_built_in_server_user([{'id': 'cbadminbucket', 'name': 'cbadminbucket', 'password': 'password'}], \
+                                      [{'id': 'cbadminbucket', 'name': 'cbadminbucket', 'roles': 'admin'}], self.master)
+        self.sleep(10)
         if self.sdk_version == 'pre-vulcan':
             connection_string = 'couchbases://' + host_ip + '/' + bucket + '?certpath=' + root_ca_path
             self.log.info("Connection string is -{0}".format(connection_string))
@@ -168,8 +171,6 @@ class x509tests(BaseTestCase):
             except Exception, ex:
                 self.log.info("Expection is  -{0}".format(ex))
         elif self.sdk_version == 'vulcan':
-            self.add_built_in_server_user([{'id': 'cbadminbucket', 'name': 'cbadminbucket', 'password': 'password'}], \
-                                      [{'id': 'cbadminbucket', 'name': 'cbadminbucket', 'roles': 'admin'}], self.master)
             key_file = x509main.CACERTFILEPATH + self.ip_address + ".key"
             chain_file = x509main.CACERTFILEPATH + "/long_chain" + self.ip_address + ".pem"
             connection_string = 'couchbases://' + host_ip + '/?certpath=' + chain_file + "&keypath=" + key_file
