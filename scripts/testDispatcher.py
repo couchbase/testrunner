2e1bfe0ab988bfae6205fa1e5bbc5aea7e8c34b1
Evgeny Makarenko <evgeny.makarenko@couchbase.com>
2020-01-27 12:13:46 -0800

    jobs retrying patch
    Change-Id: Ifee86dd202ec045477cfb83132cff1d86c9254b1
    Reviewed-on: http://review.couchbase.org/121222
    Tested-by: <evgeny.makarenko@couchbase.com>
    Reviewed-by: Jagadesh Munta <jagadesh.munta@couchbase.com>

--- scripts/testDispatcher.py
+++ scripts/testDispatcher.py
@@ -64,14 +64,15 @@ def main():
     # whether to use production version of a test_suite_executor or test version
     parser.add_option('-l','--launch_job', dest='launch_job', default='test_suite_executor')
     parser.add_option('-f','--jenkins_server_url', dest='jenkins_server_url', default='http://qa.sc.couchbase.com')
+    parser.add_option('-m','--retry_params', dest='retry_params', default='')
 
+    # set of parameters for testing purposes.
+    #TODO: delete them after successful testing
 
     # dashboardReportedParameters is of the form param1=abc,param2=def
     parser.add_option('-d','--dashboardReportedParameters', dest='dashboardReportedParameters', default=None)
 
     options, args = parser.parse_args()
-
-
 
     print 'the run is', options.run
     print 'the  version is', options.version
@@ -82,11 +83,13 @@ def main():
     print 'os', options.os
     #print 'url', options.url
 
-
     print 'url is', options.url
     print 'cherrypick command is', options.cherrypick
 
     print 'the reportedParameters are', options.dashboardReportedParameters
+
+    print 'retry params are', options.retry_params
+
 
 
     # What do we do with any reported parameters?
@@ -261,10 +264,14 @@ def main():
                          'iniFile={5}&parameters={6}&os={7}&initNodes={' \
                          '8}&installParameters={9}&branch={10}&slave={' \
                          '11}&owners={12}&mailing_list={13}&mode={14}&timeout={15}'
+
+    launchString = launchString + '&retry_params=' + urllib.quote(options.retry_params)
+
     if options.url is not None:
         launchString = launchString + '&url=' + options.url
     if options.cherrypick is not None:
         launchString = launchString + '&cherrypick=' + urllib.quote(options.cherrypick)
+
 
     summary = []
 
