8b0555a67a6e15720e32f060a5341d1297786897
Evgeny Makarenko <evgeny.makarenko@couchbase.com>
2020-01-31 13:02:24 -0800

    test failers auto retriggering
    Change-Id: I4e71e718243c9fbfd8c60b6f8b2975d8d169c3b2
    Reviewed-on: http://review.couchbase.org/121524
    Reviewed-by: Jagadesh Munta <jagadesh.munta@couchbase.com>
    Tested-by: <evgeny.makarenko@couchbase.com>
    Reviewed-on: http://review.couchbase.org/122446
    Reviewed-by: Ajay Bhullar <ajay.bhullar@couchbase.com>
    Tested-by: Girish Benakappa <girish.benakappa@couchbase.com>

--- pytests/tuqquery/fake_test_suite.py
+++ pytests/tuqquery/fake_test_suite.py
@@ -1,3 +1,4 @@
+import random
 from tuq import QueryTests
 
 class FakeTests(QueryTests):
@@ -5,16 +6,44 @@ class FakeTests(QueryTests):
     def suite_setUp(self):
         super(FakeTests, self).suite_setUp()
 
-    def test_negative_1(self):
-        self.assertEquals(True, False, "Negative test 1 is failed")
+    def test_1(self):
+        i = random.randint(0, 1)
+        self.assertEquals(i, 1, "Test 1 is failed")
 
-    def test_negative_2(self):
-        self.assertEquals(True, False, "Negative test 2 is failed")
+    def test_2(self):
+        i = random.randint(0, 1)
+        self.assertEquals(i, 1, "Test 1 is failed")
 
-    def test_positive_1(self):
-        self.assertEquals(True, True, "Positive test 1 is failed")
+    def test_3(self):
+        i = random.randint(0, 1)
+        self.assertEquals(i, 1, "Test 1 is failed")
 
-    def test_positive_2(self):
-        self.assertEquals(True, False, "Positive test 2 is failed")
+    def test_4(self):
+        i = random.randint(0, 1)
+        self.assertEquals(i, 1, "Test 1 is failed")
+
+    def test_5(self):
+        i = random.randint(0, 1)
+        self.assertEquals(i, 1, "Test 1 is failed")
+
+    def test_6(self):
+        i = random.randint(0, 1)
+        self.assertEquals(i, 1, "Test 1 is failed")
+
+    def test_7(self):
+        i = random.randint(0, 1)
+        self.assertEquals(i, 1, "Test 1 is failed")
+
+    def test_8(self):
+        i = random.randint(0, 1)
+        self.assertEquals(i, 1, "Test 1 is failed")
+
+    def test_9(self):
+        i = random.randint(0, 1)
+        self.assertEquals(i, 1, "Test 1 is failed")
+
+    def test_10(self):
+        i = random.randint(0, 1)
+        self.assertEquals(i, 1, "Test 1 is failed")
 
 
