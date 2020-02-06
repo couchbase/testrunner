424c968b4b70e6ed348f7bd5080f2d2fdbb0126f
vikas chaudhary <vikas.chaudhary@couchbase.com>
2020-02-06 15:09:43 +0530

    fix for conflicts with dac5de6c2
    Change-Id: I663f7f669d16c8b1c3366ab4682aa473f72a9b50
    Reviewed-on: http://review.couchbase.org/122448
    Reviewed-by: Mihir Kamdar <mihir.kamdar@couchbase.com>
    Reviewed-by: Ajay Bhullar <ajay.bhullar@couchbase.com>
    Tested-by: Girish Benakappa <girish.benakappa@couchbase.com>

--- lib/membase/api/rest_client.py
+++ lib/membase/api/rest_client.py
@@ -4419,6 +4419,22 @@ class RestConnection(object):
         return content
 
     '''
+            deploy the Function 
+        '''
+
+    def deploy_function_by_name(self, name):
+        authorization = base64.encodestring('%s:%s' % (self.username, self.password))
+        url = "api/v1/functions/" + name + "/settings"
+        body = {"deployment_status": True, "processing_status": True}
+        api = self.eventing_baseUrl + url
+        headers = {'Content-type': 'application/json', 'Authorization': 'Basic %s' % authorization}
+        status, content, header = self._http_request(api, 'POST', headers=headers,
+                                                     params=json.dumps(body).encode("ascii", "ignore"))
+        if not status:
+            raise Exception(content)
+        return content
+
+    '''
         undeploy the Function 
     '''
     def undeploy_function(self, name):
