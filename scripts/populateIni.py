ec0c7b34680c736a3657856f3d3151925b761c20
Jagadesh Munta <jagadesh.munta@couchbase.com>
2020-05-08 19:07:23 -0700

    CBQE-5748: populateIni.py - parameterizing username / password or other in the ini file
    Change-Id: Ib5b910d5118369ad4b87f3cd3e7be1ad52eb5035
    Reviewed-on: http://review.couchbase.org/c/testrunner/+/127670
    Reviewed-by: Mihir Kamdar <mihir.kamdar@couchbase.com>
    Tested-by: Jagadesh Munta <jagadesh.munta@couchbase.com>

--- scripts/populateIni.py
+++ scripts/populateIni.py
@@ -1,13 +1,6 @@
-
-import sys
-import urllib.request, urllib.error, urllib.parse
-import urllib.request, urllib.parse, urllib.error
-import httplib2
 import json
-import string
-import time
-import ast
 from optparse import OptionParser
+import configparser
 
 
 # takes an ini template as input, standard out is populated with the server pool
@@ -25,13 +18,18 @@ def main():
     parser.add_option('-i', '--inifile', dest='inifile')
     parser.add_option('-o', '--outputFile', dest='outputFile')
     parser.add_option('-p', '--os', dest='os')
+    parser.add_option('-k', '--keyValue', dest='keyValue')
+    parser.add_option('-r', '--replaceValue', dest='replaceValue')
     options, args = parser.parse_args()
-
 
     print('the ini file is', options.inifile)
 
-    if not options.servers.startswith('['):
-        options.servers='['+options.servers+']'
+    servers = []
+    if options.servers:
+        if not options.servers.startswith('['):
+            options.servers='['+options.servers+']'
+        servers = json.loads(options.servers)
+
     print('the server info is', options.servers)
 
     addPoolServers = []
@@ -42,13 +40,21 @@ def main():
         print('the additional server pool info is', options.addPoolServers)
         addPoolServers = json.loads(options.addPoolServers)
 
-    servers = json.loads(options.servers)
+    if options.keyValue:
+        if options.outputFile:
+            update_config(options.inifile, options.keyValue, options.outputFile)
+        else:
+            update_config(options.inifile, options.keyValue, None)
 
-    f = open(options.inifile)
+    if options.keyValue and options.outputFile:
+        f = open(options.outputFile)
+    else:
+        f = open(options.inifile)
+
     data = f.readlines()
 
     for i in range( len(data) ):
-          if 'dynamic' in data[i]:
+          if 'dynamic' in data[i] and servers:
              data[i] = data[i].replace('dynamic', servers[0])
              servers.pop(0)
           elif addPoolServers and options.addPoolServerId in data[i]:
@@ -61,12 +67,43 @@ def main():
               if 'password:couchbase' in data[i]:
                   data[i] = data[i].replace('couchbase', 'Membase123')
 
+          if options.replaceValue:
+              for oldnew in options.replaceValue.split(','):
+                  old, new = oldnew.split("=")
+                  if old in data[i]:
+                    data[i] = data[i].replace(old, new)
+
+
     for d in data:
-          print(d, end=' ')
+          print(d.strip())
 
-    f = open(options.outputFile, 'w')
-    f.writelines(data)
+    if options.outputFile:
+        f = open(options.outputFile, 'w')
+        f.writelines(data)
+    else:
+        for d in data:
+            print(d.strip())
 
+def update_config(config_in_file, json_object, config_out_file):
+    config = configparser.RawConfigParser(delimiters=(':', ':'))
+    config.read(config_in_file)
+    key_values = json.loads(json_object)
+    for section in config.sections():
+        for key in key_values.keys():
+            try:
+                old_value = config.get(section, key)
+                config.set(section, key, key_values.get(key))
+                #print("Replaced {}==> {}".format(old_value,key_values.get(key)))
+            except Exception as e:
+                #print(e)
+                pass
+
+    try:
+        if config_out_file:
+            with open(config_out_file, 'w') as configfile:
+                config.write(configfile, space_around_delimiters=False)
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
