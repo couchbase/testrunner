b873f071a92e6428816012f8d4aba1fabf5a41bf
Pavithra Mahamani <pavithra.mahamani@couchbase.com>
2020-06-03 10:39:43 -0700

    Inconsistent behavoir with docker py, switching to docker cli
    Change-Id: Ia6b25edade27d8922cd697267d7588c41861f0cb
    Reviewed-on: http://review.couchbase.org/c/testrunner/+/129755
    Tested-by: <pavithra.mahamani@couchbase.com>
    Reviewed-by: Arunkumar Senthilnathan <arun.couchbase@yahoo.com>

--- scripts/java_sdk_setup.py
+++ scripts/java_sdk_setup.py
@@ -1,50 +1,28 @@
 import subprocess
-import docker
 
 
 class JavaSdkSetup(object):
     def __init__(self):
-        self.base_dir = "java_sdk_client/collections/"
-        self.image_tag = "jsc:latest"
-        self.client = docker.APIClient()
-        self.build_docker_image()
+        self.run()
 
-    def _run_cmds(self):
-        cmds = ["mvn -f " + self.base_dir + "pom.xml clean install  > /dev/null && echo 0 || echo 1;",
-                "chmod 777 " + self.base_dir + "configure.sh  > /dev/null && echo 0 || echo 1;"]
+    def run(self):
+        cmds = ["chmod 777 java_sdk_client/collections/configure.sh  > /dev/null && echo 0 || echo 1;",
+                "mvn -f java_sdk_client/collections/pom.xml clean install  > /dev/null && echo 0 || echo 1;"
+                "docker build -t jsc java_sdk_client/collections  > /dev/null && echo 0 || echo 1;"
+                ]
         for cmd in cmds:
-            self.__execute_on_slave(cmd, 30)
-
-    def __execute_on_slave(self, command, timeout):
-        print(command)
-        return subprocess.Popen(command, stdout=subprocess.PIPE, shell=True).wait(timeout)
-
-    def build_docker_image(self):
-        # Only remove dangling images
-        self._cleanup(True)
-        num_retries = 2
-        while num_retries:
-            if self._inspect_docker_image():
-                print("Docker image built")
-                break
-            else:
-                num_retries -= 1
+            num_retries = 2
+            while num_retries:
                 try:
-                    # Remove all images
-                    self._cleanup(False)
-                    self._run_cmds()
-                    response = [line for line in self.client.build(path=self.base_dir, rm=True, tag="jsc")]
+                    print(cmd + "\n")
+                    if self._execute_on_slave(cmd, 30):
+                        num_retries -= 1
+                    else:
+                        break
                 except Exception as e:
-                    raise("Unable to build docker image ".format(e))
-        else:
-            raise("Unable to build docker image after 3 retries")
+                    raise("Unable to perform {0} due to {1}".format(cmd, e))
+            else:
+                raise("Unable to perform {0} after 3 retries".format(cmd))
 
-    def _inspect_docker_image(self):
-        if len(self.client.images(name=self.image_tag)) == 1:
-            return True
-        else:
-            return False
-
-    def _cleanup(self, dangling=True):
-        self.client.prune_images(filters={"dangling": dangling})
-        self.client.prune_containers()
+    def _execute_on_slave(self, command, timeout):
+        return subprocess.Popen(command, stdout=subprocess.PIPE, shell=True).wait(timeout)
