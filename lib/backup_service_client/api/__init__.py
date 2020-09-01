a91bad4224d5a4aa4173848925d2929c0c269793
Asad Zaidi <asad.zaidi@couchbase.com>
2020-09-01 09:32:41 +0100

    CBQE-6037: Add's Swagger BackupService REST API
    Adds automatically generated Swagger REST API for the BackupService.
    
    Change-Id: I79484c8043b06453d9b1cf35ccfc4435924accbe
    Reviewed-on: http://review.couchbase.org/c/testrunner/+/135382
    Reviewed-by: Arunkumar Senthilnathan <arun.couchbase@yahoo.com>
    Reviewed-by: Carlos Gonzalez <carlos.gonzalez@couchbase.com>
    Tested-by: Asad Zaidi <asad.zaidi@couchbase.com>

---
+++ lib/backup_service_client/api/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from backup_service_client.api.active_repository_api import ActiveRepositoryApi
+from backup_service_client.api.cloud_credentials_api import CloudCredentialsApi
+from backup_service_client.api.cluster_api import ClusterApi
+from backup_service_client.api.configuration_api import ConfigurationApi
+from backup_service_client.api.import_api import ImportApi
+from backup_service_client.api.plan_api import PlanApi
+from backup_service_client.api.repository_api import RepositoryApi
