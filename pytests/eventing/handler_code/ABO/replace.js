48e2e75e0474d2a96265be258825d4c5ad3c31bf
vikas chaudhary <vikas.chaudhary@couchbase.com>
2020-10-13 13:48:40 +0530

    Fixing testware issues
    Change-Id: Ieaefa9986148a4337b161381f4d49825786d54d6
    Reviewed-on: http://review.couchbase.org/c/testrunner/+/137912
    Tested-by: Balakumaran G <balakumaran.gopal@couchbase.com>
    Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>

--- pytests/eventing/handler_code/ABO/replace.js
+++ pytests/eventing/handler_code/ABO/replace.js
@@ -17,16 +17,20 @@ function OnUpdate(doc, meta) {
 
 function OnDelete(meta, options) {
     log("Doc deleted/expired", meta.id);
+    try{
     var result = couchbase.get(dst_bucket,meta);
     log(result);
     if(result.success && result.meta.cas != undefined){
             var meta_data={"id": meta.id,"cas":result.meta.cas};
-            var result1= couchbase.replace(dst_bucket,meta_data,doc);
+            var result1= couchbase.replace(dst_bucket,meta_data,"hello");
             log(result1);
             if(result1.success){
                 var doc={"id":meta.id};
                 var result2= couchbase.delete(dst_bucket,doc);
                 log(result2);
         }
+    }
+    }catch(e){
+        log(e);
     }
 }
