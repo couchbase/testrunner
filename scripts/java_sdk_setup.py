1d685fe4a4f19e09abc31f9d309e2d74d57c4f3c
Pavithra Mahamani <pavithra.mahamani@couchbase.com>
2020-10-15 16:32:36 -0700

    Removing docker setup steps
    Change-Id: I7eda41664f75a3162ac6375d9bc0e0b545b6bdbf
    Reviewed-on: http://review.couchbase.org/c/testrunner/+/138277
    Reviewed-by: Arunkumar Senthilnathan <arun.couchbase@yahoo.com>
    Tested-by:  Pavithra Mahamani <pavithra.mahamani@couchbase.com>

--- scripts/java_sdk_setup.py
+++ scripts/java_sdk_setup.py
@@ -6,23 +6,11 @@ class JavaSdkSetup(object):
         self.run()
 
     def run(self):
-        cmds = ["chmod 777 java_sdk_client/collections/configure.sh  > /dev/null && echo 0 || echo 1;",
-                "mvn -f java_sdk_client/collections/pom.xml clean install  > /dev/null && echo 0 || echo 1;"
-                "docker build -t jsc java_sdk_client/collections  > /dev/null && echo 0 || echo 1;"
-                ]
-        for cmd in cmds:
-            num_retries = 2
-            while num_retries:
-                try:
-                    print(cmd + "\n")
-                    if self._execute_on_slave(cmd, 30):
-                        num_retries -= 1
-                    else:
-                        break
-                except Exception as e:
-                    raise("Unable to perform {0} due to {1}".format(cmd, e))
-            else:
-                raise("Unable to perform {0} after 3 retries".format(cmd))
+        cmd = "mvn -f java_sdk_client/collections/pom.xml clean install  > /dev/null && echo 0 || echo 1;"
+        try:
+            self._execute_on_slave(cmd, 30)
+        except Exception:
+            print("WARNING: Exception occurred while compiling java_sdk_client..continuing")
 
     def _execute_on_slave(self, command, timeout):
         return subprocess.Popen(command, stdout=subprocess.PIPE, shell=True).wait(timeout)
