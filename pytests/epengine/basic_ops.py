002af4b2ed4521bc2679ce34eb3417f1e78f2bf8
Ashwin <ashwintrojan+github@gmail.com>
2020-11-05 15:51:44 +0530

    Fixing assert str value in basic_ops test
    Also moving DGM cases to the end of conf_file
    
    Change-Id: I15c1ead72f1e650222a87db76d4bcdc683be383f
    Reviewed-on: http://review.couchbase.org/c/testrunner/+/139653
    Tested-by: Balakumaran G <balakumaran.gopal@couchbase.com>
    Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>

--- pytests/epengine/basic_ops.py
+++ pytests/epengine/basic_ops.py
@@ -268,12 +268,12 @@ class basic_ops(BaseTestCase):
             else:
                 self.assertNotEqual("API is accessible from localhost only", output[0])
 
-    def verify_stat(self,items, value=b"active" ):
+    def verify_stat(self, items, value=b"active"):
         mc = MemcachedClient(self.master.ip, 11210)
         mc.sasl_auth_plain(self.master.rest_username, self.master.rest_password)
         mc.bucket_select('default')
         stats = mc.stats()
-        self.assertEqual(stats['ep_compression_mode'], value)
+        self.assertEqual(str(stats['ep_compression_mode']), str(value))
         self.assertEqual(int(stats['ep_item_compressor_num_compressed']), items)
         self.assertNotEqual(int(stats['vb_active_itm_memory']), int(stats['vb_active_itm_memory_uncompressed']))
 
