9065ac959f283cc1d7038a78af1852c4f9ef01fb
saigon <baolavn@gmail.com>
2021-04-26 17:01:24 -0700

    Add more logs to debug create primary index issue in upgrade.
    Change-Id: I848cc01d84879d429170ad6c9c163bb380721801
    Reviewed-on: http://review.couchbase.org/c/testrunner/+/152162
    Tested-by: Thuan Nguyen <soccon@gmail.com>
    Reviewed-by: Ajay Bhullar <ajay.bhullar@couchbase.com>

--- lib/couchbase_helper/tuq_helper.py
+++ lib/couchbase_helper/tuq_helper.py
@@ -710,9 +710,12 @@ class N1QLHelper():
                 self.query += " USING VIEW "
             if self.use_rest:
                 try:
+                    self.log.info("Check if index existed")
                     check = self._is_index_in_list(bucket.name, "#primary", server=server)
                     if not check:
+                        self.log.info("Create primary index")
                         self.run_cbq_query(server=server, query_params={'timeout': '900s'})
+                        self.log.info("Check if index is online")
                         check = self.is_index_online_and_in_list(bucket.name, "#primary", server=server)
                         if not check:
                             raise Exception(" Timed-out Exception while building primary index for bucket {0} !!!".format(bucket.name))
