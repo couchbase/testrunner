642954be4f13d55ae0387f46ec14d9b4eb959178
Jake Rawsthorne <jake.rawsthorne@couchbase.com>
2021-05-27 11:55:14 +0100

    CBQE-6926: Mac install failure due to old process not being cleaned up
    Change-Id: I06e92bff1b2c479f75bff82aae133dbfd1fa506b
    Reviewed-on: http://review.couchbase.org/c/testrunner/+/154532
    Reviewed-by: Jagadesh Munta <jagadesh.munta@couchbase.com>
    Tested-by: Jake Rawsthorne <jake.rawsthorne@couchbase.com>

--- scripts/install_constants.py
+++ scripts/install_constants.py
@@ -102,10 +102,14 @@ CMDS = {
             "rm -rf ~/Library/Application\ Support/Couchbase; "
             "rm -rf ~/Library/Application\ Support/membase; "
             "rm -rf ~/Library/Python/couchbase-py; "
+            "launchctl list | grep couchbase-server | xargs -n 3 | cut -f3 -d' ' | xargs -n 1 launchctl stop; "
+            "kill -9 `ps -ef |egrep Couchbase | xargs|cut -f1 -d' '`; "
             "umount /Volumes/Couchbase* > /dev/null && echo 1 || echo 0",
         "pre_install": "HDIUTIL_DETACH_ATTACH",
         "install":
             "rm -rf /Applications\Couchbase\ Server.app; "
+            "launchctl list | grep couchbase-server | xargs -n 3 | cut -f3 -d' ' | xargs -n 1 launchctl stop; "
+            "kill -9 `ps -ef |egrep Couchbase | xargs|cut -f1 -d' '`; "
             "cp -R mountpoint/Couchbase\ Server.app /Applications/Couchbase\ Server.app; "
             "open /Applications/Couchbase\ Server.app > /dev/null && echo 1 || echo 0",
         "post_install": "launchctl list | grep couchbase-server > /dev/null && echo 1 || echo 0",
@@ -173,10 +177,12 @@ NON_ROOT_CMDS = {
             "rm -rf ~/Library/Application\ Support/Couchbase; "
             "rm -rf ~/Library/Application\ Support/membase; "
             "rm -rf ~/Library/Python/couchbase-py; "
+            "launchctl list | grep couchbase-server | xargs -n 3 | cut -f3 -d' ' | xargs -n 1 launchctl stop; "
             "umount /Volumes/Couchbase* > /dev/null && echo 1 || echo 0",
         "pre_install": "HDIUTIL_DETACH_ATTACH",
         "install":
             "rm -rf /Applications\Couchbase\ Server.app; "
+            "launchctl list | grep couchbase-server | xargs -n 3 | cut -f3 -d' ' | xargs -n 1 launchctl stop; "
             "cp -R mountpoint/Couchbase\ Server.app /Applications/Couchbase\ Server.app; "
             "open /Applications/Couchbase\ Server.app > /dev/null && echo 1 || echo 0",
         "post_install": "launchctl list | grep couchbase-server > /dev/null && echo 1 || echo 0",
