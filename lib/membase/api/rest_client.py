dbb0623b5f0157e871d517b58cebb8b94a007da2
Pavithra Mahamani <pavithra.mahamani@couchbase.com>
2021-06-02 09:18:00 -0700

    fixing unreachable code
    Change-Id: I847d3bb92bf8ecba7be2ba46f700b593ad3462fb
    Reviewed-on: http://review.couchbase.org/c/testrunner/+/154956
    Reviewed-by: Arunkumar Senthilnathan <arun.couchbase@yahoo.com>
    Tested-by:  Pavithra Mahamani <pavithra.mahamani@couchbase.com>

--- lib/membase/api/rest_client.py
+++ lib/membase/api/rest_client.py
@@ -1434,7 +1434,8 @@ class RestConnection(object):
             except ValueError:
                 time.sleep(10)
                 retries -= 1
-        raise Exception("create replication failed: status:{0},content:{1}".format(status, content))
+            finally:
+                raise Exception("create replication failed: status:{0},content:{1}".format(status, content))
 
     def get_replications(self):
         replications = []
