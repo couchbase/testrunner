6cd8630683d485fe4c34e2391c0b5eed20d07a19
pierrecouch <pierre.regazzoni@couchbase.com>
2021-12-17 09:47:24 -0800

    Update for TLS test cases on MH
    Change-Id: I461cb5507a1c95100c0c0a4eb1bdbf7aeb7dc46e
    Reviewed-on: https://review.couchbase.org/c/testrunner/+/167847
    Reviewed-by: Ajay Bhullar <ajay.bhullar@couchbase.com>
    Tested-by: Pierre Regazzoni <pierre.regazzoni@couchbase.com>

--- lib/membase/api/rest_client.py
+++ lib/membase/api/rest_client.py
@@ -3713,6 +3713,7 @@ class RestConnection(object):
         protocol = "http"
         if CbServer.use_https:
             protocol = "https"
+            server.port = str(CbServer.ssl_port_map.get(str(server.port), str(server.port)))
         api = "%s://%s:%s/" % (protocol, server.ip, server.port) + "settings/querySettings/curlWhitelist"
         headers = self._create_headers_with_auth('Administrator', 'password')
         status, content, response = self._http_request(api, "POST", headers=headers,
