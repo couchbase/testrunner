2cb116f0c861b55a002cf1249261f5023a56a253
Girish Benakappa <girish.benakappa@couchbase.com>
2022-02-11 16:53:15 -0800

    fix for debian
    Change-Id: I21d2e68bd172bace21dd0f6fc974aa962a08d825
    Reviewed-on: https://review.couchbase.org/c/testrunner/+/170615
    Tested-by: Girish Benakappa <girish.benakappa@couchbase.com>
    Reviewed-by: Mihir Kamdar <mihir.kamdar@couchbase.com>

--- lib/testconstants.py
+++ lib/testconstants.py
@@ -140,7 +140,8 @@ MACOS_NAME = {"10.10":"Yosemite", "10.11":"El Capitan", "10.12":"Sierra", "10.13
               "10.14":"Mojave", "10.15":"Catalina"}
 
 SYSTEMD_SERVER = ["centos 8", "centos 7", "suse 12", "suse 15", "ubuntu 16.04", "ubuntu 18.04",
-                  "ubuntu 20.04", "debian 8", "debian 9", "debian 10", "rhel 8", "oel 7", "oel 8", "amazon linux release 2 (karoo)"]
+                  "ubuntu 20.04", "debian 8", "debian 9", "debian 10", "rhel 8", "oel 7", "oel 8", "debian 11",
+                  "amazon linux release 2 (karoo)"]
 WIN_NUM_ERLANG_PROCESS = 4
 WIN_MEMBASE_DATA_PATH = '/cygdrive/c/Program\ Files/Membase/Server/var/lib/membase/data/'
 WIN_COUCHBASE_DATA_PATH = '/cygdrive/c/Program\ Files/Couchbase/Server/var/lib/couchbase/data/'
