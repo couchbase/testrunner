2a61ac28b926039fa67cf1cd8652de1e69dc1521
shaazin19 <shaazin.sheikh@couchbase.com>
2022-04-11 16:17:54 +0530

    CBQE-7527: 3/n Test port security with encryption enabled and certificates
    'test_port_security_with_encryption' scans ports of all the nodes in
    the cluster when the cluster encryption level is set to all and strict
    'test_port_security_with_certificates' scans ports of all the nodes in
    the cluster with the certificates uploaded
    
    Change-Id: Iabb19ebb17c0bbf6dad49950d86bd2d40ab7b85d
    Reviewed-on: https://review.couchbase.org/c/testrunner/+/173555
    Tested-by: Balakumaran G <balakumaran.gopal@couchbase.com>
    Reviewed-by: Balakumaran G <balakumaran.gopal@couchbase.com>

--- pytests/security/testssl.py
+++ pytests/security/testssl.py
@@ -1,10 +1,12 @@
 import time
 
-from pytests.basetestcase import BaseTestCase
 from lib.Cb_constants.CBServer import CbServer
-from pytests.security.testssl_util import TestSSL
 from lib.membase.api.rest_client import RestConnection
 from lib.remote.remote_util import RemoteMachineShellConnection
+from pytests.basetestcase import BaseTestCase
+from pytests.security.testssl_util import TestSSL
+from pytests.security.x509_multiple_CA_util import x509main
+from pytests.security.ntonencryptionBase import ntonencryptionBase
 
 
 class TestSSLTests(BaseTestCase):
@@ -90,7 +92,7 @@ class TestSSLTests(BaseTestCase):
                 shell.start_couchbase()
                 shell.disconnect()
         except Exception as e:
-            self.log.fail(e)
+            self.fail(e)
         self.test_tls_min_version()
 
     def test_port_security(self):
@@ -101,7 +103,6 @@ class TestSSLTests(BaseTestCase):
             self.log.info("Testing node {0}".format(node.ip))
             ports_to_scan = self.get_service_ports(node)
             ports_to_scan.extend(self.ports_to_scan)
-            scan_count = 0
             for node_port in ports_to_scan:
                 self.log.info("Port being tested: {0}".format(node_port))
                 cmd = self.testssl.TEST_SSL_FILENAME + " --warnings off --color 0 {0}:{1}" \
@@ -113,32 +114,33 @@ class TestSSLTests(BaseTestCase):
                 output = output.decode().split("\n")
                 check_next = 0
                 stmt = ""
+                scan_count = 0
                 for line in output:
                     if check_next == 1:
                         if stmt == "Certificate Validity":
                             if ">= 10 years is way too long" in line:
-                                self.log.fail(">= 10 years is way too long. (Fix moved to "
-                                              "Morpheus)".format(node_port))
-                                check_next = 0
-                                stmt = ""
+                                self.log.info(">= 10 years is way too long. (Fix moved to "
+                                              "Morpheus MB-50249)".format(node_port))
+                            check_next = 0
+                            stmt = ""
 
                     # Testing Protocols
-                    elif "SSLv2" in line or "SSLv3" in line:
+                    elif "SSLv2  " in line or "SSLv3  " in line:
                         scan_count = scan_count + 1
                         if "offered (NOT ok)" in line:
-                            self.log.fail("SSLvx is offered on port {0}".format(node_port))
+                            self.fail("SSLvx is offered on port {0}".format(node_port))
 
                     # Testing Cipher Categories
                     elif "LOW: 64 Bit + DES, RC[2,4]" in line:
                         scan_count = scan_count + 1
                         if "offered (NOT ok)" in line:
-                            self.log.fail("Cipher is not ok on port {0}".format(node_port))
+                            self.fail("Cipher is not ok on port {0}".format(node_port))
 
                     # Testing Server's Cipher Preferences
                     elif "Has server cipher order?" in line:
                         scan_count = scan_count + 1
                         if "no" in line:
-                            self.log.fail("Server cipher ordering not set".format(node_port))
+                            self.fail("Server cipher ordering not set".format(node_port))
 
                     # Testing Server Defaults
                     elif "Certificate Validity" in line:
@@ -150,45 +152,74 @@ class TestSSLTests(BaseTestCase):
                     elif "Heartbleed (CVE-2014-0160)" in line:
                         scan_count = scan_count + 1
                         if "VULNERABLE (NOT ok)" in line:
-                            self.log.fail("Heartbleed vulnerability on port {0}".format(node_port))
+                            self.fail("Heartbleed vulnerability on port {0}".format(node_port))
                     elif "ROBOT" in line:
                         scan_count = scan_count + 1
                         if "VULNERABLE (NOT ok)" in line:
-                            self.log.fail("Robot vulnerability on port {0}".format(node_port))
+                            self.fail("Robot vulnerability on port {0}".format(node_port))
                     elif "Secure Client-Initiated Renegotiation" in line:
                         scan_count = scan_count + 1
                         if "VULNERABLE (NOT ok)" in line:
-                            self.log.fail("Renegotiation vulnerability on port {0}"
-                                          .format(node_port))
+                            self.fail("Renegotiation vulnerability on port {0}"
+                                      .format(node_port))
                     elif "CRIME, TLS" in line:
                         scan_count = scan_count + 1
                         if "VULNERABLE (NOT ok)" in line:
-                            self.log.fail("Crime vulnerability on port {0}".format(node_port))
+                            self.fail("Crime vulnerability on port {0}".format(node_port))
                     elif "POODLE, SSL (CVE-2014-3566)" in line:
                         scan_count = scan_count + 1
                         if "VULNERABLE (NOT ok)" in line:
-                            self.log.fail("Poodle vulnerability on port {0}".format(node_port))
+                            self.fail("Poodle vulnerability on port {0}".format(node_port))
                     elif "SWEET32" in line:
                         scan_count = scan_count + 1
                         if "VULNERABLE (NOT ok)" in line:
-                            self.log.fail("Sweet32 vulnerability on port {0}".format(node_port))
+                            self.fail("Sweet32 vulnerability on port {0}".format(node_port))
                     elif "LOGJAM (CVE-2015-4000)" in line:
                         scan_count = scan_count + 1
                         if "VULNERABLE (NOT ok)" in line:
-                            self.log.fail("LogJam vulnerability on port {0}".format(node_port))
+                            self.fail("LogJam vulnerability on port {0}".format(node_port))
                     elif "LUCKY13 (CVE-2013-0169)" in line:
                         scan_count = scan_count + 1
                         if "VULNERABLE (NOT ok)" in line:
-                            self.log.fail("Lucky13 vulnerability on port {0}".format(node_port))
+                            self.fail("Lucky13 vulnerability on port {0}".format(node_port))
                     elif "RC4 (CVE-2013-2566, CVE-2015-2808)" in line:
                         scan_count = scan_count + 1
                         if "VULNERABLE (NOT ok)" in line:
-                            self.log.fail("RC4 ciphers detected on port {0}".format(node_port))
+                            self.fail("RC4 ciphers detected on port {0}".format(node_port))
 
-                    # Find out
                     elif "Medium grade encryption" in line:
                         scan_count = scan_count + 1
                         if "not offered (OK)" not in line:
-                            self.log.fail("Medium grade encryption is offered on port {0}"
-                                          .format(node_port))
-            self.log.info("Scan count = {0}".format(scan_count))
+                            self.fail("Medium grade encryption is offered on port {0}"
+                                      .format(node_port))
+                self.assertTrue(scan_count == 14,
+                                msg="Test didn't complete all the scans for port {0}"
+                                .format(node_port))
+
+    def test_port_security_with_encryption(self):
+        """
+        Scanning the ports to test vulnerabilities with cluster encryption enabled
+        """
+        ntonencryptionBase().setup_nton_cluster(servers=self.servers, ntonStatus="enable",
+                                                clusterEncryptionLevel="all")
+        self.test_port_security()
+        CbServer.use_https = True
+        ntonencryptionBase().setup_nton_cluster(servers=self.servers, ntonStatus="enable",
+                                                clusterEncryptionLevel="strict")
+        self.test_port_security()
+
+    def test_port_security_with_certificates(self):
+        """
+        Scanning the ports to test vulnerabilities with certificates
+        """
+        self.x509 = x509main(host=self.master)
+        self.x509.generate_multiple_x509_certs(servers=self.servers)
+        for server in self.servers:
+            _ = self.x509.upload_root_certs(server)
+        self.x509.upload_node_certs(servers=self.servers)
+        self.x509.delete_unused_out_of_the_box_CAs(server=self.master)
+        self.x509.upload_client_cert_settings(server=self.servers[0])
+        CbServer.use_https = True
+        ntonencryptionBase().setup_nton_cluster(servers=self.servers,
+                                                clusterEncryptionLevel="strict")
+        self.test_port_security()
