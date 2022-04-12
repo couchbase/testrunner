9a175926016f2e7abe9645c0a281cda60a86d66e
pavithra-mahamani <pavithra.mahamani@couchbase.com>
2022-04-11 17:34:30 -0700

    Performing docker cleanup in tearDown
    Change-Id: I86d845d869a67310a4a7357fb9076b6260a84fcb
    Reviewed-on: https://review.couchbase.org/c/testrunner/+/173598
    Tested-by: Ashwin <ashwin.trojan@gmail.com>
    Reviewed-by: <anitha.kuberan@couchbase.com>

--- pytests/e2e/e2e_base.py
+++ pytests/e2e/e2e_base.py
@@ -8,13 +8,18 @@ class E2EBaseTest(BaseTestCase):
     def setUp(self):
         super(E2EBaseTest, self).setUp()
         self.log = logger.Logger.get_logger()
+        self.service_handler = DeployE2EServices(self.master.ip,
+                                                 "Administrator", "password")
         self.setup_services()
 
     def tearDown(self):
+        self.service_handler.deleteExistingDockerContainersOnHost()
+        self.service_handler.deleteExistingDockerImagesOnHost()
+
         super(E2EBaseTest, self).tearDown()
 
+
     def setup_services(self):
-        self.service_handler = DeployE2EServices(self.master.ip, "Administrator", "password")
         self.service_handler.deploy()
         self.booking_endpoint = self.service_handler.getBookingEndpoint()
         self.profile_endpoint = self.service_handler.getProfileEndpoint()
