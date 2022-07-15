063dc3a2b4670e44f4016c45a98baeac30c6e29d
riteshagarwalcouchbase <ritesh.agarwal@couchbase.com>
2022-07-15 13:33:49 -0700

    Import capella stuff only when capella flag is on.
    Change-Id: I2e828c343e23a1a344328ea325ea2ab5749b4c18
    Reviewed-on: https://review.couchbase.org/c/testrunner/+/177629
    Reviewed-by: Girish Benakappa <girish.benakappa@couchbase.com>
    Tested-by: Ritesh Agarwal <ritesh.agarwal@couchbase.com>

--- lib/membase/api/rest_client.py
+++ lib/membase/api/rest_client.py
@@ -1,8 +1,6 @@
-from membase.api.capella_rest_client import RestConnection as CapellaRestConnection
-from membase.api.on_prem_rest_client import RestConnection as OnPremRestConnection, RestHelper, Bucket, vBucket
 from TestInput import TestInputSingleton
 
 if TestInputSingleton.input and TestInputSingleton.input.param("capella_run", False):
-    RestConnection = CapellaRestConnection
+    from membase.api.capella_rest_client import RestConnection
 else:
-    RestConnection = OnPremRestConnection
+    from membase.api.on_prem_rest_client import RestConnection, RestHelper, Bucket, vBucket
