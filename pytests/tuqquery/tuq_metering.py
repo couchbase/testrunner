cb0a14601adbe39c2a037e87bace36ddbd8a2eca
pierrecouch <pierre.regazzoni@couchbase.com>
2022-07-27 11:58:02 -0700

    Update metering unit
    Change-Id: I873811f2eae306bc93c70fa612aff7d529ff14bb
    Reviewed-on: https://review.couchbase.org/c/testrunner/+/178062
    Tested-by: Couchbase QE
    Reviewed-by: Ajay Bhullar <ajay.bhullar@couchbase.com>

--- pytests/tuqquery/tuq_metering.py
+++ pytests/tuqquery/tuq_metering.py
@@ -25,11 +25,11 @@ class QueryMeteringTests(QueryTests):
     def suite_tearDown(self):
         super(QueryMeteringTests, self).suite_tearDown()
 
-    def get_metering_index(self, bucket):
+    def get_metering_index(self, bucket='default'):
         ru, wu = 0, 0
         api = f"http://{self.server.ip}:9102/_metering"
-        ru_pattern = re.compile('meter_ru_total{bucket="default",for="index",unbilled="",variant=""} (\d+)')
-        wu_pattern = re.compile('meter_wu_total{bucket="default",for="index",unbilled="",variant=""} (\d+)')
+        ru_pattern = re.compile(f'meter_ru_total{{bucket="{bucket}",for="index",unbilled="",variant=""}} (\d+)')
+        wu_pattern = re.compile(f'meter_wu_total{{bucket="{bucket}",for="index",unbilled="",variant=""}} (\d+)')
         response = requests.get(api)
         if ru_pattern.search(response.text):
             ru = int(ru_pattern.findall(response.text)[0])
@@ -37,12 +37,18 @@ class QueryMeteringTests(QueryTests):
             wu = int(wu_pattern.findall(response.text)[0])
         return ru, wu
 
-    def assert_cu(self, result, expected, cu="kvWU"):
-        if 'computeUnits' in result.keys() and cu in result['computeUnits'].keys():
-            actual = result['computeUnits'][cu]
-            self.assertEqual(actual, expected, f'Expected {expected} unit but got {actual}')
+    def assert_billing_unit(self, result, expected, unit="ru", service="kv"):
+        if 'billingUnits' in result.keys():
+            if unit in result['billingUnits'].keys():
+                if service in result['billingUnits'][unit].keys():
+                    actual = result['billingUnits'][unit][service]
+                    self.assertEqual(actual, expected, f'Expected {expected} unit but got {actual}')
+                else:
+                    self.fail(f"result['billingUnits'][{unit}] does not contain {service}, result['billingUnits'][{unit}] is: {result['billingUnits'][unit]}")
+            else:
+                self.fail(f"result['billingUnits'] does not contain {unit}, result['billingUnits'] is: {result['billingUnits']}")
         else:
-            self.fail(f'Result does not contain computeUnits or {cu} unit: {result}')
+            self.fail(f'result does not contain billingUnits, result is: {result}')
 
     def test_kv_insert(self):
         insert_query = f'INSERT INTO {self.bucket} (key k, value v) SELECT uuid() as k , {self.doc} as v FROM array_range(0,{self.doc_count}) d'
@@ -50,7 +56,7 @@ class QueryMeteringTests(QueryTests):
 
         expected_wu = self.doc_count * math.ceil( self.doc_size / self.kv_wu)
         result = self.run_cbq_query(insert_query)
-        self.assert_cu(result, expected_wu)
+        self.assert_billing_unit(result, expected_wu, unit='wu', service='kv')
 
     def test_kv_delete(self):
         insert_query = f'INSERT INTO {self.bucket} (key k, value v) SELECT uuid() as k , {self.doc} as v FROM array_range(0,{self.doc_count}) d'
@@ -59,7 +65,7 @@ class QueryMeteringTests(QueryTests):
 
         expected_wu = self.doc_count
         result = self.run_cbq_query(f'DELETE from {self.bucket}')
-        self.assert_cu(result, expected_wu)
+        self.assert_billing_unit(result, expected_wu, unit='wu', service='kv')
 
     def test_kv_update(self):
         insert_query = f'INSERT INTO {self.bucket} (key k, value v) SELECT uuid() as k , {{"name": "San Francisco"}} as v FROM array_range(0,{self.doc_count}) d'
@@ -72,8 +78,8 @@ class QueryMeteringTests(QueryTests):
         expected_wu = self.doc_count * math.ceil( doc_write_size / self.kv_wu)
         expected_ru = self.doc_count * math.ceil( doc_read_size / self.kv_ru)
         result = self.run_cbq_query(f'UPDATE {self.bucket} SET name = repeat("a", {self.value_size})')
-        self.assert_cu(result, expected_wu, cu="kvWU")
-        self.assert_cu(result, expected_ru, cu="kvRU")
+        self.assert_billing_unit(result, expected_wu, unit='wu', service='kv')
+        self.assert_billing_unit(result, expected_ru, unit='ru', service='kv')
 
     def test_kv_select(self):
         insert_query = f'INSERT INTO {self.bucket} (key k, value v) SELECT uuid() as k , {self.doc} as v FROM array_range(0,{self.doc_count}) d'
@@ -82,7 +88,7 @@ class QueryMeteringTests(QueryTests):
 
         expected_ru = self.doc_count * math.ceil( self.doc_size / self.kv_ru)
         result = self.run_cbq_query(f'SELECT * FROM {self.bucket}')
-        self.assert_cu(result, expected_ru, cu="kvRU")
+        self.assert_billing_unit(result, expected_ru, unit='ru', service='kv')
 
     def test_kv_select_count(self):
         insert_query = f'INSERT INTO {self.bucket} (key k, value v) SELECT uuid() as k , {self.doc} as v FROM array_range(0,{self.doc_count}) d'
@@ -91,12 +97,12 @@ class QueryMeteringTests(QueryTests):
 
         # for count(*) we should get this from meta and no need to read from KV
         result = self.run_cbq_query(f'SELECT count(*) as count FROM {self.bucket}')
-        self.assertTrue('computeUnits' not in result.keys(), f'We should not have got computeUnits from query')
+        self.assertTrue('billingUnits' not in result.keys() or 'ru' not in result['billingUnits'].keys(), f'We should not have got billingUnits[ru] from query')
         self.assertEqual(result['results'][0]['count'], self.doc_count, f'We expected {self.doc_count} but for diff result: {result}')
 
         # for count(field) we need to scan all KV doc
         expected_ru = self.doc_count * math.ceil( self.doc_size / self.kv_ru)
         result = self.run_cbq_query(f'SELECT count(name) as count FROM {self.bucket}')
-        self.assert_cu(result, expected_ru, cu="kvRU")
+        self.assert_billing_unit(result, expected_ru, unit='ru', service='kv')
         self.assertEqual(result['results'][0]['count'], self.doc_count, f'We expected {self.doc_count} but for diff result: {result}')
 
