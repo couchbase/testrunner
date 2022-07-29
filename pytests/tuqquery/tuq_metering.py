17393cc04108ee26bc8b8c1ce2c5d59d0fdcc879
pierrecouch <pierre.regazzoni@couchbase.com>
2022-07-28 17:05:49 -0700

    Add metering and throttling test cases
    Change-Id: Ic48f660260ce7c92d366f574b0cd8f1b5d544498
    Reviewed-on: https://review.couchbase.org/c/testrunner/+/178162
    Reviewed-by: Ajay Bhullar <ajay.bhullar@couchbase.com>
    Tested-by: Pierre Regazzoni <pierre.regazzoni@couchbase.com>

--- pytests/tuqquery/tuq_metering.py
+++ pytests/tuqquery/tuq_metering.py
@@ -10,11 +10,13 @@ class QueryMeteringTests(QueryTests):
         self.bucket = "default"
         self.doc_count = self.input.param("doc_count", 10)
         self.value_size = self.input.param("value_size", 256)
-        self.kv_wu = 1024
-        self.kv_ru = 4096
+        self.kv_wu, self.kv_ru = 1024, 4096
+        self.index_wu, self.index_ru = 1024, 256
         self.doc = {"name": "a"*self.value_size}
-        self.doc_size= 36 + len(json.dumps(self.doc)) # doc length is key (uuid) size + doc size
-        self.log.info(f'Doc count: {self.doc_count} - Doc size is: {self.doc_size}')
+        self.index_key_size = len ("a"*self.value_size) # index is on name
+        self.doc_key_size = 36 # use uuid()
+        self.doc_size= len(json.dumps(self.doc))
+        self.log.info(f'Doc count: {self.doc_count} - Doc key size is: {self.doc_key_size} - Doc size is: {self.doc_size}')
 
     def suite_setUp(self):
         super(QueryMeteringTests, self).suite_setUp()
@@ -27,14 +29,26 @@ class QueryMeteringTests(QueryTests):
 
     def get_metering_index(self, bucket='default'):
         ru, wu = 0, 0
-        api = f"http://{self.server.ip}:9102/_metering"
-        ru_pattern = re.compile(f'meter_ru_total{{bucket="{bucket}",for="index",unbilled="",variant=""}} (\d+)')
-        wu_pattern = re.compile(f'meter_wu_total{{bucket="{bucket}",for="index",unbilled="",variant=""}} (\d+)')
-        response = requests.get(api)
-        if ru_pattern.search(response.text):
-            ru = int(ru_pattern.findall(response.text)[0])
-        if wu_pattern.search(response.text):
-            wu = int(wu_pattern.findall(response.text)[0])
+        api = f"https://{self.server.ip}:19102/_metering"
+        idx_ru_pattern = re.compile(f'meter_ru_total{{bucket="{bucket}",for="index",unbilled="",variant=""}} (\d+)')
+        idx_wu_pattern = re.compile(f'meter_wu_total{{bucket="{bucket}",for="index",unbilled="",variant=""}} (\d+)')
+        response = requests.get(api, verify=False)
+        if idx_ru_pattern.search(response.text):
+            ru = int(idx_ru_pattern.findall(response.text)[0])
+        if idx_wu_pattern.search(response.text):
+            wu = int(idx_wu_pattern.findall(response.text)[0])
+        return ru, wu
+
+    def get_metering_kv(self, bucket='default'):
+        ru, wu = 0, 0
+        api = f"https://{self.server.ip}:18091/metrics"
+        kv_ru_pattern = re.compile(f'meter_ru_total{{bucket="{bucket}"}} (\d+)')
+        kv_wu_pattern = re.compile(f'meter_wu_total{{bucket="{bucket}"}} (\d+)')
+        response = requests.get(api, auth=requests.auth.HTTPBasicAuth(self.rest.username, self.rest.password), verify=False)
+        if kv_ru_pattern.search(response.text):
+            ru = int(kv_ru_pattern.findall(response.text)[0])
+        if kv_wu_pattern.search(response.text):
+            wu = int(kv_wu_pattern.findall(response.text)[0])
         return ru, wu
 
     def assert_billing_unit(self, result, expected, unit="ru", service="kv"):
@@ -42,7 +56,7 @@ class QueryMeteringTests(QueryTests):
             if unit in result['billingUnits'].keys():
                 if service in result['billingUnits'][unit].keys():
                     actual = result['billingUnits'][unit][service]
-                    self.assertEqual(actual, expected, f'Expected {expected} unit but got {actual}')
+                    self.assertEqual(actual, expected, f'Expected {expected} {service} {unit} unit but got {actual}')
                 else:
                     self.fail(f"result['billingUnits'][{unit}] does not contain {service}, result['billingUnits'][{unit}] is: {result['billingUnits'][unit]}")
             else:
@@ -54,7 +68,7 @@ class QueryMeteringTests(QueryTests):
         insert_query = f'INSERT INTO {self.bucket} (key k, value v) SELECT uuid() as k , {self.doc} as v FROM array_range(0,{self.doc_count}) d'
         self.run_cbq_query(f'DELETE from {self.bucket}')
 
-        expected_wu = self.doc_count * math.ceil( self.doc_size / self.kv_wu)
+        expected_wu = self.doc_count * math.ceil( (self.doc_size + self.doc_key_size) / self.kv_wu)
         result = self.run_cbq_query(insert_query)
         self.assert_billing_unit(result, expected_wu, unit='wu', service='kv')
 
@@ -72,8 +86,8 @@ class QueryMeteringTests(QueryTests):
         self.run_cbq_query(f'DELETE from {self.bucket}')
         self.run_cbq_query(insert_query)
 
-        doc_read_size = 36 + len(json.dumps({"name": "San Francisco"}))
-        doc_write_size = 36 + len(json.dumps({"name": "a"*self.value_size}))
+        doc_read_size = self.doc_key_size + len(json.dumps({"name": "San Francisco"}))
+        doc_write_size = self.doc_key_size + len(json.dumps({"name": "a"*self.value_size}))
 
         expected_wu = self.doc_count * math.ceil( doc_write_size / self.kv_wu)
         expected_ru = self.doc_count * math.ceil( doc_read_size / self.kv_ru)
@@ -86,7 +100,7 @@ class QueryMeteringTests(QueryTests):
         self.run_cbq_query(f'DELETE from {self.bucket}')
         self.run_cbq_query(insert_query)
 
-        expected_ru = self.doc_count * math.ceil( self.doc_size / self.kv_ru)
+        expected_ru = self.doc_count * math.ceil( (self.doc_size + self.doc_key_size) / self.kv_ru)
         result = self.run_cbq_query(f'SELECT * FROM {self.bucket}')
         self.assert_billing_unit(result, expected_ru, unit='ru', service='kv')
 
@@ -101,8 +115,43 @@ class QueryMeteringTests(QueryTests):
         self.assertEqual(result['results'][0]['count'], self.doc_count, f'We expected {self.doc_count} but for diff result: {result}')
 
         # for count(field) we need to scan all KV doc
-        expected_ru = self.doc_count * math.ceil( self.doc_size / self.kv_ru)
+        expected_ru = self.doc_count * math.ceil( (self.doc_size + self.doc_key_size) / self.kv_ru)
         result = self.run_cbq_query(f'SELECT count(name) as count FROM {self.bucket}')
         self.assert_billing_unit(result, expected_ru, unit='ru', service='kv')
         self.assertEqual(result['results'][0]['count'], self.doc_count, f'We expected {self.doc_count} but for diff result: {result}')
+
+    def test_gsi_select(self):
+        insert_query = f'INSERT INTO {self.bucket} (key k, value v) SELECT uuid() as k , {self.doc} as v FROM array_range(0,{self.doc_count}) d'
+        self.run_cbq_query(f'DELETE from {self.bucket}')
+        self.run_cbq_query(insert_query)
+
+        self.run_cbq_query(f'CREATE INDEX idx_name IF NOT EXISTS on {self.bucket}(name)')
+        self.wait_for_all_indexes_online()
+
+        expected_kv_ru = self.doc_count * math.ceil( (self.doc_size + self.doc_key_size) / self.kv_ru)
+        expected_index_ru = math.ceil(self.doc_count * (self.doc_key_size + self.index_key_size) / self.index_ru)
+        result = self.run_cbq_query(f'SELECT name, city FROM {self.bucket} WHERE name = repeat("a", {self.value_size})')
+        self.assert_billing_unit(result, expected_kv_ru, unit='ru', service = 'kv')
+        self.assert_billing_unit(result, expected_index_ru, unit='ru', service = 'index')
+
+    def test_gsi_create(self):
+        insert_query = f'INSERT INTO {self.bucket} (key k, value v) SELECT uuid() as k , {self.doc} as v FROM array_range(0,{self.doc_count}) d'
+        self.run_cbq_query(f'DELETE from {self.bucket}')
+        self.run_cbq_query(f'DROP INDEX idx_name IF EXISTS on {self.bucket}')
+        self.run_cbq_query(insert_query)
+
+        before_index_ru, before_index_wu = self.get_metering_index(self.bucket)
+        before_kv_ru, before_kv_wu = self.get_metering_kv(self.bucket)
+        self.run_cbq_query(f'CREATE INDEX idx_name on {self.bucket}(name)')
+        self.wait_for_all_indexes_online()
+        after_index_ru, after_index_wu = self.get_metering_index(self.bucket)
+        after_kv_ru, after_kv_wu = self.get_metering_kv(self.bucket)
+
+        expected_index_wu = self.doc_count * math.ceil((self.doc_key_size + self.index_key_size) / self.index_wu)
+        expected_kv_ru = self.doc_count * math.ceil( (self.doc_size + self.doc_key_size) / self.kv_ru)
+
+        self.assertEqual(before_index_ru, after_index_ru) # no index ru
+        self.assertEqual(expected_index_wu, after_index_wu - before_index_wu)
+        self.assertEqual(before_kv_wu, after_kv_wu) # no kv wu
+        self.assertEqual(expected_kv_ru, after_kv_ru - before_kv_ru)
 
---
+++ pytests/tuqquery/tuq_throttling.py
@@ -0,0 +1,93 @@
+from .tuq import QueryTests
+import re
+import requests
+
+class QueryThrottlingTests(QueryTests):
+    def setUp(self):
+        super(QueryThrottlingTests, self).setUp()
+        self.bucket = "default"
+        self.kv_wu = 1024
+        self.kv_ru = 4096
+        self.kvThrottleLimit = self.get_throttle_limits(service='kvThrottleLimit')
+        self.log.info(f'kvThrottleLimit is {self.kvThrottleLimit}')
+
+    def suite_setUp(self):
+        super(QueryThrottlingTests, self).suite_setUp()
+
+    def tearDown(self):
+        super(QueryThrottlingTests, self).tearDown()
+
+    def suite_tearDown(self):
+        super(QueryThrottlingTests, self).suite_tearDown()
+
+    def get_throttle_limits(self, service='kvThrottleLimit'):
+        api = f"https://{self.server.ip}:18091/settings/throttle"
+        response = requests.get(api, auth = requests.auth.HTTPBasicAuth(self.rest.username, self.rest.password), verify=False)
+        throttle_limits = response.json()
+        return throttle_limits[service]
+
+    def set_throttle_limits(self, value=5000, service='kvThrottleLimit'):
+        api = f"https://{self.server.ip}:18091/settings/throttle"
+        data = {}
+        data[service] = value
+        response = requests.post(api, data=data, auth = requests.auth.HTTPBasicAuth(self.rest.username, self.rest.password), verify=False)
+        if response.status_code not in (200,201):
+            self.fail(f'Fail to set throttle limits: {response.text}')
+
+    def get_throttling_metrics(self, bucket='default', service='kv'):
+        throttle_count_total, throttle_seconds_total = 0, 0
+        api = f"https://{self.server.ip}:18091/metrics"
+        throttle_seconds_pattern = re.compile(f'throttle_seconds_total{{bucket="{bucket}",for="{service}"}} (\d+)')
+        throttle_count_pattern = re.compile(f'throttle_count_total{{bucket="{bucket}",for="{service}"}} (\d+)')
+        response = requests.get(api, auth = requests.auth.HTTPBasicAuth(self.rest.username, self.rest.password), verify=False)
+        if throttle_seconds_pattern.search(response.text):
+            throttle_seconds_total = int(throttle_seconds_pattern.findall(response.text)[0])
+        if throttle_count_pattern.search(response.text):
+            throttle_count_total = int(throttle_count_pattern.findall(response.text)[0])
+        return throttle_count_total, throttle_seconds_total
+
+    def test_kv_insert(self):
+        before_throttle_count_total, before_throttle_seconds_total = self.get_throttling_metrics()
+        self.log.info(f'Throttle before, count: {before_throttle_count_total}, seconds: {before_throttle_seconds_total}')
+        self.run_cbq_query(f'INSERT INTO {self.bucket} (key k, value v) SELECT uuid() as k , {{"city": repeat("a", {self.kv_wu})}} as v FROM array_range(0,{self.kvThrottleLimit}) d')
+        after_throttle_count_total, after_throttle_seconds_total = self.get_throttling_metrics()
+        self.log.info(f'Throttle after, count: {after_throttle_count_total}, seconds: {after_throttle_seconds_total}')
+        self.assertTrue(after_throttle_count_total > before_throttle_count_total)
+
+    def test_kv_select(self):
+        self.run_cbq_query(f'DELETE from {self.bucket}')
+        self.run_cbq_query(f'INSERT INTO {self.bucket} (key k, value v) SELECT uuid() as k , {{"city": repeat("a", {self.kv_ru})}} as v FROM array_range(0,{self.kvThrottleLimit}) d')
+        before_throttle_count_total, before_throttle_seconds_total = self.get_throttling_metrics()
+        self.log.info(f'Throttle before, count: {before_throttle_count_total}, seconds: {before_throttle_seconds_total}')
+        self.sleep(3)
+        self.run_cbq_query(f'SELECT count(city) FROM {self.bucket}')
+        after_throttle_count_total, after_throttle_seconds_total = self.get_throttling_metrics()
+        self.log.info(f'Throttle after, count: {after_throttle_count_total}, seconds: {after_throttle_seconds_total}')
+        self.assertTrue(after_throttle_count_total > before_throttle_count_total)
+
+    def test_kv_below_throttle(self):
+        before_throttle_count_total, before_throttle_seconds_total = self.get_throttling_metrics()
+        self.log.info(f'Throttle before, count: {before_throttle_count_total}, seconds: {before_throttle_seconds_total}')
+        self.run_cbq_query(f'INSERT INTO {self.bucket} (key k, value v) SELECT uuid() as k , {{"city": repeat("a", {self.kv_wu})}} as v FROM array_range(0,{self.kvThrottleLimit/10}) d')
+        after_throttle_count_total, after_throttle_seconds_total = self.get_throttling_metrics()
+        self.log.info(f'Throttle after, count: {after_throttle_count_total}, seconds: {after_throttle_seconds_total}')
+        self.assertEqual(after_throttle_count_total, before_throttle_count_total)
+
+    def test_kv_set_throttle(self):
+        self.run_cbq_query(f'DELETE from {self.bucket}')
+        self.run_cbq_query(f'INSERT INTO {self.bucket} (key k, value v) SELECT uuid() as k , {{"city": repeat("a", {self.kv_ru})}} as v FROM array_range(0,{self.kvThrottleLimit}) d')
+
+        self.set_throttle_limits(value=int(self.kvThrottleLimit*30/100), service='kvThrottleLimit')
+        result = self.run_cbq_query(f'SELECT count(city) FROM {self.bucket}')
+        execution_time_at_30pct = float(result['metrics']['executionTime'][:-1])
+
+        self.set_throttle_limits(value=self.kvThrottleLimit, service='kvThrottleLimit')
+        result = self.run_cbq_query(f'SELECT count(city) FROM {self.bucket}')
+        execution_time_at_100pct = float(result['metrics']['executionTime'][:-1])
+
+        # We expect the execution with higher throttle level (100pct) to be faster than the run at lower
+        # throttle level (30pct) by some factor, in this case 1.75, to be within margin as we cannot 
+        # garantee expected execution time to be consistant.
+        self.assertTrue( execution_time_at_30pct > execution_time_at_100pct * 1.75 )
+        
+
