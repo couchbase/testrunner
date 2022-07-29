b34cc53b3632388dd6c40978103b748c3cdc2add
pierrecouch <pierre.regazzoni@couchbase.com>
2022-07-29 10:02:48 -0700

    Add gsi throttling test cases
    Change-Id: I4e75dbc23d529189b2bbe5f69e0e821a6d0c870c
    Reviewed-on: https://review.couchbase.org/c/testrunner/+/178218
    Reviewed-by: Ajay Bhullar <ajay.bhullar@couchbase.com>
    Tested-by: Pierre Regazzoni <pierre.regazzoni@couchbase.com>

--- pytests/tuqquery/tuq_throttling.py
+++ pytests/tuqquery/tuq_throttling.py
@@ -6,8 +6,8 @@ class QueryThrottlingTests(QueryTests):
     def setUp(self):
         super(QueryThrottlingTests, self).setUp()
         self.bucket = "default"
-        self.kv_wu = 1024
-        self.kv_ru = 4096
+        self.kv_wu, self.kv_ru = 1024, 4096
+        self.index_wu, self.index_ru = 1024, 256
         self.kvThrottleLimit = self.get_throttle_limits(service='kvThrottleLimit')
         self.log.info(f'kvThrottleLimit is {self.kvThrottleLimit}')
 
@@ -37,8 +37,8 @@ class QueryThrottlingTests(QueryTests):
     def get_throttling_metrics(self, bucket='default', service='kv'):
         throttle_count_total, throttle_seconds_total = 0, 0
         api = f"https://{self.server.ip}:18091/metrics"
-        throttle_seconds_pattern = re.compile(f'throttle_seconds_total{{bucket="{bucket}",for="{service}"}} (\d+)')
-        throttle_count_pattern = re.compile(f'throttle_count_total{{bucket="{bucket}",for="{service}"}} (\d+)')
+        throttle_seconds_pattern = re.compile(f'throttle_seconds_total{{bucket="{bucket}",for="{service}".*}} (\d+)')
+        throttle_count_pattern = re.compile(f'throttle_count_total{{bucket="{bucket}",for="{service}".*}} (\d+)')
         response = requests.get(api, auth = requests.auth.HTTPBasicAuth(self.rest.username, self.rest.password), verify=False)
         if throttle_seconds_pattern.search(response.text):
             throttle_seconds_total = int(throttle_seconds_pattern.findall(response.text)[0])
@@ -89,5 +89,29 @@ class QueryThrottlingTests(QueryTests):
         # throttle level (30pct) by some factor, in this case 1.75, to be within margin as we cannot 
         # garantee expected execution time to be consistant.
         self.assertTrue( execution_time_at_30pct > execution_time_at_100pct * 1.75 )
-        
+
+    def test_gsi_create(self):
+        self.run_cbq_query(f'DELETE from {self.bucket}')
+        self.run_cbq_query(f'DROP INDEX idx_name IF EXISTS on {self.bucket}')
+        self.run_cbq_query(f'INSERT INTO {self.bucket} (key k, value v) SELECT uuid() as k , {{"city": repeat("a", {self.index_wu})}} as v FROM array_range(0,{self.kvThrottleLimit}) d')
+
+        before_throttle_count_total, before_throttle_seconds_total = self.get_throttling_metrics(service='index')
+        self.log.info(f'Throttle before, count: {before_throttle_count_total}, seconds: {before_throttle_seconds_total}')
+        self.run_cbq_query(f'CREATE INDEX idx_name on {self.bucket}(city)')
+        self.wait_for_all_indexes_online
+        after_throttle_count_total, after_throttle_seconds_total = self.get_throttling_metrics(service='index')
+        self.log.info(f'Throttle after, count: {after_throttle_count_total}, seconds: {after_throttle_seconds_total}')
+        self.assertTrue(after_throttle_count_total > before_throttle_count_total)
+
+    def test_gsi_below_throttle(self):
+        self.run_cbq_query(f'DELETE from {self.bucket}')
+        self.run_cbq_query(f'DROP INDEX idx_name IF EXISTS on {self.bucket}')
+        self.run_cbq_query(f'INSERT INTO {self.bucket} (key k, value v) SELECT uuid() as k , {{"city": repeat("a", {self.kv_wu})}} as v FROM array_range(0,{self.kvThrottleLimit/10}) d')
+
+        before_throttle_count_total, before_throttle_seconds_total = self.get_throttling_metrics()
+        self.log.info(f'Throttle before, count: {before_throttle_count_total}, seconds: {before_throttle_seconds_total}')
+        self.run_cbq_query(f'CREATE INDEX idx_name on {self.bucket}(city)')
+        after_throttle_count_total, after_throttle_seconds_total = self.get_throttling_metrics()
+        self.log.info(f'Throttle after, count: {after_throttle_count_total}, seconds: {after_throttle_seconds_total}')
+        self.assertEqual(after_throttle_count_total, before_throttle_count_total)
 
