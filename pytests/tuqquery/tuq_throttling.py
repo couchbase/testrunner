57c41c82fdbc055b7f6fa42e1ef02b9653f5515d
pierrecouch <pierre.regazzoni@couchbase.com>
2022-10-13 15:06:54 -0700

    Storage Limit test cases
    Change-Id: Ia9ffe3d674c034a783a38c876fedf7f6c2e5c34a
    Reviewed-on: https://review.couchbase.org/c/testrunner/+/181321
    Reviewed-by: Ajay Bhullar <ajay.bhullar@couchbase.com>
    Tested-by: Pierre Regazzoni <pierre.regazzoni@couchbase.com>

--- pytests/tuqquery/tuq_throttling.py
+++ pytests/tuqquery/tuq_throttling.py
@@ -1,6 +1,5 @@
 from .tuq import QueryTests
-import re
-import requests
+from lib.metering_throttling import throttling
 from membase.api.exception import CBQError
 
 class QueryThrottlingTests(QueryTests):
@@ -11,8 +10,9 @@ class QueryThrottlingTests(QueryTests):
         self.index_wu, self.index_ru = 1024, 256
         self.scope_limit, self.collection_limit = 100, 100
         self.gsi_limit = 200
-        self.kvThrottleLimit = self.get_throttle_limits(service='kvThrottleLimit')
-        self.log.info(f'kvThrottleLimit is {self.kvThrottleLimit}')
+        self.test_limit=self.input.param("test_limit", 'indexStorageLimit')
+        self.throttle = throttling(self.server.ip, self.rest.username, self.rest.password)
+        self.kvThrottleLimit = 5000
 
     def suite_setUp(self):
         super(QueryThrottlingTests, self).suite_setUp()
@@ -23,56 +23,30 @@ class QueryThrottlingTests(QueryTests):
     def suite_tearDown(self):
         super(QueryThrottlingTests, self).suite_tearDown()
 
-    def get_throttle_limits(self, service='kvThrottleLimit'):
-        api = f"https://{self.server.ip}:18091/settings/throttle"
-        response = requests.get(api, auth = requests.auth.HTTPBasicAuth(self.rest.username, self.rest.password), verify=False)
-        throttle_limits = response.json()
-        return throttle_limits[service]
-
-    def set_throttle_limits(self, value=5000, service='kvThrottleLimit'):
-        api = f"https://{self.server.ip}:18091/settings/throttle"
-        data = {}
-        data[service] = value
-        response = requests.post(api, data=data, auth = requests.auth.HTTPBasicAuth(self.rest.username, self.rest.password), verify=False)
-        if response.status_code not in (200,201):
-            self.fail(f'Fail to set throttle limits: {response.text}')
-
-    def get_throttling_metrics(self, bucket='default', service='kv'):
-        throttle_count_total, throttle_seconds_total = 0, 0
-        api = f"https://{self.server.ip}:18091/metrics"
-        throttle_seconds_pattern = re.compile(f'throttle_seconds_total{{bucket="{bucket}",for="{service}".*}} (\d+)')
-        throttle_count_pattern = re.compile(f'throttle_count_total{{bucket="{bucket}",for="{service}".*}} (\d+)')
-        response = requests.get(api, auth = requests.auth.HTTPBasicAuth(self.rest.username, self.rest.password), verify=False)
-        if throttle_seconds_pattern.search(response.text):
-            throttle_seconds_total = int(throttle_seconds_pattern.findall(response.text)[0])
-        if throttle_count_pattern.search(response.text):
-            throttle_count_total = int(throttle_count_pattern.findall(response.text)[0])
-        return throttle_count_total, throttle_seconds_total
-
     def test_kv_insert(self):
-        before_throttle_count_total, before_throttle_seconds_total = self.get_throttling_metrics()
+        before_throttle_count_total, before_throttle_seconds_total = self.throttle.get_metrics()
         self.log.info(f'Throttle before, count: {before_throttle_count_total}, seconds: {before_throttle_seconds_total}')
         self.run_cbq_query(f'INSERT INTO {self.bucket} (key k, value v) SELECT uuid() as k , {{"city": repeat("a", {self.kv_wu})}} as v FROM array_range(0,{self.kvThrottleLimit}) d')
-        after_throttle_count_total, after_throttle_seconds_total = self.get_throttling_metrics()
+        after_throttle_count_total, after_throttle_seconds_total = self.throttle.get_metrics()
         self.log.info(f'Throttle after, count: {after_throttle_count_total}, seconds: {after_throttle_seconds_total}')
         self.assertTrue(after_throttle_count_total > before_throttle_count_total)
 
     def test_kv_select(self):
         self.run_cbq_query(f'DELETE from {self.bucket}')
         self.run_cbq_query(f'INSERT INTO {self.bucket} (key k, value v) SELECT uuid() as k , {{"city": repeat("a", {self.kv_ru})}} as v FROM array_range(0,{self.kvThrottleLimit}) d')
-        before_throttle_count_total, before_throttle_seconds_total = self.get_throttling_metrics()
+        before_throttle_count_total, before_throttle_seconds_total = self.throttle.get_metrics()
         self.log.info(f'Throttle before, count: {before_throttle_count_total}, seconds: {before_throttle_seconds_total}')
         self.sleep(3)
         self.run_cbq_query(f'SELECT count(city) FROM {self.bucket}')
-        after_throttle_count_total, after_throttle_seconds_total = self.get_throttling_metrics()
+        after_throttle_count_total, after_throttle_seconds_total = self.throttle.get_metrics()
         self.log.info(f'Throttle after, count: {after_throttle_count_total}, seconds: {after_throttle_seconds_total}')
         self.assertTrue(after_throttle_count_total > before_throttle_count_total)
 
     def test_kv_below_throttle(self):
-        before_throttle_count_total, before_throttle_seconds_total = self.get_throttling_metrics()
+        before_throttle_count_total, before_throttle_seconds_total = self.throttle.get_metrics()
         self.log.info(f'Throttle before, count: {before_throttle_count_total}, seconds: {before_throttle_seconds_total}')
         self.run_cbq_query(f'INSERT INTO {self.bucket} (key k, value v) SELECT uuid() as k , {{"city": repeat("a", {self.kv_wu})}} as v FROM array_range(0,{self.kvThrottleLimit/10}) d')
-        after_throttle_count_total, after_throttle_seconds_total = self.get_throttling_metrics()
+        after_throttle_count_total, after_throttle_seconds_total = self.throttle.get_metrics()
         self.log.info(f'Throttle after, count: {after_throttle_count_total}, seconds: {after_throttle_seconds_total}')
         self.assertEqual(after_throttle_count_total, before_throttle_count_total)
 
@@ -80,11 +54,11 @@ class QueryThrottlingTests(QueryTests):
         self.run_cbq_query(f'DELETE from {self.bucket}')
         self.run_cbq_query(f'INSERT INTO {self.bucket} (key k, value v) SELECT uuid() as k , {{"city": repeat("a", {self.kv_ru})}} as v FROM array_range(0,{self.kvThrottleLimit}) d')
 
-        self.set_throttle_limits(value=int(self.kvThrottleLimit*30/100), service='kvThrottleLimit')
+        self.throttle.set_bucket_limit(value=int(self.kvThrottleLimit*30/100), service='kvThrottleLimit')
         result = self.run_cbq_query(f'SELECT count(city) FROM {self.bucket}')
         execution_time_at_30pct = float(result['metrics']['executionTime'][:-1])
 
-        self.set_throttle_limits(value=self.kvThrottleLimit, service='kvThrottleLimit')
+        self.throttle.set_bucket_limit(value=self.kvThrottleLimit, service='kvThrottleLimit')
         result = self.run_cbq_query(f'SELECT count(city) FROM {self.bucket}')
         execution_time_at_100pct = float(result['metrics']['executionTime'][:-1])
 
@@ -98,11 +72,11 @@ class QueryThrottlingTests(QueryTests):
         self.run_cbq_query(f'DROP INDEX idx_name IF EXISTS on {self.bucket}')
         self.run_cbq_query(f'INSERT INTO {self.bucket} (key k, value v) SELECT uuid() as k , {{"city": repeat("a", {self.index_wu})}} as v FROM array_range(0,{self.kvThrottleLimit}) d')
 
-        before_throttle_count_total, before_throttle_seconds_total = self.get_throttling_metrics(service='index')
+        before_throttle_count_total, before_throttle_seconds_total = self.throttle.get_metrics(service='index')
         self.log.info(f'Throttle before, count: {before_throttle_count_total}, seconds: {before_throttle_seconds_total}')
         self.run_cbq_query(f'CREATE INDEX idx_name on {self.bucket}(city)')
         self.wait_for_all_indexes_online
-        after_throttle_count_total, after_throttle_seconds_total = self.get_throttling_metrics(service='index')
+        after_throttle_count_total, after_throttle_seconds_total = self.throttle.get_metrics(service='index')
         self.log.info(f'Throttle after, count: {after_throttle_count_total}, seconds: {after_throttle_seconds_total}')
         self.assertTrue(after_throttle_count_total > before_throttle_count_total)
 
@@ -111,10 +85,10 @@ class QueryThrottlingTests(QueryTests):
         self.run_cbq_query(f'DROP INDEX idx_name IF EXISTS on {self.bucket}')
         self.run_cbq_query(f'INSERT INTO {self.bucket} (key k, value v) SELECT uuid() as k , {{"city": repeat("a", {self.kv_wu})}} as v FROM array_range(0,{self.kvThrottleLimit/10}) d')
 
-        before_throttle_count_total, before_throttle_seconds_total = self.get_throttling_metrics()
+        before_throttle_count_total, before_throttle_seconds_total = self.throttle.get_metrics()
         self.log.info(f'Throttle before, count: {before_throttle_count_total}, seconds: {before_throttle_seconds_total}')
         self.run_cbq_query(f'CREATE INDEX idx_name on {self.bucket}(city)')
-        after_throttle_count_total, after_throttle_seconds_total = self.get_throttling_metrics()
+        after_throttle_count_total, after_throttle_seconds_total = self.throttle.get_metrics()
         self.log.info(f'Throttle after, count: {after_throttle_count_total}, seconds: {after_throttle_seconds_total}')
         self.assertEqual(after_throttle_count_total, before_throttle_count_total)
 
@@ -160,4 +134,37 @@ class QueryThrottlingTests(QueryTests):
             self.assertEqual(error['msg'], f'GSI CreateIndex() - cause: Limit for number of indexes that can be created per bucket has been reached. Limit : {self.gsi_limit}')
         finally:
             for i in range(self.gsi_limit):
+                self.run_cbq_query(f'DROP INDEX idx{i} IF EXISTS ON {self.bucket}')
+
+    def test_limit_storage(self):
+        self.run_cbq_query(f'INSERT INTO {self.bucket} (key k, value v) SELECT uuid() as k , {{"city": "Paris"}} as v FROM array_range(0,1000) d')
+
+        # Set stroage limit to 0 to simulate we have reached limit
+        self.throttle.set_bucket_limit(bucket=self.bucket, value=0, service=self.test_limit)
+        current_limit = self.throttle.get_bucket_limit(bucket=self.bucket, service=self.test_limit)
+        self.assertEqual(current_limit, 0)
+
+        limits = {
+            'indexStorageLimit': {
+                'statement': f'CREATE INDEX index_name ON {self.bucket}(a)',
+                'code': 4350,
+                'msg': 'GSI CreateIndex() - cause: Bucket\'s disk size limit has been reached.'},
+            'dataStorageLimit': { # Error code/message might need to be changed based on MB-54113
+                'statement': f'INSERT INTO {self.bucket} (key, value) values (uuid(), {{"city": "Lyon"}})',
+                'code': 12009,
+                'msg': 'DML Error, possible causes include concurrent modification. Failed to perform INSERT'}
+        }
+        try:
+            # Repeat operation until we hit limit as setting limit is not immediate
+            for i in range(30):
+                self.run_cbq_query(limits[self.test_limit]['statement'].replace('index_name', f'idx{i}'))
+                self.sleep(i)
+            self.fail(f'We should have error out at this point since storage limit should be reached')
+        except CBQError as ex:
+            error = self.process_CBQE(ex)
+            self.assertEqual(error['code'], limits[self.test_limit]['code'])
+            self.assertEqual(error['msg'][:84], limits[self.test_limit]['msg'])
+        finally:
+            self.throttle.set_bucket_limit(bucket=self.bucket, value=500, service=self.test_limit)
+            for i in range(20):
                 self.run_cbq_query(f'DROP INDEX idx{i} IF EXISTS ON {self.bucket}')
