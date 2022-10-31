3225420ff0cf571c64d0cf4d3aad52be136448b9
pierrecouch <pierre.regazzoni@couchbase.com>
2022-10-31 13:40:26 -0700

    Add provisioned sanity tests
    Change-Id: I80c96e3e65ff5d941a6c80f50be689b5d3e319b3
    Reviewed-on: https://review.couchbase.org/c/testrunner/+/182113
    Reviewed-by: Girish Benakappa <girish.benakappa@couchbase.com>
    Tested-by: Pierre Regazzoni <pierre.regazzoni@couchbase.com>

--- pytests/capella_basetestcase.py
+++ pytests/capella_basetestcase.py
@@ -1,14 +1,15 @@
-from couchbase_helper.data_analysis_helper import DataAnalysisResultAnalyzer, DataAnalyzer, DataCollector
+from lib.couchbase_helper.data_analysis_helper import DataAnalysisResultAnalyzer, DataAnalyzer, DataCollector
 from lib.Cb_constants.CBServer import CbServer
 from basetestcase import OnPremBaseTestCase
 import copy
-from cluster_config import ClusterConfig
-from membase.helper.bucket_helper import BucketOperationHelper
+from lib.cluster_config import ClusterConfig
+from lib.membase.helper.bucket_helper import BucketOperationHelper
 from lib.capella.utils import CapellaAPI, CapellaCredentials
 import lib.capella.utils as capella_utils
 from TestInput import TestInputServer, TestInputSingleton
-from couchbase_helper.cluster import Cluster
-import logger
+from lib.couchbase_helper.cluster import Cluster
+from lib import logger
+
 class BaseTestCase(OnPremBaseTestCase):
     def setUp(self):
         try:
@@ -35,47 +36,41 @@ class BaseTestCase(OnPremBaseTestCase):
 
             capella_credentials = CapellaCredentials(self.input.capella)
             CbServer.capella_credentials = capella_credentials
-
             self.capella_api = CapellaAPI(capella_credentials)
 
-            cluster_details = self.create_capella_config()
+            if CbServer.capella_cluster_id is None:
+                cluster_details = self.create_capella_config()
+                cluster_id = self.capella_api.create_cluster_and_wait(cluster_details)
+                CbServer.capella_cluster_id = cluster_id
+                self.capella_api.create_db_user(cluster_id, self.input.membase_settings.rest_username, self.input.membase_settings.rest_password)
+                self.cluster_id = CbServer.capella_cluster_id
+                servers = self.capella_api.get_nodes_formatted(self.cluster_id, self.input.membase_settings.rest_username, self.input.membase_settings.rest_password)
+                self.cluster_config = ClusterConfig(self.input.membase_settings.rest_username, self.input.membase_settings.rest_password, self.create_input_servers())
+                self.cluster_config.update_servers(servers)
 
-            cluster_id = self.capella_api.create_cluster_and_wait(cluster_details)
-
-            self.cluster_id = cluster_id
-            CbServer.capella_cluster_id = cluster_id
-
-            self.capella_api.create_db_user(self.cluster_id, self.input.membase_settings.rest_username, self.input.membase_settings.rest_password)
-
-            servers = self.capella_api.get_nodes_formatted(self.cluster_id, self.input.membase_settings.rest_username, self.input.membase_settings.rest_password)
-
-            self.cluster_config = ClusterConfig(self.input.membase_settings.rest_username, self.input.membase_settings.rest_password, self.create_input_servers())
-            self.cluster_config.update_servers(servers)
-
-            if not self.skip_buckets_handle:
-                BucketOperationHelper.delete_all_buckets_or_assert(self.servers, self)
-
-            self.bucket_base_params = {'membase': {}}
-        
-            shared_params = self._create_bucket_params(server=self.master, size=self.bucket_size,
-                                                    replicas=self.num_replicas,
-                                                    enable_replica_index=self.enable_replica_index,
-                                                    eviction_policy=self.eviction_policy, bucket_priority=None,
-                                                    lww=self.lww, maxttl=self.maxttl,
-                                                    compression_mode=self.compression_mode)
-
-            membase_params = copy.deepcopy(shared_params)
-            membase_params['bucket_type'] = 'membase'
-            self.bucket_base_params['membase']['non_ephemeral'] = membase_params
-
-            membase_ephemeral_params = copy.deepcopy(shared_params)
-            membase_ephemeral_params['bucket_type'] = 'ephemeral'
-            self.bucket_base_params['membase']['ephemeral'] = membase_ephemeral_params
-
-            self.bucket_base_params['membase']['non_ephemeral']['size'] = self.bucket_size
-            self.bucket_base_params['membase']['ephemeral']['size'] = self.bucket_size
-
-            self._bucket_creation()
+                if not self.skip_buckets_handle:
+                    BucketOperationHelper.delete_all_buckets_or_assert(self.servers, self)
+                self.bucket_base_params = {'membase': {}}
+                shared_params = self._create_bucket_params(server=self.master, size=self.bucket_size,
+                                                        replicas=self.num_replicas,
+                                                        enable_replica_index=self.enable_replica_index,
+                                                        eviction_policy=self.eviction_policy, bucket_priority=None,
+                                                        lww=self.lww, maxttl=self.maxttl,
+                                                        compression_mode=self.compression_mode)
+                membase_params = copy.deepcopy(shared_params)
+                membase_params['bucket_type'] = 'membase'
+                self.bucket_base_params['membase']['non_ephemeral'] = membase_params
+                membase_ephemeral_params = copy.deepcopy(shared_params)
+                membase_ephemeral_params['bucket_type'] = 'ephemeral'
+                self.bucket_base_params['membase']['ephemeral'] = membase_ephemeral_params
+                self.bucket_base_params['membase']['non_ephemeral']['size'] = self.bucket_size
+                self.bucket_base_params['membase']['ephemeral']['size'] = self.bucket_size
+                self._bucket_creation()
+            else:
+                self.cluster_id = CbServer.capella_cluster_id
+                servers = self.capella_api.get_nodes_formatted(self.cluster_id, self.input.membase_settings.rest_username, self.input.membase_settings.rest_password)
+                self.cluster_config = ClusterConfig(self.input.membase_settings.rest_username, self.input.membase_settings.rest_password, self.create_input_servers())
+                self.cluster_config.update_servers(servers)
         except Exception:
             self.tearDown()
             raise
@@ -83,6 +78,7 @@ class BaseTestCase(OnPremBaseTestCase):
     def tearDown(self):
         # cluster was created during setup so destroy it
         if hasattr(self, 'cluster_id'):
+            CbServer.capella_cluster_id = None
             self.capella_api.delete_cluster(self.cluster_id)
 
     def create_capella_config(self):
