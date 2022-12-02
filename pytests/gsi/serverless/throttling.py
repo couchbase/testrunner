d67abae881e3a9f4b1e494d872b849acf331448e
Ajay Bhullar <ajay.bhullar@couchbase.com>
2022-12-01 18:57:41 -0800

    Add new tests for 2i throttling tests.
    Change-Id: I88c5326781b782a61e32018015c4861624395772
    Reviewed-on: https://review.couchbase.org/c/testrunner/+/183586
    Reviewed-by: Pierre Regazzoni <pierre.regazzoni@couchbase.com>
    Tested-by: Ajay Bhullar <ajay.bhullar@couchbase.com>

--- pytests/gsi/serverless/throttling.py
+++ pytests/gsi/serverless/throttling.py
@@ -1,16 +1,21 @@
 from serverless.serverless_basetestcase import ServerlessBaseTestCase
 from lib.metering_throttling import throttling
+from TestInput import TestInputSingleton
 import random, string
 import threading
+from membase.api.exception import CBQError
 import time
 
 class GSIThrottle(ServerlessBaseTestCase):
     def setUp(self):
+        self.input = TestInputSingleton.input
         self.doc_count = 10
         self.scope = '_default'
         self.collection = '_default'
+        self.gsi_limit = 200
         self.doc_value = ''.join(random.choices(string.ascii_lowercase, k=5048))
         self.doc_value2 = ''.join(random.choices(string.ascii_lowercase, k=5048))
+        self.test_limit=self.input.param("test_limit", 'indexStorageLimit')
         self.composite_doc = {"fname": f'{self.doc_value}', "lname": f'{self.doc_value2}'}
         return super().setUp()
 
@@ -79,7 +84,7 @@ class GSIThrottle(ServerlessBaseTestCase):
         result = self.run_query(database1,
                                 f'INSERT INTO {self.collection} (key k, value v) select uuid() as k , {self.composite_doc} as v from array_range(0,{index_limit*4}) d')
         result2 = self.run_query(database2,
-                                f'INSERT INTO {self.collection} (key k, value v) select uuid() as k , {self.composite_doc} as v from array_range(0,{index_limit/10}) d')
+                                f'INSERT INTO {self.collection} (key k, value v) select uuid() as k , {self.composite_doc} as v from array_range(0,{index_limit/12}) d')
 
         # get throttle count before
         before_count_database1, _ = throttle.get_metrics(database1.id, service='index')
@@ -102,6 +107,58 @@ class GSIThrottle(ServerlessBaseTestCase):
         after_count_database2, _ = throttle.get_metrics(database2.id, service='index')
         self.assertTrue(after_count_database1 > before_count_database1)
         self.assertEqual(after_count_database2, before_count_database2)
+
+    def test_gsi_limit(self):
+        self.provision_databases()
+        for database in self.databases.values():
+            throttle = throttling(database.rest_host, database.admin_username, database.admin_password)
+            indexes = self.run_query(database,'SELECT raw count(*) FROM system:indexes WHERE `using` = "gsi"')
+            count = indexes['results'][0]
+            for i in range(self.gsi_limit - count):
+                self.run_query(database,f'CREATE INDEX idx{i} IF NOT EXISTS ON {self.collection}(a)')
+            try:
+                self.run_query(database,f'CREATE INDEX idx{self.gsi_limit+1} IF NOT EXISTS ON {self.collection}(a)')
+                self.fail(f'We should not be able to create index beyond limit of {self.gsi_limit}')
+            except CBQError as ex:
+                error = self.process_CBQE(ex)
+                self.assertEqual(error['code'], 4350)
+                self.assertEqual(error['msg'], f'GSI CreateIndex() - cause: Limit for number of indexes that can be created per bucket has been reached. Limit : {self.gsi_limit + 1}')
+            finally:
+                for i in range(self.gsi_limit):
+                    self.run_query(database,f'DROP INDEX idx{i} IF EXISTS ON {self.collection}')
+
+    def test_limit_storage(self):
+        self.provision_databases()
+        for database in self.databases.values():
+
+            self.run_query(database, f'INSERT INTO {self.collection} (key k, value v) SELECT uuid() as k , {{"city": "Paris"}} as v FROM array_range(0,1000) d')
+
+            # Set stroage limit to 0 to simulate we have reached limit
+            throttle = throttling(database.rest_host, database.admin_username, database.admin_password)
+            throttle.set_bucket_limit(database.id, 0, service=self.test_limit)
+            current_limit = throttle.get_bucket_limit(database.id, self.test_limit)
+            self.assertEqual(current_limit, 0)
+
+            limits = {
+                'indexStorageLimit': {
+                    'statement': f'CREATE INDEX index_name ON {self.collection}(a)',
+                    'code': 4350,
+                    'msg': 'GSI CreateIndex() - cause: Bucket\'s disk size limit has been reached.'},
+                'dataStorageLimit': { # Error code/message might need to be changed based on MB-54113
+                    'statement': f'INSERT INTO {self.collection} (key, value) values (uuid(), {{"city": "Lyon"}})',
+                    'code': 12009,
+                    'msg': 'DML Error, possible causes include concurrent modification. Failed to perform INSERT'}
+            }
+            try:
+                # Repeat operation until we hit limit as setting limit is not immediate
+                for i in range(30):
+                    self.run_query(database, limits[self.test_limit]['statement'].replace('index_name', f'idx{i}'))
+                    time.sleep(i)
+                self.fail(f'We should have error out at this point since storage limit should be reached')
+            except CBQError as ex:
+                error = self.process_CBQE(ex)
+                self.assertEqual(error['code'], limits[self.test_limit]['code'])
+                self.assertEqual(error['msg'][:84], limits[self.test_limit]['msg'])
 
     def run_async_create(self, database, prefix):
         idx_name = "idx_name" + prefix
