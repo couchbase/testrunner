ba09a4dfef74a73b45982a9ff15daf52ddb6bd38
YASH N DODDERI <yash.dodderi@couchbase.com>
2023-03-27 11:30:29 +0530

    Changes for gsi sanity provisoned tests
    Change-Id: I59a029f61b1297240bc295775ddf490bc8a05305
    Reviewed-on: https://review.couchbase.org/c/testrunner/+/188801
    Reviewed-by: Hemant Kumar Rajput <Hemant.rajput@couchbase.com>
    Tested-by: <yash.dodderi@couchbase.com>

--- pytests/gsi/collections_index_basics.py
+++ pytests/gsi/collections_index_basics.py
@@ -36,8 +36,8 @@ class CollectionsIndexBasics(BaseSecondaryIndexingTests):
         self.cluster.create_standard_bucket(name=self.test_bucket, port=11222,
                                             bucket_params=self.bucket_params)
         self.buckets = self.rest.get_buckets()
-        self._create_server_groups()
         if not self.capella_run:
+            self._create_server_groups()
             self.cb_version = float(self.cb_version.split('-')[0][0:3])
         self.log.info("==============  CollectionsIndexBasics setup has completed ==============")
 
