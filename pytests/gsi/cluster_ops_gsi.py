7e4098349ce35737b56c6892f90d4d3bca537531
hrajput89 <hemant.rajput@couchbase.com>
2023-09-20 10:32:05 +0530

    Fixing gsi stats count validation
    Change-Id: Ie0e3a64c01add54e4a4d308f8dea8bc5f16ed44a
    Reviewed-on: https://review.couchbase.org/c/testrunner/+/197406
    Reviewed-by: <pavan.pb@couchbase.com>
    Tested-by: Hemant Kumar Rajput <Hemant.rajput@couchbase.com>

--- pytests/gsi/cluster_ops_gsi.py
+++ pytests/gsi/cluster_ops_gsi.py
@@ -2,11 +2,12 @@ from .base_gsi import BaseSecondaryIndexingTests
 from membase.api.rest_client import RestConnection, RestHelper
 from membase.helper.cluster_helper import ClusterOperationHelper
 
+
 class SecondaryIndexingClusterOpsTests(BaseSecondaryIndexingTests):
 
     def setUp(self):
         super(SecondaryIndexingClusterOpsTests, self).setUp()
-        server = self.get_nodes_from_services_map(service_type = "n1ql")
+        server = self.get_nodes_from_services_map(service_type="n1ql")
         self.rest = RestConnection(server)
 
     def tearDown(self):
@@ -19,46 +20,46 @@ class SecondaryIndexingClusterOpsTests(BaseSecondaryIndexingTests):
         pass
 
     def test_remove_bucket_and_query(self):
-    	#Initialization operation
-        self.run_multi_operations(buckets = self.buckets,
-            query_definitions = self.query_definitions,
-            create_index=True, drop_index = False,
-            query_with_explain = self.run_query_with_explain, query = self.run_query)
-        #Remove bucket and recreate it
+        # Initialization operation
+        self.run_multi_operations(buckets=self.buckets,
+                                  query_definitions=self.query_definitions,
+                                  create_index=True, drop_index=False,
+                                  query_with_explain=self.run_query_with_explain, query=self.run_query)
+        # Remove bucket and recreate it
         for bucket in self.buckets:
-        	self.rest.delete_bucket(bucket.name)
-        #Verify the result set is empty
-        self.verify_index_absence(query_definitions = self.query_definitions, buckets = self.buckets)
+            self.rest.delete_bucket(bucket.name)
+        # Verify the result set is empty
+        self.verify_index_absence(query_definitions=self.query_definitions, buckets=self.buckets)
 
     def test_change_bucket_properties(self):
-    	#Initialization operation
-        self.run_multi_operations(buckets = self.buckets,
-            query_definitions = self.query_definitions,
-            create_index = True, drop_index = False,
-            query_with_explain = True, query = True)
+        # Initialization operation
+        self.run_multi_operations(buckets=self.buckets,
+                                  query_definitions=self.query_definitions,
+                                  create_index=True, drop_index=False,
+                                  query_with_explain=True, query=True)
 
-        #Change Bucket Properties
+        # Change Bucket Properties
         for bucket in self.buckets:
             self.rest.change_bucket_props(bucket,
-                      ramQuotaMB=None,
-                      replicaNumber=0,
-                      proxyPort=None,
-                      replicaIndex=None,
-                      flushEnabled=False)
+                                          ramQuotaMB=None,
+                                          replicaNumber=0,
+                                          proxyPort=None,
+                                          replicaIndex=None,
+                                          flushEnabled=False)
 
-        #Run query and query explain
-        self.run_multi_operations(buckets = self.buckets,
-            query_definitions = self.query_definitions,
-            create_index = False, drop_index = True,
-            query_with_explain = True, query = True)
+        # Run query and query explain
+        self.run_multi_operations(buckets=self.buckets,
+                                  query_definitions=self.query_definitions,
+                                  create_index=False, drop_index=True,
+                                  query_with_explain=True, query=True)
 
     def test_flush_bucket_and_query(self):
-        #Initialization operation
+        # Initialization operation
         self.run_multi_operations(buckets=self.buckets,
-            query_definitions=self.query_definitions,
-            create_index=True, drop_index=False,
-            query_with_explain=True, query=True)
-        #Remove bucket and recreate it
+                                  query_definitions=self.query_definitions,
+                                  create_index=True, drop_index=False,
+                                  query_with_explain=True, query=True)
+        # Remove bucket and recreate it
         for bucket in self.buckets:
             self.rest.flush_bucket(bucket.name)
         self.sleep(120)
@@ -67,7 +68,7 @@ class SecondaryIndexingClusterOpsTests(BaseSecondaryIndexingTests):
         while rollback_exception and query_try_count < 10:
             self.sleep(5)
             query_try_count += 1
-            #Query and bucket with empty result set
+            # Query and bucket with empty result set
             try:
                 self.multi_query_using_index_with_emptyresult(
                     query_definitions=self.query_definitions, buckets=self.buckets)
@@ -81,56 +82,56 @@ class SecondaryIndexingClusterOpsTests(BaseSecondaryIndexingTests):
         self.assertFalse(rollback_exception, "Indexer still in rollback after 50 secs.")
 
     def test_delete_create_bucket_and_query(self):
-    	#Initialization operation
-        self.run_multi_operations(buckets = self.buckets,
-            query_definitions = self.query_definitions,
-            create_index = True, drop_index = False,
-            query_with_explain = self.run_query_with_explain, query = self.run_query)
-        #Remove bucket and recreate it
+        # Initialization operation
+        self.run_multi_operations(buckets=self.buckets,
+                                  query_definitions=self.query_definitions,
+                                  create_index=True, drop_index=False,
+                                  query_with_explain=self.run_query_with_explain, query=self.run_query)
+        # Remove bucket and recreate it
         for bucket in self.buckets:
-        	self.rest.delete_bucket(bucket.name)
+            self.rest.delete_bucket(bucket.name)
         self.sleep(2)
-        #Flush bucket and recreate it
+        # Flush bucket and recreate it
         self._bucket_creation()
         self.sleep(2)
         ClusterOperationHelper.wait_for_ns_servers_or_assert(self.servers, self)
-        #Verify the result set is empty
-        self.verify_index_absence(query_definitions = self.query_definitions, buckets = self.buckets)
+        # Verify the result set is empty
+        self.verify_index_absence(query_definitions=self.query_definitions, buckets=self.buckets)
         index_map = self.get_index_stats()
         # 32 stats are stats for index service. Anything beyond this is index specific
-        self.assertTrue(len(index_map) == 31, "Index Stats still show {0}".format(index_map))
+        self.assertTrue(len(index_map) == 32, "Index Stats still show {0}".format(index_map))
 
     def test_data_loss(self):
-        #Initialization operation
-        self.run_multi_operations(buckets = self.buckets,
-            query_definitions = self.query_definitions,
-            create_index = True, drop_index = False,
-            query_with_explain = False, query = False)
+        # Initialization operation
+        self.run_multi_operations(buckets=self.buckets,
+                                  query_definitions=self.query_definitions,
+                                  create_index=True, drop_index=False,
+                                  query_with_explain=False, query=False)
         self._verify_bucket_count_with_index_count()
         try:
             servr_out = self.servers[1:self.nodes_init]
             failover_task = self.cluster.async_failover([self.master],
-                        failover_nodes = servr_out, graceful=False)
+                                                        failover_nodes=servr_out, graceful=False)
             failover_task.result()
             rebalance = self.cluster.async_rebalance(self.servers[:1],
-                                    [], servr_out)
+                                                     [], servr_out)
             rebalance.result()
             # get the items in the index and check if the data loss is reflected correctly
             self.sleep(2)
         except Exception as ex:
             raise
         finally:
-            self.run_multi_operations(buckets = self.buckets,
-            query_definitions = self.query_definitions,
-            create_index = False, drop_index = True,
-            query_with_explain = False, query = False)
+            self.run_multi_operations(buckets=self.buckets,
+                                      query_definitions=self.query_definitions,
+                                      create_index=False, drop_index=True,
+                                      query_with_explain=False, query=False)
 
     def test_tombstone_removal_impact(self):
-        #Initialization operation
-        self.run_multi_operations(buckets = self.buckets,
-            query_definitions = self.query_definitions,
-            create_index = True, drop_index = False,
-            query_with_explain = False, query = False)
+        # Initialization operation
+        self.run_multi_operations(buckets=self.buckets,
+                                  query_definitions=self.query_definitions,
+                                  create_index=True, drop_index=False,
+                                  query_with_explain=False, query=False)
         self.sleep(20)
         self._verify_bucket_count_with_index_count()
         try:
@@ -144,16 +145,16 @@ class SecondaryIndexingClusterOpsTests(BaseSecondaryIndexingTests):
                 task.result()
             self.sleep(10)
             # run compaction and analyze results
-            self.run_multi_operations(buckets = self.buckets, query_definitions = self.query_definitions,
-                create_index = True, drop_index = False, query_with_explain = True, query = True)
+            self.run_multi_operations(buckets=self.buckets, query_definitions=self.query_definitions,
+                                      create_index=True, drop_index=False, query_with_explain=True, query=True)
         except Exception as ex:
             self.log.info(str(ex))
             raise
         finally:
-            self.run_multi_operations(buckets = self.buckets,
-            query_definitions = self.query_definitions,
-            create_index = False, drop_index = True,
-            query_with_explain = False, query = False)
+            self.run_multi_operations(buckets=self.buckets,
+                                      query_definitions=self.query_definitions,
+                                      create_index=False, drop_index=True,
+                                      query_with_explain=False, query=False)
 
     def test_maxttl_setting(self):
         """
@@ -163,10 +164,10 @@ class SecondaryIndexingClusterOpsTests(BaseSecondaryIndexingTests):
         :return:
         """
         maxttl = int(self.input.param("maxttl", None))
-        self.run_multi_operations(buckets = self.buckets,
-            query_definitions = self.query_definitions,
-            create_index = True, drop_index = False,
-            query_with_explain = False, query = False)
+        self.run_multi_operations(buckets=self.buckets,
+                                  query_definitions=self.query_definitions,
+                                  create_index=True, drop_index=False,
+                                  query_with_explain=False, query=False)
         self.sleep(20)
         self._verify_bucket_count_with_index_count()
         self.sleep(maxttl, "waiting for docs to be expired automatically per maxttl rule")
