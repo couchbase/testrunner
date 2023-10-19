d535d24ce999c015265ab74ba09b45e6621211c4
mohsin-couchbase <mohsin.ahmed@couchbase.com>
2023-10-19 09:14:27 +0530

    Extended test to validate tls 1 and 1.1 are not supported
    Change-Id: I4fefbc8705895116d442599e6ef8fcd5d3c4ca88
    Reviewed-on: https://review.couchbase.org/c/testrunner/+/199069
    Reviewed-by: shaazin19 <shaazin.sheikh@couchbase.com>
    Tested-by: shaazin19 <shaazin.sheikh@couchbase.com>
    (cherry picked from commit 4d75267d2fd5b7d7c9d74a7a2684920b7f841ed1)
    Reviewed-on: https://review.couchbase.org/c/testrunner/+/207881
    Reviewed-by: <mohsin.ahmed@couchbase.com>

--- pytests/security/testssl.py
+++ pytests/security/testssl.py
@@ -49,7 +49,8 @@ class TestSSLTests(BaseTestCase):
         Verifies the TLS minimum version of the cluster with the check_version
         """
         tls_versions = ["1.3  ", "1.2  ", "1.1  ", "1  "]
-        for check_version in tls_versions:
+        min_tls_versions = ["1.3  ", "1.2  "]
+        for check_version in min_tls_versions:
             self.log.info("Verifying for minimum version = {0}".format(check_version))
             rest = RestConnection(self.master)
             rest.set_min_tls_version(version="tlsv" + check_version.strip())
