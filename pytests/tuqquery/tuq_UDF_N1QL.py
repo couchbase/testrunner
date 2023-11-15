6654e13dfd2c0710aa3cdcf706cedd51f9d6118e
Pierre Regazzoni <pierre.regazzoni@couchbase.com>
2023-11-15 14:48:15 -0800

    Fix expected error
    Change-Id: I9b308478ac1e8c64d3756f079e687e9aefcee759
    Reviewed-on: https://review.couchbase.org/c/testrunner/+/200968
    Tested-by: Couchbase QE
    Reviewed-by: Ajay Bhullar <ajay.bhullar@couchbase.com>

--- pytests/tuqquery/tuq_UDF_N1QL.py
+++ pytests/tuqquery/tuq_UDF_N1QL.py
@@ -2192,7 +2192,7 @@ class QueryUDFN1QLTests(QueryTests):
         result = self.run_cbq_query(f"EXECUTE FUNCTION {function_name}()")
         expected_result = [
             {
-                'caller': 'couchbase:2112', 'code': 12009, 'icode': 'Duplicate Key: k004',
+                'caller': 'couchbase:2117', 'code': 12009, 'icode': 'Duplicate Key: k004',
                 'key': 'datastore.couchbase.DML_error',
                 'message': 'DML Error, possible causes include concurrent modification. Failed to perform INSERT on key k004',
                 'reason': {'caller': 'couchbase:1985', 'code': 17012, 'key': 'dml.statement.duplicatekey', 'message': 'Duplicate Key: k004'},
@@ -2200,4 +2200,6 @@ class QueryUDFN1QLTests(QueryTests):
                 'stack': 'Error\n    at error_handling (functions/n1ql.js:1:190)'
             }
         ]
-        self.assertEqual(result['results'], expected_result)
+        self.log.info(result['results'])
+        self.assertTrue('Duplicate Key: k004' in str(result),
+                        f"Error is not what we expected {str(result)}")
