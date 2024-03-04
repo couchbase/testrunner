9385825b841875df832eca416e7195ab6ae1cebf
Umang Agrawal <umang.agrawal@couchbase.com>
2024-03-04 15:25:31 +0530

    Fixed invite user function which was missed in this change 206151: Revert 'Reverting the verify new user verification change'
    Change-Id: I96216bcdd4a0d9e9e2f4d2da37115685c3a57b01
    Reviewed-on: https://review.couchbase.org/c/testrunner/+/206638
    Reviewed-by: Ashwin <ashwin.govindarajulu@couchbase.com>
    Tested-by: Umang Agrawal <umang.agrawal@couchbase.com>

--- scripts/capella.py
+++ scripts/capella.py
@@ -24,8 +24,8 @@ def invite_user(api_url, user, password, tenant):
     # invite user
     try:
         api = CapellaAPI(api_url, None, None, user, password)
-        resp = api.invite_new_user(
-            tenant, user_email, user_password)
+        resp = api.create_user(
+            tenant, user_name, user_email, user_password)
         if resp.status_code != 200:
             print("Creating capella User {0} failed: {1}".format(
                 user_name, resp.content))
