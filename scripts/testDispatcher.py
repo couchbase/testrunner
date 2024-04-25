2c1e2d6f6e70eca054368686184f9aa75b9569a7
Umang Agrawal <umang.agrawal@couchbase.com>
2024-04-25 14:02:41 +0530

    Fixed internal token parameter missing from invite user method
    Change-Id: I1a82f7381a56eef8b130c76ddfce81b653607037
    Reviewed-on: https://review.couchbase.org/c/testrunner/+/209064
    Reviewed-by: Abhay Aggrawal <abhay.aggrawal@couchbase.com>
    Tested-by: Abhay Aggrawal <abhay.aggrawal@couchbase.com>

--- scripts/testDispatcher.py
+++ scripts/testDispatcher.py
@@ -880,7 +880,8 @@ def main():
                 if options.serverType in [
                     SERVERLESS_ONCLOUD, PROVISIONED_ONCLOUD, SERVERLESS_COLUMNAR]:
                     print(f'CAPELLA: Inviting new user to capella tenant {options.capella_tenant} on {options.capella_url}')
-                    invited_user, invited_password = capella.invite_user( options.capella_url, options.capella_user, options.capella_password, options.capella_tenant)
+                    invited_user, invited_password = capella.invite_user(
+                        options.capella_token,options.capella_url, options.capella_user, options.capella_password, options.capella_tenant)
                     if invited_user is None or invited_password is None:
                         print("CAPELLA: We could not invite user to capella cluster. Skipping job.")
                         job_index += 1
