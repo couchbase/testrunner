6f0f241c212a9f631ab7d68df82d91e1ea34c680
Umang Agrawal <umang.agrawal@couchbase.com>
2024-05-10 15:35:08 +0530

    Fix for invite user method for test dispatcher
    Change-Id: Ie1adee7af9ec6c055f8e7440b31693ef9fd9b897
    Reviewed-on: https://review.couchbase.org/c/testrunner/+/209774
    Reviewed-by: <pavan.pb@couchbase.com>
    Tested-by: Umang Agrawal <umang.agrawal@couchbase.com>

--- scripts/capella.py
+++ scripts/capella.py
@@ -1,10 +1,11 @@
 from uuid import uuid4
-import requests, json
 import urllib3
 import sys
+from lib.capellaAPI.capella.dedicated.CapellaAPI import CapellaAPI
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 sys.path.extend(('.', 'lib'))
-from lib.capellaAPI.capella.dedicated.CapellaAPI import CapellaAPI
+
 
 def seed_email(email):
     uuid = uuid4()
@@ -15,6 +16,7 @@ def seed_email(email):
         prefix = name
     return "{}+{}@{}".format(prefix, uuid, domain)
 
+
 def invite_user(token, api_url, user, password, tenant):
     user_email = seed_email(user)
     user_name = user_email.split("@")[0]
@@ -23,20 +25,40 @@ def invite_user(token, api_url, user, password, tenant):
     # invite user
     try:
         api = CapellaAPI(api_url, None, None, user, password)
+
+        # Sign-Up a new user
         resp = api.signup_user(
             user_name, user_email, user_password, tenant, token)
         resp.raise_for_status()
         verify_token = resp.headers["Vnd-project-Avengers-com-e2e-token"]
-        user_id = resp.json()["userId"]
-    except Exception as e:
-        print("ERROR: failed to invite user")
-        return None, None
-        # verify user
-    try:
+
+        # Verify the user using the internal token
         resp = api.verify_email(verify_token)
         resp.raise_for_status()
+
+        # Invite the user to the organisation passed in the tenant parameter.
+        # it is required because when a user sign's up, he is assigned a new
+        # organisation, but we need to perform tests in the organisation
+        # mentioned in tenant parameter.
+        resp = api.invite_new_user(tenant, user_email, user_password)
+        resp.raise_for_status()
+
+        # Reassign the username and password for capella API object to that
+        # of the user that was created above.
+        api.user = user_email
+        api.pwd = user_password
+        api.jwt = None
+        resp = api.fetch_all_invitations()
+        resp.raise_for_status()
+        invitations = resp.json()
+        invitation_id = ""
+        for invitation in invitations:
+            if invitation["tenant"]["id"] == tenant:
+                invitation_id = invitation["id"]
+
+        resp = api.manage_invitation(invitation_id, "accept")
+        resp.raise_for_status()
         return user_email, user_password
-    except:
-        print("ERROR: failed to verify user")
+    except Exception as err:
+        print(str(err))
         return None, None
-        # update user password
