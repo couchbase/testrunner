87762e4c51d656f53400343cf8a7296e52c0a149
Raghav S K <raghav.sk@couchbase.com>
2024-05-23 02:27:27 +0530

    Adding mariner linux to SYSTEMD_SERVER
    Change-Id: I64ea9e9cb8ccdb15939d2436bab41e10569d9ede
    Reviewed-on: https://review.couchbase.org/c/testrunner/+/210330
    Reviewed-by: Raghav S K <raghav.sk@couchbase.com>
    Reviewed-by: Ashwin <ashwin.govindarajulu@couchbase.com>
    Tested-by: Raghav S K <raghav.sk@couchbase.com>

--- lib/testconstants.py
+++ lib/testconstants.py
@@ -189,7 +189,10 @@ SYSTEMD_SERVER = ["centos 8", "centos 7",
                   "oel 7", "oel 8", "oel 9",
                   "alma 9",
                   "rocky 9",
-                  "amazon linux release 2 (karoo)","amazon linux release 2023 (amazon linux)"]
+                  "amazon linux release 2 (karoo)",
+                  "amazon linux release 2023 (amazon linux)",
+                  "cbl-mariner/linux 2"]
+
 WIN_NUM_ERLANG_PROCESS = 4
 WIN_MEMBASE_DATA_PATH = '/cygdrive/c/Program\ Files/Membase/Server/var/lib/membase/data/'
 WIN_COUCHBASE_DATA_PATH = '/cygdrive/c/Program\ Files/Couchbase/Server/var/lib/couchbase/data/'
