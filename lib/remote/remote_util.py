52182766b24d0626fe7ccf6f174df1e80a214e40
Raghav S K <raghav.sk@couchbase.com>
2024-05-23 01:46:38 +0530

    Disabling iptables -F for Mariner Linux
    Change-Id: Iff0cc5c9f622a7c3a95d52d7dfa73ff0dca526f6
    Reviewed-on: https://review.couchbase.org/c/testrunner/+/210333
    Tested-by: Raghav S K <raghav.sk@couchbase.com>
    Reviewed-by: Raghav S K <raghav.sk@couchbase.com>
    Reviewed-by: Ashwin <ashwin.govindarajulu@couchbase.com>

--- lib/remote/remote_util.py
+++ lib/remote/remote_util.py
@@ -3330,6 +3330,10 @@ class RemoteMachineShellConnection(KeepRefs):
         else:
             self.extract_remote_info()
 
+        if "iptables -F" in command and self.info.distribution_type == "CBL-Mariner/Linux":
+            msg = "iptables -F is disabled on Mariner Linux"
+            return [msg], [msg]
+
         if self.info.type.lower() == 'windows':
             self.use_sudo = False
 
