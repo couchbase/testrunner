6476024a14250a67ef6a7090302c5c7036c2983a
Sarthak Dua <sarthak.dua@couchbase.com>
2024-08-21 09:41:59 +0530

    fixed moving_top for fts in neo
    Change-Id: I320db05642b51f476cbc518f8782955a22d84a64
    Reviewed-on: https://review.couchbase.org/c/testrunner/+/214712
    Tested-by: Sarthak Dua <sarthak.dua@couchbase.com>
    Reviewed-by: Sarthak Dua <sarthak.dua@couchbase.com>

--- pytests/fts/moving_topology_fts.py
+++ pytests/fts/moving_topology_fts.py
@@ -1,5 +1,3 @@
-import threading
-
 from lib import global_vars
 from lib.SystemEventLogLib.fts_service_events import SearchServiceEvents
 from .fts_base import FTSBaseTest, FTSException
@@ -11,7 +9,6 @@ from lib.memcached.helper.data_helper import MemcachedClientHelper
 from lib.membase.api.rest_client import RestConnection, RestHelper
 import json
 import time
-
 
 class MovingTopFTS(FTSBaseTest):
 
@@ -29,18 +26,23 @@ class MovingTopFTS(FTSBaseTest):
         super(MovingTopFTS, self).setUp()
         rest = RestConnection(self._cb_cluster.get_fts_nodes()[0])
         rest.set_disableFileTransferRebalance(self.disable_file_transfer_rebalance)
-        if not self.capella_run:
-            self.index_path = rest.get_index_path()
-            if self.index_path == "/data":
-                self.reset_data_mount_point(self._cb_cluster.get_fts_nodes())
+        self.index_path = rest.get_index_path()
+        if self.index_path == "/data":
+            self.reset_data_mount_point(self._cb_cluster.get_fts_nodes())
 
     def tearDown(self):
         super(MovingTopFTS, self).tearDown()
-        if not self.capella_run and self.index_path == "/data":
+        if self.index_path == "/data":
             try:
                 self.reset_data_mount_point(self._cb_cluster.get_fts_nodes())
             except Exception as err:
                 self.log.info(str(err))
+
+    def suite_setUp(self):
+        self.log.info("*** MovingTopFTS: suite_setUp() ***")
+
+    def suite_tearDown(self):
+        self.log.info("*** MovingTopFTS: suite_tearDown() ***")
 
     """ Topology change during indexing"""
 
