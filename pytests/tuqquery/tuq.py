a11e20b74d93ec5299e9f0202eb14cf4e0333afa
ajaybhullar <ajay.bhullar@couchbase.com>
2025-01-09 14:10:34 -0800

    Change curl conversion to output what it is trying to convert in case of conversion failure
    Change-Id: Id2e9d9dcfdcec380f0e37c28cb3797ad00eac0da
    Reviewed-on: https://review.couchbase.org/c/testrunner/+/221712
    Tested-by: Ajay Bhullar <ajay.bhullar@couchbase.com>
    Reviewed-by: Ritesh Agarwal <ritesh.agarwal@couchbase.com>

--- pytests/tuqquery/tuq.py
+++ pytests/tuqquery/tuq.py
@@ -3126,26 +3126,35 @@ class QueryTests(BaseTestCase):
     '''Convert output of remote_util.execute_commands_inside to json'''
 
     def convert_to_json(self, output_curl):
-        new_curl = "{" + output_curl
-        json_curl = json.loads(new_curl)
-        return json_curl
+        try:
+            new_curl = "{" + output_curl
+            json_curl = json.loads(new_curl)
+            return json_curl
+        except Exception as ex:
+            self.info.log(output_curl)
 
     '''Convert output of remote_util.execute_command to json
        (stripping all white space to match execute_command_inside output)'''
 
     def convert_list_to_json(self, output_of_curl):
-        new_list = [string.replace(" ", "") for string in output_of_curl]
-        concat_string = ''.join(new_list)
-        json_output = json.loads(concat_string)
-        return json_output
+        try:
+            new_list = [string.replace(" ", "") for string in output_of_curl]
+            concat_string = ''.join(new_list)
+            json_output = json.loads(concat_string)
+            return json_output
+        except Exception as ex:
+            self.info.log(output_of_curl)
 
     '''Convert output of remote_util.execute_command to json to match the output of run_cbq_query'''
 
     def convert_list_to_json_with_spacing(self, output_of_curl):
-        new_list = [string.strip() for string in output_of_curl]
-        concat_string = ''.join(new_list)
-        json_output = json.loads(concat_string)
-        return json_output
+        try:
+            new_list = [string.strip() for string in output_of_curl]
+            concat_string = ''.join(new_list)
+            json_output = json.loads(concat_string)
+            return json_output
+        except Exception as ex:
+            self.info.log(output_of_curl)
 
     ##############################################################################################
     #
