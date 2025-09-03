7fd372e5cf0e5a656f19e3786ee81035fc6bdcb7
Ashwin <ashwin.govindarajulu@couchbase.com>
2025-09-03 12:40:22 +0530

    Fixing eviction tests
    Issue with the float convertion
    
    Change-Id: I6918f2c3dc427c57a821e800bcd8bb0c0d74ae4f
    Reviewed-on: https://review.couchbase.org/c/testrunner/+/233189
    Reviewed-by: <shiva.adky@couchbase.com>
    Tested-by: Ashwin <ashwin.govindarajulu@couchbase.com>

--- pytests/eviction/evictionbase.py
+++ pytests/eviction/evictionbase.py
@@ -7,6 +7,7 @@ from memcached.helper.data_helper import VBucketAwareMemcached
 from membase.api.rest_client import RestConnection
 import memcacheConstants as constants
 
+
 class EvictionBase(BaseTestCase):
 
     def setUp(self):
@@ -17,24 +18,26 @@ class EvictionBase(BaseTestCase):
     def tearDown(self):
         super(EvictionBase, self).tearDown()
 
-
     def load_set_to_be_evicted(self, ttl, end=100):
-        gen_create = BlobGenerator('ejected', 'ejected-', 128, start=0, end=end)
+        gen_create = BlobGenerator('ejected', 'ejected-', 128, start=0,
+                                   end=end)
         self._load_all_buckets(self.master, gen_create, "create", ttl)
 
-
     def load_ejected_set(self, end=100):
-        gen_create = BlobGenerator('ejected', 'ejected-', 128, start=0, end=end)
+        gen_create = BlobGenerator('ejected', 'ejected-', 128, start=0,
+                                   end=end)
         self._load_all_buckets(self.master, gen_create, "create", 0)
 
     def stat(self, key):
-        stats =  StatsCommon.get_stats([self.master], 'default', "", key)
+        stats = StatsCommon.get_stats([self.master], 'default', "", key)
         val = list(stats.values())[0]
         if val.isdigit():
             val = int(val)
+        elif val.replace('.', '').replace('-', '').isdigit():  # Handle float
+            val = float(val)
         return val
 
-    def load_to_dgm(self, active = 75, ttl = 0, prefix='dgmkv'):
+    def load_to_dgm(self, active=75, ttl=0, prefix='dgmkv'):
         """
             decides how many items to load to enter active% dgm state
             where active is an integer value between 0 and 100
@@ -45,33 +48,37 @@ class EvictionBase(BaseTestCase):
         batch_items = 50000
 
         # go into heavy dgm
-        while curr_active > active:
+        while float(curr_active) > float(active):
             curr_items = self.stat('curr_items')
-            gen_create = BlobGenerator('dgmkv', 'dgmkv-', doc_size, start=curr_items + 1, end=curr_items+50000)
-            gen_create = BlobGenerator(prefix, prefix+'-', doc_size, start=curr_items + 1, end=curr_items+batch_items)
+            gen_create = BlobGenerator('dgmkv', 'dgmkv-', doc_size,
+                                       start=curr_items + 1,
+                                       end=curr_items+50000)
+            gen_create = BlobGenerator(prefix, prefix+'-', doc_size,
+                                       start=curr_items + 1,
+                                       end=curr_items+batch_items)
             total_items += batch_items
             try:
                 self._load_all_buckets(self.master, gen_create, "create", ttl)
-            except:
+            except Exception:
                 pass
             curr_active = self.stat('vb_active_perc_mem_resident')
 
         return total_items
 
-
-    def get_kv_store(self, index = 1):
+    def get_kv_store(self, index=1):
         bucket = [b for b in self.buckets if b.name == 'default'][0]
         kv_store = bucket.kvs[index]
         return kv_store
 
-
-    def ops_on_ejected_set(self, action, start = 0, end = 100,  ttl = 0):
+    def ops_on_ejected_set(self, action, start=0, end=100, ttl=0):
         kv_store = self.get_kv_store()
-        gen_reader = BlobGenerator('ejected', 'ejected-', 128, start=start, end=end)
-        self.cluster.load_gen_docs(self.master, 'default', gen_reader, kv_store, action, exp = ttl,
+        gen_reader = BlobGenerator('ejected', 'ejected-', 128, start=start,
+                                   end=end)
+        self.cluster.load_gen_docs(self.master, 'default', gen_reader,
+                                   kv_store, action, exp=ttl,
                                    compression=self.sdk_compression)
 
-    def run_expiry_pager(self, ts = 15):
+    def run_expiry_pager(self, ts=15):
         ClusterOperationHelper.flushctl_set(self.master, "exp_pager_stime", ts)
         self.log.info("wait for expiry pager to run on all these nodes")
 
