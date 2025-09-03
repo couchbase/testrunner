c455236d8a12c63d6ffb347787dcb47a23a04cd9
Ashwin <ashwin.govindarajulu@couchbase.com>
2025-09-03 13:54:57 +0530

    CBQE-8609: Fixing epeng bucketflush tests
    Removing unwanted else clause for try block
    
    Change-Id: I7c7737e63be516f4861fa110def341cb1256cf67
    Reviewed-on: https://review.couchbase.org/c/testrunner/+/233196
    Reviewed-by: <shiva.adky@couchbase.com>
    Tested-by: Ashwin <ashwin.govindarajulu@couchbase.com>

--- pytests/flush/bucketflush.py
+++ pytests/flush/bucketflush.py
@@ -1,10 +1,12 @@
 import json
 import time
+
 from basetestcase import BaseTestCase
 from couchbase_helper.documentgenerator import BlobGenerator
 from mc_bin_client import MemcachedError
 from threading import Thread
 from memcached.helper.data_helper import MemcachedClientHelper
+
 
 class BucketFlushTests(BaseTestCase):
 
@@ -14,7 +16,8 @@ class BucketFlushTests(BaseTestCase):
         self.value_size = self.input.param("value_size", 256)
         self.data_op = self.input.param("data_op", "create")
         self.use_ascii = self.input.param("use_ascii", "False")
-        self.gen_create = BlobGenerator('bucketflush', 'bucketflush-', self.value_size, end=self.num_items)
+        self.gen_create = BlobGenerator('bucketflush', 'bucketflush-',
+                                        self.value_size, end=self.num_items)
         try:
             self.default_test_setup()
         except Exception as e:
@@ -37,23 +40,22 @@ class BucketFlushTests(BaseTestCase):
 
     """Helper function to wait for persistence and then verify data/stats on all buckets"""
     def persist_and_verify(self):
-
         self._wait_for_stats_all_buckets(self.servers[:self.nodes_in + 1])
-        self._verify_all_buckets(self.master, max_verify=self.max_verify, timeout=360)
+        self._verify_all_buckets(self.master, max_verify=self.max_verify,
+                                 timeout=360)
         self._verify_stats_all_buckets(self.servers[:self.nodes_in + 1])
 
     """Basic test for bucket flush functionality. Test loads data in bucket and then calls Flush. Verify curr_items=0 after flush.
         Works with multiple nodes/buckets."""
     def bucketflush(self):
-
-
         # MB-18068 known issue with bucket flush
         time.sleep(5)
         for bucket in self.buckets:
             self.cluster.bucket_flush(self.master, bucket)
 
         for bucket in self.buckets:
-            self.cluster.wait_for_stats(self.servers[:self.nodes_in + 1], bucket, '', 'curr_items', '==', 0)
+            self.cluster.wait_for_stats(self.servers[:self.nodes_in + 1],
+                                        bucket, '', 'curr_items', '==', 0)
 
     """Test case for empty bucket. Work with multiple nodes/buckets."""
     def bucketflush_empty(self):
@@ -70,16 +72,16 @@ class BucketFlushTests(BaseTestCase):
     """Test case to check client behavior with bucket flush while loading/updating/deleting data"""
     def bucketflush_with_data_ops(self):
         try:
-            tasks = self._async_load_all_buckets(self.master, self.gen_create, self.data_op, 0)
+            tasks = self._async_load_all_buckets(self.master, self.gen_create,
+                                                 self.data_op, 0)
             for bucket in self.buckets:
                 self.cluster.bucket_flush(self.master, bucket)
             for task in tasks:
                 task.result()
         except MemcachedError as exp:
-            self.assertEqual(exp.status, 134, msg="Unexpected Exception - {0}".format(exp))
+            self.assertEqual(exp.status, 134,
+                             msg="Unexpected Exception - {0}".format(exp))
             self.log.info("Expected Exception Caught - {0}".format(exp))
         except Exception as exp:
             self.log.info("Unxpected Exception Caught - {0}".format(exp))
             self.fail("Unexpected exception caught- {0}".format(exp))
-        else:
-            self.fail("All buckets may not have been flushed")
