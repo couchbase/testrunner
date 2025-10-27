b126dfb7fef75daa962a592d896e114cafb45f95
Dananjay <dananjay.s@couchbase.com>
2025-10-27 18:21:58 +0530

    CBQE-8606: Updating slave name on executor script
    Change-Id: If52ef8588b8f085656319d916cd9d3679910eb53
    Reviewed-on: https://review.couchbase.org/c/testrunner/+/235810
    Reviewed-by: Ashwin <ashwin.govindarajulu@couchbase.com>
    Tested-by: <dananjay.s@couchbase.com>

--- scripts/executor_script.sh
+++ scripts/executor_script.sh
@@ -82,10 +82,8 @@ cat /tmp/testexec_reformat.$$.ini
 # To make sure the files exists
 touch /tmp/testexec_reformat.$$.ini /tmp/testexec.$$.ini
 
-if [[ "${slave}" = "bhive_slave_test" ]]; then
-  echo "Running on bhive_slave_test"
-  export PATH=/opt/maven/bin:/opt/java-8/bin:/root/.pyenv/shims:/root/.pyenv/bin:$PATH
-  echo "PATH is: $PATH"
+if [[ "${slave}" = "bhive_slave" ]]; then
+  echo "Running on bhive_slave"
   cd magma_loader/DocLoader
   mvn clean install
   cd ../..
