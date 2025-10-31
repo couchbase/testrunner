813d66f8dcd015dfd384afc903bfb8097d54ee85
Ashwin <ashwin.govindarajulu@couchbase.com>
2025-10-31 07:48:21 +0530

    Adding 7.2.9 and 7.6.8 versions under build-sanity-trigger.py
    Change-Id: I340fe4e0e36b1ae52c4445ffd0cca412705ba30d
    Reviewed-on: https://review.couchbase.org/c/testrunner/+/235965
    Reviewed-by: Ashwin <ashwin.govindarajulu@couchbase.com>
    Tested-by: Ashwin <ashwin.govindarajulu@couchbase.com>

--- scripts/build-sanity-trigger.py
+++ scripts/build-sanity-trigger.py
@@ -743,6 +743,36 @@ VERSION_DATA = {
         },
         "testrunner_branch": "neo",
     },
+    "7.2.5": {
+        "platforms": {
+            "debian12": True
+        },
+        "testrunner_branch": "neo",
+    },
+    "7.2.6": {
+        "platforms": {
+            "debian12": True
+        },
+        "testrunner_branch": "neo",
+    },
+    "7.2.7": {
+        "platforms": {
+            "debian12": True
+        },
+        "testrunner_branch": "neo",
+    },
+    "7.2.8": {
+            "platforms": {
+                "debian12": True
+            },
+            "testrunner_branch": "neo",
+    },
+    "7.2.9": {
+        "platforms": {
+            "debian12": True
+        },
+        "testrunner_branch": "neo",
+    },
     "7.5.0": {
         "platforms": {
             "debian12": True
@@ -754,6 +784,12 @@ VERSION_DATA = {
             "debian12": True
         },
         "testrunner_branch": "master",
+    },
+    "7.6.8": {
+        "platforms": {
+            "debian12": True
+        },
+        "testrunner_branch": "trinity",
     },
     "8.0.0": {
         "platforms": {
@@ -937,4 +973,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
