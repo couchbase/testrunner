49c30da8134bd19ebebfb43faf890d49de2d80d3
Pierre Regazzoni <pierre.regazzoni@couchbase.com>
2025-11-14 13:24:57 -0800

    Use legacy mode for MD4
    Change-Id: I2308e8fca0fa24725830ef21db6414405ed69b6d
    Reviewed-on: https://review.couchbase.org/c/testrunner/+/236569
    Reviewed-by: Ajay Bhullar <ajay.bhullar@couchbase.com>
    Tested-by: Pierre Regazzoni <pierre.regazzoni@couchbase.com>

--- pytests/tuqquery/tuq_hashbytes.py
+++ pytests/tuqquery/tuq_hashbytes.py
@@ -29,7 +29,7 @@ class QueryHashBytesTests(QueryTests):
         else:
             string = value
         if algorithm.upper() == "MD4":
-            cmd = subprocess.getoutput(f'/bin/echo -n {string} | openssl md4')
+            cmd = subprocess.getoutput(f'/bin/echo -n {string} | openssl md4 -provider legacy')
             return cmd.replace("(stdin)= ", "")
         if algorithm.upper() == "MD5":
             return hashlib.md5(string.encode()).hexdigest()
