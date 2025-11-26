58f1a91ae50e595e0bf2db812be22b44e251ba21
Ashwin <ashwin.govindarajulu@couchbase.com>
2025-11-26 15:58:53 +0530

    Updating new_install constant to restart timesyncd before running apt-update
    Change-Id: I8f14aa86fe7342c0e8fec1ba7e1d613b11f1d337
    Reviewed-on: https://review.couchbase.org/c/testrunner/+/237001
    Reviewed-by: <shivam.paliwal@couchbase.com>
    Tested-by: Ashwin <ashwin.govindarajulu@couchbase.com>

--- scripts/install_constants.py
+++ scripts/install_constants.py
@@ -131,6 +131,10 @@ CMDS = {
             "apt purge -y ntp 2>/dev/null || true;"
             "apt autoremove -y 2>/dev/null || true;"
 
+            # Restart just in case to make apt-update work
+            # which can fail due to TLS errors due to time mismatch
+            "systemctl restart systemd-timesyncd ;"
+
             # Install chrony
             "apt update && apt install -y chrony ; "
             # Enable and start chrony
